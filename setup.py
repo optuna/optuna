abce6bc76e452dd6211aeafd6c6f24cff24cfdaf
Takuya Akiba <t.akiba.65536@gmail.com>
2018-01-23 16:06:55 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='pfnopt',
+    version='0.0.1',
+    description='',
+    author='Takuya Akiba',
+    author_email='akiba@preferred.jp',
+    packages=find_packages(),
+    test_requires=['pytest']
+)
