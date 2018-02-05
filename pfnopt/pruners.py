dfff9a867c90943ef83bd02d04f6b8299bb6e96b
Takuya Akiba <t.akiba.65536@gmail.com>
2018-02-05 16:38:54 +0900

    Add startup trials feature for MeidianPruner

--- pfnopt/pruners.py
+++ pfnopt/pruners.py
@@ -12,7 +12,12 @@ class MedianPruner(BasePruner):
      median of intermediate results of previous trials at the same step.
     """
 
+    n_startup_trials = 5  # TODO: parameterize
+
     def prune(self, storage, study_id, trial_id, step):
+        if trial_id < self.n_startup_trials:
+            return False
+
         best_intermediate_result = storage.get_best_intermediate_result_over_steps(
             study_id, trial_id)
         median = storage.get_median_intermediate_result_over_trials(
