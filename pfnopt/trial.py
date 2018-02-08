7abfcbd36c8b76249b0a000e18a3a83d205957c7
Takuya Akiba <t.akiba.65536@gmail.com>
2018-02-08 17:01:33 +0900

    Remove TODO as I decided not to use namedtuple here

--- pfnopt/trial.py
+++ pfnopt/trial.py
@@ -1,7 +1,5 @@
-# TODO: if appropriate, change to namedtuple
 class Trial(object):
 
-    # TODO: add meta data
     def __init__(self, trial_id):
         self.trial_id = trial_id
         self.params = {}
