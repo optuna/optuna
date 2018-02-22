8ae30840ebd0b397307984bd63bf0a8b5b2ff6ef
Takuya Akiba <t.akiba.65536@gmail.com>
2018-02-22 18:23:31 +0900

    Add metaclass specification to base sampler class

--- pfnopt/samplers/base.py
+++ pfnopt/samplers/base.py
@@ -1,4 +1,10 @@
+import abc
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
 class BaseSampler(object):
 
+    @abc.abstractmethod
     def sample(self, distribution, observation_pairs):
         raise NotImplementedError
