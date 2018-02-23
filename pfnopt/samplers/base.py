55a5a0b943ea70260c6f9ac2d5bf77e35ea8351e
Takuya Akiba <t.akiba.65536@gmail.com>
2018-02-23 11:34:15 +0900

    Change sampler interface to directly interact with storage

--- pfnopt/samplers/base.py
+++ pfnopt/samplers/base.py
@@ -1,10 +1,14 @@
 import abc
 import six
 
+from pfnopt.storage._base import BaseStorage  # NOQA
+from pfnopt.distributions import _BaseDistribution  # NOQA
+
 
 @six.add_metaclass(abc.ABCMeta)
 class BaseSampler(object):
 
     @abc.abstractmethod
-    def sample(self, distribution, observation_pairs):
+    def sample(self, storage, study_id, param_name, param_distribution):
+        # type: (BaseStorage, int, str, _BaseDistribution) -> float
         raise NotImplementedError
--- pfnopt/samplers/random.py
+++ pfnopt/samplers/random.py
@@ -1,25 +1,29 @@
 import numpy
 
+from pfnopt.samplers.base import BaseSampler
+from pfnopt.storage._base import BaseStorage  # NOQA
 from pfnopt import distributions
-from pfnopt.samplers import base
+
+from typing import Optional  # NOQA
 
 
-class RandomSampler(base.BaseSampler):
+class RandomSampler(BaseSampler):
 
     def __init__(self, seed=None):
+        # type: (Optional[int]) -> None
         self.seed = seed
-
         self.rng = numpy.random.RandomState(seed)
 
-    def sample(self, distribution, observation_pairs):
-        if isinstance(distribution, distributions.UniformDistribution):
-            return self.rng.uniform(distribution.low, distribution.high)
-        elif isinstance(distribution, distributions.LogUniformDistribution):
-            log_low = numpy.log(distribution.low)
-            log_high = numpy.log(distribution.high)
+    def sample(self, storage, study_id, param_name, param_distribution):
+        # type: (BaseStorage, int, str, distributions._BaseDistribution) -> float
+        if isinstance(param_distribution, distributions.UniformDistribution):
+            return self.rng.uniform(param_distribution.low, param_distribution.high)
+        elif isinstance(param_distribution, distributions.LogUniformDistribution):
+            log_low = numpy.log(param_distribution.low)
+            log_high = numpy.log(param_distribution.high)
             return numpy.exp(self.rng.uniform(log_low, log_high))
-        elif isinstance(distribution, distributions.CategoricalDistribution):
-            choices = distribution.choices
+        elif isinstance(param_distribution, distributions.CategoricalDistribution):
+            choices = param_distribution.choices
             return self.rng.randint(len(choices))
         else:
             raise NotImplementedError
