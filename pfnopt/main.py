e9388a527ae1796dba5c224a6a59a5a8226933f4
Shotaro Sano <sano@preferred.jp>
2018-03-08 15:07:43 +0900

    Fix parser

--- pfnopt/main.py
+++ pfnopt/main.py
@@ -3,6 +3,7 @@ from cliff.command import Command
 from cliff.commandmanager import CommandManager
 import sys
 
+from pfnopt.storage import RDBStorage
 from pfnopt.study import create_new_study
 
 
@@ -20,15 +21,14 @@ class MakeStudy(Command):
 
     def get_parser(self, prog_name):
         parser = super(MakeStudy, self).get_parser(prog_name)
-
         parser.add_argument('--url', '-u', dest='url', required=True)
+        return parser
 
     def take_action(self, parsed_args):
-        create_new_study()
+        storage = RDBStorage(parsed_args.url)
+        print(create_new_study(storage).study_uuid)
 
 
 def main():
     return PFNOptApp().run(sys.argv[1:])
 
-
-main()
