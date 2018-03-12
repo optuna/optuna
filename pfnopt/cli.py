3ca7cdc816a7463a12c0ed4079fdcb1791b7ece8
Shotaro Sano <sano@preferred.jp>
2018-03-12 18:20:33 +0900

    Add missing type-hint comment.

--- pfnopt/cli.py
+++ pfnopt/cli.py
@@ -12,6 +12,8 @@ from pfnopt.study import create_new_study
 class PFNOptApp(App):
 
     def __init__(self):
+        # type: () -> None
+
         super(PFNOptApp, self).__init__(
             description='',
             version='0.0.1',
@@ -23,12 +25,14 @@ class MakeStudy(Command):
 
     def get_parser(self, prog_name):
         # type: (str) -> ArgumentParser
+
         parser = super(MakeStudy, self).get_parser(prog_name)
         parser.add_argument('--url', '-u', dest='url', required=True)
         return parser
 
     def take_action(self, parsed_args):
         # type: (Namespace) -> None
+
         storage = RDBStorage(parsed_args.url)
         study_uuid = create_new_study(storage).study_uuid
         print(study_uuid)
