fcb870e1c0a82c227ad41ce7d1be8d0c126d3146
Shotaro Sano <sano@preferred.jp>
2018-03-12 18:13:32 +0900

    Make the test code Python2 compatible.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -19,7 +19,7 @@ class TestMakeStudy(unittest.TestCase):
         assert subprocess.check_call(command) == 0
 
         # command output should be in uuid string format
-        study_uuid = subprocess.check_output(command).decode().strip()
+        study_uuid = str(subprocess.check_output(command).decode().strip())
         uuid_re = r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
         assert re.match(uuid_re, study_uuid) is not None
 
