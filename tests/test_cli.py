1c0dd215112f1a63ac6d3d99f41d8c626bca745f
Shotaro Sano <sano@preferred.jp>
2018-03-12 19:01:08 +0900

    Use with block to ensure closing temporary file in case of test failure.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,21 +8,19 @@ from pfnopt.storage import RDBStorage
 def test_mkstudy_command():
     # type: () -> None
 
-    tf = tempfile.NamedTemporaryFile()
-    db_url = 'sqlite:///{}'.format(tf.name)
-    command = ['pfnopt', 'mkstudy', '--url', db_url]
+    with tempfile.NamedTemporaryFile() as tf:
+        db_url = 'sqlite:///{}'.format(tf.name)
+        command = ['pfnopt', 'mkstudy', '--url', db_url]
 
-    # command exit code should be 0
-    assert subprocess.check_call(command) == 0
+        # command exit code should be 0
+        assert subprocess.check_call(command) == 0
 
-    # command output should be in uuid string format
-    study_uuid = str(subprocess.check_output(command).decode().strip())
-    uuid_re = r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
-    assert re.match(uuid_re, study_uuid) is not None
+        # command output should be in uuid string format
+        study_uuid = str(subprocess.check_output(command).decode().strip())
+        uuid_re = r'^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
+        assert re.match(uuid_re, study_uuid) is not None
 
-    # uuid should be stored in storage
-    storage = RDBStorage(db_url)
-    study_id = storage.get_study_id_from_uuid(study_uuid)
-    assert study_id == 2
-
-    tf.close()
+        # uuid should be stored in storage
+        storage = RDBStorage(db_url)
+        study_id = storage.get_study_id_from_uuid(study_uuid)
+        assert study_id == 2
