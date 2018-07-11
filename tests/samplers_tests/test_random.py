b71980bc498e521106ee8d730accdc852ca6055f
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2018-07-11 09:52:41 +0900

    fix ranges of sampler values in unit tests.

--- tests/samplers_tests/test_random.py
+++ tests/samplers_tests/test_random.py
@@ -12,7 +12,7 @@ def test_uniform():
     sampler = pfnopt.samplers.RandomSampler(seed=123)
     distribution = pfnopt.distributions.UniformDistribution(-1., 1.)
     points = np.array([sampler.sample(storage, study, 'x', distribution) for i in range(100)])
-    assert np.all(points > -1.)
+    assert np.all(points >= -1.)
     assert np.all(points < 1.)
 
 
@@ -23,14 +23,14 @@ def test_quniform():
     study = 1
 
     sampler = pfnopt.samplers.RandomSampler(seed=123)
-    distribution = pfnopt.distributions.QUniformDistribution(-10., 10., 1)
+    distribution = pfnopt.distributions.QUniformDistribution(-10.5, 10.5, 1)
     points = np.array([sampler.sample(storage, study, 'x', distribution) for i in range(100)])
     assert np.all(points >= -10)
     assert np.all(points <= 10)
     round_points = np.round(points)
     np.testing.assert_almost_equal(round_points, points)
 
-    distribution = pfnopt.distributions.QUniformDistribution(-10., 10., 0.1)
+    distribution = pfnopt.distributions.QUniformDistribution(-10.05, 10.05, 0.1)
     points = np.array([sampler.sample(storage, study, 'x', distribution) for i in range(100)])
     assert np.all(points >= -10)
     assert np.all(points <= 10)
--- tests/samplers_tests/test_tpe.py
+++ tests/samplers_tests/test_tpe.py
@@ -10,7 +10,7 @@ def test_uniform():
     distribution = pfnopt.distributions.UniformDistribution(-1., 1.)
     points = np.array([sampler._sample_uniform(
         distribution=distribution, below=[], above=[]) for i in range(100)])
-    assert np.all(points > -1.)
+    assert np.all(points >= -1.)
     assert np.all(points < 1.)
 
 
@@ -18,7 +18,7 @@ def test_quniform():
     # type: () -> None
 
     sampler = pfnopt.samplers.TPESampler(seed=123)
-    distribution = pfnopt.distributions.QUniformDistribution(-10., 10., 1)
+    distribution = pfnopt.distributions.QUniformDistribution(-10.5, 10.5, 1)
     points = np.array([sampler._sample_quniform(
         distribution=distribution, below=[], above=[]) for i in range(100)])
     assert np.all(points >= -10)
@@ -26,7 +26,7 @@ def test_quniform():
     round_points = np.round(points)
     np.testing.assert_almost_equal(round_points, points)
 
-    distribution = pfnopt.distributions.QUniformDistribution(-10., 10., 0.1)
+    distribution = pfnopt.distributions.QUniformDistribution(-10.05, 10.05, 0.1)
     points = np.array([sampler._sample_quniform(
         distribution=distribution, below=[], above=[]) for i in range(100)])
     assert np.all(points >= -10)
