53e38cd217379db9232ee0a77b45e7df7603e036
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2018-07-17 17:39:40 +0900

    Replace value checking with type checking.

--- tests/samplers_tests/test_samplers.py
+++ tests/samplers_tests/test_samplers.py
@@ -63,5 +63,4 @@ def test_int(sampler_class):
     points = np.array([sampler.sample(storage, study_id, 'x', distribution) for _ in range(100)])
     assert np.all(points >= -10)
     assert np.all(points <= 10)
-    round_points = np.round(points)
-    np.testing.assert_almost_equal(round_points, points)
+    assert points.dtype == int
