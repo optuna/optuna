32aeee0dab932717920759a0cf09718227cfe0d1
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2018-08-03 10:18:24 +0900

    Divide a pruner test to simplify test scenarios.

--- tests/test_pruners.py
+++ tests/test_pruners.py
@@ -3,31 +3,47 @@ import pytest
 import pfnopt
 
 
-parametrize_pruner_args = pytest.mark.parametrize(
-    'n_startup_trials,n_warmup_steps',
-    [(5, 0), (0, 5)]
-)
+@pytest.mark.parametrize('n_startup_trials', [0, 5])
+def test_median_pruner_n_startup_trials(n_startup_trials):
+    # type: (int) -> None
 
-
-@parametrize_pruner_args
-def test_median_pruner(n_startup_trials, n_warmup_steps):
-    # type: (int, int) -> None
-
-    margin = 5
-    pruner = pfnopt.pruners.MedianPruner(n_startup_trials, n_warmup_steps)
+    pruner = pfnopt.pruners.MedianPruner(n_startup_trials, 0)
     study = pfnopt.study.create_study()
-    for _ in range(n_startup_trials + margin):
+    for count in range(n_startup_trials + 5):
         trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
-        for step in range(1, n_warmup_steps + margin + 1):
-            # Always greater than past trials.
-            trial.report(trial.trial_id, step)
+        for step in range(1, 6):
+            # Report a trial count as an objective value that always exceeds a pruner threshold.
+            trial.report(count, step)
             result = pruner.prune(storage=study.storage, study_id=study.study_id,
                                   trial_id=trial.trial_id, step=step)
-            if trial.trial_id < n_startup_trials:
-                # Start up
+            if trial.trial_id == 0:
+                # Pruning does not happen at the first trial.
                 assert result is False
-            elif step <= n_warmup_steps or trial.trial_id == 0:
-                # Warm up
+            elif trial.trial_id < n_startup_trials:
+                # Pruning does not happen if the number of trials is less than n_startup_trials.
+                assert result is False
+            else:
+                assert result is True
+
+
+@pytest.mark.parametrize('n_warmup_steps', [0, 5])
+def test_median_pruner_n_warmup_steps(n_warmup_steps):
+    # type: (int) -> None
+
+    pruner = pfnopt.pruners.MedianPruner(0, n_warmup_steps)
+    study = pfnopt.study.create_study()
+    for count in range(5):
+        trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
+        for step in range(1, n_warmup_steps + 6):
+            # Report a trial count as an objective value that always exceeds a pruner threshold.
+            trial.report(count, step)
+            result = pruner.prune(storage=study.storage, study_id=study.study_id,
+                                  trial_id=trial.trial_id, step=step)
+            if trial.trial_id == 0:
+                # Pruning does not happen at the first trial.
+                assert result is False
+            if step <= n_warmup_steps or trial.trial_id == 0:
+                # Pruning does not happen if the number of steps is less than n_warmup_steps.
                 assert result is False
             else:
                 assert result is True
