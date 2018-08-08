17132e97bf06d2e113b9e71f140d0862250b285c
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2018-08-08 10:31:50 +0900

    Add a test case when a trial has no intermediate values.

--- tests/test_pruners.py
+++ tests/test_pruners.py
@@ -9,7 +9,7 @@ def test_median_pruner_without_reports():
     trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
     pruner = pfnopt.pruners.MedianPruner(0, 0)
 
-    # A pruner is not activated if a trial has no intermediate results.
+    # A pruner is not activated if no trials are reported.
     assert not pruner.prune(storage=study.storage, study_id=study.study_id,
                             trial_id=trial.trial_id, step=1)
 
@@ -27,6 +27,27 @@ def test_median_pruner_with_one_trial():
                             trial_id=trial.trial_id, step=1)
 
 
+def test_median_pruner_without_intermediate_values():
+    # type: () -> None
+
+    pruner = pfnopt.pruners.MedianPruner(0, 0)
+    study = pfnopt.study.create_study()
+
+    trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
+    trial.report(1, 1)
+    study.storage.set_trial_state(trial.trial_id, TrialState.COMPLETE)
+
+    trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
+    # A pruner is not activated if a trial has no intermediate values.
+    assert not pruner.prune(storage=study.storage, study_id=study.study_id,
+                            trial_id=trial.trial_id, step=1)
+
+    trial.report(2, 1)
+    # A pruner is activated if a trial has an intermediate value.
+    assert pruner.prune(storage=study.storage, study_id=study.study_id,
+                        trial_id=trial.trial_id, step=1)
+
+
 def test_median_pruner_n_startup_trials():
     # type: () -> None
 
@@ -37,16 +58,16 @@ def test_median_pruner_n_startup_trials():
     trial.report(1, 1)
     study.storage.set_trial_state(trial.trial_id, TrialState.COMPLETE)
 
-    # A pruner is not activated during startup trials.
     trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
     trial.report(2, 1)
+    # A pruner is not activated during startup trials.
     assert not pruner.prune(storage=study.storage, study_id=study.study_id,
                             trial_id=trial.trial_id, step=1)
     study.storage.set_trial_state(trial.trial_id, TrialState.COMPLETE)
 
-    # A pruner is activated after startup trials.
     trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
     trial.report(3, 1)
+    # A pruner is activated after startup trials.
     assert pruner.prune(storage=study.storage, study_id=study.study_id,
                         trial_id=trial.trial_id, step=1)
 
@@ -62,13 +83,13 @@ def test_median_pruner_n_warmup_steps():
     trial.report(1, 2)
     study.storage.set_trial_state(trial.trial_id, TrialState.COMPLETE)
 
-    # A pruner is not activated during warm-up steps.
     trial = pfnopt.trial.Trial(study, study.storage.create_new_trial_id(study.study_id))
     trial.report(2, 1)
+    # A pruner is not activated during warm-up steps.
     assert not pruner.prune(storage=study.storage, study_id=study.study_id,
                             trial_id=trial.trial_id, step=1)
 
-    # A pruner is activated after warm-up steps.
     trial.report(2, 2)
+    # A pruner is activated after warm-up steps.
     assert pruner.prune(storage=study.storage, study_id=study.study_id,
                         trial_id=trial.trial_id, step=2)
