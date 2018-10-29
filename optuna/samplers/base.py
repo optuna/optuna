5003d05eeb56e858eb4cc7e66473e95a6126853b
Shotaro Sano <sano@preferred.jp>
2018-10-29 15:24:25 +0900

    Add docstring for samplers.

--- optuna/samplers/base.py
+++ optuna/samplers/base.py
@@ -8,7 +8,33 @@ from optuna.storages.base import BaseStorage  # NOQA
 @six.add_metaclass(abc.ABCMeta)
 class BaseSampler(object):
 
+    """Base class for samplers."""
+
     @abc.abstractmethod
     def sample(self, storage, study_id, param_name, param_distribution):
         # type: (BaseStorage, int, str, BaseDistribution) -> float
+
+        """Sample a parameter based on the previous trials and the given distribution.
+
+        Note that this method is not supposed to be called by library users. Instead,
+        :class:`optuna.trial.Trial` provides user interfaces to sample parameters in an objective
+        function.
+
+        Args:
+            storage:
+                Storage object.
+            study_id:
+                Identifier of the target study.
+            param_name:
+                Name of the sampled parameter.
+            param_distribution:
+                Distribution object that specifies a prior and/or scale of the sampling algorithm.
+
+        Returns:
+            A float value in the internal representation of Optuna.
+
+            .. seealso::
+                :attr:`optuna.structs.FrozenTrial.params_in_internal_repr`
+        """
+
         raise NotImplementedError
