75f2648cfceba9bd195e00ef05feb32bbbbbca63
Shotaro Sano <sano@preferred.jp>
2018-11-19 15:25:32 +0900

    Print optimization result in `pruning_simple.py`.

--- examples/pruning/pruning_simple.py
+++ examples/pruning/pruning_simple.py
@@ -45,3 +45,20 @@ def objective(trial):
 if __name__ == '__main__':
     study = optuna.create_study()
     study.optimize(objective, n_trials=100)
+
+    pruned_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.PRUNED]
+    complete_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.COMPLETE]
+
+    print('Study statistics: ')
+    print('  Number of finished trials: ', len(study.trials))
+    print('  Number of pruned trials: ', len(pruned_trials))
+    print('  Number of complete trials: ', len(complete_trials))
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: ', trial.value)
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
