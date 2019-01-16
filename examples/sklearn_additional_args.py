0a0ebaf6741e0eaf312f6db232473ff42a7599e8
djKooks <inylove82@gmail.com>
2019-01-16 18:57:32 +0900

    define cv to specific value

--- examples/sklearn_additional_args.py
+++ examples/sklearn_additional_args.py
@@ -31,9 +31,8 @@ class Objective(object):
         else:
             rf_max_depth = int(trial.suggest_loguniform('rf_max_depth', 2, 32))
             classifier_obj = sklearn.ensemble.RandomForestClassifier(max_depth=rf_max_depth)
-
-        cv = sklearn.model_selection.ShuffleSplit()
-        score = sklearn.model_selection.cross_val_score(classifier_obj, x, y, n_jobs=-1, cv=cv)
+        
+        score = sklearn.model_selection.cross_val_score(classifier_obj, x, y, n_jobs=-1, cv=3)
         accuracy = score.mean()
         return 1.0 - accuracy
 
