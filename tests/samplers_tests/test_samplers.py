aca0080a5424428fcdb0cc97070bc2e92f0594a5
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2019-02-19 17:06:41 +0900

    Take care of startup trials when testing sampler.sample.

--- tests/samplers_tests/test_samplers.py
+++ tests/samplers_tests/test_samplers.py
@@ -1,9 +1,18 @@
+import math
 import numpy as np
 import pytest
 import typing  # NOQA
 
 import optuna
+from optuna.distributions import CategoricalDistribution
+from optuna.distributions import DiscreteUniformDistribution
+from optuna.distributions import IntUniformDistribution
+from optuna.distributions import LogUniformDistribution
+from optuna.distributions import UniformDistribution
+from optuna.samplers import BaseSampler  # NOQA
 
+if optuna.types.TYPE_CHECKING:
+    from optuna.trial import T  # NOQA
 
 parametrize_sampler = pytest.mark.parametrize(
     'sampler_class',
@@ -12,56 +21,162 @@ parametrize_sampler = pytest.mark.parametrize(
 
 
 @parametrize_sampler
-def test_uniform(sampler_class):
-    # type: (typing.Callable[[], optuna.samplers.BaseSampler]) -> None
+@pytest.mark.parametrize('distribution', [UniformDistribution(-1., 1.),
+                                          UniformDistribution(0., 1.),
+                                          UniformDistribution(-1., 0.)])
+def test_uniform(sampler_class, distribution):
+    # type: (typing.Callable[[], BaseSampler], UniformDistribution) -> None
 
-    storage = optuna.storages.get_storage(None)
-    study_id = storage.create_new_study_id()
+    def objective(trial):
+        # type: (optuna.trial.Trial) -> float
 
-    sampler = sampler_class()
-    distribution = optuna.distributions.UniformDistribution(-1., 1.)
-    points = np.array([sampler.sample(storage, study_id, 'x', distribution) for _ in range(100)])
-    assert np.all(points >= -1.)
-    assert np.all(points < 1.)
+        return trial.suggest_uniform('x', distribution.low, distribution.high)
 
+    def check(study):
+        # type: (optuna.study.Study) -> None
 
-@parametrize_sampler
-def test_discrete_uniform(sampler_class):
-    # type: (typing.Callable[[], optuna.samplers.BaseSampler]) -> None
+        points = np.array([study.sampler.sample(study.storage, study.study_id, 'x', distribution)
+                           for _ in range(100)])
+        assert np.all(points >= distribution.low)
+        assert np.all(points < distribution.high)
 
-    sampler = sampler_class()
+    _study = optuna.study.create_study(sampler=sampler_class())
 
-    # Test to sample integer value: q = 1
-    storage = optuna.storages.get_storage(None)
-    study_id = storage.create_new_study_id()
+    check(_study)
 
-    distribution = optuna.distributions.DiscreteUniformDistribution(-10., 10., 1.)
-    points = np.array([sampler.sample(storage, study_id, 'x', distribution) for _ in range(100)])
-    assert np.all(points >= -10)
-    assert np.all(points <= 10)
-    round_points = np.round(points)
-    np.testing.assert_almost_equal(round_points, points)
+    # Execute optimization for samplers which have startup trials such as TPESampler.
+    _study.optimize(objective, n_trials=10)
 
-    # Test to sample quantized floating point value: [-10.2, 10.2], q = 0.1
-    distribution = optuna.distributions.DiscreteUniformDistribution(-10.2, 10.2, 0.1)
-    points = np.array([sampler.sample(storage, study_id, 'y', distribution) for _ in range(100)])
-    assert np.all(points >= -10.2)
-    assert np.all(points <= 10.2)
-    round_points = np.round(10 * points)
-    np.testing.assert_almost_equal(round_points, 10 * points)
+    check(_study)
 
 
 @parametrize_sampler
-def test_int(sampler_class):
-    # type: (typing.Callable[[], optuna.samplers.BaseSampler]) -> None
+@pytest.mark.parametrize('distribution', [LogUniformDistribution(1e-7, 1.)])
+def test_log_uniform(sampler_class, distribution):
+    # type: (typing.Callable[[], BaseSampler], LogUniformDistribution) -> None
 
-    sampler = sampler_class()
-    storage = optuna.storages.get_storage(None)
-    study_id = storage.create_new_study_id()
+    def objective(trial):
+        # type: (optuna.trial.Trial) -> float
 
-    distribution = optuna.distributions.IntUniformDistribution(-10, 10)
-    points = np.array([sampler.sample(storage, study_id, 'x', distribution) for _ in range(100)])
-    assert np.all(points >= -10)
-    assert np.all(points <= 10)
-    round_points = np.round(points)
-    np.testing.assert_almost_equal(round_points, points)
+        return trial.suggest_loguniform('x', distribution.low, distribution.high)
+
+    def check(study):
+        # type: (optuna.study.Study) -> None
+
+        points = np.array([study.sampler.sample(study.storage, study.study_id, 'x', distribution)
+                           for _ in range(100)])
+        assert np.all(points >= distribution.low)
+        assert np.all(points < distribution.high)
+
+    _study = optuna.study.create_study(sampler=sampler_class())
+
+    check(_study)
+
+    # Execute optimization for samplers which have startup trials such as TPESampler.
+    _study.optimize(objective, n_trials=10)
+
+    check(_study)
+
+
+@parametrize_sampler
+@pytest.mark.parametrize('distribution', [DiscreteUniformDistribution(-10, 10, 1),
+                                          DiscreteUniformDistribution(-10.2, 10.2, 0.1),
+                                          DiscreteUniformDistribution(64., 1312., 160.),
+                                          DiscreteUniformDistribution(0, 10, math.pi)])
+def test_discrete_uniform(sampler_class, distribution):
+    # type: (typing.Callable[[], BaseSampler], DiscreteUniformDistribution) -> None
+
+    def objective(trial):
+        # type: (optuna.trial.Trial) -> float
+
+        return trial.suggest_discrete_uniform('x', distribution.low, distribution.high,
+                                              distribution.q)
+
+    def check(study):
+        # type: (optuna.study.Study) -> None
+
+        points = np.array([study.sampler.sample(study.storage, study.study_id, 'x', distribution)
+                           for _ in range(100)])
+        assert np.all(points >= distribution.low)
+        assert np.all(points <= distribution.high)
+
+        # Check all points are multiples of distribution.q except endpoints.
+        points = points[points != distribution.low]
+        points = points[points != distribution.high]
+        points /= distribution.q
+        round_points = np.round(points)
+        np.testing.assert_almost_equal(round_points, points)
+
+    _study = optuna.study.create_study(sampler=sampler_class())
+
+    check(_study)
+
+    # Execute optimization for samplers which have startup trials such as TPESampler.
+    _study.optimize(objective, n_trials=10)
+
+    check(_study)
+
+
+@parametrize_sampler
+@pytest.mark.parametrize('distribution', [IntUniformDistribution(-10, 10),
+                                          IntUniformDistribution(0, 10),
+                                          IntUniformDistribution(-10, 0)])
+def test_int(sampler_class, distribution):
+    # type: (typing.Callable[[], BaseSampler], IntUniformDistribution) -> None
+
+    def objective(trial):
+        # type: (optuna.trial.Trial) -> float
+
+        return trial.suggest_int('x', distribution.low, distribution.high)
+
+    def check(study):
+        # type: (optuna.study.Study) -> None
+
+        points = np.array([study.sampler.sample(study.storage, study.study_id, 'x', distribution)
+                           for _ in range(100)])
+        assert np.all(points >= distribution.low)
+        assert np.all(points <= distribution.high)
+
+    _study = optuna.study.create_study(sampler=sampler_class())
+
+    check(_study)
+
+    # Execute optimization for samplers which have startup trials such as TPESampler.
+    _study.optimize(objective, n_trials=10)
+
+    check(_study)
+
+
+@parametrize_sampler
+@pytest.mark.parametrize('choices', [(1, 2, 3),
+                                     ('a', 'b', 'c')])
+def test_categorical(sampler_class, choices):
+    # type: (typing.Callable[[], BaseSampler], typing.Tuple[T, ...]) -> None
+
+    distribution = CategoricalDistribution(choices)
+
+    def objective(trial):
+        # type: (optuna.trial.Trial) -> float
+
+        trial.suggest_categorical('x', choices)
+        return 1.0
+
+    def check(study):
+        # type: (optuna.study.Study) -> None
+
+        points = np.array([study.sampler.sample(study.storage, study.study_id, 'x', distribution)
+                           for _ in range(100)])
+        # 'x' value is corresponding to an index of distribution.choices.
+        assert np.all(points >= 0)
+        assert np.all(points <= len(distribution.choices) - 1)
+        round_points = np.round(points)
+        np.testing.assert_almost_equal(round_points, points)
+
+    _study = optuna.study.create_study(sampler=sampler_class())
+
+    check(_study)
+
+    # Execute optimization for samplers which have startup trials such as TPESampler.
+    _study.optimize(objective, n_trials=10)
+
+    check(_study)
