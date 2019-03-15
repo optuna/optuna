b25b25c8c1a7270bb563030b7168a3f3669bdbfd
Takeru Ohta <phjgt308@gmail.com>
2019-03-15 15:08:54 +0900

    Sort imports in env.py.

--- optuna/storages/rdb/alembic/env.py
+++ optuna/storages/rdb/alembic/env.py
@@ -1,12 +1,10 @@
 from __future__ import with_statement
 
+from alembic import context
 import logging
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
-from alembic import context
 
 import optuna
 
