90c9238404275d75247e2e045d621991b50a28cb
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-04-02 01:15:52 +0900

    Update test_sklearn.py

--- tests/integration_tests/test_sklearn.py
+++ tests/integration_tests/test_sklearn.py
@@ -2,57 +2,37 @@ import pytest
 from sklearn.datasets import make_blobs
 from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import SGDClassifier
-from sklearn.svm import LinearSVC
 
 from optuna import distributions
 from optuna import integration
 from optuna import samplers
 
 
+@pytest.mark.parametrize('enable_pruning', [True, False])
 @pytest.mark.filterwarnings('ignore::UserWarning')
-def test_fit():
-    # type: () -> None
-
-    X, y = make_blobs(n_samples=10)
-    est = LinearSVC()
-    param_dist = {'C': distributions.LogUniformDistribution(1e-04, 1e+03)}
-    sampler = samplers.TPESampler(seed=0)
-    tpe_search = integration.OptunaSearchCV(
-        est,
-        param_dist,
-        cv=3,
-        error_score='raise',
-        max_iter=5,
-        sampler=sampler
-    )
-
-    with pytest.raises(NotFittedError):
-        tpe_search._check_is_fitted()
-
-    tpe_search.fit(X, y)
-    tpe_search._check_is_fitted()
-
-
-@pytest.mark.filterwarnings('ignore::UserWarning')
-def test_fit_with_pruning():
-    # type: () -> None
+def test_optuna_search(enable_pruning):
+    # type: (bool) -> None
 
     X, y = make_blobs(n_samples=10)
     est = SGDClassifier(max_iter=5, tol=1e-03)
     param_dist = {'alpha': distributions.LogUniformDistribution(1e-04, 1e+03)}
     sampler = samplers.TPESampler(seed=0)
-    tpe_search = integration.OptunaSearchCV(
+    optuna_search = integration.OptunaSearchCV(
         est,
         param_dist,
         cv=3,
-        enable_pruning=True,
+        enable_pruning=enable_pruning,
         error_score='raise',
         max_iter=5,
+        return_train_score=True,
         sampler=sampler
     )
 
     with pytest.raises(NotFittedError):
-        tpe_search._check_is_fitted()
+        optuna_search._check_is_fitted()
 
-    tpe_search.fit(X, y)
-    tpe_search._check_is_fitted()
+    optuna_search.fit(X, y)
+    optuna_search.trials_dataframe()
+    optuna_search.decision_function(X)
+    optuna_search.predict(X)
+    optuna_search.score(X, y)
