80f2930151b0eeaa393ff0441917f783da3f1561
Rishav Chourasia <rishav.chourasia@gmail.com>
2019-04-11 11:10:41 +0530

    fix: Addressing the CR.

--- examples/pytorch_simple.py
+++ examples/pytorch_simple.py
@@ -14,14 +14,14 @@ We have the following two ways to execute this example:
 
 (2) Execute through CLI.
     $ STUDY_NAME=`optuna create-study --storage sqlite:///example.db`
-    $ optuna study optimize mxnet_simple.py objective --n-trials=100 --study $STUDY_NAME \
+    $ optuna study optimize pytorch_simple.py objective --n-trials=100 --study $STUDY_NAME \
       --storage sqlite:///example.db
 
 """
 
 from __future__ import print_function
 
-import tempfile
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -30,27 +30,27 @@ import torch.utils.data
 from torchvision import datasets
 from torchvision import transforms
 
-
-if torch.cuda.is_available():
-    DEVICE = torch.device('cuda')
-else:
-    DEVICE = torch.device('cpu')
+DEVICE = torch.device('cpu')
 BATCHSIZE = 128
 CLASSES = 10
-DIR = tempfile.mkdtemp()
+DIR = os.getcwd()
 EPOCHS = 10
 LOG_INTERVAL = 10
+N_TRAIN_EXAMPLES = BATCHSIZE * 30
+N_TEST_EXAMPLES = BATCHSIZE * 10
 
 
 class Net(nn.Module):
+    # Constructor for trial network.
     def __init__(self, trial):
         super(Net, self).__init__()
         self.layers = []
         self.dropouts = []
 
+        # We optimize the number of layers, hidden untis in each layer and drouputs.
         n_layers = trial.suggest_int('n_layers', 1, 3)
         dropout = trial.suggest_uniform('dropout', 0.2, 0.5)
-        input_dim = 28*28
+        input_dim = 28 * 28
         for i in range(n_layers):
             output_dim = int(trial.suggest_loguniform('n_units_l{}'.format(i), 4, 128))
             self.layers.append(nn.Linear(input_dim, output_dim))
@@ -59,14 +59,17 @@ class Net(nn.Module):
 
         self.layers.append(nn.Linear(input_dim, CLASSES))
 
+        # assinging the layers as class variables (PyTorch requirement).
         for idx, layer in enumerate(self.layers):
             setattr(self, 'fc{}'.format(idx), layer)
 
+        # assinging the dropouts as class variables (PyTorch requirement).
         for idx, dropout in enumerate(self.dropouts):
             setattr(self, 'drop{}'.format(idx), dropout)
 
+    # Forward pass computation function.
     def forward(self, data):
-        data = data.view(-1, 28*28)
+        data = data.view(-1, 28 * 28)
         for layer, dropout in zip(self.layers, self.dropouts):
             data = F.relu(layer(data))
             data = dropout(data)
@@ -74,79 +77,82 @@ class Net(nn.Module):
 
 
 def get_mnist():
-    # Load MNIST dataset
-    kwargs = {'num_workers': 1, 'pin_memory': True} if DEVICE.type == 'cuda' else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(DIR, train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=BATCHSIZE, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(DIR, train=False,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=BATCHSIZE, shuffle=True, **kwargs)
+    # Load MNIST dataset.
+    train_loader = torch.utils.data.DataLoader(datasets.MNIST(DIR,
+                                                              train=True,
+                                                              download=True,
+                                                              transform=transforms.ToTensor()),
+                                               batch_size=BATCHSIZE,
+                                               shuffle=True)
+    test_loader = torch.utils.data.DataLoader(datasets.MNIST(DIR,
+                                                             train=False,
+                                                             transform=transforms.ToTensor()),
+                                              batch_size=BATCHSIZE,
+                                              shuffle=True)
 
     return (train_loader, test_loader)
 
 
 def objective(trial):
 
-    # Generate the model
+    # Generate the model.
     model = Net(trial).to(DEVICE)
 
-    # Generate the optimizers
+    # Generate the optimizers.
     optimizer_name = trial.suggest_categorical('optimizer', ['Adam', 'RMSprop', 'SGD'])
     lr = trial.suggest_uniform('lr', 1e-5, 1e-1)
     optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
-    # Get the MNIST dataset
+    # Get the MNIST dataset.
     train_loader, test_loader = get_mnist()
 
-    # Train the model
+    # Training of the model.
     model.train()
     for epoch in range(EPOCHS):
         for batch_idx, (data, target) in enumerate(train_loader):
+            # Limiting training data for faster epochs.
+            if batch_idx * BATCHSIZE >= N_TRAIN_EXAMPLES:
+                break
+
             data, target = data.to(DEVICE), target.to(DEVICE)
 
-            # Zero gradient buffers
+            # Zeroing out gradient buffers.
             optimizer.zero_grad()
-            # Forward pass
+            # Performing a forward pass.
             output = model(data)
-            # Negative Log Likelihood loss
+            # Computing negative Log Likelihood loss.
             loss = F.nll_loss(output, target)
-            # Backward pass
+            # Performing a backward pass.
             loss.backward()
-            # Update weights
+            # Updating the weights.
             optimizer.step()
 
-            # Printing statistics
+            # Printing statistics.
             if batch_idx % LOG_INTERVAL == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(train_loader.dataset),
-                    100. * batch_idx / len(train_loader), loss.item()))
+                    epoch, batch_idx * len(data), N_TRAIN_EXAMPLES,
+                    100. * batch_idx * BATCHSIZE / N_TRAIN_EXAMPLES, loss.item()))
 
-    # Test the model
+    # Validation of the model.
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for data, target in test_loader:
+        for batch_idx, (data, target) in enumerate(test_loader):
+            # Limiting testing data.
+            if batch_idx * BATCHSIZE >= N_TEST_EXAMPLES:
+                break
             data, target = data.to(DEVICE), target.to(DEVICE)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # Sum up batch losses.
+            pred = output.argmax(dim=1, keepdim=True)  # Get the index of the max log-probability.
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
-    accuracy = 100. * correct / len(test_loader.dataset)
+    test_loss /= N_TEST_EXAMPLES
+    accuracy = 100. * correct / N_TEST_EXAMPLES
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset), accuracy))
+        test_loss, correct, N_TEST_EXAMPLES, accuracy))
 
     return accuracy
 
