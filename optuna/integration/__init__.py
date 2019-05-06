c24632f79e95e72240d0f8a5a56bdbfc49eb38a1
Y-oHr-N <kon.y.ohr.n@gmail.com>
2019-05-07 06:59:10 +0900

    Fix a flake8 error

--- optuna/integration/__init__.py
+++ optuna/integration/__init__.py
@@ -26,8 +26,8 @@ if sys.version_info[0] == 2 or TYPE_CHECKING:
     from optuna.integration.chainermn import ChainerMNStudy  # NOQA
     from optuna.integration.keras import KerasPruningCallback  # NOQA
     from optuna.integration.lightgbm import LightGBMPruningCallback  # NOQA
-    from optuna.integration.sklearn import OptunaSearchCV  # NOQA
     from optuna.integration.mxnet import MXNetPruningCallback  # NOQA
+    from optuna.integration.sklearn import OptunaSearchCV  # NOQA
     from optuna.integration.tensorflow import TensorFlowPruningHook  # NOQA
     from optuna.integration.xgboost import XGBoostPruningCallback  # NOQA
 else:
