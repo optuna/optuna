a48369beef57e0d47d72aeccaaada65b74728fd2
c-bata <contact@c-bata.link>
2019-05-26 12:13:24 +0900

    Add example to store/load training model

---
+++ examples/pruning/caching_model.py
@@ -0,0 +1,78 @@
+"""
+Optuna example that demonstrates a pruner.
+
+In this example, we optimize a classifier configuration using scikit-learn. Note that, to enable
+the pruning feature, the following 2 methods are invoked after each step of the iterative training.
+
+(1) :func:`optuna.trial.Trial.report`
+(2) :func:`optuna.trial.Trial.should_prune`
+
+You can run this example as follows:
+    $ python simple.py
+
+"""
+import pickle
+
+import sklearn.datasets
+import sklearn.linear_model
+import sklearn.model_selection
+
+import optuna
+
+
+# FYI: Objective functions can take additional arguments
+# (https://optuna.readthedocs.io/en/stable/faq.html#objective-func-additional-args).
+def objective(trial):
+    serialized_model, previous_step = trial.get_saved_model()
+    if serialized_model:
+        clf = pickle.loads(serialized_model)
+    else:
+        alpha = trial.suggest_loguniform('alpha', 1e-5, 1e-1)
+        clf = sklearn.linear_model.SGDClassifier(alpha=alpha)
+
+    iris = sklearn.datasets.load_iris()
+    classes = list(set(iris.target))
+    train_x, test_x, train_y, test_y = \
+        sklearn.model_selection.train_test_split(iris.data, iris.target, test_size=0.25)
+
+    for step in range(100):
+        if previous_step > step:
+            continue
+
+        clf.partial_fit(train_x, train_y, classes=classes)
+
+        # Report intermediate objective value.
+        intermediate_value = 1.0 - clf.score(test_x, test_y)
+        trial.report(intermediate_value, step)
+
+        # Handle pruning based on the intermediate value.
+        if trial.should_prune(step):
+            raise optuna.structs.TrialPruned()
+
+        if trial.should_suspend(step):
+            serialized = pickle.dumps(clf)
+            trial.save_model(step, serialized)
+
+    return 1.0 - clf.score(test_x, test_y)
+
+
+if __name__ == '__main__':
+    study = optuna.create_study()
+    study.optimize(objective, n_trials=100)
+
+    pruned_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.PRUNED]
+    complete_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.COMPLETE]
+
+    print('Study statistics: ')
+    print('  Number of finished trials: ', len(study.trials))
+    print('  Number of pruned trials: ', len(pruned_trials))
+    print('  Number of complete trials: ', len(complete_trials))
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: ', trial.value)
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
