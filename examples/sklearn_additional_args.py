7adef9815dc5f4abe42828938131df859863b328
c-bata <contact@c-bata.link>
2019-06-05 01:46:10 +0900

    Fix deprecation of sklearn_additional_args

--- examples/sklearn_additional_args.py
+++ examples/sklearn_additional_args.py
@@ -27,10 +27,10 @@ class Objective(object):
         classifier_name = trial.suggest_categorical('classifier', ['SVC', 'RandomForest'])
         if classifier_name == 'SVC':
             svc_c = trial.suggest_loguniform('svc_c', 1e-10, 1e10)
-            classifier_obj = sklearn.svm.SVC(C=svc_c)
+            classifier_obj = sklearn.svm.SVC(C=svc_c, gamma='auto')
         else:
             rf_max_depth = int(trial.suggest_loguniform('rf_max_depth', 2, 32))
-            classifier_obj = sklearn.ensemble.RandomForestClassifier(max_depth=rf_max_depth)
+            classifier_obj = sklearn.ensemble.RandomForestClassifier(max_depth=rf_max_depth, n_estimators=10)
 
         score = sklearn.model_selection.cross_val_score(classifier_obj, x, y, n_jobs=-1, cv=3)
         accuracy = score.mean()
