3fe09674fe0f84d631460e5fc81bc0975279bbe2
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-08-08 17:14:31 +0100

    Added catboost example
    This is still a work in progress, the idea
    was to add an initial stub. There are still
    more available parameters to tune.

---
+++ examples/catboost_simple.py
@@ -0,0 +1,71 @@
+"""
+Optuna example that optimizes a classifier configuration for cancer dataset using Catboost.
+
+In this example, we optimize the validation accuracy of cancer detection using Catboost.
+We optimize both the choice of booster model and their hyperparameters.
+
+We have following two ways to execute this example:
+
+(1) Execute this code directly.
+    $ python catboost_simple.py
+
+
+(2) Execute through CLI.
+    $ STUDY_NAME=`optuna create-study --direction maximize --storage sqlite:///example.db`
+    $ optuna study optimize catboost_simple.py objective --n-trials=100 --study $STUDY_NAME \
+      --storage sqlite:///example.db
+
+"""
+
+import catboost as cb
+import numpy as np
+import sklearn.datasets
+import sklearn.metrics
+from sklearn.model_selection import train_test_split
+
+import optuna
+
+
+def objective(trial):
+    data, target = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.3)
+
+    param = {
+        'objective'         : trial.suggest_categorical('objective', ['Logloss', 'CrossEntropy']),
+        'iterations'        : trial.suggest_int('num_leaves', 500, 2000),
+        'colsample_bylevel' : trial.suggest_uniform('colsample_bylevel', 0.01, 0.1),
+        'depth'             : trial.suggest_int('depth', 1, 16),
+        'boosting_type'   : trial.suggest_categorical('boosting_type', ['Ordered', 'Plain']),
+        'learning_rate'     : trial.suggest_loguniform('learning_rate', 1e-6, 1.0)
+        # On GPU machines only
+        # 'max_leaves'        : trial.suggest_int('num_leaves', 1, 1000),
+        # 'grow_policy'       : trial.suggest_categorical('grow_policy', ['SymmetricTree', 'Depthwise', 'Lossguide']),
+    }
+
+    gbm = cb.CatBoostClassifier(**param)
+
+    gbm.fit(train_x, train_y,
+            eval_set=[(test_x, test_y)],
+            verbose=0, early_stopping_rounds=100)
+
+    preds = gbm.predict(test_x)
+    pred_labels = np.rint(preds)
+    accuracy = sklearn.metrics.accuracy_score(test_y, pred_labels)
+    return accuracy
+
+
+if __name__ == '__main__':
+    study = optuna.create_study(direction='maximize')
+    # study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=5)
+
+    print('Number of finished trials: {}'.format(len(study.trials)))
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: {}'.format(trial.value))
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
