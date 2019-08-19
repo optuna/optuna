e2930d5d388e03d17b7f0292be204893b82762a8
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2019-08-19 20:56:21 +0200

    Add conditional parameter

--- examples/dask_ml_simple.py
+++ examples/dask_ml_simple.py
@@ -3,8 +3,8 @@ Optuna example that optimizes a classifier configuration for the Iris dataset us
 
 In this example, we optimize a logistic regression classifier configuration for the Iris dataset.
 The classifier is from dask-ml while the dataset is from sklearn.
-We optimize both the choice of solver (admm, gradient descent, or lbfgs)
-and the regularization strength (C).
+We optimize the choice of solver (admm, gradient descent, or newton),
+the regularization (penalty) when relevant and its strength (C).
 
 We have the following two ways to execute this example:
 
@@ -34,11 +34,18 @@ def objective(trial):
     X, y = iris.data, iris.target
     X, y = da.from_array(X, chunks=len(X) // 5), da.from_array(y, chunks=len(y) // 5)
 
-    solver = trial.suggest_categorical('solver', ['admm', 'gradient_descent', 'lbfgs'])
+    solver = trial.suggest_categorical('solver', ['admm', 'gradient_descent', 'proximal_grad'])
     C = trial.suggest_uniform('C', 0.0, 1.0)
+
+    if solver == 'admm' or solver == 'proximal_grad':
+        penalty = trial.suggest_categorical('penalty', ['l1', 'l2', 'elastic_net'])
+    else:
+        # 'penalty' parameter isn't relevant for this solver, so we always specify 'l2' as the dummy value.
+        penalty = 'l2'
+
     max_iter = 100
 
-    classifier = LogisticRegression(max_iter=max_iter, solver=solver, C=C)
+    classifier = LogisticRegression(max_iter=max_iter, solver=solver, C=C, penalty=penalty)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
     classifier.fit(X_train, y_train)
@@ -52,7 +59,7 @@ if __name__ == '__main__':
     study = optuna.create_study(direction='maximize')
 
     # This is used to initialize the workers that will be used by Dask-ML.
-    cluster = LocalCluster(n_workers=4, threads_per_worker=2)
+    cluster = LocalCluster(n_workers=2)
     with Client(cluster) as client:
         study.optimize(objective, n_trials=100)
 
