911f7fff475526df23e6970e909eb152ac6f23a0
Stephane Couvreur <stephane.couvreur.sueron@gmail.com>
2019-08-29 14:05:22 +0100

    Reduced n_trials and increased timeout

--- examples/keras_simple.py
+++ examples/keras_simple.py
@@ -82,7 +82,7 @@ def objective(trial):
 
 if __name__ == '__main__':
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=200, timeout=200)
+    study.optimize(objective, n_trials=100, timeout=600)
 
     print('Number of finished trials: {}'.format(len(study.trials)))
 
