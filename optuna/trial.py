15a374ecb1c8153c2da9195a13f1fa8115569e37
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2019-09-10 16:51:27 +0900

    Use decimal to calculate new high.

--- optuna/trial.py
+++ optuna/trial.py
@@ -700,10 +700,14 @@ class FixedTrial(BaseTrial):
 def _adjust_discrete_uniform_high(name, low, high, q):
     # type: (str, float, float, float) -> float
 
-    r = high - low
+    d_high = decimal.Decimal(str(high))
+    d_low = decimal.Decimal(str(low))
+    d_q = decimal.Decimal(str(q))
 
-    if (decimal.Decimal(str(r)) % decimal.Decimal(str(q))) != decimal.Decimal('0'):
-        high = (r // q) * q + low
+    d_r = d_high - d_low
+
+    if d_r % d_q != decimal.Decimal('0'):
+        high = float((d_r // d_q) * d_q + d_low)
         logger = logging.get_logger(__name__)
         logger.warning('The range of parameter `{}` is not divisible by `q`, and is '
                        'replaced by [{}, {}].'.format(name, low, high))
