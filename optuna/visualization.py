0daacb8645e99eeebb22c9ad992bf86737731c0a
suecharo <suehiro619@gmail.com>
2019-09-11 14:36:17 +0900

    Add support for plotly 4.0.0 to plot_intermediate_values.

--- optuna/visualization.py
+++ optuna/visualization.py
@@ -8,7 +8,7 @@ if type_checking.TYPE_CHECKING:
 try:
     import plotly.graph_objs as go
     from plotly.offline import init_notebook_mode
-    from plotly.offline import iplot
+    from IPython.display import display, HTML
     _available = True
 except ImportError as e:
     _import_error = e
@@ -45,22 +45,47 @@ def plot_intermediate_values(study):
 
     _check_plotly_availability()
     init_notebook_mode(connected=True)
-    layout = go.Layout(showlegend=False)
-    iplot(go.Figure(_get_intermediate_values_data(study), layout))
+    figure = _get_intermediate_plot(study)
+    figure.show()
 
 
-def _get_intermediate_values_data(study):
+def _get_intermediate_plot(study):
     # type: (Study) -> List[go.Scatter]
 
-    target_status = [TrialState.PRUNED, TrialState.COMPLETE, TrialState.RUNNING]
-    trials = [t for t in study.trials if t.state in target_status]
+    layout = go.Layout(
+        title='Intermediate Values Plot',
+        xaxis={'title': 'Step'},
+        yaxis={'title': 'Intermediate Value'},
+        showlegend=False
+    )
 
-    intermediate_values = [t.intermediate_values for t in trials]
-    intermediate_values = [iv for iv in intermediate_values if len(iv) != 0]
+    try:
+        df = study.trials_dataframe()
+        df = df.rename(columns={'number': 'trial_id'})
+    except Exception:  # empty study
+        return go.Figure(data=[], layout=layout)
 
-    data = [go.Scatter(x=list(iv.keys()), y=list(iv.values())) for iv in intermediate_values]
+    if 'intermediate_values' not in df:
+        return go.Figure(data=[], layout=layout)
 
-    return data
+    target_state = [TrialState.PRUNED, TrialState.COMPLETE, TrialState.RUNNING]
+    dst_df = df[df['state'].isin(target_state)]
+    dst_df = dst_df[dst_df['intermediate_values'].isnull().all(axis=1) == False]
+    traces = []
+    for __, row in dst_df.iterrows():
+        trace = go.Scatter(
+            x=row['intermediate_values'].index,
+            y=row['intermediate_values'],
+            mode='lines+markers',
+            marker={
+                'maxdisplayed': 10
+            },
+            name='Trial{}'.format(row['trial_id'][0]))
+        traces.append(trace)
+
+    figure = go.Figure(data=traces, layout=layout)
+
+    return figure
 
 
 def _check_plotly_availability():
@@ -70,5 +95,15 @@ def _check_plotly_availability():
         raise ImportError(
             'Plotly is not available. Please install plotly to use this feature. '
             'Plotly can be installed by executing `$ pip install plotly`. '
+            'For further information, please refer to the installation guide of plotly. '
+            '(The actual import error is as follows: ' + str(_import_error) + ')')
+
+    from distutils.version import StrictVersion
+    from plotly import __version__ as plotly_version
+    if StrictVersion(plotly_version) < StrictVersion('4.0.0'):
+        raise ImportError(
+            'Your version of Plotly is ' + plotly_version + ' . '
+            'Please install plotly version 4.0.0 or higher. '
+            'Plotly can be installed by executing `$ pip install -U plotly>=4.0.0`. '
             'For further information, please refer to the installation guide of plotly. '
             '(The actual import error is as follows: ' + str(_import_error) + ')')
