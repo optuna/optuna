923d3a696c517a5e29819922937e844e19a091b7
sfujiwara <shuhei.fujiwara@gmail.com>
2019-09-15 04:22:47 +0900

    fix for pep8 h405

--- optuna/integration/tfkeras.py
+++ optuna/integration/tfkeras.py
@@ -19,6 +19,7 @@ except ImportError as e:
 
 class TFKerasPruningCallback(Callback):
     """tf.keras callback to prune unpromising trials.
+
     This callback is intend to be compatible for TensorFlow v1 and v2,
     but only tested with TensorFlow v1.
 
