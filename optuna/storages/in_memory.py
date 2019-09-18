362186200ed9181f73aebe13c32e4fe168b4204c
Keita Oda <oda@preferred.jp>
2019-09-18 14:26:20 +0900

    Update when trial is completed.

--- optuna/storages/in_memory.py
+++ optuna/storages/in_memory.py
@@ -250,24 +250,25 @@ class InMemoryStorage(base.BaseStorage):
     def update_cache(self, trial_id):
         # type: (int) -> None
 
-        if self.best_trial_id is None:
-            self.best_trial_id = trial_id
-            return
-        best_value = self.trials[self.best_trial_id].value
-        new_value = self.trials[trial_id].value
-        if best_value is None:
-            self.best_trial_id = trial_id
-            return
-        if new_value is None:
-            return
-        if (self.get_study_direction(IN_MEMORY_STORAGE_STUDY_ID) ==
-                structs.StudyDirection.MAXIMIZE):
-            if best_value < new_value:
+        if trial.state == structs.TrialState.COMPLETE:
+            if self.best_trial_id is None:
+                self.best_trial_id = trial_id
+                return
+            best_value = self.trials[self.best_trial_id].value
+            new_value = self.trials[trial_id].value
+            if best_value is None:
+                self.best_trial_id = trial_id
+                return
+            if new_value is None:
+                return
+            if (self.get_study_direction(IN_MEMORY_STORAGE_STUDY_ID) ==
+                    structs.StudyDirection.MAXIMIZE):
+                if best_value < new_value:
+                    self.best_trial_id = trial_id
+                return
+            if best_value > new_value:
                 self.best_trial_id = trial_id
             return
-        if best_value > new_value:
-            self.best_trial_id = trial_id
-        return
 
     def set_trial_intermediate_value(self, trial_id, step, intermediate_value):
         # type: (int, int, float) -> bool
