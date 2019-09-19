d35901028dda77b8c928726ba1a99eff8fb964df
suecharo <suehiro619@gmail.com>
2019-09-19 16:26:50 +0900

    Fix trial_state, tests, var_name, merge_error

--- optuna/visualization.py
+++ optuna/visualization.py
@@ -211,27 +211,27 @@ def _get_parallel_coordinate_plot(study, params=[]):
         title='Parallel Coordinate Plot',
     )
 
-    trials = [t for t in study.trials if t.state in TARGET_STATUS]
+    trials = [t for t in study.trials if t.state == TrialState.COMPLETE]
 
     if len(study.trials) == 0:
         logger.warning('Your study does not have any completed trials.')
         return go.Figure(data=[], layout=layout)
 
-    param_names = {p_name for t in trials for p_name in t.params.keys()}
+    all_params = {p_name for t in trials for p_name in t.params.keys()}
     if len(params) != 0:
-        for p_name in params:
-            if p_name not in param_names:
-                logger.warning('Parameter {} does not exist in your study.'.format(p_name))
+        for input_p_name in params:
+            if input_p_name not in all_params:
+                logger.warning('Parameter {} does not exist in your study.'.format(input_p_name))
                 return go.Figure(data=[], layout=layout)
-        param_names = set(params)
-    sorted_param_names = sorted(list(param_names))
+        all_params = set(params)
+    sorted_params = sorted(list(all_params))
 
     dims = [{
         'label': 'Objective Value',
         'values': tuple([t.value for t in trials]),
         'range': (min([t.value for t in trials]), max([t.value for t in trials]))
     }]
-    for p_name in sorted_param_names:
+    for p_name in sorted_params:
         values = []
         for t in trials:
             if p_name in t.params:
@@ -265,112 +265,6 @@ def _get_parallel_coordinate_plot(study, params=[]):
                 ),
                 showscale=True,
                 reversescale=True if study.direction == StudyDirection.MINIMIZE else False
-            )
-        )
-    ]
-
-    figure = go.Figure(data=traces, layout=layout)
-
-    return figure
-
-
-def plot_parallel_coordinate(study, params=[]):
-    # type: (Study, List[str]) -> None
-    """Inside Jupyter notebook, plot optimization history of all trials in a study.
-
-        Note that, If a parameter contains missing values, a trial with missing values is not
-        plotted.
-
-    Example:
-
-        The following code snippet shows how to plot optimization history inside Jupyter Notebook.
-
-        .. code::
-
-            import optuna
-
-            def objective(trial):
-                # Intermediate values are supposed to be reported inside the objective function.
-                ...
-
-            study = optuna.create_study()
-            study.optimize(n_trials=100)
-
-            optuna.visualization.plot_parallel_coordinate(study, params=['param_a', 'param_b'])
-
-    Args:
-        study:
-            A :class:`~optuna.study.Study` object whose trials are plotted for their objective
-            values.
-        params:
-            Parameter list to visualize. The default is all parameters.
-    """
-
-    _check_plotly_availability()
-    init_notebook_mode(connected=True)
-    figure = _get_parallel_coordinate_plot(study, params)
-    figure.show()
-
-
-def _get_parallel_coordinate_plot(study, params=[]):
-    # type: (Study, List[str]) -> Figure
-
-    layout = go.Layout(
-        title='Parallel Coordinate Plot',
-    )
-
-    trials = study.trials
-
-    param_names = set()
-    for t in trials:
-        for p_name in t.params.keys():
-            param_names.add(p_name)
-    if len(params) != 0:
-        for p_name in params:
-            if p_name not in param_names:
-                logger.warning("")
-                return go.Figure(data=[], layout=layout)
-        param_names = set(params)
-    sorted_param_names = sorted(list(param_names))
-
-    dims = [{
-        'label': 'Objective Value',
-        'values': tuple([t.value for t in trials]),
-        'range': (min([t.value for t in trials]), max([t.value for t in trials]))
-    }]
-    for p_name in sorted_param_names:
-        values = []
-        for t in trials:
-            if p_name in t.params:
-                values.append(t.params[p_name])
-        is_categorical = False
-        try:
-            tuple(map(float, values))
-        except (TypeError, ValueError):
-            from collections import defaultdict
-            vocab = defaultdict(lambda: len(vocab))  # type: DefaultDict[str, int]
-            values = [vocab[v] for v in values]
-            is_categorical = True
-        dim = {
-            'label': p_name,
-            'values': tuple(values),
-            'range': (min(values), max(values))
-        }
-        if is_categorical:
-            dim['tickvals'] = list(range(len(vocab)))
-            dim['ticktext'] = list(sorted(vocab.items(), key=lambda x: x[1]))
-        dims.append(dim)
-
-    traces = [
-        go.Parcoords(
-            dimensions=dims,
-            line=dict(
-                color=[t.value for t in trials],
-                colorscale='blues',
-                colorbar=dict(
-                    title='Objective Value'
-                ),
-                showscale=True,
             )
         )
     ]
