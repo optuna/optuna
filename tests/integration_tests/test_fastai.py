2dfb21614b997a8917ac9b2e88dfd64ff45e4550
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2019-10-09 19:18:03 +0900

    fix tests

--- tests/integration_tests/test_fastai.py
+++ tests/integration_tests/test_fastai.py
@@ -1,10 +1,10 @@
+from functools import partial
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.utils.data import TensorDataset
+from torch.utils.data import Dataset
 from fastai.basic_data import DataBunch
 from fastai.basic_train import Learner
-from fastai.metrics import Acc
+from fastai.metrics import accuracy
 import numpy as np
 import pytest
 
@@ -14,32 +14,50 @@ from optuna.testing.integration import create_running_trial
 from optuna.testing.integration import DeterministicPruner
 
 
+# https://docs.fast.ai/basic_data.html#Using-a-custom-Dataset-in-fastai
+class ArrayDataset(Dataset):
+    "Sample numpy array dataset"
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.c = 2
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, i):
+        return self.x[i], self.y[i]
+
+
 @pytest.fixture(scope='session')
-def test_fastai_pruning_callback(tmpdir_factory):
+def tmpdir(tmpdir_factory):
+    return tmpdir_factory.mktemp('fastai_integration_test')
+
+
+def test_fastai_pruning_callback(tmpdir):
     # type: () -> None
 
-    train_x = torch.from_numpy(np.zeros((16, 20), np.float32))
-    train_y = torch.from_numpy(np.zeros((16, ), np.int32))
-    valid_x = torch.from_numpy(np.zeros((4, 20), np.float32))
-    valid_y = torch.from_numpy(np.zeros((4, ), np.int32))
-    train_ds = TensorDataset(train_x, train_y)
-    valid_ds = TensorDataset(valid_x, valid_y)
+    train_x = np.zeros((16, 20), np.float32)
+    train_y = np.zeros((16, ), np.int64)
+    valid_x = np.zeros((4, 20), np.float32)
+    valid_y = np.zeros((4, ), np.int64)
+    train_ds = ArrayDataset(train_x, train_y)
+    valid_ds = ArrayDataset(valid_x, valid_y)
 
     data_bunch = DataBunch.create(
         train_ds=train_ds,
         valid_ds=valid_ds,
         test_ds=None,
-        path=tmpdir_factory.mktemp('fastai_integration_test'),
+        path=tmpdir,
         bs=1  # batch size
     )
 
     def objective(trial):
         # type: (optuna.trial.Trial) -> float
 
-        model = nn.Sequential(nn.Linear(20, 1), F.sigmoid)
-        learn = Learner(data_bunch, model, F.nll_loss, metrics=[Acc])
+        model = nn.Sequential(nn.Linear(20, 1), nn.Sigmoid())
+        learn = Learner(data_bunch, model, metrics=[accuracy])
 
-        learn.fit(1, 1e-3)
+        learn.fit(13, callbacks=[FastaiPruningCallback(learn, trial, 'valid_loss')])
 
         return 1.0
 
@@ -53,35 +71,40 @@ def test_fastai_pruning_callback(tmpdir_factory):
     assert study.trials[0].value == 1.0
 
 
-@pytest.fixture(scope='session')
-def test_fastai_pruning_callback_observation_isnan(tmpdir_factory):
+def test_fastai_pruning_callback_observation_isnan(tmpdir):
     # type: () -> None
 
     study = optuna.create_study(pruner=DeterministicPruner(True))
     trial = create_running_trial(study, 1.0)
 
-    # FastaiPruningCallback requires `learn`, a bit similar to Keras Model.
-    train_x = torch.from_numpy(np.zeros((1, 20), np.float32))
-    train_y = torch.from_numpy(np.zeros((1, ), np.int32))
-    valid_x = torch.from_numpy(np.zeros((1, 20), np.float32))
-    valid_y = torch.from_numpy(np.zeros((1, ), np.int32))
-    train_ds = TensorDataset(train_x, train_y)
-    valid_ds = TensorDataset(valid_x, valid_y)
+    # FastaiPruningCallback requires `fastai.basic_train.Learner`.
+    train_x = np.zeros((1, 20), np.float32)
+    train_y = np.zeros((1, ), np.int64)
+    valid_x = np.zeros((1, 20), np.float32)
+    valid_y = np.zeros((1, ), np.int64)
+    train_ds = ArrayDataset(train_x, train_y)
+    valid_ds = ArrayDataset(valid_x, valid_y)
 
     data_bunch = DataBunch.create(
         train_ds=train_ds,
         valid_ds=valid_ds,
         test_ds=None,
-        path=tmpdir_factory.mktemp('fastai_integration_test'),
+        path=tmpdir,
         bs=1  # batch size
     )
-    model = nn.Sequential(nn.Linear(20, 1), F.sigmoid)
-    learn = Learner(data_bunch, model, F.nll_loss, metrics=[Acc])
 
-    callback = FastaiPruningCallback(learn, trial, 'valid_loss')
+    model = nn.Sequential(nn.Linear(20, 1), nn.Sigmoid())
+    learn = Learner(data_bunch, model, metrics=[accuracy])
+
+    callback = FastaiPruningCallback(learn)
+    callback.register_trial_monitor(trial, 'valid_loss')
+
+    # N.B. This is necessary to set `callback._index_to_monitor`.
+    learn.fit(1)
 
     with pytest.raises(optuna.structs.TrialPruned):
-        callback.on_epoch_end(1, torch.Tensor(1.0), [1.0])
+        callback.on_epoch_end(1, torch.from_numpy(np.ones((1,))), [1.0])
 
     with pytest.raises(optuna.structs.TrialPruned):
-        callback.on_epoch_end(1, torch.Tensor(1.0), [float('nan')])
+        callback.on_epoch_end(
+            1, torch.from_numpy(np.ones((1,))), [float('nan')])
