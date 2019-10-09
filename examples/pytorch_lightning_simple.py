69f1e2c4e9e32b72c743fff25c3aede384efb135
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2019-10-09 19:13:01 +0900

    Update examples/pytorch_lightning_simple.py
    Co-Authored-By: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- examples/pytorch_lightning_simple.py
+++ examples/pytorch_lightning_simple.py
@@ -176,7 +176,7 @@ def objective(trial):
 
 if __name__ == '__main__':
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
+    study.optimize(objective, n_trials=100, timeout=600)
 
     print('Number of finished trials: {}'.format(len(study.trials)))
 
