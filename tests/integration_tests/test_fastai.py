6ca2f8b1ff8ddae72a7cd1042d76b8a250fbceb8
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2019-10-17 18:58:28 +0900

    reflect fastai integration changes

--- tests/integration_tests/test_fastai.py
+++ tests/integration_tests/test_fastai.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from fastai.basic_data import DataBunch
 from fastai.basic_train import Learner
 from fastai.metrics import accuracy
@@ -55,9 +57,11 @@ def test_fastai_pruning_callback(tmpdir):
         # type: (optuna.trial.Trial) -> float
 
         model = nn.Sequential(nn.Linear(20, 1), nn.Sigmoid())
-        learn = Learner(data_bunch, model, metrics=[accuracy])
+        learn = Learner(data_bunch, model, metrics=[accuracy], callback_fns=[
+            partial(FastaiPruningCallback, trial=trial, monitor='valid_loss')
+        ])
 
-        learn.fit(13, callbacks=[FastaiPruningCallback(learn, trial, 'valid_loss')])
+        learn.fit(1)
 
         return 1.0
 
@@ -69,41 +73,3 @@ def test_fastai_pruning_callback(tmpdir):
     study.optimize(objective, n_trials=1)
     assert study.trials[0].state == optuna.structs.TrialState.COMPLETE
     assert study.trials[0].value == 1.0
-
-
-def test_fastai_pruning_callback_observation_isnan(tmpdir):
-    # type: () -> None
-
-    study = optuna.create_study(pruner=DeterministicPruner(True))
-    trial = create_running_trial(study, 1.0)
-
-    # FastaiPruningCallback requires `fastai.basic_train.Learner`.
-    train_x = np.zeros((1, 20), np.float32)
-    train_y = np.zeros((1, ), np.int64)
-    valid_x = np.zeros((1, 20), np.float32)
-    valid_y = np.zeros((1, ), np.int64)
-    train_ds = ArrayDataset(train_x, train_y)
-    valid_ds = ArrayDataset(valid_x, valid_y)
-
-    data_bunch = DataBunch.create(
-        train_ds=train_ds,
-        valid_ds=valid_ds,
-        test_ds=None,
-        path=tmpdir,
-        bs=1  # batch size
-    )
-
-    model = nn.Sequential(nn.Linear(20, 1), nn.Sigmoid())
-    learn = Learner(data_bunch, model, metrics=[accuracy])
-
-    callback = FastaiPruningCallback(learn)
-    callback.register_trial_monitor(trial, 'valid_loss')
-
-    # N.B. This is necessary to set `callback._index_to_monitor`.
-    learn.fit(1)
-
-    with pytest.raises(optuna.structs.TrialPruned):
-        callback.on_epoch_end(1, torch.from_numpy(np.ones((1, ))), [1.0])
-
-    with pytest.raises(optuna.structs.TrialPruned):
-        callback.on_epoch_end(1, torch.from_numpy(np.ones((1, ))), [float('nan')])
