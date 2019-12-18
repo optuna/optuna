5c4942ccbf5bc7827f6a6bf979aa4626617c5780
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2019-12-18 10:06:49 +0900

    Remove direction option from create_study.

--- examples/mlflow/keras_mlflow.py
+++ examples/mlflow/keras_mlflow.py
@@ -93,7 +93,7 @@ def objective(trial):
 
 
 if __name__ == '__main__':
-    study = optuna.create_study(direction='minimize')
+    study = optuna.create_study()
     study.optimize(objective, n_trials=100, timeout=600, callbacks=[mlflow_callback])
 
     print('Number of finished trials: {}'.format(len(study.trials)))
