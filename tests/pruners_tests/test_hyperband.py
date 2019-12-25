38435cacfe647c97bcb5feb5d1ac4cee686c54c8
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2019-12-25 16:44:48 +0900

    Update tests
    - introduce `n_brackets`
    - test on `TPESampler` as currently we do not limit the support of
      `HyperbandPruner` to `RandomSampler`

--- tests/pruners_tests/test_hyperband.py
+++ tests/pruners_tests/test_hyperband.py
@@ -8,6 +8,7 @@ if type_checking.TYPE_CHECKING:
 
 MIN_RESOURCE = 1
 REDUCTION_FACTOR = 2
+N_BRACKETS = 4
 EARLY_STOPPING_RATE_LOW = 0
 EARLY_STOPPING_RATE_HIGH = 3
 N_REPORTS = 10
@@ -20,13 +21,10 @@ def test_hyperband_pruner_intermediate_values():
     pruner = optuna.pruners.HyperbandPruner(
         min_resource=MIN_RESOURCE,
         reduction_factor=REDUCTION_FACTOR,
-        min_early_stopping_rate_low=EARLY_STOPPING_RATE_LOW,
-        min_early_stopping_rate_high=EARLY_STOPPING_RATE_HIGH
+        n_brackets=N_BRACKETS
     )
-    assert pruner._n_pruners == EARLY_STOPPING_RATE_HIGH - EARLY_STOPPING_RATE_LOW + 1
-    n_pruners = pruner._n_pruners
 
-    study = optuna.study.create_study(sampler=optuna.samplers.RandomSampler(), pruner=pruner)
+    study = optuna.study.create_study(pruner=pruner)
 
     def objective(trial):
         # type: (Trial) -> float
@@ -36,10 +34,10 @@ def test_hyperband_pruner_intermediate_values():
 
         return 1.0
 
-    study.optimize(objective, n_trials=n_pruners * EXPECTED_N_TRIALS_PER_BRACKET)
+    study.optimize(objective, n_trials=N_BRACKETS * EXPECTED_N_TRIALS_PER_BRACKET)
 
     trials = study.trials
-    assert len(trials) == n_pruners * EXPECTED_N_TRIALS_PER_BRACKET
+    assert len(trials) == N_BRACKETS * EXPECTED_N_TRIALS_PER_BRACKET
 
 
 def test_bracket_study():
@@ -48,8 +46,7 @@ def test_bracket_study():
     pruner = optuna.pruners.HyperbandPruner(
         min_resource=MIN_RESOURCE,
         reduction_factor=REDUCTION_FACTOR,
-        min_early_stopping_rate_low=EARLY_STOPPING_RATE_LOW,
-        min_early_stopping_rate_high=EARLY_STOPPING_RATE_HIGH
+        n_brackets=N_BRACKETS
     )
     study = optuna.study.create_study(pruner=pruner)
     bracket_study = pruner._create_bracket_study(study, 0)
