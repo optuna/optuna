62562c3beb35d93bd8795c5a871ff6c49e4a6185
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-01-08 13:40:53 +0900

    update test

--- tests/samplers_tests/tpe_tests/test_sampler.py
+++ tests/samplers_tests/tpe_tests/test_sampler.py
@@ -39,9 +39,10 @@ def test_get_observation_pairs():
     # direction=minimize.
     study = optuna.create_study(direction='minimize')
     study.optimize(objective, n_trials=5, catch=(RuntimeError,))
-    study._storage.create_new_trial(study._study_id)  # Create a running trial.
+    trial_number = study._storage.create_new_trial(study._study_id)  # Create a running trial.
+    trial = study._storage.get_trial(trial_number)
 
-    assert tpe.sampler._get_observation_pairs(study, 'x') == (
+    assert tpe.sampler._get_observation_pairs(study, 'x', trial) == (
         [5.0, 5.0, 5.0, 5.0],
         [
             (-float('inf'), 5.0),   # COMPLETE
@@ -49,14 +50,14 @@ def test_get_observation_pairs():
             (-3, float('inf')),  # PRUNED (with a NaN intermediate value; it's treated as infinity)
             (float('inf'), 0.0)  # PRUNED (without intermediate values)
         ])
-    assert tpe.sampler._get_observation_pairs(study, 'y') == ([], [])
+    assert tpe.sampler._get_observation_pairs(study, 'y', trial) == ([], [])
 
     # direction=maximize.
     study = optuna.create_study(direction='maximize')
     study.optimize(objective, n_trials=4)
     study._storage.create_new_trial(study._study_id)  # Create a running trial.
 
-    assert tpe.sampler._get_observation_pairs(study, 'x') == (
+    assert tpe.sampler._get_observation_pairs(study, 'x', trial) == (
         [5.0, 5.0, 5.0, 5.0],
         [
             (-float('inf'), -5.0),   # COMPLETE
@@ -64,4 +65,4 @@ def test_get_observation_pairs():
             (-3, float('inf')),  # PRUNED (with a NaN intermediate value; it's treated as infinity)
             (float('inf'), 0.0)  # PRUNED (without intermediate values)
         ])
-    assert tpe.sampler._get_observation_pairs(study, 'y') == ([], [])
+    assert tpe.sampler._get_observation_pairs(study, 'y', trial) == ([], [])
