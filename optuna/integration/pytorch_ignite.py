7c74d6e578d82e7164fb338c1d60405fb93c4337
Takeru Ohta <phjgt308@gmail.com>
2020-01-21 11:02:57 +0900

    Fix epoch within error message as well.

--- optuna/integration/pytorch_ignite.py
+++ optuna/integration/pytorch_ignite.py
@@ -56,7 +56,7 @@ class PyTorchIgnitePruningHandler(object):
         score = engine.state.metrics[self._metric]
         self._trial.report(score, self._trainer.state.epoch)
         if self._trial.should_prune():
-            message = "Trial was pruned at {} epoch.".format(engine.state.epoch)
+            message = "Trial was pruned at {} epoch.".format(self._trainer.state.epoch)
             raise optuna.exceptions.TrialPruned(message)
 
 
