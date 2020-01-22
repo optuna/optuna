6f75ef6b458dab8e93e181700e0700ae80523b4a
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-01-22 23:35:50 +0900

    make pruning optional

--- examples/fastai_simple.py
+++ examples/fastai_simple.py
@@ -2,28 +2,37 @@
 Optuna example that demonstrates a pruner for fastai.
 
 In this example, we optimize the hyperparameters of a convolutional neural network for hand-written
-digit recognition in terms of validation loss. The network is implemented by fastai and evaluated
-on MNIST dataset. Throughout the training of neural networks, a pruner observes intermediate
-results and stops unpromising trials.
+digit recognition in terms of validation accuracy. The network is implemented by fastai and
+evaluated on MNIST dataset. Throughout the training of neural networks, a pruner observes
+intermediate results and stops unpromising trials.
+Note that this example will take longer than the other examples
+as this uses the entire MNIST dataset.
 
-You can run this example as follows:
-    $ python fastai_integration.py
-
+You can run this example as follows, pruning can be turned on and off with the `--pruning`
+argument.
+    $ python fastai_integration.py [--pruning]
 """
 
+import argparse
 from functools import partial
 
 from fastai.vision import ImageDataBunch
 from fastai.vision import Learner
+from fastai.vision import URLs
+from fastai.vision import accuracy
 from fastai.vision import simple_cnn
 from fastai.vision import untar_data
-from fastai.vision import URLs
+
 import optuna
 from optuna.integration import FastAIPruningCallback
 
 
+BATCHSIZE = 128
+EPOCHS = 10
+
+
 path = untar_data(URLs.MNIST_SAMPLE)
-data = ImageDataBunch.from_folder(path)
+data = ImageDataBunch.from_folder(path, bs=BATCHSIZE)
 
 
 def objective(trial: optuna.trial.Trial) -> float:
@@ -38,13 +47,32 @@ def objective(trial: optuna.trial.Trial) -> float:
     model = simple_cnn(n_channels)
 
     learn = Learner(
-        data, model, silent=True,
+        data, model, silent=True, metrics=[accuracy],
         callback_fns=[partial(FastAIPruningCallback, trial=trial, monitor='valid_loss')])
-    learn.fit(10)
+    learn.fit(EPOCHS)
 
     return learn.validate()[-1].item()
 
 
 if __name__ == '__main__':
-    study = optuna.create_study()
+    parser = argparse.ArgumentParser(description='FastAI example.')
+    parser.add_argument(
+        '--pruning', '-p', action='store_true',
+        help='Activate the pruning feature. `MedianPruner` stops unpromising '
+             'trials at the early stages of training.')
+    args = parser.parse_args()
+
+    pruner = optuna.pruners.MedianPruner() if args.pruning else optuna.pruners.NopPruner()
+    study = optuna.create_study(direction='maximize', pruner=pruner)
     study.optimize(objective, n_trials=100, timeout=600)
+
+    print('Number of finished trials: {}'.format(len(study.trials)))
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: {}'.format(trial.value))
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
