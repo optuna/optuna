9de0f4bb442013573465cb17e0434004093acd60
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-01-29 19:20:29 +0900

    add reference that helped me select default value

--- optuna/pruners/hyperband.py
+++ optuna/pruners/hyperband.py
@@ -53,6 +53,8 @@ class HyperbandPruner(BasePruner):
             :class:`~optuna.pruners.SuccessiveHalvingPruner`.
         n_brackets:
             The number of :class:`~optuna.pruners.SuccessiveHalvingPruner`\\ s (brackets).
+            For those who are curious why this defaults to :math`4`, see
+            https://github.com/optuna/optuna/pull/809#discussion_r361363897.
         min_early_stopping_rate_low:
             A parameter for specifying the minimum early-stopping rate.
             This parameter is related to a parameter that is referred to as :math:`r` and used in
