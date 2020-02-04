abc55319c54f9caa046610dc67e4a8b8480f2aaf
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2020-02-04 18:26:44 +0900

    improve message

---
+++ optuna/warning_decorators.py
@@ -0,0 +1,102 @@
+import functools
+from typing import Any
+from typing import Callable
+import warnings
+
+
+_DOCSTRING_TEMPLATE = """
+
+.. note::
+    Added in v{} as experimental feature. The interface can change in the future.
+    See the details in https://github.com/optuna/optuna/releases/tag/v{}
+"""
+
+
+class ExperimentalWarning(UserWarning):
+    """Experimental Warning class.
+
+    This implementation exists here because the policy of `FutureWarning` has been changed
+    since Python 3.7 was released.
+    """
+
+    pass
+
+
+def _validate_version(version: str) -> None:
+
+    if not isinstance(version, str) or len(version.split('.')) != 3:
+        raise ValueError(
+            'Invalid version specification. Must follow `x.y.z` format but `{}` is given'.format(
+                version))
+
+
+def experimental(version: str) -> Any:
+    """Decorator for experimental functions or methods.
+
+    Args:
+        version: The version number.
+    """
+
+    _validate_version(version)
+
+    def _experimental(func: Callable[[Any], Any]) -> Callable[[Any], Any]:
+
+        docstring = _DOCSTRING_TEMPLATE.format(version, version)
+        if func.__doc__ is None:
+            func.__doc__ = ''
+        func.__doc__ += docstring
+
+        # TODO(crcrpar): Annotate this correctly.
+        @functools.wraps(func)
+        def new_func(*args: Any, **kwargs: Any) -> Any:
+            """Wrapped function."""
+
+            warnings.simplefilter('always', UserWarning)
+            warnings.warn(
+                "{} is experimental (supported from v{}). "
+                "The interface can change in the future.".format(func.__name__, version),
+                ExperimentalWarning
+            )
+
+            return func(*args, **kwargs)  # type: ignore
+
+        return new_func
+
+    return _experimental
+
+
+def experimental_class(version: str) -> Any:
+    """Decorator for experimental class implementation.
+
+    Args:
+        version: The version number.
+    """
+
+    _validate_version(version)
+
+    def _experimental_class(cls: Any) -> Any:
+        """Decorates a class as experimental.
+
+        This decorator is supposed to be applied to the experimental class.
+        """
+
+        _original_init = cls.__init__
+
+        def wrapped_init(*args: Any, **kwargs: Any) -> None:
+            warnings.simplefilter('always', UserWarning)
+            warnings.warn(
+                "{} is experimental (supported from v{}). "
+                "The interface can change in the future.".format(cls.__name__, version),
+                ExperimentalWarning
+            )
+
+            _original_init(*args, **kwargs)
+
+        cls.__init__ = wrapped_init
+
+        if cls.__doc__ is None:
+            cls.__doc__ = ''
+        cls.__doc__ += _DOCSTRING_TEMPLATE.format(version, version)
+        return cls
+
+    return _experimental_class
---
+++ tests/test_warning_decorators.py
@@ -0,0 +1,61 @@
+import inspect
+from typing import Any
+import pytest
+
+from optuna import warning_decorators
+from optuna.warning_decorators import ExperimentalWarning
+
+
+def _sample_func(_: Any) -> int:
+
+    return 10
+
+
+class _Sample(object):
+
+    pass
+
+
+@pytest.mark.parametrize('version', ['1.1.0', '1.1', 100, None])
+def test_experimental_decorator(version: str) -> None:
+
+    if version != '1.1.0':
+        with pytest.raises(ValueError):
+            warning_decorators.experimental(version)
+    else:
+        decorator_experimental = warning_decorators.experimental(version)
+        assert (
+            callable(decorator_experimental) and
+            decorator_experimental.__name__ == '_experimental'
+        )
+
+        decorated_sample_func = decorator_experimental(_sample_func)
+        assert decorated_sample_func.__name__ == '_sample_func'
+        assert (
+            decorated_sample_func.__doc__ ==
+            warning_decorators._DOCSTRING_TEMPLATE.format(version, version)
+        )
+
+
+@pytest.mark.parametrize('version', ['1.1.0', '1.1', 100, None])
+def test_experimental_class_decorator(version: str) -> None:
+
+    if version != '1.1.0':
+        with pytest.raises(ValueError):
+            warning_decorators.experimental_class(version)
+    else:
+        decorator_experimental = warning_decorators.experimental_class(version)
+        assert (
+            callable(decorator_experimental) and
+            decorator_experimental.__name__ == '_experimental_class'
+        )
+
+        decorated_sample = decorator_experimental(_Sample)
+        assert decorated_sample.__name__ == '_Sample'
+        assert (
+            decorated_sample.__doc__ ==
+            warning_decorators._DOCSTRING_TEMPLATE.format(version, version)
+        )
+
+        with pytest.warns(ExperimentalWarning):
+            decorated_sample()
