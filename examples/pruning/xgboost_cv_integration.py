29644a879de5737b725edaddd2f91b8c6695333a
yutayamazaki <tppymd@gmail.com>
2020-02-10 04:15:05 +0900

    Add an example of xgboost.cv using XGBoostPruningCallback

---
+++ examples/pruning/xgboost_cv_integration.py
@@ -0,0 +1,69 @@
+"""
+Optuna example that demonstrates a pruner for XGBoost.cv.
+
+In this example, we optimize the validation auc of cancer detection using XGBoost.
+We optimize both the choice of booster model and their hyperparameters. Throughout
+training of models, a pruner observes intermediate results and stop unpromising trials.
+
+You can run this example as follows:
+    $ python xgboost_cv_integration.py
+
+"""
+
+import sklearn.datasets
+import xgboost as xgb
+
+import optuna
+
+
+def objective(trial):
+    train_x, train_y = sklearn.datasets.load_breast_cancer(return_X_y=True)
+    dtrain = xgb.DMatrix(train_x, label=train_y)
+
+    param = {
+        'silent': 1,
+        'objective': 'binary:logistic',
+        'eval_metric': 'auc',
+        'booster': trial.suggest_categorical('booster', ['gbtree', 'gblinear', 'dart']),
+        'lambda': trial.suggest_loguniform('lambda', 1e-8, 1.0),
+        'alpha': trial.suggest_loguniform('alpha', 1e-8, 1.0)
+    }
+
+    if param['booster'] == 'gbtree' or param['booster'] == 'dart':
+        param['max_depth'] = trial.suggest_int('max_depth', 1, 9)
+        param['eta'] = trial.suggest_loguniform('eta', 1e-8, 1.0)
+        param['gamma'] = trial.suggest_loguniform('gamma', 1e-8, 1.0)
+        param['grow_policy'] = trial.suggest_categorical('grow_policy', ['depthwise', 'lossguide'])
+    if param['booster'] == 'dart':
+        param['sample_type'] = trial.suggest_categorical('sample_type', ['uniform', 'weighted'])
+        param['normalize_type'] = trial.suggest_categorical('normalize_type', ['tree', 'forest'])
+        param['rate_drop'] = trial.suggest_loguniform('rate_drop', 1e-8, 1.0)
+        param['skip_drop'] = trial.suggest_loguniform('skip_drop', 1e-8, 1.0)
+
+    pruning_callback = optuna.integration.XGBoostPruningCallback(trial, 'test-auc')
+    history = xgb.cv(
+        param,
+        dtrain,
+        num_boost_round=100,
+        callbacks=[pruning_callback]
+    )
+
+    best_auc = history['test-auc-mean'].max()
+    return best_auc
+
+
+if __name__ == '__main__':
+    pruner = optuna.pruners.MedianPruner(n_warmup_steps=5)
+    study = optuna.create_study(pruner=pruner, direction='maximize')
+    study.optimize(objective, n_trials=100)
+
+    print('Number of finished trials: {}'.format(len(study.trials)))
+
+    print('Best trial:')
+    trial = study.best_trial
+
+    print('  Value: {}'.format(trial.value))
+
+    print('  Params: ')
+    for key, value in trial.params.items():
+        print('    {}: {}'.format(key, value))
