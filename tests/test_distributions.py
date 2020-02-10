7aa49701cdccf08ddaaa23a0c16f8fa7451272c3
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2020-02-11 06:52:13 +0900

    update test_distribution

--- tests/test_distributions.py
+++ tests/test_distributions.py
@@ -25,7 +25,7 @@ EXAMPLE_JSONS = {
     'l': '{"name": "LogUniformDistribution", "attributes": {"low": 0.001, "high": 100}}',
     'du': '{"name": "DiscreteUniformDistribution",'
     '"attributes": {"low": 1.0, "high": 10.0, "q": 2.0}}',
-    'iu': '{"name": "IntUniformDistribution", "attributes": {"low": 1, "high": 10}',
+    'iu': '{"name": "IntUniformDistribution", "attributes": {"low": 1, "high": 10}}',
     'c1': '{"name": "CategoricalDistribution", "attributes": {"choices": [2.71, -Infinity]}}',
     'c2': '{"name": "CategoricalDistribution", "attributes": {"choices": ["Roppongi", "Azabu"]}}'
 }
