366ae376b4b2a28cbc648f2637f13a700ed2cda6
nuka137 <n.a.sch137@gmail.com>
2020-02-17 00:36:36 +0000

    TensorFlow estimator integration example with only v2 APIs

--- examples/pruning/tensorflow_estimator_integration.py
+++ examples/pruning/tensorflow_estimator_integration.py
@@ -14,8 +14,8 @@ You can run this example as follows:
 import shutil
 import tempfile
 
-import numpy as np
 import tensorflow as tf
+import tensorflow_datasets as tfds
 
 import optuna
 
@@ -24,90 +24,78 @@ BATCH_SIZE = 128
 TRAIN_STEPS = 1000
 EVAL_STEPS = 100
 PRUNING_INTERVAL_STEPS = 50
+N_TRAIN_BATCHES = 3000
+N_TEST_BATCHES = 1000
 
 
-def create_network(trial, features):
+def preprocess(image, label):
+    image = tf.reshape(image, [-1, 28*28])
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    label = tf.cast(label, tf.int32)
+    return {'x': image}, label
+
+
+def train_input_fn():
+    data = tfds.load(name='mnist', as_supervised=True)
+    train_ds = data['train']
+    train_ds = train_ds.map(preprocess).shuffle(60000).batch(BATCH_SIZE).take(N_TRAIN_BATCHES)
+    return train_ds
+
+
+def eval_input_fn():
+    data = tfds.load(name='mnist', as_supervised=True)
+    test_ds = data['test']
+    test_ds = test_ds.map(preprocess).shuffle(10000).batch(BATCH_SIZE).take(N_TEST_BATCHES)
+    return test_ds
+
+
+def create_classifier(trial):
     # We optimize the numbers of layers and their units.
-    input_layer = tf.reshape(features['x'], [-1, 784])
-    prev_layer = input_layer
 
     n_layers = trial.suggest_int('n_layers', 1, 3)
+    hidden_units = []
     for i in range(n_layers):
         n_units = trial.suggest_int('n_units_l{}'.format(i), 1, 128)
-        prev_layer = tf.keras.layers.Dense(units=n_units, activation=tf.nn.relu)(prev_layer)
-
-    logits = tf.keras.layers.Dense(units=10)(prev_layer)
-    return logits
-
-
-def model_fn(trial, features, labels, mode):
-    logits = create_network(trial, features)
-
-    predictions = {
-        "classes": tf.argmax(input=logits, axis=1),
-        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
-    }
-
-    if mode == tf.estimator.ModeKeys.PREDICT:
-        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
-
-    loss = tf.compat.v1.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-    if mode == tf.estimator.ModeKeys.TRAIN:
-        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.001)
-        train_op = optimizer.minimize(
-            loss=loss, global_step=tf.compat.v1.train.get_or_create_global_step())
-        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
-
-    eval_metric_ops = {
-        "accuracy": tf.compat.v1.metrics.accuracy(
-            labels=labels, predictions=predictions["classes"])
-    }
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
-
-
-def objective(trial):
-    (train_data, train_labels), (eval_data, eval_labels) = tf.keras.datasets.mnist.load_data()
-
-    train_data = train_data / np.float32(255)
-    train_labels = train_labels.astype(np.int32)
-
-    eval_data = eval_data / np.float32(255)
-    eval_labels = eval_labels.astype(np.int32)
+        hidden_units.append(n_units)
 
     config = tf.estimator.RunConfig(
         save_summary_steps=PRUNING_INTERVAL_STEPS, save_checkpoints_steps=PRUNING_INTERVAL_STEPS)
 
     model_dir = "{}/{}".format(MODEL_DIR, trial.number)
-    mnist_classifier = tf.estimator.Estimator(
-        model_fn=lambda features, labels, mode: model_fn(trial, features, labels, mode),
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=[tf.feature_column.numeric_column('x', shape=[28*28])],
+        hidden_units=hidden_units,
         model_dir=model_dir,
+        n_classes=10,
+        optimizer=lambda: tf.keras.optimizers.Adam(learning_rate=0.01),
         config=config)
+
+    return classifier
+
+
+def objective(trial):
+    classifier = create_classifier(trial)
 
     optuna_pruning_hook = optuna.integration.TensorFlowPruningHook(
         trial=trial,
-        estimator=mnist_classifier,
+        estimator=classifier,
         metric="accuracy",
         run_every_steps=PRUNING_INTERVAL_STEPS,
     )
 
-    train_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
-        x={"x": train_data}, y=train_labels, batch_size=BATCH_SIZE, num_epochs=None, shuffle=True)
-
     train_spec = tf.estimator.TrainSpec(
         input_fn=train_input_fn, max_steps=TRAIN_STEPS, hooks=[optuna_pruning_hook])
-
-    eval_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn(
-        x={"x": eval_data}, y=eval_labels, num_epochs=1, shuffle=False)
 
     eval_spec = tf.estimator.EvalSpec(
         input_fn=eval_input_fn, steps=EVAL_STEPS, start_delay_secs=0, throttle_secs=0)
 
-    eval_results, _ = tf.estimator.train_and_evaluate(mnist_classifier, train_spec, eval_spec)
+    eval_results, _ = tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)
+
     return float(eval_results['accuracy'])
 
 
-def main(unused_argv):
+def main():
     study = optuna.create_study(direction='maximize')
     study.optimize(objective, n_trials=25)
     pruned_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.PRUNED]
@@ -130,4 +118,4 @@ def main(unused_argv):
 
 
 if __name__ == "__main__":
-    tf.compat.v1.app.run()
+    main()
