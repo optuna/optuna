4e78724fcbf961f5639f77c1f575827c2ff43839
nuka137 <n.a.sch137@gmail.com>
2020-02-28 00:37:38 +0000

    Add examples for doctest to optuna/prunes/*.py

--- optuna/pruners/percentile.py
+++ optuna/pruners/percentile.py
@@ -65,16 +65,43 @@ class PercentilePruner(BasePruner):
 
     Example:
 
-        .. code::
+        .. testsetup::
 
-            >>> from optuna import create_study
-            >>> from optuna.pruners import PercentilePruner
-            >>>
-            >>> def objective(trial):
-            >>>     ...
-            >>>
-            >>> study = create_study(pruner=PercentilePruner(25.0))
-            >>> study.optimize(objective)
+            import numpy as np
+            from sklearn.model_selection import train_test_split
+
+            np.random.seed(seed=0)
+            X = np.random.randn(200).reshape(-1, 1)
+            y = np.where(X[:, 0] < 0.5, 0, 1)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+            classes = np.unique(y)
+
+        .. testcode::
+
+            import optuna
+            from sklearn.linear_model import SGDClassifier
+
+            def objective(trial):
+                alpha = trial.suggest_uniform('alpha', 0.0, 1.0)
+                clf = SGDClassifier(alpha=alpha)
+                n_train_iter = 100
+
+                for step in range(n_train_iter):
+                    clf.partial_fit(X_train, y_train, classes=classes)
+
+                    intermediate_value = clf.score(X_test, y_test)
+                    trial.report(intermediate_value, step)
+
+                    if trial.should_prune():
+                        raise optuna.exceptions.TrialPruned()
+
+                return clf.score(X_test, y_test)
+
+            study = optuna.create_study(
+                direction='maximize',
+                pruner=optuna.pruners.PercentilePruner(25.0, n_startup_trials=5,
+                                                       n_warmup_steps=30, interval_steps=10))
+            study.optimize(objective, n_trials=20)
 
     Args:
         percentile:
