fe67177374c46b0b17aca79f4156bc6bb5f142e0
himkt <himkt@cookpad.com>
2020-02-29 12:39:17 +0900

    Add tests for two-sided threshold

--- tests/pruners_tests/test_threshold.py
+++ tests/pruners_tests/test_threshold.py
@@ -36,3 +36,31 @@ def test_threshold_pruner_with_lt():
     trial.report(1, 2)
     assert pruner.prune(
         study=study, trial=study._storage.get_trial(trial._trial_id))
+
+
+def test_threshold_pruner_with_two_side():
+    # type: () -> None
+
+    study = optuna.study.create_study()
+    trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
+    pruner = optuna.pruners.ThresholdPruner(lower_bound=0, upper_bound=1)
+
+    trial.report(-0.1, 1)
+    assert pruner.prune(
+        study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(0, 2)
+    assert not pruner.prune(
+        study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(0.4, 3)
+    assert not pruner.prune(
+        study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(1, 4)
+    assert not pruner.prune(
+        study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(1.1, 5)
+    assert pruner.prune(
+        study=study, trial=study._storage.get_trial(trial._trial_id))
