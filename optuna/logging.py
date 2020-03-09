e885a65d11da7f085b3b4323c94f3da3ef29fa92
bigbird555 <kengo-phoenix@g.ecc.u-tokyo.ac.jp>
2020-03-10 06:40:54 +0900

    revert #NOQA comments

--- optuna/logging.py
+++ optuna/logging.py
@@ -1,12 +1,12 @@
 import colorlog
 import logging
-from logging import CRITICAL
-from logging import DEBUG
-from logging import ERROR
-from logging import FATAL
-from logging import INFO
-from logging import WARN
-from logging import WARNING
+from logging import CRITICAL #NOQA
+from logging import DEBUG #NOQA
+from logging import ERROR #NOQA
+from logging import FATAL #NOQA
+from logging import INFO #NOQA
+from logging import WARN #NOQA
+from logging import WARNING #NOQA
 import threading
 
 from optuna import type_checking
