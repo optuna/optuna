72cfd227731d7f71bf51f80f230de554268917c4
bigbird555 <kengo-phoenix@g.ecc.u-tokyo.ac.jp>
2020-03-09 17:53:16 +0900

    get rid of unnecessary imports

--- optuna/logging.py
+++ optuna/logging.py
@@ -1,12 +1,5 @@
 import colorlog
 import logging
-from logging import CRITICAL
-from logging import DEBUG
-from logging import ERROR
-from logging import FATAL
-from logging import INFO
-from logging import WARN
-from logging import WARNING
 import threading
 
 from optuna import type_checking
