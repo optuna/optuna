50456b3f07713f6c83cd22292060c80727977d4b
harupy <hkawamura0130@gmail.com>
2020-03-10 10:40:31 +0900

    Add plotly cdn

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -94,6 +94,7 @@ if not on_rtd:
 # html_theme_options = {}
 
 html_favicon = '../image/favicon.ico'
+html_js_files = ["https://cdn.plot.ly/plotly-latest.min.js"]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
