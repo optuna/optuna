a4de2a52ef842cfdef90e9c8bda0c7d9f25f1d03
harupy <hkawamura0130@gmail.com>
2020-03-14 16:32:39 +0900

    Add plot_intermediate_values

--- optuna/visualization/intermediate_values.py
+++ optuna/visualization/intermediate_values.py
@@ -35,6 +35,11 @@ def plot_intermediate_values(study):
 
             optuna.visualization.plot_intermediate_values(study)
 
+        .. raw:: html
+
+            <iframe src="../_static/plot_intermediate_values.html" width="100%" height="600px" frameborder="0">
+            </iframe>
+
     Args:
         study:
             A :class:`~optuna.study.Study` object whose trials are plotted for their intermediate
