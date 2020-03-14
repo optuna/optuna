7cafbe1e4621ce06da59b1396f0825deff2cc42d
harupy <hkawamura0130@gmail.com>
2020-03-14 15:30:09 +0900

    Use iframe

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -99,7 +99,7 @@ html_js_files = ["https://cdn.plot.ly/plotly-latest.min.js"]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'plotly_figures']
 html_css_files = ["css/custom.css"]
 
 # Custom sidebar templates, must be a dictionary that maps document names
