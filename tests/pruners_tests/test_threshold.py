a01c9361a4cb44e5b61d88b05ef0b54be581ee42
himkt <himkt@cookpad.com>
2020-03-17 23:00:42 +0900

    Add tests for n_warmup_steps and interval_steps

--- tests/pruners_tests/test_threshold.py
+++ tests/pruners_tests/test_threshold.py
@@ -10,12 +10,10 @@ def test_threshold_pruner_with_ub() -> None:
     pruner = optuna.pruners.ThresholdPruner(upper=2, n_warmup_steps=0, interval_steps=1)
 
     trial.report(1, 1)
-    assert not pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(3, 2)
-    assert pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
 
 def test_threshold_pruner_with_lt() -> None:
@@ -25,12 +23,10 @@ def test_threshold_pruner_with_lt() -> None:
     pruner = optuna.pruners.ThresholdPruner(lower=2, n_warmup_steps=0, interval_steps=1)
 
     trial.report(3, 1)
-    assert not pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(1, 2)
-    assert pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
 
 def test_threshold_pruner_with_two_side() -> None:
@@ -40,24 +36,19 @@ def test_threshold_pruner_with_two_side() -> None:
     pruner = optuna.pruners.ThresholdPruner(lower=0, upper=1, n_warmup_steps=0, interval_steps=1)
 
     trial.report(-0.1, 1)
-    assert pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(0, 2)
-    assert not pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(0.4, 3)
-    assert not pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(1, 4)
-    assert not pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
     trial.report(1.1, 5)
-    assert pruner.prune(
-        study=study, trial=study._storage.get_trial(trial._trial_id))
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
 
 
 def test_threshold_pruner_with_invalid_inputs() -> None:
@@ -66,13 +57,55 @@ def test_threshold_pruner_with_invalid_inputs() -> None:
         optuna.pruners.ThresholdPruner(lower=False, upper=1)  # type: ignore
 
     with pytest.raises(ValueError):
-        optuna.pruners.ThresholdPruner(lower='0', upper=1)  # type: ignore
+        optuna.pruners.ThresholdPruner(lower="0", upper=1)  # type: ignore
 
     with pytest.raises(ValueError):
         optuna.pruners.ThresholdPruner(lower=0, upper=False)
 
     with pytest.raises(ValueError):
-        optuna.pruners.ThresholdPruner(lower=0, upper='1')  # type: ignore
+        optuna.pruners.ThresholdPruner(lower=0, upper="1")  # type: ignore
 
     with pytest.raises(ValueError):
         optuna.pruners.ThresholdPruner(lower=None, upper=None)
+
+
+def test_threshold_pruner_n_warmup_steps() -> None:
+    study = optuna.study.create_study()
+    trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
+    pruner = optuna.pruners.ThresholdPruner(lower=0, upper=1, n_warmup_steps=2)
+
+    trial.report(-10, 1)
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(100, 2)
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(-100, 3)
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(1, 4)
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(1000, 5)
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+
+def test_threshold_pruner_interval_steps() -> None:
+    study = optuna.study.create_study()
+    trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
+    pruner = optuna.pruners.ThresholdPruner(lower=0, upper=1, interval_steps=2)
+
+    trial.report(-10, 1)
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(100, 2)
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(-100, 3)
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(10, 4)
+    assert not pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
+
+    trial.report(1000, 5)
+    assert pruner.prune(study=study, trial=study._storage.get_trial(trial._trial_id))
