8e345abfc36e7e71b4590d9f862a1348dc41be7d
harupy <hkawamura0130@gmail.com>
2020-03-17 16:42:32 +0900

    Address comments on code style

--- docs/source/scripts/plot_intermediate_values.py
+++ docs/source/scripts/plot_intermediate_values.py
@@ -1,15 +1,17 @@
 import os
 
-import optuna
 import plotly
+
+import optuna
+
+
+def objective(trial):
+    x = trial.suggest_uniform('x', -100, 100)
+    y = trial.suggest_categorical('y', [-1, 0, 1])
+    return x ** 2 + y
 
 
 def main():
-    def objective(trial):
-        x = trial.suggest_uniform('x', -100, 100)
-        y = trial.suggest_categorical('y', [-1, 0, 1])
-        return x ** 2 + y
-
     sampler = optuna.samplers.TPESampler(seed=10)
     study = optuna.create_study(sampler=sampler)
     study.optimize(objective, n_trials=10)
