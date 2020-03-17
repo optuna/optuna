6418778e8dbffea3c29ad7efdeb92c9362a73c3e
himkt <himkt@cookpad.com>
2020-03-17 19:14:29 +0900

    Apply black

--- optuna/visualization/parallel_coordinate.py
+++ optuna/visualization/parallel_coordinate.py
@@ -19,9 +19,7 @@ if is_available():
 logger = get_logger(__name__)
 
 
-def plot_parallel_coordinate(
-        study: Study,
-        params: Optional[List[str]] = None) -> 'go.Figure':
+def plot_parallel_coordinate(study: Study, params: Optional[List[str]] = None) -> "go.Figure":
     """Plot the high-dimentional parameter relationships in a study.
 
     Note that, If a parameter contains missing values, a trial with missing values is not plotted.
@@ -59,33 +57,31 @@ def plot_parallel_coordinate(
     return _get_parallel_coordinate_plot(study, params)
 
 
-def _get_parallel_coordinate_plot(
-        study: Study,
-        params: Optional[List[str]] = None) -> 'go.Figure':
+def _get_parallel_coordinate_plot(study: Study, params: Optional[List[str]] = None) -> "go.Figure":
 
-    layout = go.Layout(
-        title='Parallel Coordinate Plot',
-    )
+    layout = go.Layout(title="Parallel Coordinate Plot",)
 
     trials = [trial for trial in study.trials if trial.state == TrialState.COMPLETE]
 
     if len(trials) == 0:
-        logger.warning('Your study does not have any completed trials.')
+        logger.warning("Your study does not have any completed trials.")
         return go.Figure(data=[], layout=layout)
 
     all_params = {p_name for t in trials for p_name in t.params.keys()}
     if params is not None:
         for input_p_name in params:
             if input_p_name not in all_params:
-                ValueError('Parameter {} does not exist in your study.'.format(input_p_name))
+                ValueError("Parameter {} does not exist in your study.".format(input_p_name))
         all_params = set(params)
     sorted_params = sorted(list(all_params))
 
-    dims = [{
-        'label': 'Objective Value',
-        'values': tuple([t.value for t in trials]),
-        'range': (min([t.value for t in trials]), max([t.value for t in trials]))
-    }]  # type: List[Dict[str, Any]]
+    dims = [
+        {
+            "label": "Objective Value",
+            "values": tuple([t.value for t in trials]),
+            "range": (min([t.value for t in trials]), max([t.value for t in trials])),
+        }
+    ]  # type: List[Dict[str, Any]]
     for p_name in sorted_params:
         values = []
         for t in trials:
@@ -98,26 +94,22 @@ def _get_parallel_coordinate_plot(
             vocab = defaultdict(lambda: len(vocab))  # type: DefaultDict[str, int]
             values = [vocab[v] for v in values]
             is_categorical = True
-        dim = {
-            'label': p_name,
-            'values': tuple(values),
-            'range': (min(values), max(values))
-        }
+        dim = {"label": p_name, "values": tuple(values), "range": (min(values), max(values))}
         if is_categorical:
-            dim['tickvals'] = list(range(len(vocab)))
-            dim['ticktext'] = list(sorted(vocab.items(), key=lambda x: x[1]))
+            dim["tickvals"] = list(range(len(vocab)))
+            dim["ticktext"] = list(sorted(vocab.items(), key=lambda x: x[1]))
         dims.append(dim)
 
     traces = [
         go.Parcoords(
             dimensions=dims,
             line={
-                'color': dims[0]['values'],
-                'colorscale': 'blues',
-                'colorbar': {'title': 'Objective Value'},
-                'showscale': True,
-                'reversescale': study.direction == StudyDirection.MINIMIZE,
-            }
+                "color": dims[0]["values"],
+                "colorscale": "blues",
+                "colorbar": {"title": "Objective Value"},
+                "showscale": True,
+                "reversescale": study.direction == StudyDirection.MINIMIZE,
+            },
         )
     ]
 
