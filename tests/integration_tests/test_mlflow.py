7b769d5a994552be01d2a40e6cb2daa1377a3776
PhilipMay <eniak.info@gmail.com>
2020-03-19 22:21:13 +0100

    mlflow test type annotation fix

--- tests/integration_tests/test_mlflow.py
+++ tests/integration_tests/test_mlflow.py
@@ -5,7 +5,7 @@ from optuna.integration.mlflow import MlflowCallback
 
 
 def test_experiment_or_study_name():
-    # type: (None) -> None
+    # type: () -> None
 
     mlflc = MlflowCallback()
     study = optuna.create_study()
