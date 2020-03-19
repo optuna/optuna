48f7a3afba9c732296eec3d79aa7ab474f2c6024
PhilipMay <eniak.info@gmail.com>
2020-03-19 22:14:36 +0100

    mlflowcallback first test

---
+++ tests/integration_tests/test_mlflow.py
@@ -0,0 +1,13 @@
+import pytest
+
+import optuna
+from optuna.integration.mlflow import MlflowCallback
+
+
+def test_experiment_or_study_name():
+    # type: (None) -> None
+
+    mlflc = MlflowCallback()
+    study = optuna.create_study()
+    with pytest.raises(ValueError):
+        mlflc(study, None)
