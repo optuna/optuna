5257236246a6d0be29f717463380756ec097e566
Philip May <eniak.info@gmail.com>
2020-03-20 06:41:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ def get_tests_require() -> List[str]:
 def get_extras_require() -> Dict[str, List[str]]:
 
     requirements = {
-        "checking": ["black", "hacking", "mypy"],
+        "checking": ["black", "hacking", "mypy",],
         "codecov": ["codecov", "pytest-cov",],
         "doctest": ["cma", "pandas", "plotly>=4.0.0", "scikit-learn>=0.19.0", "scikit-optimize", "mlflow",],
         "document": ["sphinx", "sphinx_rtd_theme", "mlflow",],
