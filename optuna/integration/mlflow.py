000660139ee14c7644d07123ef6a809c28149386
PhilipMay <eniak.info@gmail.com>
2020-03-21 07:33:05 +0100

    experiment creation fix

--- optuna/integration/mlflow.py
+++ optuna/integration/mlflow.py
@@ -59,23 +59,23 @@ class MlflowCallback(object):
     def __call__(self, study, trial):
         # type: (optuna.study.Study, optuna.structs.FrozenTrial) -> None
 
-        # set experiment
-        if self._experiment is None:
-            if (
-                study.study_name is None
-                or study.study_name == "no-name-00000000-0000-0000-0000-000000000000"
-            ):
-                raise ValueError("Either 'experiment' or 'study.study_name' must be set!")
-            else:
-                mlflow.set_experiment(study.study_name)
-        else:
-            mlflow.set_experiment(self._experiment)
-
         # set tracking_uri
         if self._tracking_uri is not None:
             mlflow.set_tracking_uri(self._tracking_uri)
 
         with mlflow.start_run(run_name=trial.number):
+            # set experiment
+            if self._experiment is None:
+                if (
+                    study.study_name is None
+                    or study.study_name == "no-name-00000000-0000-0000-0000-000000000000"
+                ):
+                    raise ValueError("Either 'experiment' or 'study.study_name' must be set!")
+                else:
+                    mlflow.set_experiment(study.study_name)
+            else:
+                mlflow.set_experiment(self._experiment)
+
             # set metric
             trial_value = trial.value if trial.value is not None else float("nan")
             metric_name = self._metric_name if self._metric_name is not None else "trial_value"
