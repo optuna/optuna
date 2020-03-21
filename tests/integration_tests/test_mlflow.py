ab38630b51fe954fc615e8e0ad47a02064a27b22
PhilipMay <eniak.info@gmail.com>
2020-03-21 19:38:04 +0100

    changed import order

--- tests/integration_tests/test_mlflow.py
+++ tests/integration_tests/test_mlflow.py
@@ -1,8 +1,8 @@
 import py
 
 import optuna
-from ..test_structs import _create_frozen_trial
 from optuna.integration.mlflow import MlflowCallback
+from ..test_structs import _create_frozen_trial
 
 
 def test_happy_case(tmpdir):
