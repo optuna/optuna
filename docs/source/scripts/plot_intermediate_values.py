e34f284eef465137c26bfb97206973b0589422a5
barneyhill <barneyhill1@gmail.com>
2020-03-24 14:24:23 +0000

    Adding a working code example for plotting intermediate values.

--- docs/source/scripts/plot_intermediate_values.py
+++ docs/source/scripts/plot_intermediate_values.py
@@ -4,17 +4,29 @@ import plotly
 
 import optuna
 
-
+def df(x):
+    return 2*x
+    
 def objective(trial):
-    x = trial.suggest_uniform("x", -100, 100)
-    y = trial.suggest_categorical("y", [-1, 0, 1])
-    return x ** 2 + y
+    
+    next_x = 1  # We start the search at x=1
+    gamma = trial.suggest_loguniform('alpha', 1e-5, 1e-1)  # Step size multiplier
+
+    # Stepping through gradient descent to find the minima of x**2
+    for step in range(100):
+        current_x = next_x
+        next_x = current_x - gamma * df(current_x)
+
+        delta = next_x - current_x
+        trial.report(current_x, step)
+        
+    return delta
 
 
 def main():
     sampler = optuna.samplers.TPESampler(seed=10)
     study = optuna.create_study(sampler=sampler)
-    study.optimize(objective, n_trials=10)
+    study.optimize(objective, n_trials=5)
 
     fig = optuna.visualization.plot_intermediate_values(study)
     fig_html = plotly.offline.plot(fig, output_type="div", include_plotlyjs="cdn", auto_open=False)
