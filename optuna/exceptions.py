dfed08909233343f7e01c44e2b9553c1ded24e53
Barney Hill <barneyhill1@gmail.com>
2020-03-30 15:40:06 +0100

    Update optuna/exceptions.py code styling
    Co-Authored-By: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/exceptions.py
+++ optuna/exceptions.py
@@ -17,9 +17,8 @@ class TrialPruned(OptunaError):
 
             import optuna
             import numpy as np
-            from sklearn.linear_model import SGDClassifier
-
             from sklearn.datasets import load_iris
+            from sklearn.linear_model import SGDClassifier
             from sklearn.model_selection import train_test_split
 
             X, y = load_iris(return_X_y=True)
