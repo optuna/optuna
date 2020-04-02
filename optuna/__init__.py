dfe3c92b7a07273407b84c3334968c344535df6b
Yusuke Tsuzuku <ytsmiling@gmail.com>
2020-04-02 11:29:26 +0900

    Apply lazy import for `optuna.dashboard`.

--- optuna/__init__.py
+++ optuna/__init__.py
@@ -1,4 +1,6 @@
-from optuna import dashboard  # NOQA
+import importlib
+import types
+
 from optuna import distributions  # NOQA
 from optuna import exceptions  # NOQA
 from optuna import importance  # NOQA
@@ -20,3 +22,29 @@ from optuna.study import load_study  # NOQA
 from optuna.study import Study  # NOQA
 from optuna.trial import Trial  # NOQA
 from optuna.version import __version__  # NOQA
+from optuna.type_checking import TYPE_CHECKING  # NOQA
+
+
+if TYPE_CHECKING:
+    from optuna import dashboard  # NOQA
+    from typing import Any  # NOQA
+else:
+
+    class _LazyImport(types.ModuleType):
+        def __init__(self, name):
+            # type: (str) -> None
+            super(_LazyImport, self).__init__(name)
+            self._name = name
+
+        def _load(self):
+            # type: () -> types.ModuleType
+            module = importlib.import_module(self._name)
+            globals()[self._name] = module
+            self.__dict__.update(module.__dict__)
+            return module
+
+        def __getattr__(self, item):
+            # type: (str) -> Any
+            return getattr(self._load(), item)
+
+    dashboard = _LazyImport("optuna.dashboard")
