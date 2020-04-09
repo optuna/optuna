6a7c7c3353cceeb7159700b66784c67c6d3defed
Takeru Ohta <phjgt308@gmail.com>
2020-04-09 13:27:37 +0900

    Add a test case for `_dominates` method.

--- tests/multi_objective/test_trial.py
+++ tests/multi_objective/test_trial.py
@@ -1,7 +1,13 @@
 import datetime
 from typing import List
+from typing import Optional
+
+import pytest
 
 import optuna
+from optuna.multi_objective.trial import FrozenMultiObjectiveTrial
+from optuna.structs import StudyDirection
+from optuna.structs import TrialState
 
 
 def test_suggest() -> None:
@@ -23,15 +29,22 @@ def test_report() -> None:
     study = optuna.multi_objective.create_study(["maximize", "minimize", "maximize"])
 
     def objective(trial: optuna.multi_objective.trial.MultiObjectiveTrial) -> List[float]:
-        trial.report([1, 2, 3], 1)
-        trial.report([10, 20, 30], 2)
+        if trial.number == 0:
+            trial.report([1, 2, 3], 1)
+            trial.report([10, 20, 30], 2)
         return [100, 200, 300]
 
-    study.optimize(objective, n_trials=1)
-    trial = study.trials[0]
+    study.optimize(objective, n_trials=2)
 
+    trial = study.trials[0]
     assert trial.intermediate_values == {1: [1, 2, 3], 2: [10, 20, 30]}
     assert trial.values == [100, 200, 300]
+    assert trial.last_step == 2
+
+    trial = study.trials[1]
+    assert trial.intermediate_values == {}
+    assert trial.values == [100, 200, 300]
+    assert trial.last_step is None
 
 
 def test_number() -> None:
@@ -108,3 +121,83 @@ def test_datetime() -> None:
 
     assert isinstance(study.trials[0].datetime_start, datetime.datetime)
     assert isinstance(study.trials[0].datetime_complete, datetime.datetime)
+
+
+def test_dominates() -> None:
+    directions = [StudyDirection.MINIMIZE, StudyDirection.MAXIMIZE]
+
+    def create_trial(
+        values: List[Optional[float]], state: TrialState = TrialState.COMPLETE,
+    ) -> FrozenMultiObjectiveTrial:
+        n_objectives = len(values)
+        trial = optuna.structs.FrozenTrial(
+            state=state,
+            intermediate_values={i: v for i, v in enumerate(values)},
+            # The following attributes aren't used in this test case.
+            number=0,
+            value=None,
+            datetime_start=None,
+            datetime_complete=None,
+            params={},
+            distributions={},
+            user_attrs={},
+            system_attrs={},
+            trial_id=0,
+        )
+        return FrozenMultiObjectiveTrial(n_objectives, trial)
+
+    # The numbers of objectives for `t0` and `t1` don't match.
+    with pytest.raises(ValueError):
+        t0 = create_trial([1])  # One objective.
+        t1 = create_trial([1, 2])  # Two objectives.
+        t0._dominates(t1, directions)
+
+    # The numbers of objectives and directions don't match.
+    with pytest.raises(ValueError):
+        t0 = create_trial([1])  # One objective.
+        t1 = create_trial([1])  # One objective.
+        t0._dominates(t1, directions)
+
+    # `t0` dominates `t1`.
+    t0 = create_trial([0, 2])
+    t1 = create_trial([1, 1])
+    assert t0._dominates(t1, directions)
+    assert not t1._dominates(t0, directions)
+
+    # `t0` dominates `t1`.
+    t0 = create_trial([0, 1])
+    t1 = create_trial([1, 1])
+    assert t0._dominates(t1, directions)
+    assert not t1._dominates(t0, directions)
+
+    # `t0` and `t1` don't dominate each other.
+    t0 = create_trial([1, 1])
+    t1 = create_trial([1, 1])
+    assert not t0._dominates(t1, directions)
+    assert not t1._dominates(t0, directions)
+
+    # `t0` and `t1` don't dominate each other.
+    t0 = create_trial([0, 1])
+    t1 = create_trial([1, 2])
+    assert not t0._dominates(t1, directions)
+    assert not t1._dominates(t0, directions)
+
+    for t0_state in [TrialState.FAIL, TrialState.WAITING, TrialState.PRUNED]:
+        t0 = create_trial([1, 1], t0_state)
+
+        for t1_state in [
+            TrialState.COMPLETE,
+            TrialState.FAIL,
+            TrialState.WAITING,
+            TrialState.PRUNED,
+        ]:
+            # If `t0` has not the COMPLETE state, it never dominates other trials.
+            t1 = create_trial([0, 2], t1_state)
+            assert not t0._dominates(t1, directions)
+
+            if t1_state == TrialState.COMPLETE:
+                # If `t0` isn't COMPLETE and `t1` is COMPLETE, `t1` dominates `t0`.
+                assert t1._dominates(t0, directions)
+            else:
+                # If both `t0` and `t1` aren't COMPLETE, they don't dominate each other.
+                assert not t1._dominates(t0, directions)
