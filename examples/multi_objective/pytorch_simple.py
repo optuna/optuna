0012cf04839542e332f2a852763caa211d37fff5
Takeru Ohta <phjgt308@gmail.com>
2020-04-13 17:54:20 +0900

    s/test/val/

--- examples/multi_objective/pytorch_simple.py
+++ examples/multi_objective/pytorch_simple.py
@@ -28,7 +28,7 @@ DIR = os.getcwd()
 EPOCHS = 10
 LOG_INTERVAL = 10
 N_TRAIN_EXAMPLES = BATCHSIZE * 30
-N_TEST_EXAMPLES = BATCHSIZE * 10
+N_VAL_EXAMPLES = BATCHSIZE * 10
 
 
 def define_model(trial):
@@ -58,13 +58,13 @@ def get_mnist():
         batch_size=BATCHSIZE,
         shuffle=True,
     )
-    test_loader = torch.utils.data.DataLoader(
+    val_loader = torch.utils.data.DataLoader(
         datasets.MNIST(DIR, train=False, transform=transforms.ToTensor()),
         batch_size=BATCHSIZE,
         shuffle=True,
     )
 
-    return train_loader, test_loader
+    return train_loader, val_loader
 
 
 def objective(trial):
@@ -78,7 +78,7 @@ def objective(trial):
     optimizer = getattr(optim, optimizer_name)(model.parameters(), lr=lr)
 
     # Get the MNIST dataset.
-    train_loader, test_loader = get_mnist()
+    train_loader, val_loader = get_mnist()
 
     # Training of the model.
     model.train()
@@ -100,16 +100,16 @@ def objective(trial):
     model.eval()
     correct = 0
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            # Limiting testing data.
-            if batch_idx * BATCHSIZE >= N_TEST_EXAMPLES:
+        for batch_idx, (data, target) in enumerate(val_loader):
+            # Limiting validation data.
+            if batch_idx * BATCHSIZE >= N_VAL_EXAMPLES:
                 break
             data, target = data.view(-1, 28 * 28).to(DEVICE), target.to(DEVICE)
             output = model(data)
             pred = output.argmax(dim=1, keepdim=True)  # Get the index of the max log-probability.
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    accuracy = correct / N_TEST_EXAMPLES
+    accuracy = correct / N_VAL_EXAMPLES
 
     flops, _params = thop.profile(model, inputs=(torch.randn(1, 28 * 28),), verbose=False)
     return flops, accuracy
