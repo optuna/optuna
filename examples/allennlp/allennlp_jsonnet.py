c9d25b88d4ca8061b8a635681039ff6466ee8b51
himkt <himkt@cookpad.com>
2020-04-16 14:54:35 +0900

    cli -> jsonnet

--- examples/allennlp/allennlp_jsonnet.py
+++ examples/allennlp/allennlp_jsonnet.py
@@ -3,19 +3,19 @@ Optuna example that optimizes a classifier configuration for IMDB movie review d
 This script is based on the example of allentune (https://github.com/allenai/allentune).
 
 In this example, we optimize the validation accuracy of
-sentiment classification using AllenNLP cli.
+sentiment classification using an AllenNLP jsonnet config file.
 Since it is too time-consuming to use the training dataset,
 we here use the validation dataset instead.
 
 We have the following two ways to execute this example:
 
 (1) Execute this code directly.
-    $ python allennlp_cli.py
+    $ python allennlp_jsonnet.py
 
 
 (2) Execute through CLI.
     $ STUDY_NAME=`optuna create-study --direction maximize --storage sqlite:///example.db`
-    $ optuna study optimize allennlp_cli.py objective --n-trials=100 --study $STUDY_NAME \
+    $ optuna study optimize allennlp_jsonnet.py objective --n-trials=100 --study $STUDY_NAME \
       --storage sqlite:///example.db
 
 """
