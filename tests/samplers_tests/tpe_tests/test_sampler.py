588f48fabedb681a8e0a81c82c492cc7638fb08a
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-04-19 18:42:55 +0900

    add max_resource input to HyperbandPruner

--- tests/samplers_tests/tpe_tests/test_sampler.py
+++ tests/samplers_tests/tpe_tests/test_sampler.py
@@ -15,7 +15,8 @@ def test_hyperopt_parameters(use_hyperband):
 
     sampler = TPESampler(**TPESampler.hyperopt_parameters())
     study = optuna.create_study(
-        sampler=sampler, pruner=optuna.pruners.HyperbandPruner() if use_hyperband else None
+        sampler=sampler,
+        pruner=optuna.pruners.HyperbandPruner(max_resource=50) if use_hyperband else None,
     )
     study.optimize(lambda t: t.suggest_uniform("x", 10, 20), n_trials=50)
 
