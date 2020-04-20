a06dadaa1808156df84a231a466f1871f51caadb
Takeru Ohta <phjgt308@gmail.com>
2020-04-20 20:53:50 +0900

    Replace `intersection_search_space()` with `IntersectionSearchSpace`.

--- optuna/samplers/cmaes.py
+++ optuna/samplers/cmaes.py
@@ -108,13 +108,14 @@ class CmaEsSampler(BaseSampler):
         self._warn_independent_sampling = warn_independent_sampling
         self._logger = optuna.logging.get_logger(__name__)
         self._cma_rng = np.random.RandomState(seed)
+        self._search_space = optuna.samplers.IntersectionSearchSpace()
 
     def infer_relative_search_space(
         self, study: "optuna.Study", trial: "optuna.trial.FrozenTrial",
     ) -> Dict[str, BaseDistribution]:
 
         search_space = {}  # type: Dict[str, BaseDistribution]
-        for name, distribution in optuna.samplers.intersection_search_space(study).items():
+        for name, distribution in self._search_space.calculate(study).items():
             if distribution.single():
                 # `cma` cannot handle distributions that contain just a single value, so we skip
                 # them. Note that the parameter values for such distributions are sampled in
