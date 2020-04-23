bae533e1b331ad5a49c84389e46c1507680aa4c4
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-04-23 12:26:07 +0900

    Remove logics in samplers

--- optuna/pruners/hyperband.py
+++ optuna/pruners/hyperband.py
@@ -3,10 +3,14 @@ from typing import List
 from typing import Optional
 import warnings
 
+import numpy
+
 from optuna._experimental import experimental
+from optuna import distributions
 from optuna import logging
 from optuna.pruners.base import BasePruner
 from optuna.pruners.successive_halving import SuccessiveHalvingPruner
+from optuna.samplers.base import BaseSampler
 from optuna.study import Study
 from optuna.trial import FrozenTrial
 
@@ -212,3 +216,27 @@ class HyperbandPruner(BasePruner):
                     return object.__getattribute__(self, attr_name)
 
         return _BracketStudy(study, bracket_index)
+
+
+class _HyperbandSampler(BaseSampler):
+    def __init__(self, sampler, hyperband_pruner):
+        self._sampler = sampler
+        self._pruner = hyperband_pruner
+
+    def infer_relative_search_space(self, study, trial):
+        study = self._pruner._create_bracket_study(
+            study, self._pruner._get_bracket_id(study, trial)
+        )
+        return self._sampler.infer_relative_search_space(study, trial)
+
+    def sample_relative(self, study, trial, search_space):
+        study = self._pruner._create_bracket_study(
+            study, self._pruner._get_bracket_id(study, trial)
+        )
+        return self._sampler.sample_relative(study, trial, search_space)
+
+    def sample_independent(self, study, trial, param_name, param_distribution):
+        study = self._pruner._create_bracket_study(
+            study, self._pruner._get_bracket_id(study, trial)
+        )
+        return self._sampler.sample_independent(study, trial, param_name, param_distribution)
