7cb5b94f15520081ae8fad5f872d7c62549214ff
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2020-05-18 18:04:25 +0900

    Revert cmaes

--- optuna/samplers/cmaes.py
+++ optuna/samplers/cmaes.py
@@ -324,10 +324,6 @@ def _initialize_x0(search_space: Dict[str, BaseDistribution]) -> Dict[str, np.nd
             log_high = math.log(distribution.high)
             log_low = math.log(distribution.low)
             x0[name] = math.exp(np.mean([log_high, log_low]))
-        elif isinstance(distribution, optuna.distributions.IntLogUniformDistribution):
-            log_high = math.log(distribution.high)
-            log_low = math.log(distribution.low)
-            x0[name] = int(math.exp(np.mean([log_high, log_low])))
         else:
             raise NotImplementedError(
                 "The distribution {} is not implemented.".format(distribution)
@@ -345,10 +341,6 @@ def _initialize_sigma0(search_space: Dict[str, BaseDistribution]) -> float:
             sigma0.append((distribution.high - distribution.low) / 6)
         elif isinstance(distribution, optuna.distributions.IntUniformDistribution):
             sigma0.append((distribution.high - distribution.low) / 6)
-        elif isinstance(distribution, optuna.distributions.IntLogUniformDistribution):
-            log_high = math.log(distribution.high)
-            log_low = math.log(distribution.low)
-            sigma0.append((log_high - log_low) / 6)
         elif isinstance(distribution, optuna.distributions.LogUniformDistribution):
             log_high = math.log(distribution.high)
             log_low = math.log(distribution.low)
@@ -374,7 +366,6 @@ def _get_search_space_bound(
                 optuna.distributions.LogUniformDistribution,
                 optuna.distributions.DiscreteUniformDistribution,
                 optuna.distributions.IntUniformDistribution,
-                optuna.distributions.IntLogUniformDistribution,
             ),
         ):
             bounds.append([dist.low, dist.high])
