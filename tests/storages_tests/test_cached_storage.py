920ad384fdfc9b1db9445ca9979695a7c5e514b0
Yusuke Tsuzuku <ytsmiling@gmail.com>
2020-05-21 06:29:45 +0900

    Concatenate with-statements.

--- tests/storages_tests/test_cached_storage.py
+++ tests/storages_tests/test_cached_storage.py
@@ -43,13 +43,14 @@ def test_cached_set() -> None:
     study_id = storage.create_new_study("test-study")
 
     trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-        with patch.object(base_storage, "set_trial_param", return_value=True) as set_mock:
-            storage.set_trial_param(
-                trial_id, "paramA", 1.2, optuna.distributions.UniformDistribution(-0.2, 2.3)
-            )
-            assert update_mock.call_count == 0
-            assert set_mock.call_count == 0
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(base_storage, "set_trial_param", return_value=True) as set_mock:
+        storage.set_trial_param(
+            trial_id, "paramA", 1.2, optuna.distributions.UniformDistribution(-0.2, 2.3)
+        )
+        assert update_mock.call_count == 0
+        assert set_mock.call_count == 0
 
 
 def test_uncached_set() -> None:
@@ -67,41 +68,52 @@ def test_uncached_set() -> None:
 
     for state in [TrialState.COMPLETE, TrialState.PRUNED, TrialState.FAIL, TrialState.WAITING]:
         trial_id = storage.create_new_trial(study_id)
-        with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-            with patch.object(base_storage, "set_trial_state", return_value=True) as set_mock:
-                storage.set_trial_state(trial_id, state)
-                assert update_mock.call_count == 1
-                assert set_mock.call_count == 0
-
-    trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-        with patch.object(base_storage, "set_trial_value", return_value=None) as set_mock:
-            storage.set_trial_value(trial_id, 0.3)
-            assert update_mock.call_count == 1
-            assert set_mock.call_count == 0
-
-    trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
         with patch.object(
-            base_storage, "set_trial_intermediate_value", return_value=None
+            base_storage, "_update_trial", return_value=True
+        ) as update_mock, patch.object(
+            base_storage, "set_trial_state", return_value=True
         ) as set_mock:
-            storage.set_trial_intermediate_value(trial_id, 3, 0.3)
+            storage.set_trial_state(trial_id, state)
             assert update_mock.call_count == 1
             assert set_mock.call_count == 0
 
     trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-        with patch.object(base_storage, "set_trial_system_attr", return_value=None) as set_mock:
-            storage.set_trial_system_attr(trial_id, "attrA", "foo")
-            assert update_mock.call_count == 1
-            assert set_mock.call_count == 0
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(base_storage, "set_trial_value", return_value=None) as set_mock:
+        storage.set_trial_value(trial_id, 0.3)
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
 
     trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-        with patch.object(base_storage, "set_trial_user_attr", return_value=None) as set_mock:
-            storage.set_trial_user_attr(trial_id, "attrB", "bar")
-            assert update_mock.call_count == 1
-            assert set_mock.call_count == 0
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(
+        base_storage, "set_trial_intermediate_value", return_value=None
+    ) as set_mock:
+        storage.set_trial_intermediate_value(trial_id, 3, 0.3)
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
+
+    trial_id = storage.create_new_trial(study_id)
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(
+        base_storage, "set_trial_system_attr", return_value=None
+    ) as set_mock:
+        storage.set_trial_system_attr(trial_id, "attrA", "foo")
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
+
+    trial_id = storage.create_new_trial(study_id)
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(
+        base_storage, "set_trial_user_attr", return_value=None
+    ) as set_mock:
+        storage.set_trial_user_attr(trial_id, "attrB", "bar")
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
 
 
 def test_cache_deletion() -> None:
@@ -114,39 +126,47 @@ def test_cache_deletion() -> None:
 
     for state in [TrialState.COMPLETE, TrialState.PRUNED, TrialState.FAIL, TrialState.WAITING]:
         trial_id = storage.create_new_trial(study_id)
-        with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-            with patch.object(base_storage, "set_trial_state", return_value=True) as set_mock:
-                with patch.object(base_storage, "get_trial", return_value=True) as get_mock:
-                    storage.get_trial(trial_id)
-                    assert update_mock.call_count == 0
-                    assert set_mock.call_count == 0
-                    assert get_mock.call_count == 0
+        with patch.object(
+            base_storage, "_update_trial", return_value=True
+        ) as update_mock, patch.object(
+            base_storage, "set_trial_state", return_value=True
+        ) as set_mock, patch.object(
+            base_storage, "get_trial", return_value=True
+        ) as get_mock:
+            storage.get_trial(trial_id)
+            assert update_mock.call_count == 0
+            assert set_mock.call_count == 0
+            assert get_mock.call_count == 0
 
-                    storage.set_trial_state(trial_id, state)
-                    assert update_mock.call_count == 1
-                    assert set_mock.call_count == 0
-                    assert get_mock.call_count == 0
+            storage.set_trial_state(trial_id, state)
+            assert update_mock.call_count == 1
+            assert set_mock.call_count == 0
+            assert get_mock.call_count == 0
 
-                    storage.get_trial(trial_id)
-                    assert update_mock.call_count == 1
-                    assert set_mock.call_count == 0
-                    assert get_mock.call_count == 1
+            storage.get_trial(trial_id)
+            assert update_mock.call_count == 1
+            assert set_mock.call_count == 0
+            assert get_mock.call_count == 1
 
     trial_id = storage.create_new_trial(study_id)
-    with patch.object(base_storage, "_update_trial", return_value=True) as update_mock:
-        with patch.object(base_storage, "set_trial_state", return_value=True) as set_mock:
-            with patch.object(base_storage, "get_trial", return_value=True) as get_mock:
-                storage.get_trial(trial_id)
-                assert update_mock.call_count == 0
-                assert set_mock.call_count == 0
-                assert get_mock.call_count == 0
+    with patch.object(
+        base_storage, "_update_trial", return_value=True
+    ) as update_mock, patch.object(
+        base_storage, "set_trial_state", return_value=True
+    ) as set_mock, patch.object(
+        base_storage, "get_trial", return_value=True
+    ) as get_mock:
+        storage.get_trial(trial_id)
+        assert update_mock.call_count == 0
+        assert set_mock.call_count == 0
+        assert get_mock.call_count == 0
 
-                storage.set_trial_state(trial_id, TrialState.RUNNING)
-                assert update_mock.call_count == 1
-                assert set_mock.call_count == 0
-                assert get_mock.call_count == 0
+        storage.set_trial_state(trial_id, TrialState.RUNNING)
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
+        assert get_mock.call_count == 0
 
-                storage.get_trial(trial_id)
-                assert update_mock.call_count == 1
-                assert set_mock.call_count == 0
-                assert get_mock.call_count == 0
+        storage.get_trial(trial_id)
+        assert update_mock.call_count == 1
+        assert set_mock.call_count == 0
+        assert get_mock.call_count == 0
