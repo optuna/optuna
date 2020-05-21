e14711beae496dc8648c8932c49714adfa452c06
bigbird555 <kengo-phoenix@g.ecc.u-tokyo.ac.jp>
2020-05-22 03:13:32 +0900

    change the order of imports from typing

--- optuna/visualization/contour.py
+++ optuna/visualization/contour.py
@@ -1,15 +1,15 @@
 import math
 
+from typing import List
+from typing import Optional
+from typing import Tuple
+
 from optuna.logging import get_logger
 from optuna.study import StudyDirection
 from optuna.trial import TrialState
 from optuna.visualization.utils import _check_plotly_availability
 from optuna.visualization.utils import _is_log_scale
 from optuna.visualization.utils import is_available
-
-from typing import List
-from typing import Optional
-from typing import Tuple
 
 from optuna.study import Study
 from optuna.trial import FrozenTrial
