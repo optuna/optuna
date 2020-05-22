3402356f54bef2b125435b5c13ba03bf564b3a9c
smly <eowner@gmail.com>
2020-05-22 13:52:13 +0900

    Change order of sentences.

--- optuna/integration/xgboost.py
+++ optuna/integration/xgboost.py
@@ -40,12 +40,11 @@ class XGBoostPruningCallback(object):
             objective function.
         observation_key:
             An evaluation metric for pruning, e.g., ``validation-error`` and
-            ``validation-merror``. Please refer to ``eval_metric`` in
+            ``validation-merror``. When using the Scikit-Learn API, the index number of
+            ``eval_set`` must be included in the ``observation_key``, e.g., ``validation_0-error``
+            and ``validation_0-merror``. Please refer to ``eval_metric`` in
             `XGBoost reference <https://xgboost.readthedocs.io/en/latest/parameter.html>`_
             for further details.
-            Note that when using the sklearn API callback, the index number of `eval_set`
-            must be included in the ``observation_key``, e.g., ``validation_0-error`` and
-            ``validation_0-merror``.
     """
 
     def __init__(self, trial, observation_key):
