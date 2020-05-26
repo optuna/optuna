57fa0a11b79bd9f248a759667dcc99d59483caf0
harupy <hkawamura0130@gmail.com>
2020-05-26 17:25:24 +0900

    Revert "Remove _original_init"
    This reverts commit ad10300b1e81ea3d8e4ff4d6a1bf07119b6e510b.

--- optuna/_experimental.py
+++ optuna/_experimental.py
@@ -70,7 +70,10 @@ def experimental(version: str, name: str = None) -> Any:
 
             This decorator is supposed to be applied to the experimental class.
             """
-            @functools.wraps(cls.__init__)
+
+            _original_init = cls.__init__
+
+            @functools.wraps(_original_init)
             def wrapped_init(self, *args, **kwargs) -> None:  # type: ignore
                 warnings.warn(
                     "{} is experimental (supported from v{}). "
@@ -80,7 +83,7 @@ def experimental(version: str, name: str = None) -> Any:
                     ExperimentalWarning,
                 )
 
-                cls.__init__(self, *args, **kwargs)
+                _original_init(self, *args, **kwargs)
 
             cls.__init__ = wrapped_init
 
