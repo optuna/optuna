c6c7205dee8b7ce0aa9f5bf8f35558a031b2a1c9
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2020-06-02 13:52:13 +0900

    Add minimum tests for _imports.py

---
+++ tests/test_imports.py
@@ -0,0 +1,26 @@
+import pytest
+
+from optuna._imports import try_import
+
+
+def test_try_import_successful() -> None:
+    with try_import() as imports:
+        pass
+
+    assert imports.is_successful()
+    imports.check()
+
+
+def test_try_import_failing() -> None:
+    # Default arguments.
+    with try_import() as imports:
+        raise ImportError
+    assert not imports.is_successful()
+    with pytest.raises(ImportError):
+        imports.check()
+
+    # Non-default arguments.
+    with try_import(catch=(SyntaxError,)) as imports:
+        raise SyntaxError
+    with pytest.raises(SyntaxError):
+        imports.check()
