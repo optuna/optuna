943792c1eef76db1d5b12433994b96ea24493ab6
harupy <hkawamura0130@gmail.com>
2020-06-16 21:20:31 +0900

    Remove unused import

--- optuna/multi_objective/trial.py
+++ optuna/multi_objective/trial.py
@@ -4,7 +4,6 @@ from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Sequence
-from typing import Tuple
 from typing import Union
 
 from optuna._experimental import experimental
