b3f0ad97c6d1a2593e6e38904d3887e8839b1085
himkt <himkt@cookpad.com>
2020-06-20 12:45:51 +0900

    Replace allennlp.data.DataLoader with torch.utils.DataLoader

--- tests/integration_tests/allennlp_tests/test_allennlp.py
+++ tests/integration_tests/allennlp_tests/test_allennlp.py
@@ -13,7 +13,7 @@ import allennlp.modules.text_field_embedders
 import allennlp.training
 import pytest
 import torch.optim
-import torch.utils.data
+from torch.utils.data import DataLoader
 
 import optuna
 from optuna.integration.allennlp import AllenNLPPruningCallback
@@ -168,7 +168,7 @@ def test_allennlp_pruning_callback() -> None:
                 text_field_embedder=embedder, seq2vec_encoder=encoder, vocab=vocab
             )
             optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
-            data_loader = allennlp.data.DataLoader(
+            data_loader = DataLoader(
                 dataset, batch_size=10, collate_fn=allennlp.data.allennlp_collate
             )
             serialization_dir = os.path.join(tmp_dir, "trial_{}".format(trial.number))
