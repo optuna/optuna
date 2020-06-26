02ccc578bb6537a33d3e86a822ea858dc5aaed64
harupy <hkawamura0130@gmail.com>
2020-06-26 22:31:44 +0900

    Specify html4_writer

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,6 +97,8 @@ html_favicon = '../image/favicon.ico'
 
 html_logo = '../image/optuna-logo.png'
 
+html4_writer = True
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
