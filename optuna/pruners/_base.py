68bf7fad640de37705e292758b1d2571845af170
harupy <hkawamura0130@gmail.com>
2020-06-30 00:37:22 +0900

    Fix _base.py

--- optuna/pruners/_base.py
+++ optuna/pruners/_base.py
@@ -2,9 +2,8 @@ import abc
 
 from optuna.type_checking import TYPE_CHECKING
 
-if TYPE_CHECKING:
-    from optuna.study import Study  # NOQA
-    from optuna.trial import FrozenTrial  # NOQA
+from optuna.study import Study
+from optuna.trial import FrozenTrial
 
 
 class BasePruner(object, metaclass=abc.ABCMeta):
