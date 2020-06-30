43bdc972d28dac2d917f59a534425fbc678660ae
harupy <hkawamura0130@gmail.com>
2020-06-30 20:39:49 +0900

    Revert "Specify html4_writer"
    This reverts commit 02ccc578bb6537a33d3e86a822ea858dc5aaed64.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,8 +97,6 @@ html_favicon = '../image/favicon.ico'
 
 html_logo = '../image/optuna-logo.png'
 
-html4_writer = True
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
