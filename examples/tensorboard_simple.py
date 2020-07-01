9dba2361d2d31abcbecddbf961823058f28c3c7f
Vladislav Skripniuk <skripniuk.v@yandex.ru>
2020-07-01 10:40:14 +0200

    Update examples/tensorboard_simple.py
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- examples/tensorboard_simple.py
+++ examples/tensorboard_simple.py
@@ -28,7 +28,6 @@ def train_test_model(num_units: int, dropout_rate: float, optimizer: str) -> flo
 
 
 def objective(trial: optuna.trial.Trial) -> float:
-
     num_units = trial.suggest_int("NUM_UNITS", 16, 32)
     dropout_rate = trial.suggest_uniform("DROPOUT_RATE", 0.1, 0.2)
     optimizer = trial.suggest_categorical("OPTIMIZER", ["sgd", "adam"])
