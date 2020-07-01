f6163e04750395dfb711e04b2dbe12ccfad3842c
Antonin RAFFIN <antonin.raffin@ensta.org>
2020-07-01 16:12:21 +0200

    Style fixes

--- examples/rl/sb3_simple.py
+++ examples/rl/sb3_simple.py
@@ -40,15 +40,20 @@ DEFAULT_HYPERPARAMS = {
 
 def sample_a2c_params(trial: optuna.Trial) -> Dict[str, Any]:
     """Sampler for A2C hyperparameters."""
-    gamma = 1. - trial.suggest_float("gamma", 0.0001, 0.1, log=True)
+    gamma = 1.0 - trial.suggest_float("gamma", 0.0001, 0.1, log=True)
     max_grad_norm = trial.suggest_float("max_grad_norm", 0.3, 5.0, log=True)
-    gae_lambda = 1. - trial.suggest_float("gae_lambda", 0.001, 0.2. log=True)
+    gae_lambda = 1.0 - trial.suggest_float("gae_lambda", 0.001, 0.2, log=True)
     n_steps = 2 ** trial.suggest_int("exponent_n_steps", 3, 10)
     learning_rate = trial.suggest_loguniform("lr", 1e-5, 1)
     ent_coef = trial.suggest_loguniform("ent_coef", 0.00000001, 0.1)
     ortho_init = trial.suggest_categorical("ortho_init", [False, True])
     net_arch = trial.suggest_categorical("net_arch", ["tiny", "small"])
     activation_fn = trial.suggest_categorical("activation_fn", ["tanh", "relu"])
+
+    # Display true values
+    trial.set_user_attr("gamma_", gamma)
+    trial.set_user_attr("gae_lambda_", gae_lambda)
+    trial.set_user_attr("n_steps", n_steps)
 
     net_arch = (
         {"pi": [64], "vf": [64],} if net_arch == "tiny" else {"pi": [64, 64], "vf": [64, 64],}
