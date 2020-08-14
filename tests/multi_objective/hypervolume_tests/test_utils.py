5f059d06eb76efd72763bc77443e459ad0e1a73b
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-08-14 11:56:51 +0900

    Remove _dominates_or_equal_to method

--- tests/multi_objective/hypervolume_tests/test_utils.py
+++ tests/multi_objective/hypervolume_tests/test_utils.py
@@ -12,21 +12,3 @@ def test_compute_2points_volume() -> None:
     p1 = np.ones(10) * 2
     p2 = np.ones(10)
     assert 1 == optuna.multi_objective._hypervolume._compute_2points_volume(p1, p2)
-
-
-def test_dominates_or_equal() -> None:
-    p1 = np.asarray([1, 1, 1])
-    p2 = np.asarray([1, 0, 0])
-    assert not optuna.multi_objective._hypervolume._dominates_or_equal(p1, p2)
-
-    p1 = np.asarray([1, 0, 0])
-    p2 = np.asarray([1, 0, 1])
-    assert optuna.multi_objective._hypervolume._dominates_or_equal(p1, p2)
-
-    p1 = np.asarray([1, 1, 1])
-    p2 = np.asarray([1, 1, 1])
-    assert optuna.multi_objective._hypervolume._dominates_or_equal(p1, p2)
-
-    p1 = np.asarray([1, 0, 1])
-    p2 = np.asarray([1, 1, 0])
-    assert not optuna.multi_objective._hypervolume._dominates_or_equal(p1, p2)
