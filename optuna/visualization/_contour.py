114ceaffccf35f2999b62739f756c6026edce022
Henry Cui <z.henry.cui@gmail.com>
2020-08-23 11:09:56 +0900

    Fix format to pass CircleCI checks

--- optuna/visualization/_contour.py
+++ optuna/visualization/_contour.py
@@ -198,10 +198,7 @@ def _generate_contour_subplot(
     scatter = go.Scatter(
         x=x_values,
         y=y_values,
-        marker={
-            "line": {"width": 0.5, "color": "Grey"},
-            "color": "black",
-        },
+        marker={"line": {"width": 0.5, "color": "Grey"}, "color": "black"},
         mode="markers",
         showlegend=False,
     )
