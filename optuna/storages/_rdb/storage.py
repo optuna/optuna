900ec714b20e4ab0cfec6de968982e477e989cfb
keisuke-umezawa <keisuke.umezawa@gmail.com>
2020-08-24 00:53:23 +0900

    Test

--- optuna/storages/_rdb/storage.py
+++ optuna/storages/_rdb/storage.py
@@ -414,20 +414,16 @@ class RDBStorage(BaseStorage):
         # Retry a couple of times. Deadlocks may occur in distributed environments.
         n_retries = 0
         while True:
-            session = self.scoped_session()
-
             try:
-                # Ensure that that study exists.
-                #
-                # Locking within a study is necessary since the creation of a trial is not an
-                # atomic operation. More precisely, the trial number computed in
-                # `_get_prepared_new_trial` is prone to race conditions without this lock.
-                models.StudyModel.find_or_raise_by_id(study_id, session, for_update=True)
+                with self._session_scope() as session:
+                    # Ensure that that study exists.
+                    #
+                    # Locking within a study is necessary since the creation of a trial is not an
+                    # atomic operation. More precisely, the trial number computed in
+                    # `_get_prepared_new_trial` is prone to race conditions without this lock.
+                    models.StudyModel.find_or_raise_by_id(study_id, session, for_update=True)
 
-                trial = self._get_prepared_new_trial(study_id, template_trial, session)
-
-                self._commit(session)
-
+                    trial = self._get_prepared_new_trial(study_id, template_trial, session)
                 break  # Successfully created trial.
             except OperationalError:
                 session.rollback()
