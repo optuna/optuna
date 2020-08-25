23317077581f281da1c999bb406b609d93ae3452
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-08-25 15:57:04 +0900

    Add some notes and usage example.

--- optuna/multi_objective/_hypervolume/base.py
+++ optuna/multi_objective/_hypervolume/base.py
@@ -4,10 +4,45 @@ import numpy as np
 
 
 class BaseHypervolume(object, metaclass=abc.ABCMeta):
-    """Base class for hypervolume calculators"""
+    """Base class for hypervolume calculators
+
+        .. note::
+            In Optuna, this class is used for computing the hypervolume of objective points.
+            In other words, the coordinate of each point represents the one of the multi-objective
+            function's values.
+
+        .. note::
+            We assume that the each objective function to be minimized. If you use some objectives
+            to be maximized, please transform inputs to this class's `compute` method for the
+            minimization problem. For example,
+
+            .. code::
+                def objective(trial):
+                    ...
+                    return accuracy, elapsed_time
+
+                class YourSampler(BaseSampler):
+                    ...
+                    def sample_relative(study, trial, search_space):
+                        ...
+                        trials = study.get_trials()
+                        solution_sets = np.ndarray([t.values for t in trials])
+
+                        # Transform the objective to be maximized into that for minimization.
+                        solution_sets = np.ndarray([[-s[0], s[1]] for s in solution_sets])
+
+                        reference_point = 2 * np.max(solution_set, axis=0)
+                        hypervolume = WFG().compute(solution_sets, reference_point)
+                        ...
+    """
 
     def compute(self, solution_set: np.ndarray, reference_point: np.ndarray) -> float:
         """Compute the hypervolume for the given solution set and reference point.
+
+        .. note::
+            We assume that all points in the solution set dominate or equal the reference point.
+            In other words, for all points in the solution set and the coordinate `i`,
+            `point[i] <= reference_point[i]`.
 
         Args:
             solution_set:
@@ -24,9 +59,9 @@ class BaseHypervolume(object, metaclass=abc.ABCMeta):
         # Validates that all points in the solution set dominate or equal the reference point.
         if not (solution_set <= reference_point).all():
             raise ValueError(
-                "All solution must dominate or equal the reference point. "
-                "That is, for all solution in the solution_set and the coordinate `i`, "
-                "`solution[i] <= reference_point[i]`."
+                "All points must dominate or equal the reference point. "
+                "That is, for all points in the solution_set and the coordinate `i`, "
+                "`point[i] <= reference_point[i]`."
             )
 
     @abc.abstractmethod
