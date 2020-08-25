5408bcb0e7ca9349fcffac3249f96b69013662c2
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-08-25 18:50:46 +0900

    Provide conmplete example

--- optuna/multi_objective/_hypervolume/base.py
+++ optuna/multi_objective/_hypervolume/base.py
@@ -15,8 +15,34 @@ class BaseHypervolume(object, metaclass=abc.ABCMeta):
             to be maximized before calling this class's `compute` method. For example,
 
             .. testcode::
+
+                import time
+
+                import numpy as np
+                from sklearn.datasets import load_iris
+                from sklearn.linear_model import SGDClassifier
+                from sklearn.model_selection import train_test_split
+
+                import optuna
+
+                X, y = load_iris(return_X_y=True)
+                X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+                classes = np.unique(y)
+                n_train_iter = 100
+
                 def objective(trial):
-                    ...
+                    start = time.time()
+
+                    alpha = trial.suggest_uniform('alpha', 0.0, 1.0)
+                    clf = SGDClassifier(alpha=alpha)
+
+                    for step in range(n_train_iter):
+                        clf.partial_fit(X_train, y_train, classes=classes)
+
+                    accuracy = clf.score(X_valid, y_valid)
+
+                    elapsed_time = time.time() - start
+
                     return accuracy, elapsed_time
 
                 study = optuna.multi_objective.create_study(["maximize", "minimize"])
