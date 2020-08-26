932ec9eb3700412969dab8f2d3cdc0d73c71e220
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-08-26 17:21:08 +0900

    Fix indent

--- optuna/multi_objective/_hypervolume/base.py
+++ optuna/multi_objective/_hypervolume/base.py
@@ -12,48 +12,48 @@ class BaseHypervolume(object, metaclass=abc.ABCMeta):
 
         .. note::
             We check that each objective is to be minimized. Transform objective values that are
-            to be maximized before calling this class's `compute` method. For example,
+            to be maximized before calling this class's `compute` method.
 
-            .. testcode::
+        .. testcode::
 
-                import time
+            import time
 
-                import numpy as np
-                from sklearn.datasets import load_iris
-                from sklearn.linear_model import SGDClassifier
-                from sklearn.model_selection import train_test_split
+            import numpy as np
+            from sklearn.datasets import load_iris
+            from sklearn.linear_model import SGDClassifier
+            from sklearn.model_selection import train_test_split
 
-                import optuna
+            import optuna
 
-                X, y = load_iris(return_X_y=True)
-                X_train, X_valid, y_train, y_valid = train_test_split(X, y)
-                classes = np.unique(y)
-                n_train_iter = 10
+            X, y = load_iris(return_X_y=True)
+            X_train, X_valid, y_train, y_valid = train_test_split(X, y)
+            classes = np.unique(y)
+            n_train_iter = 10
 
-                def objective(trial):
-                    start = time.time()
+            def objective(trial):
+                start = time.time()
 
-                    alpha = trial.suggest_uniform('alpha', 0.0, 1.0)
-                    clf = SGDClassifier(alpha=alpha)
+                alpha = trial.suggest_uniform('alpha', 0.0, 1.0)
+                clf = SGDClassifier(alpha=alpha)
 
-                    for step in range(n_train_iter):
-                        clf.partial_fit(X_train, y_train, classes=classes)
+                for step in range(n_train_iter):
+                    clf.partial_fit(X_train, y_train, classes=classes)
 
-                    accuracy = clf.score(X_valid, y_valid)
+                accuracy = clf.score(X_valid, y_valid)
 
-                    elapsed_time = time.time() - start
+                elapsed_time = time.time() - start
 
-                    return accuracy, elapsed_time
+                return accuracy, elapsed_time
 
-                study = optuna.multi_objective.create_study(["maximize", "minimize"])
-                study.optimize(objective, n_trials=10)
-                trials = study.get_pareto_front_trials()
-                solution_sets = np.ndarray([t.values for t in trials])
-                # Transform the objective to be maximized into that for minimization.
-                solution_sets = np.ndarray([[-s[0], s[1]] for s in solution_sets])
-                reference_point = 2 * np.max(solution_set, axis=0)
-                hypervolume = WFG().compute(solution_sets, reference_point)
-                print("Hypervolume of the Pareto solutions is {}.".format(hypervolume))
+            study = optuna.multi_objective.create_study(["maximize", "minimize"])
+            study.optimize(objective, n_trials=10)
+            trials = study.get_pareto_front_trials()
+            solution_sets = np.ndarray([t.values for t in trials])
+            # Transform the objective to be maximized into that for minimization.
+            solution_sets = np.ndarray([[-s[0], s[1]] for s in solution_sets])
+            reference_point = 2 * np.max(solution_set, axis=0)
+            hypervolume = WFG().compute(solution_sets, reference_point)
+            print("Hypervolume of the Pareto solutions is {}.".format(hypervolume))
     """
 
     def compute(self, solution_set: np.ndarray, reference_point: np.ndarray) -> float:
