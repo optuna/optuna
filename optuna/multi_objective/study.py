7aa7a19b202adde71455f21b4419764de9a63134
Norihito Ishida <46351322+norihitoishida@users.noreply.github.com>
2020-08-30 16:18:12 +0900

    Update optuna/multi_objective/study.py
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- optuna/multi_objective/study.py
+++ optuna/multi_objective/study.py
@@ -50,21 +50,21 @@ def create_study(
 
     Example:
 
-            .. testcode::
+        .. testcode::
 
-                import optuna
+            import optuna
 
-                def objective(trial):
-                    # Binh and Korn function.
-                    x = trial.suggest_float("x", 0, 5)
-                    y = trial.suggest_float("y", 0, 3)
+            def objective(trial):
+                # Binh and Korn function.
+                x = trial.suggest_float("x", 0, 5)
+                y = trial.suggest_float("y", 0, 3)
 
-                    v0 = 4 * x ** 2 + 4 * y ** 2
-                    v1 = (x - 5) ** 2 + (y - 5) ** 2
-                    return v0, v1
+                v0 = 4 * x ** 2 + 4 * y ** 2
+                v1 = (x - 5) ** 2 + (y - 5) ** 2
+                return v0, v1
 
-                study = optuna.multi_objective.create_study(["minimize", "minimize"])
-                study.optimize(objective, n_trials=3)
+            study = optuna.multi_objective.create_study(["minimize", "minimize"])
+            study.optimize(objective, n_trials=3)
 
     Args:
         directions:
