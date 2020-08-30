82cce467ac941608940a86f6736de0f1c7ee6e65
norihitoishida <norihitoishida@gmail.com>
2020-08-30 10:36:20 +0900

    Fix Binh and Korn function

--- optuna/multi_objective/study.py
+++ optuna/multi_objective/study.py
@@ -239,7 +239,7 @@ class MultiObjectiveStudy(object):
                     x = trial.suggest_float("x", 0, 5)
                     y = trial.suggest_float("y", 0, 3)
 
-                    v0 = (4 * x) ** 2 + (4 * y) ** 2
+                    v0 = 4 * x ** 2 + 4 * y ** 2
                     v1 = (x - 5) ** 2 + (y - 5) ** 2
                     return v0, v1
 
