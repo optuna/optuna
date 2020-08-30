913c73a60980f0afab3d5f12fbfc47340617ce7c
Norihito Ishida <46351322+norihitoishida@users.noreply.github.com>
2020-08-30 10:34:20 +0900

    Update optuna/multi_objective/study.py
    Co-authored-by: Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>

--- optuna/multi_objective/study.py
+++ optuna/multi_objective/study.py
@@ -136,7 +136,7 @@ def load_study(
                 x = trial.suggest_float("x", 0, 5)
                 y = trial.suggest_float("y", 0, 3)
 
-                v0 = (4 * x) ** 2 + (4 * y) ** 2
+                v0 = 4 * x ** 2 + 4 * y ** 2
                 v1 = (x - 5) ** 2 + (y - 5) ** 2
                 return v0, v1
 
