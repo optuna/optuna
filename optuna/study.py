76f25864572ab49be9f176f7cdd94e5bf09272b2
norihitoishida <norihitoishida@gmail.com>
2020-08-30 09:40:29 +0900

    Update example

--- optuna/study.py
+++ optuna/study.py
@@ -181,12 +181,18 @@ class Study(BaseStudy):
 
                 import optuna
 
+                def objective(trial):
+                    x = trial.suggest_float("x", 0, 1)
+                    y = trial.suggest_float("y", 0, 1)
+                    return x ** 2 + y ** 2
+
                 study = optuna.create_study()
 
+                study.set_user_attr("objective function", "quadratic fuction")
+                study.set_user_attr("dimensions", 2)
                 study.set_user_attr("contributors", ["Akiba", "Sano"])
-                study.set_user_attr("dataset", "MNIST")
 
-                study.user_attrs  # {"contributors": ["Akiba", "Sano"], "dataset": "MNIST"}
+                study.user_attrs
 
         Returns:
             A dictionary containing all user attributes.
