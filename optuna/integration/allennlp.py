16f8173a2ea34ea4d45c36c773f26962c4586c62
himkt <himkt@cookpad.com>
2020-09-01 00:18:05 +0900

    Apply black for allennlp.py

--- optuna/integration/allennlp.py
+++ optuna/integration/allennlp.py
@@ -183,9 +183,9 @@ class AllenNLPPruningCallback(EpochCallback):
     """
 
     def __init__(
-            self,
-            trial: Optional[optuna.trial.Trial] = None,
-            monitor: Optional[str] = None,
+        self,
+        trial: Optional[optuna.trial.Trial] = None,
+        monitor: Optional[str] = None,
     ):
         _imports.check()
 
