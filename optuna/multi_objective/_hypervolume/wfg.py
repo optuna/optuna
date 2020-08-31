1d416dacd3add4ebba71878de5ad259ccc65bcc6
himkt <himkt@cookpad.com>
2020-08-31 22:43:04 +0900

    Use 2d calculator in WFG

--- optuna/multi_objective/_hypervolume/wfg.py
+++ optuna/multi_objective/_hypervolume/wfg.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+from optuna.multi_objective._hypervolume import _compute_2d
 from optuna.multi_objective._hypervolume import _compute_2points_volume
 from optuna.multi_objective._hypervolume import BaseHypervolume
 
@@ -23,6 +24,14 @@ class WFG(BaseHypervolume):
     def _compute_rec(self, solution_set: np.ndarray) -> float:
         assert self._reference_point is not None
         n_points = solution_set.shape[0]
+        _shape = self._reference_point.shape
+
+        if len(_shape) == 1 and _shape[0] == 2:
+            return _compute_2d(solution_set, self._reference_point)
+
+        if len(_shape) == 2:
+            if (_shape[0] == 2 and _shape[1] == 1) or (_shape[0] == 1 and _shape[1] == 2):
+                return _compute_2d(solution_set, self._reference_point)
 
         if n_points == 1:
             return _compute_2points_volume(solution_set[0], self._reference_point)
