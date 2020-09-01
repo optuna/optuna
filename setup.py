ba9f957b3815a68a35f64f144d38528939ebc52b
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2020-09-01 13:58:06 +0900

    Constrain lightgbm version to avoid error with feature_pre_filter

--- setup.py
+++ setup.py
@@ -76,7 +76,7 @@ def get_extras_require() -> Dict[str, List[str]]:
         "example": [
             "catboost",
             "chainer",
-            "lightgbm",
+            "lightgbm<3.0.0",
             "mlflow",
             "mpi4py",
             "mxnet",
@@ -119,7 +119,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             "chainer>=5.0.0",
             "cma",
             "fakeredis",
-            "lightgbm",
+            "lightgbm<3.0.0",
             "mlflow",
             "mpi4py",
             "mxnet",
@@ -163,7 +163,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             # https://github.com/optuna/optuna/issues/1000.
             "chainer>=5.0.0",
             "cma",
-            "lightgbm",
+            "lightgbm<3.0.0",
             "mlflow",
             "mpi4py",
             "mxnet",
