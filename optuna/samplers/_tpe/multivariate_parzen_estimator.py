a938a398e5df9e9eb7b346088d9c5210cdab1d85
kstoneriv3 <k.stoneriv@gmail.com>
2020-09-03 22:01:27 +0900

    change `*_size` to `n_*`

--- optuna/samplers/_tpe/multivariate_parzen_estimator.py
+++ optuna/samplers/_tpe/multivariate_parzen_estimator.py
@@ -32,6 +32,7 @@ class _MultivariateParzenEstimator:
 
         self._search_space = search_space
         self._parameters = parameters
+        self._n_observations = next(iter(multivariate_observations.values())).size
         self._weights = self._calculate_weights(multivariate_observations)
 
         self._low = {}  # type: Dict[str, Optional[float]]
@@ -115,14 +116,14 @@ class _MultivariateParzenEstimator:
     def log_pdf(self, multivariate_samples: Dict[str, np.ndarray]) -> np.ndarray:
 
         multivariate_samples = self._transform_to_uniform(multivariate_samples)
-        weight_size = len(self._weights)
-        sample_size = next(iter(multivariate_samples.values())).size
-        if sample_size == 0:
+        n_weights = len(self._weights)
+        n_samples = next(iter(multivariate_samples.values())).size
+        if n_samples == 0:
             return np.asarray([], dtype=float)
         # We compute log pdf (compoment_log_pdf)
-        # for each sample in multivariate_samples (of size sample_size)
-        # for each component of `_MultivariateParzenEstimator` (of size weight_size).
-        component_log_pdf = np.zeros((sample_size, weight_size))
+        # for each sample in multivariate_samples (of size n_samples)
+        # for each component of `_MultivariateParzenEstimator` (of size n_weights).
+        component_log_pdf = np.zeros((n_samples, n_weights))
         for param_name, dist in self._search_space.items():
             samples = multivariate_samples[param_name]
             if isinstance(dist, distributions.CategoricalDistribution):
@@ -166,13 +167,13 @@ class _MultivariateParzenEstimator:
         consider_prior = self._parameters.consider_prior
         prior_weight = self._parameters.prior_weight
         weights_func = self._parameters.weights
-        observation_size = next(iter(multivariate_observations.values())).size
+        n_observations = self._n_observations
         if consider_prior:
-            weights = np.empty(observation_size + 1)
-            weights[:-1] = weights_func(observation_size)
+            weights = np.empty(n_observations + 1)
+            weights[:-1] = weights_func(n_observations)
             weights[-1] = prior_weight
         else:
-            weights = weights_func(observation_size)
+            weights = weights_func(n_observations)
         weights /= weights.sum()
         return weights
 
@@ -313,17 +314,18 @@ class _MultivariateParzenEstimator:
     ) -> np.ndarray:
 
         observations = observations.astype(int)
+        n_observations = self._n_observations
         distribution = self._search_space[param_name]
         assert isinstance(distribution, distributions.CategoricalDistribution)
         choices = distribution.choices
         consider_prior = self._parameters.consider_prior
         prior_weights = self._parameters.prior_weight
         if consider_prior:
-            shape = (observations.size + 1, len(choices))
+            shape = (n_observations + 1, len(choices))
         else:
-            shape = (observations.size, len(choices))
-        weights = np.full(shape, fill_value=prior_weights / observations.size)
-        weights[np.arange(observations.size), observations] += 1
+            shape = (n_observations, len(choices))
+        weights = np.full(shape, fill_value=prior_weights / n_observations)
+        weights[np.arange(n_observations), observations] += 1
         weights /= weights.sum(axis=1, keepdims=True)
         return weights
 
@@ -331,7 +333,7 @@ class _MultivariateParzenEstimator:
         self, observations: np.ndarray, param_name: str
     ) -> Tuple[np.ndarray, np.ndarray]:
 
-        observation_size = observations.size
+        n_observations = self._n_observations
         consider_prior = self._parameters.consider_prior
         consider_magic_clip = self._parameters.consider_magic_clip
         sigmas0 = self._sigmas0
@@ -341,7 +343,7 @@ class _MultivariateParzenEstimator:
         assert low is not None
         assert high is not None
 
-        if observation_size == 0:
+        if n_observations == 0:
             consider_prior = True
 
         if consider_prior:
@@ -349,13 +351,13 @@ class _MultivariateParzenEstimator:
             prior_mu = 0.5 * (low + high)
             prior_sigma = 1.0 * (high - low)
 
-            mus = np.empty(observation_size + 1)
-            mus[:observation_size] = observations
-            mus[observation_size] = prior_mu
+            mus = np.empty(n_observations + 1)
+            mus[:n_observations] = observations
+            mus[n_observations] = prior_mu
 
-            sigmas = np.empty(observation_size + 1)
-            sigmas[:observation_size] = sigmas0 * (high - low)
-            sigmas[observation_size] = prior_sigma
+            sigmas = np.empty(n_observations + 1)
+            sigmas[:n_observations] = sigmas0 * (high - low)
+            sigmas[n_observations] = prior_sigma
 
         else:
             mus = observations
