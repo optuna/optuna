b8f72dd16098eac44da964c67bcb40dacbc57155
ytknzw <ytknzw@gmail.com>
2020-09-04 23:12:35 +0900

    Implement `plot_param_importances`, `_get_param_importance_plot`, _get_distribution`, `_get_color`.

--- optuna/visualization/matplotlib/__init__.py
+++ optuna/visualization/matplotlib/__init__.py
@@ -1,1 +1,2 @@
+from optuna.visualization.matplotlib._param_importances import plot_param_importances  # NOQA
 from optuna.visualization.matplotlib._utils import is_available  # NOQA
--- optuna/visualization/matplotlib/_matplotlib_imports.py
+++ optuna/visualization/matplotlib/_matplotlib_imports.py
@@ -7,7 +7,9 @@ with try_import() as _imports:  # NOQA
     # TODO(ytknzw): Add specific imports.
     import matplotlib  # NOQA
     from matplotlib import pyplot as plt  # NOQA
+    from matplotlib import cm  # NOQA
     from matplotlib.axes._axes import Axes  # NOQA
+    from matplotlib.patches import Rectangle  # NOQA
 
     from matplotlib import __version__ as matplotlib_version
 
