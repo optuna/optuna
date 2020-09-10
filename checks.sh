bb660e322a7c03bd19a112b589d9583244c45fb3
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-09-10 15:12:28 +0900

    Add -u option and installation check

--- checks.sh
+++ checks.sh
@@ -1,20 +1,53 @@
 #!/bin/bash
 
-if [[ $((black . --check) 2>&1 | grep unchanged) ]] ; then
-  echo "Success in black."
-else
-  echo "Failed. The code will be formatted by black."
-  black .
-  exit 1
+res_pip_list=$(pip freeze)
+if [[ ! $(echo $res_pip_list | grep black) ]] ; then
+  pip install black
+fi
+if [[ ! $(echo $res_pip_list | grep flake8) ]] ; then
+  pip install flake8
+fi
+if [[ ! $(echo $res_pip_list | grep mypy) ]] ; then
+  pip install mypy
 fi
 
-if [[ $(flake8 .) ]] ; then
+black_update=0
+while getopts "u" OPT
+do
+  case $OPT in
+    u) black_update=1
+       ;;
+    *) ;;
+  esac
+done
+
+res_black=$(black . --check 2>&1)
+if [[ $(echo $res_black | grep reformatted) ]] ; then
+  if [ $black_update == 1 ] ; then
+    echo "Failed with black. The code will be formatted by black."
+    black .
+  else
+    echo "Failed with black."
+    echo "$res_black"
+    exit 1
+  fi
+else
+  echo "Success in black."
+fi
+
+
+res_flake8=$(flake8 .)
+if [[ $res_flake8 ]] ; then
+  echo "Failed with flake8."
+  echo "$res_flake8"
   exit 1
 fi
 echo "Success in flake8."
 
-if [[ ! $((mypy .) 2>&1 | grep Success) ]] ; then
-  mypy .
+res_mypy=$(mypy .)
+if [[ ! $(echo $res_mypy | grep Success) ]] ; then
+  echo "Failed with mypy."
+  echo "$res_mypy"
   exit 1
 fi
 echo "Success in mypy"
