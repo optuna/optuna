17275cf7c128b4dcdc3ddd096f78ca904788642d
upura <upura0@gmail.com>
2020-09-12 15:29:37 +0900

    Add autosummary template path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['_templates', '_templates/autosummary']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
