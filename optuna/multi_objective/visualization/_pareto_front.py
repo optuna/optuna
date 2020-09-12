5692b6521e23c785034134d36c445283f7eecc8a
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2020-09-12 13:53:58 +0900

    Fix indents

--- optuna/multi_objective/visualization/_pareto_front.py
+++ optuna/multi_objective/visualization/_pareto_front.py
@@ -29,12 +29,12 @@ def plot_pareto_front(
             import optuna
 
             def objective(trial):
-               x = trial.suggest_float("x", 0, 5)
-               y = trial.suggest_float("y", 0, 3)
+                x = trial.suggest_float("x", 0, 5)
+                y = trial.suggest_float("y", 0, 3)
 
-               v0 = 4 * x ** 2 + 4 * y ** 2
-               v1 = (x - 5) ** 2 + (y - 5) ** 2
-               return v0, v1
+                v0 = 4 * x ** 2 + 4 * y ** 2
+                v1 = (x - 5) ** 2 + (y - 5) ** 2
+                return v0, v1
 
             study = optuna.multi_objective.create_study(["minimize", "minimize"])
             study.optimize(objective, n_trials=50)
