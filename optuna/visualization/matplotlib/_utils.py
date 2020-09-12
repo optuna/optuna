0db54af9220f4ec484f2aeb1291c73ca23819a4f
ytknzw <ytknzw@gmail.com>
2020-09-12 17:28:48 +0900

    Update optuna/visualization/matplotlib/_utils.py
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/visualization/matplotlib/_utils.py
+++ optuna/visualization/matplotlib/_utils.py
@@ -12,7 +12,7 @@ def is_available() -> bool:
 
     .. note::
 
-        :mod:`~optuna.visualization` module depends on Matplotlib version 3.0.0 or higher. If a
+        :mod:`~optuna.visualization.matplotlib` module depends on Matplotlib version 3.0.0 or higher. If a
         supported version of Matplotlib isn't installed in your environment, this function will
         return :obj:`False`. In such a case, please execute ``$ pip install -U matplotlib>=3.0.0``
         to install Matplotlib.
