f637a04d6f823f39446d727221e2af716e72d9d5
ytknzw <ytknzw@gmail.com>
2020-09-12 16:49:20 +0900

    Remove logger.

--- optuna/visualization/matplotlib/_edf.py
+++ optuna/visualization/matplotlib/_edf.py
@@ -1,10 +1,7 @@
-from optuna.logging import get_logger
 from optuna.visualization.matplotlib._matplotlib_imports import _imports
 
 if _imports.is_successful():
     from optuna.visualization.matplotlib._matplotlib_imports import Axes
-
-_logger = get_logger(__name__)
 
 
 def plot_edf() -> Axes:
