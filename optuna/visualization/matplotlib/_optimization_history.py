82adefa3d9b9bfa8f2bc5e7b210e6c3a17d31d54
ytknzw <ytknzw@gmail.com>
2020-09-13 00:20:44 +0900

    Add logger.

--- optuna/visualization/matplotlib/_optimization_history.py
+++ optuna/visualization/matplotlib/_optimization_history.py
@@ -1,7 +1,4 @@
-from optuna.logging import get_logger
 from optuna.visualization.matplotlib._matplotlib_imports import _imports
-
-
 
 if _imports.is_successful():
     from optuna.visualization.matplotlib._matplotlib_imports import Axes
