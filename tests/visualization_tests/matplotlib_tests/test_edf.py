6e4f9007b0426773ca4f572f7629860c43569c84
ytknzw <ytknzw@gmail.com>
2020-09-12 22:38:07 +0900

    Update test cases.

--- tests/visualization_tests/matplotlib_tests/test_edf.py
+++ tests/visualization_tests/matplotlib_tests/test_edf.py
@@ -8,24 +8,24 @@ from optuna.visualization.matplotlib import plot_edf
 def test_plot_optimization_history(direction: str) -> None:
     # Test with no studies.
     figure = plot_edf(create_study(direction=direction))
-    assert figure.has_data is False
+    assert figure.has_data() is False
 
     # Test with no trials.
     figure = plot_edf(create_study(direction=direction))
-    assert figure.has_data is False
+    assert figure.has_data() is False
 
     figure = plot_edf([create_study(direction=direction), create_study(direction=direction)])
-    assert figure.has_data is False
+    assert figure.has_data() is False
 
     # Test with a study.
     study0 = create_study(direction=direction)
     study0.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
     figure = plot_edf(study0)
-    assert figure.has_data is True
+    assert figure.has_data() is True
 
     # Test with two studies.
     # TODO(ytknzw): Add more specific assertion with the numbers of the studies.
     study1 = create_study(direction=direction)
     study1.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
     figure = plot_edf([study0, study1])
-    assert figure.has_data is True
+    assert figure.has_data() is True
