713742625e702e200c0b32866408cd378f4ca271
upura <upura0@gmail.com>
2020-09-13 16:03:14 +0900

    Revert to a previous setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates', '_templates/autosummary']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
