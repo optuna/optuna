2452329d84831bcac156fab43fa8bd96b8a14a5d
ytknzw <ytknzw@gmail.com>
2020-09-13 10:24:17 +0900

    Correct imports.

--- optuna/visualization/matplotlib/_optimization_history.py
+++ optuna/visualization/matplotlib/_optimization_history.py
@@ -1,6 +1,7 @@
-from optuna.logging import get_logger
+from optuna.visualization.matplotlib._matplotlib_imports import _imports
 
-_logger = get_logger(__name__)
+if _imports.is_successful():
+    from optuna.visualization.matplotlib._matplotlib_imports import Axes
 
 
 def plot_optimization_history() -> Axes:
