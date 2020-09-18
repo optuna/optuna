208ca8774cb129a738dcd984744c2436691e5bd4
ytknzw <ytknzw@gmail.com>
2020-09-18 13:26:47 +0900

    Replace duplicated example with `..seealso::` to `optuna.visualization.plot_param_importances`.

--- optuna/visualization/matplotlib/_param_importances.py
+++ optuna/visualization/matplotlib/_param_importances.py
@@ -54,28 +54,7 @@ def plot_param_importances(
 ) -> Axes:
     """Plot hyperparameter importances with Matplotlib.
 
-    Example:
-
-        The following code snippet shows how to plot hyperparameter importances.
-
-        .. testcode::
-
-            import optuna
-
-            def objective(trial):
-                x = trial.suggest_int("x", 0, 2)
-                y = trial.suggest_float("y", -1.0, 1.0)
-                z = trial.suggest_float("z", 0.0, 1.5)
-                return x ** 2 + y ** 3 - z ** 4
-
-            study = optuna.create_study(sampler=optuna.samplers.RandomSampler())
-            study.optimize(objective, n_trials=100)
-
-            optuna.visualization.plot_param_importances(study)
-
-    .. seealso::
-
-        This function visualizes the results of :func:`optuna.importance.get_param_importances`.
+    .. seealso::  optuna.visualization.plot_param_importances
 
     Args:
         study:
