7b0fab83bc8d383501364d77238509a55cedae78
ytknzw <ytknzw@gmail.com>
2020-09-18 13:23:01 +0900

    Replace duplicated example with `..seealso::` to `optuna.visualization.plot_optimization_history`.

--- optuna/visualization/matplotlib/_optimization_history.py
+++ optuna/visualization/matplotlib/_optimization_history.py
@@ -15,23 +15,7 @@ _logger = get_logger(__name__)
 def plot_optimization_history(study: Study) -> Axes:
     """Plot optimization history of all trials in a study with Matplotlib.
 
-    Example:
-
-        The following code snippet shows how to plot optimization history.
-
-        .. testcode::
-
-            import optuna
-
-            def objective(trial):
-                x = trial.suggest_uniform('x', -100, 100)
-                y = trial.suggest_categorical('y', [-1, 0, 1])
-                return x ** 2 + y
-
-            study = optuna.create_study()
-            study.optimize(objective, n_trials=10)
-
-            optuna.visualization.plot_optimization_history(study)
+    .. seealso::  optuna.visualization.plot_optimization_history
 
     Args:
         study:
