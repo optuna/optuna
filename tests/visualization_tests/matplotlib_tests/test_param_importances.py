b437a82547e00ed55c48b26b8506fc36347e45b4
ytknzw <ytknzw@gmail.com>
2020-09-23 22:21:40 +0900

    Change assertions.

--- tests/visualization_tests/matplotlib_tests/test_param_importances.py
+++ tests/visualization_tests/matplotlib_tests/test_param_importances.py
@@ -12,24 +12,24 @@ def test_plot_param_importances() -> None:
     # Test with no trial.
     study = create_study()
     figure = plot_param_importances(study)
-    assert figure.has_data() is False
+    assert not figure.has_data()
 
     study = prepare_study_with_trials(with_c_d=True)
 
     # Test with a trial.
     # TODO(ytknzw): Add more specific assertion with the test case.
     figure = plot_param_importances(study)
-    assert figure.has_data() is True
+    assert figure.has_data()
 
     # Test with an evaluator.
     # TODO(ytknzw): Add more specific assertion with the test case.
     plot_param_importances(study, evaluator=MeanDecreaseImpurityImportanceEvaluator())
-    assert figure.has_data() is True
+    assert figure.has_data()
 
     # Test with a trial to select parameter.
     # TODO(ytknzw): Add more specific assertion with the test case.
     figure = plot_param_importances(study, params=["param_b"])
-    assert figure.has_data() is True
+    assert figure.has_data()
 
     # Test with wrong parameters.
     with pytest.raises(ValueError):
@@ -43,4 +43,4 @@ def test_plot_param_importances() -> None:
     study = create_study()
     study.optimize(fail_objective, n_trials=1, catch=(ValueError,))
     figure = plot_param_importances(study)
-    assert figure.has_data() is False
+    assert not figure.has_data()
