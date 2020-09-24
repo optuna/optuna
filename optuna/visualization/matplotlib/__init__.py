244c5c5258c642f8b250dfaa4581c29563f17143
ytknzw <ytknzw@gmail.com>
2020-09-24 17:34:21 +0900

    Change import order.

--- optuna/visualization/matplotlib/__init__.py
+++ optuna/visualization/matplotlib/__init__.py
@@ -1,3 +1,3 @@
-from optuna.visualization.matplotlib._edf import plot_edf  # NOQA
 from optuna.visualization.matplotlib._contour import plot_contour  # NOQA
+from optuna.visualization.matplotlib._edf import plot_edf  # NOQA
 from optuna.visualization.matplotlib._utils import is_available  # NOQA
