4c4938418169f5a9a9d26df72d8caa14e83ec64e
keisuke-umezawa <keisuke.umezawa@gmail.com>
2020-09-30 23:55:19 +0900

    Fix checks

--- optuna/multi_objective/study.py
+++ optuna/multi_objective/study.py
@@ -16,9 +16,9 @@ from optuna._experimental import experimental
 from optuna._study_direction import StudyDirection
 from optuna.pruners import NopPruner
 from optuna.storages import BaseStorage
-from optuna.study import Study
 from optuna.study import create_study as _create_study
 from optuna.study import load_study as _load_study
+from optuna.study import Study
 from optuna.trial import FrozenTrial
 from optuna.trial import Trial
 from optuna.trial import TrialState
