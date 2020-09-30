c74918552944fc29ea5b75670652da37f13fc6aa
keisuke-umezawa <keisuke.umezawa@gmail.com>
2020-09-30 23:31:53 +0900

    Add module:: optuna.pruners

--- optuna/pruners/__init__.py
+++ optuna/pruners/__init__.py
@@ -1,4 +1,5 @@
-import optuna
+from typing import TYPE_CHECKING
+
 from optuna.pruners._base import BasePruner  # NOQA
 from optuna.pruners._hyperband import HyperbandPruner  # NOQA
 from optuna.pruners._median import MedianPruner  # NOQA
@@ -7,10 +8,12 @@ from optuna.pruners._percentile import PercentilePruner  # NOQA
 from optuna.pruners._successive_halving import SuccessiveHalvingPruner  # NOQA
 from optuna.pruners._threshold import ThresholdPruner  # NOQA
 
+if TYPE_CHECKING:
+    from optuna.study import Study
+    from optuna.trial import FrozenTrial
 
-def _filter_study(
-    study: "optuna.study.Study", trial: "optuna.trial.FrozenTrial"
-) -> "optuna.study.Study":
+
+def _filter_study(study: "Study", trial: "FrozenTrial") -> "Study":
     if isinstance(study.pruner, HyperbandPruner):
         # Create `_BracketStudy` to use trials that have the same bracket id.
         pruner = study.pruner  # type: HyperbandPruner
