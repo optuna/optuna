509952f5a1f6f77f4f0a02468a3ab71d3bb55121
nyanhi <ksuzuki19937.mm@gmail.com>
2020-10-02 16:52:43 +0900

    fix - type hint for _LightGBMBaseTuner._best_booster_with_trial_number.

--- optuna/integration/_lightgbm_tuner/optimize.py
+++ optuna/integration/_lightgbm_tuner/optimize.py
@@ -384,7 +384,9 @@ class _LightGBMBaseTuner(_BaseTuner):
         self._parse_args(*args, **kwargs)
         self._start_time = None  # type: Optional[float]
         self._optuna_callbacks = optuna_callbacks
-        self._best_booster_with_trial_number = None  # type: Optional[Tuple[lgb.Booster, int]]
+        self._best_booster_with_trial_number = (
+            None
+        )  # type: Optional[Tuple[Union[lgb.Booster, lgb.CVBooster], int]]
         self._model_dir = model_dir
 
         # Should not alter data since `min_data_in_leaf` is tuned.
