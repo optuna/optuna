67dde30fecde8e5e59426da3ecf2653532707543
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2020-10-06 19:42:12 +0900

    Update installation of horovod.

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ def get_extras_require() -> Dict[str, List[str]]:
         "example": [
             "catboost",
             "chainer",
+            "horovod[pytorch]",
             "lightgbm",
             "mlflow",
             "mpi4py",
@@ -117,7 +118,6 @@ def get_extras_require() -> Dict[str, List[str]]:
             "chainer>=5.0.0",
             "cma",
             "fakeredis",
-            "horovod[tensorflow,keras,pytorch,mxnet]",
             "lightgbm",
             "matplotlib>=3.0.0",
             "mlflow",
@@ -157,7 +157,6 @@ def get_extras_require() -> Dict[str, List[str]]:
             # https://github.com/optuna/optuna/issues/1000.
             "chainer>=5.0.0",
             "cma",
-            "horovod[tensorflow,keras,pytorch,mxnet]",
             "lightgbm",
             "mlflow",
             "mpi4py",
