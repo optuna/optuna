2c5497d2e01c5ef65b627fe142c1696dafa83d6b
Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>
2020-10-12 15:52:21 +0900

    Update optuna/_dataframe.py
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/_dataframe.py
+++ optuna/_dataframe.py
@@ -11,7 +11,7 @@ from optuna.trial._state import TrialState
 
 
 with try_import() as _imports:
-    # `trials_dataframe` is disabled if pandas is not available.
+    # `Study.trials_dataframe` is disabled if pandas is not available.
     import pandas as pd  # NOQA
 
 # Required for type annotation in `Study.trials_dataframe`.
