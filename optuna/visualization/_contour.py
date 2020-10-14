772c62dd0006ad09999d77f1fad368da9df49209
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-10-14 22:23:56 +0800

    Removed redundant import

--- optuna/visualization/_contour.py
+++ optuna/visualization/_contour.py
@@ -4,7 +4,6 @@ from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
-from typing import Union
 
 from optuna._study_direction import StudyDirection
 from optuna.logging import get_logger
