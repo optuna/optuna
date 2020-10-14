e3d914ce5a5e89cb77105289db4880ca55e241ca
carefree0910 <syameimaru_kurumi@pku.edu.cn>
2020-10-14 12:44:46 +0800

    Followed `isort`

--- optuna/visualization/_contour.py
+++ optuna/visualization/_contour.py
@@ -1,10 +1,10 @@
 import math
+from typing import Any
 from typing import Dict
 from typing import List
 from typing import Optional
 from typing import Tuple
 from typing import Union
-from typing import Any
 
 from optuna._study_direction import StudyDirection
 from optuna.logging import get_logger
