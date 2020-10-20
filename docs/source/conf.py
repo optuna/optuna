3a44e3a23d4e17b4834c28b06a0c444ba7e975ff
harupy <17039389+harupy@users.noreply.github.com>
2020-10-20 09:04:35 +0900

    fix extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ extensions = [
     'cliff.sphinxext',
     'sphinx_gallery.gen_gallery',
     'matplotlib.sphinxext.plot_directive',
-    'plotly_directive',
+    'sphinx_plotly_directive',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
