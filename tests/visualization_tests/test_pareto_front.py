4440ffc234595109bff6d2dbb4e897fe0123862b
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-10-21 02:13:53 +0900

    Add tests for visualization.plot_pareto_front()

---
+++ tests/visualization_tests/test_pareto_front.py
@@ -0,0 +1,144 @@
+import pytest
+
+import optuna
+from optuna.visualization import plot_pareto_front
+
+
+@pytest.mark.parametrize("include_dominated_trials", [False, True])
+def test_plot_pareto_front_2d(include_dominated_trials: bool) -> None:
+    # Test with no trial.
+    study = optuna.create_study(direction=["minimize", "minimize"])
+    figure = plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+    assert len(figure.data) == 1
+    assert figure.data[0]["x"] == ()
+    assert figure.data[0]["y"] == ()
+
+    # Test with three trials.
+    study.enqueue_trial({"x": 1, "y": 1})
+    study.enqueue_trial({"x": 1, "y": 0})
+    study.enqueue_trial({"x": 0, "y": 1})
+    study.optimize(lambda t: [t.suggest_int("x", 0, 1), t.suggest_int("y", 0, 1)], n_trials=3)
+
+    figure = plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+    assert len(figure.data) == 1
+    if include_dominated_trials:
+        # The last elements come from dominated trial that is enqueued firstly.
+        assert figure.data[0]["x"] == (1, 0, 1)
+        assert figure.data[0]["y"] == (0, 1, 1)
+    else:
+        assert figure.data[0]["x"] == (1, 0)
+        assert figure.data[0]["y"] == (0, 1)
+
+    assert figure.layout.xaxis.title.text == "Objective 0"
+    assert figure.layout.yaxis.title.text == "Objective 1"
+
+    # Test with `names` argument.
+    with pytest.raises(ValueError):
+        plot_pareto_front(study, names=[], include_dominated_trials=include_dominated_trials)
+
+    with pytest.raises(ValueError):
+        plot_pareto_front(study, names=["Foo"], include_dominated_trials=include_dominated_trials)
+
+    with pytest.raises(ValueError):
+        plot_pareto_front(
+            study,
+            names=["Foo", "Bar", "Baz"],
+            include_dominated_trials=include_dominated_trials,
+        )
+
+    figure = plot_pareto_front(
+        study,
+        names=["Foo", "Bar"],
+        include_dominated_trials=include_dominated_trials,
+    )
+    assert figure.layout.xaxis.title.text == "Foo"
+    assert figure.layout.yaxis.title.text == "Bar"
+
+
+@pytest.mark.parametrize("include_dominated_trials", [False, True])
+def test_plot_pareto_front_3d(include_dominated_trials: bool) -> None:
+    # Test with no trial.
+    study = optuna.create_study(direction=["minimize", "minimize", "minimize"])
+    figure = plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+    assert len(figure.data) == 1
+    assert figure.data[0]["x"] == ()
+    assert figure.data[0]["y"] == ()
+    assert figure.data[0]["z"] == ()
+
+    # Test with three trials.
+    study.enqueue_trial({"x": 1, "y": 1, "z": 1})
+    study.enqueue_trial({"x": 1, "y": 0, "z": 1})
+    study.enqueue_trial({"x": 1, "y": 1, "z": 0})
+    study.optimize(
+        lambda t: [t.suggest_int("x", 0, 1), t.suggest_int("y", 0, 1), t.suggest_int("z", 0, 1)],
+        n_trials=3,
+    )
+
+    figure = plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+    assert len(figure.data) == 1
+    if include_dominated_trials:
+        # The last elements come from dominated trial that is enqueued firstly.
+        assert figure.data[0]["x"] == (1, 1, 1)
+        assert figure.data[0]["y"] == (0, 1, 1)
+        assert figure.data[0]["z"] == (1, 0, 1)
+    else:
+        assert figure.data[0]["x"] == (1, 1)
+        assert figure.data[0]["y"] == (0, 1)
+        assert figure.data[0]["z"] == (1, 0)
+
+    assert figure.layout.scene.xaxis.title.text == "Objective 0"
+    assert figure.layout.scene.yaxis.title.text == "Objective 1"
+    assert figure.layout.scene.zaxis.title.text == "Objective 2"
+
+    # Test with `names` argument.
+    with pytest.raises(ValueError):
+        plot_pareto_front(study, names=[], include_dominated_trials=include_dominated_trials)
+
+    with pytest.raises(ValueError):
+        plot_pareto_front(study, names=["Foo"], include_dominated_trials=include_dominated_trials)
+
+    with pytest.raises(ValueError):
+        plot_pareto_front(
+            study,
+            names=["Foo", "Bar"],
+            include_dominated_trials=include_dominated_trials,
+        )
+
+    with pytest.raises(ValueError):
+        plot_pareto_front(
+            study,
+            names=["Foo", "Bar", "Baz", "Qux"],
+            include_dominated_trials=include_dominated_trials,
+        )
+
+    figure = plot_pareto_front(study, names=["Foo", "Bar", "Baz"])
+    assert figure.layout.scene.xaxis.title.text == "Foo"
+    assert figure.layout.scene.yaxis.title.text == "Bar"
+    assert figure.layout.scene.zaxis.title.text == "Baz"
+
+
+@pytest.mark.parametrize("include_dominated_trials", [False, True])
+def test_plot_pareto_front_unsupported_dimensions(include_dominated_trials: bool) -> None:
+    # Unsupported: n_objectives == 1.
+    with pytest.raises(ValueError):
+        study = optuna.create_study(direction=["minimize"])
+        study.optimize(lambda t: [0], n_trials=1)
+        plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+
+    # Supported: n_objectives == 2.
+    study = optuna.create_study(direction=["minimize", "minimize"])
+    study.optimize(lambda t: [0, 0], n_trials=1)
+    plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+
+    # Supported: n_objectives == 3.
+    study = optuna.create_study(direction=["minimize", "minimize", "minimize"])
+    study.optimize(lambda t: [0, 0, 0], n_trials=1)
+    plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
+
+    # Unsupported: n_objectives == 4.
+    with pytest.raises(ValueError):
+        study = optuna.create_study(direction=
+            ["minimize", "minimize", "minimize", "minimize"]
+        )
+        study.optimize(lambda t: [0, 0, 0, 0], n_trials=1)
+        plot_pareto_front(study, include_dominated_trials=include_dominated_trials)
