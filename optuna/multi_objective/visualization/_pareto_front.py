3627dcaff25812a639a26fbd5deedb3b99137945
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-10-21 00:45:39 +0900

    Revert multi_objective/visualization

--- optuna/multi_objective/visualization/_pareto_front.py
+++ optuna/multi_objective/visualization/_pareto_front.py
@@ -1,7 +1,6 @@
 import json
 from typing import List
 from typing import Optional
-from typing import Union
 
 import optuna
 from optuna import multi_objective
@@ -22,7 +21,7 @@ _logger = optuna.logging.get_logger(__name__)
 @deprecated("2.3.0", "3.0.0")
 @experimental("2.0.0")
 def plot_pareto_front(
-    study: Union["optuna.Study", MultiObjectiveStudy],
+    study:  MultiObjectiveStudy,
     names: Optional[List[str]] = None,
     include_dominated_trials: bool = False,
 ) -> "go.Figure":
@@ -86,11 +85,9 @@ def plot_pareto_front(
 
 
 def _get_non_pareto_front_trials(
-    study: Union["optuna.Study", MultiObjectiveStudy],
-    pareto_trials: List[
-        Union["optuna.trial.FrozenTrial", "multi_objective.trial.FrozenMultiObjectiveTrial"]
-    ],
-) -> List[Union["optuna.trial.FrozenTrial", "multi_objective.trial.FrozenMultiObjectiveTrial"]]:
+    study: MultiObjectiveStudy,
+    pareto_trials: List["multi_objective.trial.FrozenMultiObjectiveTrial"],
+) -> List["multi_objective.trial.FrozenMultiObjectiveTrial"]:
 
     non_pareto_trials = []
     for trial in study.get_trials():
@@ -100,9 +97,7 @@ def _get_non_pareto_front_trials(
 
 
 def _get_pareto_front_2d(
-    study: Union["optuna.Study", MultiObjectiveStudy],
-    names: Optional[List[str]],
-    include_dominated_trials: bool = False,
+    study: MultiObjectiveStudy, names: Optional[List[str]], include_dominated_trials: bool = False,
 ) -> "go.Figure":
     if names is None:
         names = ["Objective 0", "Objective 1"]
@@ -119,21 +114,9 @@ def _get_pareto_front_2d(
         point_colors += ["red"] * len(non_pareto_trials)
         trials += non_pareto_trials
 
-    value0 = []
-    value1 = []
-    for t in trials:
-        if isinstance(t, FrozenMultiObjectiveTrial):  # Backwards compatibility.
-            v0 = t.values[0]
-            v1 = t.values[1]
-        else:
-            v0 = t.value[0]
-            v1 = t.value[1]
-        value0.append(v0)
-        value1.append(v1)
-
     data = go.Scatter(
-        x=value0,
-        y=value1,
+        x=[t.values[0] for t in trials],
+        y=[t.values[1] for t in trials],
         text=[_make_hovertext(t) for t in trials],
         mode="markers",
         hovertemplate="%{text}<extra></extra>",
@@ -161,26 +144,10 @@ def _get_pareto_front_3d(
         point_colors += ["red"] * len(non_pareto_trials)
         trials += non_pareto_trials
 
-    value0 = []
-    value1 = []
-    value2 = []
-    for t in trials:
-        if isinstance(t, FrozenMultiObjectiveTrial):  # Backwards compatibility.
-            v0 = t.values[0]
-            v1 = t.values[1]
-            v2 = t.values[2]
-        else:
-            v0 = t.value[0]
-            v1 = t.value[1]
-            v2 = t.value[2]
-        value0.append(v0)
-        value1.append(v1)
-        value2.append(v2)
-
     data = go.Scatter3d(
-        x=value0,
-        y=value1,
-        z=value2,
+        x=[t.values[0] for t in trials],
+        y=[t.values[1] for t in trials],
+        z=[t.values[2] for t in trials],
         text=[_make_hovertext(t) for t in trials],
         mode="markers",
         hovertemplate="%{text}<extra></extra>",
@@ -193,11 +160,8 @@ def _get_pareto_front_3d(
     return go.Figure(data=data, layout=layout)
 
 
-def _make_hovertext(trial: Union["optuna.trial.FrozenTrial", FrozenMultiObjectiveTrial]) -> str:
-    if isinstance(trial, FrozenMultiObjectiveTrial):  # Backwards compatibility.
-        value = trial.values
-    else:
-        value = trial.value
-
-    text = json.dumps({"number": trial.number, "values": value, "params": trial.params}, indent=2)
+def _make_hovertext(trial: FrozenMultiObjectiveTrial) -> str:
+    text = json.dumps(
+        {"number": trial.number, "values": trial.values, "params": trial.params}, indent=2
+    )
     return text.replace("\n", "<br>")
