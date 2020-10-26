fd6cf7204a083cb564e77b6135e12c1971073921
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-10-26 16:06:32 +0900

    Fix

--- optuna/samplers/__init__.py
+++ optuna/samplers/__init__.py
@@ -5,4 +5,4 @@ from optuna.samplers._random import RandomSampler  # NOQA
 from optuna.samplers._search_space import intersection_search_space  # NOQA
 from optuna.samplers._search_space import IntersectionSearchSpace  # NOQA
 from optuna.samplers._tpe.sampler import TPESampler  # NOQA
-from optuna.samplers.multi_objective  # NOQA
+from optuna.samplers import multi_objective  # NOQA
