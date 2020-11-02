f699fed3528dbfe4a09f522eff17f139d45c30d6
Crissman <crissman@preferred.jp>
2020-11-02 10:26:17 +0900

    black

--- examples/gluon_simple.py
+++ examples/gluon_simple.py
@@ -30,36 +30,50 @@ import optuna
 
 # Parse CLI arguments
 
-parser = argparse.ArgumentParser(description='MXNet Gluon MNIST Example')
-parser.add_argument('--batch-size', type=int, default=100,
-                    help='batch size for training and testing (default: 100)')
-parser.add_argument('--epochs', type=int, default=10,
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--cuda', action='store_true', default=False,
-                    help='Train on GPU with CUDA')
-parser.add_argument('--log-interval', type=int, default=100, metavar='N',
-                    help='how many batches to wait before logging training status')
+parser = argparse.ArgumentParser(description="MXNet Gluon MNIST Example")
+parser.add_argument(
+    "--batch-size",
+    type=int,
+    default=100,
+    help="batch size for training and testing (default: 100)",
+)
+parser.add_argument(
+    "--epochs", type=int, default=10, help="number of epochs to train (default: 10)"
+)
+parser.add_argument("--cuda", action="store_true", default=False, help="Train on GPU with CUDA")
+parser.add_argument(
+    "--log-interval",
+    type=int,
+    default=100,
+    metavar="N",
+    help="how many batches to wait before logging training status",
+)
 opt = parser.parse_args()
 
 
 # define network
+
 
 def network(trial):
     net = nn.Sequential()
     n_layers = trial.suggest_int("n_layers", 1, 3)
     for i in range(n_layers):
         nodes = trial.suggest_int("n_units_l{}".format(i), 4, 128)
-        net.add(nn.Dense(nodes, activation='relu'))
+        net.add(nn.Dense(nodes, activation="relu"))
     net.add(nn.Dense(10))
     return net
 
+
 # data
 
+
 def transformer(data, label):
-    data = data.reshape((-1,)).astype(np.float32)/255
+    data = data.reshape((-1,)).astype(np.float32) / 255
     return data, label
 
+
 # train
+
 
 def test(ctx, val_data, net):
     metric = mx.metric.Accuracy()
@@ -80,23 +94,26 @@ def objective(trial):
     epochs = opt.epochs
 
     train_data = gluon.data.DataLoader(
-        gluon.data.vision.MNIST('./data', train=True).transform(transformer),
-        batch_size=opt.batch_size, shuffle=True, last_batch='discard')
+        gluon.data.vision.MNIST("./data", train=True).transform(transformer),
+        batch_size=opt.batch_size,
+        shuffle=True,
+        last_batch="discard",
+    )
 
     val_data = gluon.data.DataLoader(
-        gluon.data.vision.MNIST('./data', train=False).transform(transformer),
-        batch_size=opt.batch_size, shuffle=False)
+        gluon.data.vision.MNIST("./data", train=False).transform(transformer),
+        batch_size=opt.batch_size,
+        shuffle=False,
+    )
 
     net = network(trial)
 
     # Collect all parameters from net and its children, then initialize them.
     net.initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
-    optimizer_name = trial.suggest_categorical("optimizer",
-                                               ["Adam", "RMSprop", "SGD"])
+    optimizer_name = trial.suggest_categorical("optimizer", ["Adam", "RMSprop", "SGD"])
     # Trainer is for updating parameters with gradient.
-    lr = trial.suggest_float('lr', 1e-5, 1e-1, log=True)
-    trainer = gluon.Trainer(net.collect_params(), optimizer_name,
-                            {'learning_rate': lr})
+    lr = trial.suggest_float("lr", 1e-5, 1e-1, log=True)
+    trainer = gluon.Trainer(net.collect_params(), optimizer_name, {"learning_rate": lr})
     metric = mx.metric.Accuracy()
     loss = gluon.loss.SoftmaxCrossEntropyLoss()
 
@@ -120,13 +137,13 @@ def objective(trial):
 
             if i % opt.log_interval == 0 and i > 0:
                 name, acc = metric.get()
-                print('[Epoch %d Batch %d] Training: %s=%f'%(epoch, i, name, acc))
+                print("[Epoch %d Batch %d] Training: %s=%f" % (epoch, i, name, acc))
 
         name, acc = metric.get()
-        print('[Epoch %d] Training: %s=%f'%(epoch, name, acc))
+        print("[Epoch %d] Training: %s=%f" % (epoch, name, acc))
 
         name, val_acc = test(ctx, val_data, net)
-        print('[Epoch %d] Validation: %s=%f'%(epoch, name, val_acc))
+        print("[Epoch %d] Validation: %s=%f" % (epoch, name, val_acc))
 
         trial.report(val_acc, epoch)
 
@@ -134,7 +151,7 @@ def objective(trial):
         if trial.should_prune():
             raise optuna.exceptions.TrialPruned()
 
-    net.save_parameters('mnist.params')
+    net.save_parameters("mnist.params")
 
     return val_acc
 
