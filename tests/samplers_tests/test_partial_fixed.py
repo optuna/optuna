bc0cd6d779e4266513d85fed225cb3f93c5f60f1
norihitoishida <norihitoishida@gmail.com>
2020-11-04 19:52:29 +0900

    fix

--- tests/samplers_tests/test_partial_fixed.py
+++ tests/samplers_tests/test_partial_fixed.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import cast
 from unittest.mock import patch
 import warnings
 
@@ -10,10 +10,10 @@ from optuna.samplers import RandomSampler
 from optuna.trial import Trial
 
 
-def test_params_identity() -> None:
+def test_fixed_sampling() -> None:
     def objective(trial: Trial) -> float:
-        x = trial.suggest_uniform("x", -10, 10)
-        y = trial.suggest_uniform("y", -10, 10)
+        x = trial.suggest_float("x", -10, 10)
+        y = trial.suggest_float("y", -10, 10)
         return x ** 2 + y ** 2
 
     study0 = optuna.create_study()
@@ -29,21 +29,23 @@ def test_params_identity() -> None:
             fixed_params={"y": 0}, base_sampler=RandomSampler(seed=42)
         )
     study1.optimize(objective, n_trials=1)
+
     x_sampled1 = study1.trials[0].params["x"]
     y_sampled1 = study1.trials[0].params["y"]
     assert x_sampled1 == x_sampled0
     assert y_sampled1 == 0
 
 
-@pytest.mark.parametrize("fixed_y", [0.5, 5])
-def test_incompatible_types(fixed_y: Union[float, int]) -> None:
+def test_float_to_int() -> None:
     def objective(trial: Trial) -> float:
         x = trial.suggest_int("x", -10, 10)
         y = trial.suggest_int("y", -10, 10)
         return x ** 2 + y ** 2
 
+    fixed_y = 0.5
+
     # Parameters of Int-type-distribution are rounded to int-type,
-    # even if they are specified as float-type.
+    # even if they are defined as float-type.
     study = optuna.create_study()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", optuna.exceptions.ExperimentalWarning)
@@ -55,14 +57,14 @@ def test_incompatible_types(fixed_y: Union[float, int]) -> None:
 
 
 @pytest.mark.parametrize("fixed_y", [-2, 2])
-def test_out_of_the_range(fixed_y: int) -> None:
+def test_out_of_the_range_numerical(fixed_y: int) -> None:
     def objective(trial: Trial) -> float:
         x = trial.suggest_int("x", -1, 1)
         y = trial.suggest_int("y", -1, 1)
         return x ** 2 + y ** 2
 
-    # It is possible to fix parameters as out-of-the-range value.
-    # Userwarnings will occur.
+    # It is possible to fix numerical parameters as out-of-the-range value.
+    # `UserWarning` will occur.
     study = optuna.create_study()
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", optuna.exceptions.ExperimentalWarning)
@@ -74,6 +76,27 @@ def test_out_of_the_range(fixed_y: int) -> None:
     assert study.trials[0].params["y"] == fixed_y
 
 
+def test_out_of_the_range_categorical() -> None:
+    def objective(trial: Trial) -> float:
+        x = trial.suggest_int("x", -1, 1)
+        y = trial.suggest_categorical("y", [-1, 0, 1])
+        y = cast(int, y)
+        return x ** 2 + y ** 2
+
+    fixed_y = 2
+
+    # It isn't possible to fix categorical parameters as out-of-the-range value.
+    # `ValueError` will occur.
+    study = optuna.create_study()
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", optuna.exceptions.ExperimentalWarning)
+        study.sampler = PartialFixedSampler(
+            fixed_params={"y": fixed_y}, base_sampler=study.sampler
+        )
+    with pytest.raises(ValueError):
+        study.optimize(objective, n_trials=1)
+
+
 def test_partial_fixed_experimental_warning() -> None:
     study = optuna.create_study()
     with pytest.warns(optuna.exceptions.ExperimentalWarning):
@@ -81,14 +104,14 @@ def test_partial_fixed_experimental_warning() -> None:
 
 
 def test_reseed_rng() -> None:
-    study = optuna.create_study()
+    base_sampler = RandomSampler()
+    study = optuna.create_study(sampler=base_sampler)
     with warnings.catch_warnings():
         warnings.simplefilter("ignore", optuna.exceptions.ExperimentalWarning)
         sampler = PartialFixedSampler(fixed_params={"x": 0}, base_sampler=study.sampler)
-    sampler.reseed_rng()
-    original_seed = sampler._rng.seed
+    original_seed = base_sampler._rng.seed
 
-    with patch.object(sampler, "reseed_rng", wraps=sampler.reseed_rng) as mock_object:
+    with patch.object(base_sampler, "reseed_rng", wraps=base_sampler.reseed_rng) as mock_object:
         sampler.reseed_rng()
         assert mock_object.call_count == 1
-        assert original_seed != sampler._rng.seed
+        assert original_seed != base_sampler._rng.seed
