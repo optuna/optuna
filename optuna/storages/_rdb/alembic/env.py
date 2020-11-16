724e88d4123d152781e267df0069af595239b7a3
HideakiImamura <imamura@ms.k.u-tokyo.ac.jp>
2020-11-17 02:14:12 +0900

    Fix alembic/env.py and unittest

--- optuna/storages/_rdb/alembic/env.py
+++ optuna/storages/_rdb/alembic/env.py
@@ -63,7 +63,11 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
