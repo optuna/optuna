f90b88b13bce8f6b04fc288b344b8d95ad92a45d
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2020-12-12 05:20:07 +0900

    Add example

--- optuna/visualization/matplotlib/_slice.py
+++ optuna/visualization/matplotlib/_slice.py
@@ -31,6 +31,27 @@ def plot_slice(study: Study, params: Optional[List[str]] = None) -> "Axes":
     .. seealso::
         Please refer to :func:`optuna.visualization.plot_slice` for an example.
 
+    Example:
+
+        The following code snippet shows how to plot the parameter relationship as slice plot.
+
+        .. plot::
+
+            import optuna
+
+
+            def objective(trial):
+                x = trial.suggest_uniform("x", -100, 100)
+                y = trial.suggest_categorical("y", [-1, 0, 1])
+                return x ** 2 + y
+
+
+            sampler = optuna.samplers.TPESampler(seed=10)
+            study = optuna.create_study(sampler=sampler)
+            study.optimize(objective, n_trials=10)
+
+            optuna.visualization.matplotlib.plot_slice(study, params=["x", "y"])
+
     Args:
         study:
             A :class:`~optuna.study.Study` object whose trials are plotted for their objective
