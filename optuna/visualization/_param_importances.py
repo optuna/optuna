6bb1e6b34e111b89413fa0917ff46dcdb9a37d28
nyanhi <ksuzuki19937.mm@gmail.com>
2020-12-25 10:28:33 +0900

    found more len(study.directions)

--- optuna/visualization/_param_importances.py
+++ optuna/visualization/_param_importances.py
@@ -106,7 +106,7 @@ def plot_param_importances(
 
     _imports.check()
 
-    if target is None and len(study.directions) > 1:
+    if target is None and study._is_multi_objective():
         raise ValueError(
             "If the `study` is being used for multi-objective optimization, "
             "please specify the `target`."
