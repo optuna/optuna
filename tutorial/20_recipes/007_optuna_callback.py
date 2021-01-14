fc1472625434d9c40d54ecf6cdc0517c4e1c6c5a
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2021-01-14 18:52:34 +0900

    update 007

--- tutorial/20_recipes/007_optuna_callback.py
+++ tutorial/20_recipes/007_optuna_callback.py
@@ -5,12 +5,13 @@ Callback for Study.optimize
 ===========================
 
 This tutorial showcases how to use & implement Optuna ``Callback`` for :func:`~optuna.study.Study.optimize`.
-By a callback, we mean a callable that takes :class:`~optuna.study.Study` and :class:`~optuna.trial.FrozenTrial` as arguments, and does some work.
 
-Note that ``callback`` in this tutorial and ``*PruningCallback``'s of :mod:`optuna.integration` are completely different.
+``Callback`` is called after the every evaluation of ``objective``, and
+it takes :class:`~optuna.study.Study` and :class:`~optuna.trial.FrozenTrial` as arguments, and does some work.
+
+:class:`~optuna.integration.MLflowCallback` is a great example.
 """
 
-from dataclasses import dataclass
 import logging
 import sys
 
@@ -20,28 +21,32 @@ import optuna
 optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
 
 ###################################################################################################
-# Stop optimization upon condition is met.
-# ----------------------------------------------------
+# Stop optimization after some trials are pruned in a row
+# -------------------------------------------------------
 #
-# This example implements a stateful callback which stops the optimization if some trials are pruned in a row.
+# This example implements a stateful callback which stops the optimization
+# if certain number of trials are pruned in a row.
+# The number of trials pruned in a row is specified by ``threshold``.
 
 
-@dataclass
 class StopWhenTrialKeepBeingPrunedCallback:
 
-    threshold: int
-    n_consequtive_pruned: int = 0
+    def __init__(self, threshold: int):
+        self.threshold = threshold
+        self._consequtive_pruned_count = 0
 
     def __call__(self, study: optuna.study.Study, trial: optuna.trial.FrozenTrial) -> None:
         if trial.state == optuna.trial.TrialState.PRUNED:
-            self.n_consequtive_pruned += 1
+            self._consequtive_pruned_count += 1
         else:
-            self.n_consequtive_pruned = 0
+            self._consequtive_pruned_count = 0
 
-        if self.n_consequtive_pruned >= self.threshold:
+        if self._consequtive_pruned_count >= self.threshold:
             study.stop()
 
 
+###################################################################################################
+# This objective prunes all the trials except for the first 5 trials (``trial.number`` starts with 0).
 def objective(trial):
     if trial.number > 4:
         raise optuna.TrialPruned
@@ -49,5 +54,13 @@ def objective(trial):
     return trial.suggest_float("x", 0, 1)
 
 
+###################################################################################################
+# Here, we set threshold to ``2``: optimization finishes once two trials are pruned in a row.
+# So, we expect this study to stop after 7 trials.
+study_stop_cb = StopWhenTrialKeepBeingPrunedCallback(2)
 study = optuna.create_study()
-study.optimize(objective, n_trials=10, callbacks=[StopWhenTrialKeepBeingPrunedCallback(2)])
+study.optimize(objective, n_trials=10, callbacks=[study_stop_cb])
+
+
+###################################################################################################
+# As you can see in the log above, the study stopped after 7 trials as expected.
