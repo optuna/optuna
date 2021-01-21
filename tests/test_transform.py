9493377de90aa93506fdf41d4dbeb854db0ccd0d
Crissman Loomis <crissman@preferred.jp>
2021-01-21 15:23:22 +0900

    Update tests/test_transform.py
    Co-authored-by: Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -95,9 +95,7 @@ def test_search_space_transform_numerical(
     expected_low = distribution.low  # type: ignore
     expected_high = distribution.high  # type: ignore
 
-    if isinstance(distribution, UniformDistribution) or isinstance(
-        distribution, LogUniformDistribution
-    ):
+    if isinstance(distribution, (UniformDistribution, LogUniformDistribution)):
         expected_high = numpy.nextafter(expected_high, expected_high - 1)
 
     if isinstance(distribution, LogUniformDistribution):
