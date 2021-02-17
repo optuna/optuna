63bd3105d5ce29d74f15093d894b9ea1e9ee4666
Tim Wolff-Piggott <twolffpiggott@gmail.com>
2021-02-17 11:24:24 +0200

    update check_study.sh to match whole words

--- examples/kubernetes/mlflow/check_study.sh
+++ examples/kubernetes/mlflow/check_study.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-optuna studies --storage "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}" | grep k8s_mlflow > /dev/null
+optuna studies --storage "postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@postgres:5432/${POSTGRES_DB}" | grep -w k8s_mlflow > /dev/null
 echo $?
