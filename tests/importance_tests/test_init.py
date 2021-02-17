1b8f8bf6049f2c336995103a0c62979787ae8d98
tohmae <t.ohmae.nx01@gmail.com>
2021-02-17 18:29:07 +0900

    modify test

--- tests/importance_tests/test_init.py
+++ tests/importance_tests/test_init.py
@@ -8,10 +8,17 @@ import pytest
 
 import optuna
 from optuna import samplers
+from optuna.distributions import CategoricalDistribution
+from optuna.distributions import DiscreteUniformDistribution
+from optuna.distributions import IntLogUniformDistribution
+from optuna.distributions import IntUniformDistribution
+from optuna.distributions import LogUniformDistribution
+from optuna.distributions import UniformDistribution
 from optuna.importance import BaseImportanceEvaluator
 from optuna.importance import FanovaImportanceEvaluator
 from optuna.importance import get_param_importances
 from optuna.importance import MeanDecreaseImpurityImportanceEvaluator
+from optuna.importance._base import _get_distributions
 from optuna.study import create_study
 from optuna.testing.storage import STORAGE_MODES
 from optuna.testing.storage import StorageSupplier
@@ -21,6 +28,149 @@ from optuna.trial import Trial
 parametrize_evaluator = pytest.mark.parametrize(
     "evaluator_init_func", [MeanDecreaseImpurityImportanceEvaluator, FanovaImportanceEvaluator]
 )
+
+
+@parametrize_storage
+def test_get_distributions(
+    storage_init_func: Callable[[], storages.BaseStorage],
+) -> None:
+    def objective(trial: Trial) -> float:
+        x1 = trial.suggest_uniform("x1", 0.1, 3)
+        x2 = trial.suggest_loguniform("x2", 0.1, 3)
+        x3 = trial.suggest_discrete_uniform("x3", 0, 3, 1)
+        x4 = trial.suggest_int("x4", -3, 3)
+        x5 = trial.suggest_int("x5", 1, 5, log=True)
+        x6 = trial.suggest_categorical("x6", [1.0, 1.1, 1.2])
+        if trial.number % 2 == 0:
+            # Conditional parameters are ignored unless `params` is specified and is not `None`.
+            x7 = trial.suggest_uniform("x7", 0.1, 3)
+
+        assert isinstance(x6, float)
+        value = x1 ** 4 + x2 + x3 - x4 ** 2 - x5 + x6
+        if trial.number % 2 == 0:
+            value += x7
+        return value
+
+    study = create_study(storage_init_func(), sampler=samplers.RandomSampler())
+    study.optimize(objective, n_trials=3)
+
+    param_distributions = _get_distributions(study, None)
+    _param_distributions = OrderedDict(
+        [
+            ("x1", UniformDistribution(high=3, low=0.1)),
+            ("x2", LogUniformDistribution(high=3, low=0.1)),
+            ("x3", DiscreteUniformDistribution(high=3, low=0, q=1)),
+            ("x4", IntUniformDistribution(high=3, low=-3, step=1)),
+            ("x5", IntLogUniformDistribution(high=5, low=1, step=1)),
+            ("x6", CategoricalDistribution(choices=(1.0, 1.1, 1.2))),
+        ]
+    )
+
+    assert isinstance(param_distributions, OrderedDict)
+    assert len(param_distributions) == 6
+    assert param_distributions == _param_distributions
+
+
+@parametrize_storage
+@pytest.mark.parametrize("params", [[], ["x1"], ["x1", "x3"], ["x1", "x4"]])
+def test_get_disributions_with_params(
+    storage_init_func: Callable[[], storages.BaseStorage],
+    params: List[str],
+) -> None:
+    def objective(trial: Trial) -> float:
+        x1 = trial.suggest_uniform("x1", 0.1, 3)
+        x2 = trial.suggest_loguniform("x2", 0.1, 3)
+        x3 = trial.suggest_discrete_uniform("x3", 0, 3, 1)
+        if trial.number % 2 == 0:
+            x4 = trial.suggest_uniform("x4", 0.1, 3)
+
+        value = x1 ** 4 + x2 + x3
+        if trial.number % 2 == 0:
+            value += x4
+        return value
+
+    study = create_study(storage_init_func())
+    study.optimize(objective, n_trials=10)
+
+    param_distributions = _get_distributions(study, params)
+    assert isinstance(param_distributions, OrderedDict)
+    assert len(param_distributions) == len(params)
+    assert all(param in param_distributions for param in params)
+    for param_name, distribution in param_distributions.items():
+        assert isinstance(param_name, str)
+
+
+def test_get_distributions_dynamic_search_space_params() -> None:
+    def objective(trial: Trial) -> float:
+        x1 = trial.suggest_uniform("x1", 0.1 + trial.number, 10.0)
+        x2 = trial.suggest_loguniform("x2", 0.1, trial.number + 3)
+        x3 = trial.suggest_discrete_uniform("x3", 0, 3, 1)
+        x4 = trial.suggest_int("x4", -3, 3)
+        x5 = trial.suggest_int("x5", 1, 5, log=True)
+        x6 = trial.suggest_categorical("x6", [1.0, 1.1, 1.2])
+        if trial.number % 2 == 0:
+            # Conditional parameters are ignored unless `params` is specified and is not `None`.
+            x7 = trial.suggest_uniform("x7", 0.1, 3)
+
+        assert isinstance(x6, float)
+        value = x1 ** 4 + x2 + x3 - x4 ** 2 - x5 + x6
+        if trial.number % 2 == 0:
+            value += x7
+        return value
+
+    study = create_study()
+    study.optimize(objective, n_trials=3)
+
+    param_distributions = _get_distributions(study, None)
+    params = ["x1", "x2", "x3", "x4", "x5", "x6"]
+    assert isinstance(param_distributions, OrderedDict)
+    assert len(param_distributions) == len(params)
+    assert all(param in param_distributions for param in params)
+    for param_name, distribution in param_distributions.items():
+        assert isinstance(param_name, str)
+        #        assert isinstance(distribution, BaseDistribution)
+        if param_name == "x1":
+            assert distribution.low == 0.1
+            assert distribution.high == 10.0
+        if param_name == "x2":
+            assert distribution.low == 0.1
+            assert distribution.high == 5
+
+
+@pytest.mark.parametrize("params", [[], ["x1"], ["x1", "x3"], ["x1", "x4"]])
+def test_get_distributions_dynamic_search_space_params_with_params(
+    params: List[str],
+) -> None:
+    def objective(trial: Trial) -> float:
+        x1 = trial.suggest_uniform("x1", 0.1 + trial.number, 10.0 + trial.number)
+        x2 = trial.suggest_loguniform("x2", 0.1, trial.number + 3)
+        x3 = trial.suggest_discrete_uniform("x3", 0, 3, 1)
+        if trial.number % 2 == 0:
+            x4 = trial.suggest_uniform("x4", 0.1, 3)
+
+        value = x1 ** 4 + x2 + x3
+        if trial.number % 2 == 0:
+            value += x4
+        return value
+
+    study = create_study()
+    study.optimize(objective, n_trials=10)
+
+    param_distributions = _get_distributions(study, params)
+    assert isinstance(param_distributions, OrderedDict)
+    assert len(param_distributions) == len(params)
+    assert all(param in param_distributions for param in params)
+    for param_name, distribution in param_distributions.items():
+        assert isinstance(param_name, str)
+        if param_name == "x1":
+            assert distribution.low == 0.1
+            if "x4" in params:
+                assert distribution.high == 18.0
+            else:
+                assert distribution.high == 19.0
+        if param_name == "x2":
+            assert distribution.low == 0.1
+            assert distribution.high == 14
 
 
 @parametrize_evaluator
@@ -82,12 +232,10 @@ def test_get_param_importances(
         study.optimize(objective, n_trials=3)
 
         param_importance = get_param_importances(study, evaluator=evaluator_init_func())
-
+        params = ["x1", "x2", "x3", "x4", "x5", "x6"]
         assert isinstance(param_importance, OrderedDict)
         assert len(param_importance) == 6
-        assert all(
-            param_name in param_importance for param_name in ["x1", "x2", "x3", "x4", "x5", "x6"]
-        )
+        assert all(param in param_importance for param in params)
         prev_importance = float("inf")
         for param_name, importance in param_importance.items():
             assert isinstance(param_name, str)
@@ -162,10 +310,10 @@ def test_get_param_importances_with_target(
             evaluator=evaluator_init_func(),
             target=lambda t: t.params["x1"] + t.params["x2"],
         )
-
+        params = ["x1", "x2", "x3"]
         assert isinstance(param_importance, OrderedDict)
         assert len(param_importance) == 3
-        assert all(param_name in param_importance for param_name in ["x1", "x2", "x3"])
+        assert all(param in param_importance for param in params)
         prev_importance = float("inf")
         for param_name, importance in param_importance.items():
             assert isinstance(param_name, str)
@@ -273,12 +421,10 @@ def test_get_param_importances_dynamic_search_space_params(
     study.optimize(objective, n_trials=3)
 
     param_importance = get_param_importances(study, evaluator=evaluator_init_func())
-
+    params = ["x1", "x2", "x3", "x4", "x5", "x6"]
     assert isinstance(param_importance, OrderedDict)
     assert len(param_importance) == 6
-    assert all(
-        param_name in param_importance for param_name in ["x1", "x2", "x3", "x4", "x5", "x6"]
-    )
+    assert all(param in param_importance for param in params)
     prev_importance = float("inf")
     for param_name, importance in param_importance.items():
         assert isinstance(param_name, str)
