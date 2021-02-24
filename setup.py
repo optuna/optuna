08434e9c67114931d4eea8ba6b9aa3c527377a37
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-02-24 19:12:54 +0900

    Fix `setup.py`

--- setup.py
+++ setup.py
@@ -160,9 +160,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             "torchvision==0.8.2+cpu ; sys_platform!='darwin'",
             "torchaudio==0.7.2",
             "allennlp<2.0.0",
-            # TODO(crcrpar): Support botorch v0.4.0.
-            # See: https://github.com/optuna/optuna/issues/2381
-            "botorch<0.4.0 ; python_version>'3.6'",
+            "botorch>=0.4.0 ; python_version>'3.6'",
             "fastai",
         ],
         "tests": ["fakeredis", "pytest"],
@@ -200,9 +198,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             "torchvision==0.8.2+cpu ; sys_platform!='darwin'",
             "torchaudio==0.7.2",
             "allennlp<2.0.0",
-            # TODO(crcrpar): Support botorch v0.4.0.
-            # See: https://github.com/optuna/optuna/issues/2381
-            "botorch<0.4.0 ; python_version>'3.6'",
+            "botorch>=0.4.0 ; python_version>'3.6'",
             "fastai",
         ],
     }
