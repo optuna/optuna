afa49c2f00e1c084a14f953324b54618b12734ac
Naoto Mizuno <naotomizuno@preferred.jp>
2021-02-24 12:51:51 +0900

    Use get_trials with states

--- examples/pytorch/pytorch_checkpoint.py
+++ examples/pytorch/pytorch_checkpoint.py
@@ -176,8 +176,8 @@ if __name__ == "__main__":
     )
     study.optimize(objective, n_trials=100, timeout=600)
 
-    pruned_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.PRUNED]
-    complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+    pruned_trials = study.get_trials(states=(optuna.trial.TrialState.PRUNED,))
+    complete_trials = study.get_trials(states=(optuna.trial.TrialState.COMPLETE,))
 
     print("Study statistics: ")
     print("  Number of finished trials: ", len(study.trials))
