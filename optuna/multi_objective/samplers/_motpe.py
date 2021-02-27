f729bf59d656feab68c584e2e6061cc3201bd204
nyanhi <ksuzuki19937.mm@gmail.com>
2021-02-27 17:51:30 +0900

    draft - replacing np.append to np.empty

--- optuna/multi_objective/samplers/_motpe.py
+++ optuna/multi_objective/samplers/_motpe.py
@@ -265,16 +265,19 @@ class MOTPEMultiObjectiveSampler(TPESampler, BaseMultiObjectiveSampler):
             assert 0 <= n_below <= len(lvals)
 
             indices = np.array(range(len(lvals)))
-            indices_below = np.array([], dtype=int)
+            indices_below = np.empty(n_below, dtype=int)
 
             # Nondomination rank-based selection
             i = 0
+            last_idx = 0
             while len(indices_below) + sum(nondomination_ranks == i) <= n_below:
-                indices_below = np.append(indices_below, indices[nondomination_ranks == i])
+                length = indices[nondomination_ranks == i].shape
+                indices_below[last_idx: last_idx+length] = indices[nondomination_ranks == i]
+                last_idx += length
                 i += 1
 
             # Hypervolume subset selection problem (HSSP)-based selection
-            subset_size = n_below - len(indices_below)
+            subset_size = n_below - last_idx
             if subset_size > 0:
                 rank_i_lvals = lvals[nondomination_ranks == i]
                 rank_i_indices = indices[nondomination_ranks == i]
@@ -284,8 +287,7 @@ class MOTPEMultiObjectiveSampler(TPESampler, BaseMultiObjectiveSampler):
                 selected_indices = self._solve_hssp(
                     rank_i_lvals, rank_i_indices, subset_size, reference_point
                 )
-                indices_below = np.append(indices_below, selected_indices)
-            assert len(indices_below) == n_below
+                indices_below[last_idx:] = selected_indices
 
             indices_above = np.setdiff1d(indices, indices_below)
 
