4c31a0698e454373536a106504db148b3da706b4
Masaki Kozuki <masaki.kozuki.2014@gmail.com>
2021-03-21 09:08:12 +0900

    add test

---
+++ tests/integration_tests/test_catalyst.py
@@ -0,0 +1,10 @@
+import pytest
+
+import optuna
+
+
+def test_warning():
+    study = optuna.create_study()
+    trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
+    with pytest.warns(FutureWarning):
+        optuna.integration.CatalystPruningCallback("abc", "def", False, trial=trial)
