2c9c845c1de8ee80b394df904fbf52a15a86b9f5
gasin <gasinsato@gmail.com>
2021-03-28 04:38:46 +0900

    just downgrade pytest<6.1.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_extras_require() -> Dict[str, List[str]]:
         # TODO(HideakiImamura) Unpin mypy version after fixing "Duplicate modules" error in
         # examples and tutorials.
         "checking": ["black", "hacking", "isort", "mypy==0.790", "blackdoc"],
-        "codecov": ["codecov<6.0.0", "pytest-cov"],
+        "codecov": ["codecov<6.1.0", "pytest-cov"],
         "doctest": [
             "cma",
             "matplotlib>=3.0.0",
@@ -129,7 +129,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             "mxnet",
             "pandas",
             "plotly>=4.0.0",
-            "pytest<6.0.0",
+            "pytest<6.1.0",
             "scikit-learn>=0.19.0,<0.23.0",
             "scikit-optimize",
             "xgboost",
@@ -151,8 +151,7 @@ def get_extras_require() -> Dict[str, List[str]]:
         ],
         "tests": [
             "fakeredis",
-            "pytest<6.0.0",
-            "pytest-cov",
+            "pytest<6.1.0",
         ],
         "optional": [
             "bokeh<2.0.0",  # optuna/cli.py, optuna/dashboard.py.
