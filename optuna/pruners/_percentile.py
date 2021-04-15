63be4937bc9c0e55c5339965bb28b293d472f336
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2021-04-15 12:32:49 +0900

    Update optuna/pruners/_percentile.py
    Co-authored-by: Crissman Loomis <crissman@preferred.jp>

--- optuna/pruners/_percentile.py
+++ optuna/pruners/_percentile.py
@@ -131,9 +131,10 @@ class PercentilePruner(BasePruner):
             If no value has been reported at the time of a pruning check, that particular check
             will be postponed until a value is reported. Value must be at least 1.
         n_min_trials:
-            Minimum number of trials to perform pruning at each step. If the number of reported
-            intermediate values in a trial is less than ``n_min_trials`` at a step,
-            the trial is not pruned at this step.
+            Minimum number of reported trial results at a step to judge whether to prune.
+            If the number of reported intermediate values from all trials at the current step
+            is less than ``n_min_trials``, the trial will not be pruned. This can be used to ensure
+            that a minimum number of trials are run to completion without being pruned.
     """
 
     def __init__(
