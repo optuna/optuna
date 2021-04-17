36d6a66e7e662a3c638515e90aa49c57648f3a53
Makoto Hiramatsu <himkt@cookpad.com>
2021-04-17 23:54:16 +0900

    Download pre-compiled tutorial from GitHub artifacts

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,12 +16,71 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+from typing import Any
+from io import BytesIO
+import os
 import pkg_resources
+import subprocess
+import time
+from zipfile import ZipFile
 
 import plotly.io as pio
+import requests
 from sphinx_gallery.sorting import FileNameSortKey
 
+
 __version__ = pkg_resources.get_distribution("optuna").version
+
+github_token = os.getenv("GITHUB_TOKEN")
+if github_token is not None:
+
+    def get_commit_id() -> str:
+        output = subprocess.check_output(["git", "rev-parse", "HEAD"])
+        return output.strip().decode("ascii")
+
+    def search_artifact(artifacts: Any, hash: str) -> str:
+        target_name = f"artifacts-{hash}"
+        artifact_names = [a["name"] for a in artifacts]
+
+        for artifact in artifacts:
+            if artifact["name"] == target_name:
+                return artifact["archive_download_url"]
+        raise RuntimeError(f"Not found {target_name} on {artifact_names}")
+
+    def download_artifact() -> None:
+        artifacts = requests.get(
+            "https://api.github.com/repos/himkt/optuna-test-rtds/actions/artifacts",
+            params=dict(per_page=20),
+        ).json()["artifacts"]
+
+        target_artifact_url = search_artifact(artifacts, commit_id)
+        artifact = requests.get(
+            target_artifact_url,
+            headers={"Authorization": f"token {github_token}"},
+        )
+        print(f"{target_artifact_url}: artifact")
+
+        if artifact.status_code != 200:
+            raise RuntimeError(f"Invalid status code {artifact.status_code}")
+
+        print("Succeeded to fetch request")
+        with ZipFile(BytesIO(artifact.content)) as f:
+            path = "tutorial"
+            f.extractall(path=path)
+            print(f"Extracted to {path}")
+
+    num_retries = 3
+    sleep_interval = 30
+
+    commit_id = get_commit_id()
+    for _ in range(num_retries):
+        try:
+            download_artifact()
+            break
+        except Exception as e:
+            print("Error: ", e)
+            print(f"Retry after {sleep_interval} sec...")
+            time.sleep(sleep_interval)
 
 # -- Project information -----------------------------------------------------
 
