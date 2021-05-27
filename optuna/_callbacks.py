61e5247a885b744e271736463dc7fab32c79d0d8
Crissman <crissman@preferred.jp>
2021-05-27 12:16:48 +0900

    fix typing for new method

--- optuna/_callbacks.py
+++ optuna/_callbacks.py
@@ -118,7 +118,7 @@ class RetryFailedTrialCallback:
         )
 
     @staticmethod
-    def retried_trial_number(trial) -> Optional[int]:
+    def retried_trial_number(trial: FrozenTrial) -> Optional[int]:
         """Return the number of the trail being retried.
 
         Args:
