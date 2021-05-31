78c326cbbb224f98168af7303501bdd42d89f0a4
Makoto Hiramatsu <himkt@cookpad.com>
2021-05-31 19:51:06 +0900

    Add newline between tests and use os.environ.pop

--- tests/integration_tests/allennlp_tests/test_allennlp.py
+++ tests/integration_tests/allennlp_tests/test_allennlp.py
@@ -256,9 +256,9 @@ def test_dump_best_config_with_environment_variables() -> None:
         best_config = json.loads(_jsonnet.evaluate_file(output_config_file))
         assert os.getenv("TRAIN_PATH") == best_config["train_data_path"]
         assert os.getenv("VALID_PATH") == best_config["validation_data_path"]
+        os.environ.pop("TRAIN_PATH")
+        os.environ.pop("VALID_PATH")
 
-        del os.environ["TRAIN_PATH"]
-        del os.environ["VALID_PATH"]
 
 def test_allennlp_pruning_callback() -> None:
     with tempfile.TemporaryDirectory() as tmp_dir:
