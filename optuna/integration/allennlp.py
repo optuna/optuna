3c34e2e1a64accb0ab15fa59721202a7b832e177
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2021-06-17 01:13:02 +0900

    Remove executor and pruner from allennlp.py

--- optuna/integration/allennlp.py
+++ optuna/integration/allennlp.py
@@ -1,3 +1,1 @@
 from optuna.integration._allennlp.dump_best_config import dump_best_config  # NOQA
-from optuna.integration._allennlp.executor import AllenNLPExecutor  # NOQA
-from optuna.integration._allennlp.pruner import AllenNLPPruningCallback  # NOQA
