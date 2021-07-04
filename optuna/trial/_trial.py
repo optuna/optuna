8540c13f5e4ab3f0d03f459d3db7e4f706c20d0b
takuya inoue <takuya_inoue@albert2005.co.jp>
2021-07-04 19:31:27 +0900

    improve warning msg

--- optuna/trial/_trial.py
+++ optuna/trial/_trial.py
@@ -587,7 +587,7 @@ class Trial(BaseTrial):
 
         if step in intermediate_values:
             # Do nothing if already reported.
-            warnings.warn("this `step` {} is already repoted.".format(step))
+            warnings.warn("The reported value is ignored because this `step` {} is already reported.".format(step))
             return
 
         self.storage.set_trial_intermediate_value(self._trial_id, step, value)
