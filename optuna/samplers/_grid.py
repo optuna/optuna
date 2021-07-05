3d175fe9ed2373c08a9361e659408dfa63fa73e1
Naoto Mizuno <naotomizuno@preferred.jp>
2021-07-05 11:02:28 +0900

    Prioritize grids that are not yet running

--- optuna/samplers/_grid.py
+++ optuna/samplers/_grid.py
@@ -208,6 +208,7 @@ class GridSampler(BaseSampler):
 
         # List up unvisited grids based on already finished ones.
         visited_grids = []
+        running_grids = []
 
         # We directly query the storage to get trials here instead of `study.get_trials`,
         # since some pruners such as `HyperbandPruner` use the study transformed
@@ -215,14 +216,19 @@ class GridSampler(BaseSampler):
         trials = study._storage.get_all_trials(study._study_id, deepcopy=False)
 
         for t in trials:
-            if (
-                t.state.is_finished()
-                and "grid_id" in t.system_attrs
-                and self._same_search_space(t.system_attrs["search_space"])
+            if "grid_id" in t.system_attrs and self._same_search_space(
+                t.system_attrs["search_space"]
             ):
-                visited_grids.append(t.system_attrs["grid_id"])
+                if t.state.is_finished():
+                    visited_grids.append(t.system_attrs["grid_id"])
+                elif t.state == TrialState.RUNNING:
+                    running_grids.append(t.system_attrs["grid_id"])
 
-        unvisited_grids = set(range(self._n_min_trials)) - set(visited_grids)
+        unvisited_grids = set(range(self._n_min_trials)) - set(visited_grids) - set(running_grids)
+
+        # If evaluations for all grids have been started, return grids that have not yet finished.
+        if len(unvisited_grids) == 0:
+            unvisited_grids = set(range(self._n_min_trials)) - set(visited_grids)
 
         return list(unvisited_grids)
 
