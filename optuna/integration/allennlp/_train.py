4691dfb8981b8dee5104a8677744f309248e6068
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2021-07-12 12:31:32 +0000

    Re-create another cached storage to load completed trials from storage

--- optuna/integration/allennlp/_train.py
+++ optuna/integration/allennlp/_train.py
@@ -11,14 +11,14 @@ from typing import Optional
 from typing import Union
 
 from allennlp.commands.train import TrainModel
-from allennlp.common import logging as common_logging
-from allennlp.common import Params
-from allennlp.common import util as common_util
 from allennlp.common.checks import check_for_gpu
 from allennlp.common.checks import ConfigurationError
+from allennlp.common import logging as common_logging
 from allennlp.common.meta import Meta
 from allennlp.common.meta import META_NAME
+from allennlp.common import Params
 from allennlp.common.plugins import import_plugins
+from allennlp.common import util as common_util
 from allennlp.data import Vocabulary
 from allennlp.models.archival import archive_model
 from allennlp.models.archival import CONFIG_NAME
@@ -30,6 +30,8 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 
+from optuna.storages import _CachedStorage
+from optuna.storages import InMemoryStorage
 from optuna import Trial
 from optuna import TrialPruned
 
@@ -166,6 +168,18 @@ def train_model_with_optuna(
             f"Number of workers in this node: {num_procs} | Number of nodes: {num_nodes} | "
             f"World size: {world_size}"
         )
+
+        assert trial is not None
+
+        if isinstance(trial.study._storage, InMemoryStorage):
+            message = "InMemoryStorage is not supported in distributed configuration."
+            message += " You have to use RDB or Redis to use Optuna with AllenNLP distributed."
+            raise ValueError(message)
+
+        elif isinstance(trial.study._storage, _CachedStorage):
+            # Reconstruct storage to purge a old cache.
+            if isinstance(trial.study._storage, _CachedStorage):
+                trial.study._storage = _CachedStorage(trial.study._storage._backend)
 
         try:
             mp.spawn(
