64c4f3c15c81d28b727ccdaade0df1cb9ae52779
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2021-07-28 06:10:58 +0000

    Update license statement

--- optuna/integration/allennlp/_train.py
+++ optuna/integration/allennlp/_train.py
@@ -1,22 +1,19 @@
 """
+This file is ported from AllenNLP original implementation.
+AllenNLP(https://github.com/allenai/allennlp/blob/main/allennlp/commands/train.py)
 Copyright [2021] The Allen Institute for Artificial Intelligence.
 
-Licensed under the Apache License, Version 2.0 (the "License");
+---
+
+This file is Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-"""
-
-"""
-This file is ported from AllenNLP original implementation.
-https://github.com/allenai/allennlp/blob/main/allennlp/commands/train.py
 """
 
 import os
