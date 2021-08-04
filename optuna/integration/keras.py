49f7d80adc8ba5e7c70c6e6e1b284ec225b55851
DeviousLab <deviouslab@gmail.com>
2021-08-04 23:38:45 +0400

    Fixed line length error
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- optuna/integration/keras.py
+++ optuna/integration/keras.py
@@ -22,7 +22,8 @@ if not _imports.is_successful():
     "Test before upgrading. "
     "REF: https://github.com/keras-team/keras/releases/tag/2.4.0. "
     "There is an alternative callback function that can be used instead: "
-    "`TFKerasPruningCallback <https://optuna.readthedocs.io/en/stable/reference/generated/optuna.integration.TFKerasPruningCallback.html>`_ ",
+    """`TFKerasPruningCallback <https://optuna.readthedocs.io/en/stable/reference/
+    generated/optuna.integration.TFKerasPruningCallback.html>`_ """,
 )
 class KerasPruningCallback(Callback):
     """Keras callback to prune unpromising trials.
