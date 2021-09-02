97a23e5a3085ab1aa340342b8a72de005564d7c7
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2021-09-02 11:30:53 +0900

    Fix mypy error.

--- tests/storages_tests/rdb_tests/test_storage.py
+++ tests/storages_tests/rdb_tests/test_storage.py
@@ -223,7 +223,7 @@ def test_upgrade(optuna_version: str) -> None:
             assert -5 <= trial.params["x"] <= 5
             assert 0 <= trial.params["y"] <= 10
             assert trial.params["z"] in (-5, 0, 5)
-            assert 0 <= trial.value <= 125
+            assert trial.value is not None and 0 <= trial.value <= 125
 
         assert study.system_attrs["c"] == 2
         assert study.user_attrs["d"] == 3
