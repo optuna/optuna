eddfbff04edeaf45174c4a7c420826ff4757c1f4
tsuga <2888173+tsuga@users.noreply.github.com>
2021-09-19 01:47:50 +0900

    Add tests for multi-metric evaluation

--- tests/integration_tests/test_sklearn.py
+++ tests/integration_tests/test_sklearn.py
@@ -7,7 +7,9 @@ from sklearn.exceptions import NotFittedError
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import SGDClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.metrics._scorer import neg_log_loss
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics._scorer import accuracy_scorer
+from sklearn.metrics._scorer import neg_mean_squared_error_scorer
 from sklearn.neighbors import KernelDensity
 
 from optuna import distributions
@@ -75,10 +77,34 @@ def test_optuna_search(enable_pruning: bool, fit_params: str) -> None:
     optuna_search.score(X, y)
 
 
+def custom_scoring(estimator, X, y):
+    y_pred = estimator.predict(X)
+    return {
+        "neg_mse": mean_squared_error(y, y_pred),
+        "acc": accuracy_score(y, y_pred),
+    }
+
+
 @pytest.mark.parametrize("enable_pruning", [True, False])
 @pytest.mark.parametrize("fit_params", ["", "coef_init"])
+@pytest.mark.parametrize(
+    "refit, scoring",
+    [
+        # single score cases
+        (False, "neg_mean_squared_error"),
+        (True, "neg_mean_squared_error"),
+        (True, neg_mean_squared_error_scorer),
+        ("neg_mean_squared_error", "neg_mean_squared_error"),
+        (neg_mean_squared_error_scorer, neg_mean_squared_error_scorer),
+        # multiple scores cases
+        ("accuracy", ["neg_mean_squared_error", "accuracy"]),
+        ("acc", custom_scoring),
+        ("score2", {"score1": "neg_mean_squared_error", "score2": "accuracy"}),
+        ("score2", {"score1": neg_mean_squared_error_scorer, "score2": accuracy_scorer}),
+    ],
+)
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_optuna_search_multimetric_list(enable_pruning: bool, fit_params: str) -> None:
+def test_optuna_search_multimetric(enable_pruning: bool, fit_params: str, refit, scoring) -> None:
 
     X, y = make_blobs(n_samples=10)
     est = SGDClassifier(max_iter=5, tol=1e-03)
@@ -91,9 +117,9 @@ def test_optuna_search_multimetric_list(enable_pruning: bool, fit_params: str) -
         error_score="raise",
         max_iter=5,
         random_state=0,
-        refit="neg_log_loss",
+        refit=refit,
         return_train_score=True,
-        scoring=["neg_log_loss", "accuracy"]
+        scoring=scoring,
     )
 
     with pytest.raises(NotFittedError):
@@ -105,15 +131,31 @@ def test_optuna_search_multimetric_list(enable_pruning: bool, fit_params: str) -
         optuna_search.fit(X, y)
 
     optuna_search.trials_dataframe()
-    optuna_search.decision_function(X)
-    optuna_search.predict(X)
-    optuna_search.score(X, y)
+    if refit:
+        optuna_search.decision_function(X)
+        optuna_search.predict(X)
+        optuna_search.score(X, y)
+    else:
+        with pytest.raises(AttributeError):
+            optuna_search.decision_function(X)
+        with pytest.raises(AttributeError):
+            optuna_search.predict(X)
+        with pytest.raises(AttributeError):
+            optuna_search.score(X, y)
 
 
 @pytest.mark.parametrize("enable_pruning", [True, False])
 @pytest.mark.parametrize("fit_params", ["", "coef_init"])
+@pytest.mark.parametrize(
+    "refit, scoring",
+    [
+        ("foobar", ["neg_mean_squared_error", "accuracy"]),
+    ],
+)
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_optuna_search_multimetric_dict(enable_pruning: bool, fit_params: str) -> None:
+def test_optuna_search_multimetric_error(
+    enable_pruning: bool, fit_params: str, refit, scoring
+) -> None:
 
     X, y = make_blobs(n_samples=10)
     est = SGDClassifier(max_iter=5, tol=1e-03)
@@ -126,23 +168,18 @@ def test_optuna_search_multimetric_dict(enable_pruning: bool, fit_params: str) -
         error_score="raise",
         max_iter=5,
         random_state=0,
-        refit="score_two",
+        refit=refit,
         return_train_score=True,
-        scoring={"score_one": neg_log_loss, "score_two": accuracy_score}
+        scoring=scoring,
     )
 
-    with pytest.raises(NotFittedError):
-        optuna_search._check_is_fitted()
-
-    if fit_params == "coef_init" and not enable_pruning:
-        optuna_search.fit(X, y, coef_init=np.ones((3, 2), dtype=np.float64))
-    else:
-        optuna_search.fit(X, y)
-
-    optuna_search.trials_dataframe()
-    optuna_search.decision_function(X)
-    optuna_search.predict(X)
-    optuna_search.score(X, y)
+    with pytest.raises(
+        ValueError, match=r"For multi-metric scoring, the parameter refit must be set to"
+    ):
+        if fit_params == "coef_init" and not enable_pruning:
+            optuna_search.fit(X, y, coef_init=np.ones((3, 2), dtype=np.float64))
+        else:
+            optuna_search.fit(X, y)
 
 
 @pytest.mark.filterwarnings("ignore::UserWarning")
@@ -168,18 +205,37 @@ def test_optuna_search_properties() -> None:
     assert optuna_search.user_attrs_ == {"dataset": "blobs"}
     assert type(optuna_search.predict_log_proba(X)) == np.ndarray
     assert type(optuna_search.predict_proba(X)) == np.ndarray
+    assert optuna_search.multimetric_ is False
+    assert "user_attrs_mean_test_score" in optuna_search.trials_dataframe().columns
+    assert "user_attrs_mean_train_score" in optuna_search.trials_dataframe().columns
+    assert "user_attrs_split0_train_score" in optuna_search.trials_dataframe().columns
+    assert "user_attrs_split0_test_score" in optuna_search.trials_dataframe().columns
+    assert (
+        optuna_search.trials_dataframe()["value"]
+        == optuna_search.trials_dataframe()["user_attrs_mean_test_score"]
+    ).all()
 
 
-# FIXME
 @pytest.mark.filterwarnings("ignore::UserWarning")
-def test_optuna_search_properties() -> None:
+def test_optuna_search_properties_multimetric() -> None:
 
     X, y = make_blobs(n_samples=10)
     est = LogisticRegression(tol=1e-03)
     param_dist = {"C": distributions.LogUniformDistribution(1e-04, 1e03)}
 
+    scoring1 = "neg_mean_squared_error"
+    scoring2 = "accuracy"
+    refit = scoring2
+
     optuna_search = integration.OptunaSearchCV(
-        est, param_dist, cv=3, error_score="raise", random_state=0, return_train_score=True
+        est,
+        param_dist,
+        cv=3,
+        error_score="raise",
+        random_state=0,
+        return_train_score=True,
+        refit=refit,
+        scoring=[scoring1, scoring2],
     )
     optuna_search.fit(X, y)
     optuna_search.set_user_attr("dataset", "blobs")
@@ -194,6 +250,19 @@ def test_optuna_search_properties() -> None:
     assert optuna_search.user_attrs_ == {"dataset": "blobs"}
     assert type(optuna_search.predict_log_proba(X)) == np.ndarray
     assert type(optuna_search.predict_proba(X)) == np.ndarray
+    assert optuna_search.multimetric_ is True
+    assert f"user_attrs_mean_test_{scoring1}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_mean_train_{scoring1}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_mean_test_{scoring2}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_mean_train_{scoring2}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_split0_test_{scoring1}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_split0_train_{scoring1}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_split0_test_{scoring2}" in optuna_search.trials_dataframe().columns
+    assert f"user_attrs_split0_train_{scoring2}" in optuna_search.trials_dataframe().columns
+    assert (
+        optuna_search.trials_dataframe()["value"]
+        == optuna_search.trials_dataframe()[f"user_attrs_mean_test_{refit}"]
+    ).all()
 
 
 @pytest.mark.filterwarnings("ignore::UserWarning")
@@ -390,6 +459,32 @@ def test_objective_error_score_nan() -> None:
         error_score=np.nan,
         random_state=0,
         return_train_score=True,
+    )
+
+    with pytest.raises(
+        ValueError,
+        match="This SGDClassifier estimator requires y to be passed, but the target y is None.",
+    ):
+        optuna_search.fit(X)
+
+
+@pytest.mark.filterwarnings("ignore::RuntimeWarning")
+def test_objective_error_score_nan_multimetric() -> None:
+
+    X, y = make_blobs(n_samples=10)
+    est = SGDClassifier(max_iter=5, tol=1e-03)
+    param_dist = {}  # type: ignore
+    optuna_search = integration.OptunaSearchCV(
+        est,
+        param_dist,
+        cv=3,
+        enable_pruning=True,
+        max_iter=5,
+        error_score=np.nan,
+        refit="accuracy",
+        random_state=0,
+        return_train_score=True,
+        scoring=["neg_mean_squared_error", "accuracy"],
     )
 
     with pytest.raises(
