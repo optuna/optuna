00ba603a8280aad29b6bdfd8b2fff4de50770fb3
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2021-10-21 10:44:58 +0900

    Update optuna/cli.py
    Co-authored-by: Hiroyuki Vincent Yamazaki <hiroyuki.vincent.yamazaki@gmail.com>

--- optuna/cli.py
+++ optuna/cli.py
@@ -818,7 +818,7 @@ class _Ask(_BaseCommand):
         else:
             if parsed_args.sampler_kwargs is not None:
                 raise ValueError(
-                    "`sampler_kwargs` is set without `sampler`. Please specify `sampler` as well."
+                    "`--sampler_kwargs` is set without `--sampler`. Please specify `--sampler` as well."
                 )
 
         if parsed_args.search_space is not None:
