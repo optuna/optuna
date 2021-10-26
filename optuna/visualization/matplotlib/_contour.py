4170a708287cbd40152cb75030ac9513af2d1447
Ivan Ega Pratama <ivan.ega.p@gmail.com>
2021-10-26 17:00:19 +0700

    Fix plot_countour bug in #3039

--- optuna/visualization/matplotlib/_contour.py
+++ optuna/visualization/matplotlib/_contour.py
@@ -399,10 +399,10 @@ def _generate_contour_subplot(
     )
     cs = None
     ax.set(xlabel=x_param, ylabel=y_param)
+    ax.set_xlim(x_values_range[0], x_values_range[1])
+    ax.set_ylim(y_values_range[0], y_values_range[1])
+    ax.set(xlabel=x_param, ylabel=y_param)
     if len(zi) > 0:
-        ax.set_xlim(x_values_range[0], x_values_range[1])
-        ax.set_ylim(y_values_range[0], y_values_range[1])
-        ax.set(xlabel=x_param, ylabel=y_param)
         if _is_log_scale(trials, x_param):
             ax.set_xscale("log")
         if _is_log_scale(trials, y_param):
