59d11ea29bd44969c520ada6e938df32e86a1de8
himkt <himkt@klis.tsukuba.ac.jp>
2021-10-31 16:19:43 +0900

    Remove comment (2)

--- setup.py
+++ setup.py
@@ -84,8 +84,6 @@ def get_extras_require() -> Dict[str, List[str]]:
         ],
         "experimental": ["redis"],
         "testing": [
-            # TODO(toshihikoyanase): Remove the version constraint after resolving the issue
-            # https://github.com/optuna/optuna/issues/1000.
             "chainer>=5.0.0",
             "cma",
             "fakeredis",
