4f75454353c6e3ecbc0e15d0c15095d06c9693ae
himkt <himkt@klis.tsukuba.ac.jp>
2021-10-31 22:24:45 +0900

    Remove unused import

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,7 +6,6 @@ from subprocess import CalledProcessError
 import tempfile
 from typing import Any
 from typing import Callable
-from typing import List
 from typing import Optional
 from typing import Tuple
 
