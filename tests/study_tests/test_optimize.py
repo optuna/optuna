bd52bc2eece80b7c3b4a02ec54e216e54796125e
nuka137 <n.a.sch137@gmail.com>
2021-10-31 04:36:26 +0000

    Address review's comment

--- tests/study_tests/test_optimize.py
+++ tests/study_tests/test_optimize.py
@@ -52,28 +52,3 @@ def test_run_trial(storage_mode: str) -> None:
         frozen_trial = study._storage.get_trial(trial._trial_id)
 
         assert frozen_trial.state == TrialState.FAIL
-
-
-# TODO(Yanase): Remove this test function after removing `optuna.structs.TrialPruned`.
-@pytest.mark.parametrize(
-    "trial_pruned_class",
-    [TrialPruned, TrialPruned_in_exceptions],
-)
-@pytest.mark.parametrize("report_value", [None, 1.2])
-def test_run_trial_with_trial_pruned(
-    trial_pruned_class: Callable[[], TrialPruned], report_value: Optional[float]
-) -> None:
-
-    study = create_study()
-
-    def func_with_trial_pruned(trial: Trial) -> float:
-
-        if report_value is not None:
-            trial.report(report_value, 1)
-
-        raise trial_pruned_class()
-
-    trial = _optimize._run_trial(study, func_with_trial_pruned, catch=())
-    frozen_trial = study._storage.get_trial(trial._trial_id)
-    assert frozen_trial.value == report_value
-    assert frozen_trial.state == TrialState.PRUNED
