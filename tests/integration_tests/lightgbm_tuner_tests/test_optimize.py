a8e7477951f8b4bf8277b7aa88fdaaebff8b91a4
nyanhi <ksuzuki19937.mm@gmail.com>
2021-11-08 12:18:19 +0900

    replace with log_evaluation callback

--- tests/integration_tests/lightgbm_tuner_tests/test_optimize.py
+++ tests/integration_tests/lightgbm_tuner_tests/test_optimize.py
@@ -10,6 +10,7 @@ from typing import Union
 from unittest import mock
 import warnings
 
+from lightgbm import log_evaluation
 import numpy as np
 import pytest
 import sklearn.datasets
@@ -461,7 +462,7 @@ class TestLightGBMTuner(object):
             num_boost_round=3,
             early_stopping_rounds=2,
             valid_sets=valid_dataset,
-            verbose_eval=False,
+            callbacks=[log_evaluation(-1)],
         )
         runner.tune_num_leaves()
         assert len(runner.study.trials) == 20
@@ -548,7 +549,9 @@ class TestLightGBMTuner(object):
         dataset = lgb.Dataset(np.zeros((10, 10)))
 
         study = optuna.create_study()
-        tuner = LightGBMTuner(params, dataset, valid_sets=dataset, study=study, verbose_eval=False)
+        tuner = LightGBMTuner(
+            params, dataset, valid_sets=dataset, study=study, callbacks=[log_evaluation(-1)]
+        )
 
         with mock.patch.object(_BaseTuner, "_get_booster_best_score", return_value=1.0):
             tuner.tune_regularization_factors()
@@ -589,7 +592,7 @@ class TestLightGBMTuner(object):
                 valid_sets=dataset,
                 study=study,
                 verbosity=verbosity,
-                verbose_eval=False,
+                callbacks=[log_evaluation(-1)],
                 time_budget=1,
             )
 
@@ -610,7 +613,7 @@ class TestLightGBMTuner(object):
             dataset,
             valid_sets=dataset,
             study=study,
-            verbose_eval=False,
+            callbacks=[log_evaluation(-1)],
             time_budget=1,
             show_progress_bar=show_progress_bar,
         )
@@ -629,7 +632,9 @@ class TestLightGBMTuner(object):
         dataset = lgb.Dataset(np.zeros((10, 10)))
 
         study = optuna.create_study()
-        tuner = LightGBMTuner(params, dataset, valid_sets=dataset, study=study, verbose_eval=False)
+        tuner = LightGBMTuner(
+            params, dataset, valid_sets=dataset, study=study, callbacks=[log_evaluation(-1)]
+        )
 
         with pytest.raises(ValueError):
             tuner.get_best_booster()
@@ -668,7 +673,7 @@ class TestLightGBMTuner(object):
                 valid_sets=dataset,
                 study=study,
                 model_dir=tmpdir,
-                verbose_eval=False,
+                callbacks=[log_evaluation(-1)],
             )
 
             with mock.patch.object(_BaseTuner, "_get_booster_best_score", return_value=0.0):
@@ -732,8 +737,8 @@ class TestLightGBMTuner(object):
             dataset,
             valid_sets=dataset,
             study=study,
+            callbacks=[log_evaluation(-1)],
             optuna_callbacks=[callback_mock],
-            verbose_eval=False,
         )
 
         with mock.patch.object(_BaseTuner, "_get_booster_best_score", return_value=1.0):
@@ -764,7 +769,7 @@ class TestLightGBMTuner(object):
             valid_sets=valid,
             early_stopping_rounds=3,
             optuna_seed=10,
-            verbose_eval=False,
+            callbacks=[log_evaluation(-1)],
         )
         tuner_first_try.run()
         best_score_first_try = tuner_first_try.best_score
@@ -775,7 +780,7 @@ class TestLightGBMTuner(object):
             valid_sets=valid,
             early_stopping_rounds=3,
             optuna_seed=10,
-            verbose_eval=False,
+            callbacks=[log_evaluation(-1)],
         )
         tuner_second_try.run()
         best_score_second_try = tuner_second_try.best_score
