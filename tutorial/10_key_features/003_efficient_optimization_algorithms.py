b6c9acc4b588671c715a81eb07e75982e53728c8
leela <prabhatreddyk@gmail.com>
2021-11-08 20:27:36 +0100

    docs: accommodated comments from #3087

--- tutorial/10_key_features/003_efficient_optimization_algorithms.py
+++ tutorial/10_key_features/003_efficient_optimization_algorithms.py
@@ -17,17 +17,17 @@ More detailed explanation of how samplers suggest parameters is in :class:`optun
 
 Optuna provides the following sampling algorithms:
 
-- CMA-ES based algorithm implemented in :class:`optuna.samplers.CmaEsSampler`
-
 - Grid Search implemented in :class:`optuna.samplers.GridSampler`
 
-- Nondominated Sorting Genetic Algorithm II implemented in :class:`optuna.samplers.NSGAIISampler`
-
-- Algorithm to enable Partial Fixed parameters implemented in :class:`optuna.samplers.PartialFixedSampler`
+- Random Search implemented in :class:`optuna.samplers.RandomSampler`
 
 - Tree-structured Parzen Estimator algorithm implemented in :class:`optuna.samplers.TPESampler`
 
-- Random Search implemented in :class:`optuna.samplers.RandomSampler`
+- CMA-ES based algorithm implemented in :class:`optuna.samplers.CmaEsSampler`
+
+- Algorithm to enable partial fixed parameters implemented in :class:`optuna.samplers.PartialFixedSampler`
+
+- Nondominated Sorting Genetic Algorithm II implemented in :class:`optuna.samplers.NSGAIISampler`
 
 The default sampler is :class:`optuna.samplers.TPESampler`.
 
@@ -64,17 +64,17 @@ print(f"Sampler is {study.sampler.__class__.__name__}")
 #
 # Optuna provides the following pruning algorithms:
 #
-# - Hyperband algorithm implemented in :class:`optuna.pruners.HyperbandPruner`
-#
 # - Median pruning algorithm implemented in :class:`optuna.pruners.MedianPruner`
 #
 # - Non-pruning algorithm implementd in :class:`optuna.pruners.NopPruner`
 #
 # - Algorithm to operate pruner with tolerance implemented in :class:`optuna.pruners.PatientPruner`
 #
-# - Algorithm to prune specified number of trials implemented in :class:`optuna.pruners.PercentilePruner`
+# - Algorithm to prune specified percentile of trials implemented in :class:`optuna.pruners.PercentilePruner`
 #
 # - Asynchronous Successive Halving algorithm implemented in :class:`optuna.pruners.SuccessiveHalvingPruner`
+#
+# - Hyperband algorithm implemented in :class:`optuna.pruners.HyperbandPruner`
 #
 # - Threshold pruning algorithm implemented in :class:`optuna.pruners.ThresholdPruner`
 #
