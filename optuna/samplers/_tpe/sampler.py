2f49e47568e9cb0e1934323fa70042dc9703aea1
makkimaki <shellingford-93.4.2@hotmail.co.jp>
2021-11-10 22:45:03 +0900

    Add: seealso link to optuna.samplers.TPESampler docstring

--- optuna/samplers/_tpe/sampler.py
+++ optuna/samplers/_tpe/sampler.py
@@ -214,6 +214,9 @@ class TPESampler(BaseSampler):
     Raises:
         ValueError:
             If ``multivariate`` is :obj:`False` and ``group`` is :obj:`True`.
+
+    .. seealso::
+        :ref:`pruning` tutorial describes more details and flexible usages.
     """
 
     def __init__(
