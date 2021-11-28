525b002c44e817e6bd84398e685c046a725d2eb2
Masahito Kumada <31849143+MasahitoKumada@users.noreply.github.com>
2021-11-28 15:57:42 +0900

    Update optuna/trial/_trial.py
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/trial/_trial.py
+++ optuna/trial/_trial.py
@@ -470,7 +470,7 @@ class Trial(BaseTrial):
             :exc:`NotImplementedError`:
                 If trial is being used for multi-objective optimization.
             :exe:`ValueError`:
-                If ``step is negative`` is specified.
+                If ``step`` is negative.
             :exe:`TypeError`:
                 If the type of ``value`` is not obj:`float`.
         """
