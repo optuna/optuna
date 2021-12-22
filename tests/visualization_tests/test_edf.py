a64ee76ccc249b79d14e0e72d39c0d69f6a0a940
NZnz <40193664+makinzm@users.noreply.github.com>
2021-12-22 17:01:04 +0900

    fix indent

--- tests/visualization_tests/test_edf.py
+++ tests/visualization_tests/test_edf.py
@@ -68,6 +68,6 @@ def test_plot_optimization_history(direction: str) -> None:
     study0 = create_study(direction=direction)
     study0.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
     figure = plot_edf(study0, target_name="Target Name")
-     _validate_edf_values(figure.data[0]["y"])
+    _validate_edf_values(figure.data[0]["y"])
     assert len(figure.data) == 1
     assert figure.layout.xaxis.title.text == "Target Name"
