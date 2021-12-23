3f6355b0ddb669b6e25c565cebc5686d1f135551
xadrianzetx <xadrianzetx@gmail.com>
2021-12-23 19:35:32 +0100

    Remove remaining instances of `LogUniformDistribution`

--- tests/visualization_tests/test_parallel_coordinate.py
+++ tests/visualization_tests/test_parallel_coordinate.py
@@ -3,7 +3,7 @@ import math
 import pytest
 
 from optuna.distributions import CategoricalDistribution
-from optuna.distributions import LogUniformDistribution
+from optuna.distributions import FloatDistribution
 from optuna.study import create_study
 from optuna.testing.visualization import prepare_study_with_trials
 from optuna.trial import create_trial
@@ -137,8 +137,8 @@ def test_plot_parallel_coordinate_log_params() -> None:
             value=0.0,
             params={"param_a": 1e-6, "param_b": 10},
             distributions={
-                "param_a": LogUniformDistribution(1e-7, 1e-2),
-                "param_b": LogUniformDistribution(1, 1000),
+                "param_a": FloatDistribution(1e-7, 1e-2, log=True),
+                "param_b": FloatDistribution(1, 1000, log=True),
             },
         )
     )
@@ -147,8 +147,8 @@ def test_plot_parallel_coordinate_log_params() -> None:
             value=1.0,
             params={"param_a": 2e-5, "param_b": 200},
             distributions={
-                "param_a": LogUniformDistribution(1e-7, 1e-2),
-                "param_b": LogUniformDistribution(1, 1000),
+                "param_a": FloatDistribution(1e-7, 1e-2, log=True),
+                "param_b": FloatDistribution(1, 1000, log=True),
             },
         )
     )
@@ -157,8 +157,8 @@ def test_plot_parallel_coordinate_log_params() -> None:
             value=0.1,
             params={"param_a": 1e-4, "param_b": 30},
             distributions={
-                "param_a": LogUniformDistribution(1e-7, 1e-2),
-                "param_b": LogUniformDistribution(1, 1000),
+                "param_a": FloatDistribution(1e-7, 1e-2, log=True),
+                "param_b": FloatDistribution(1, 1000, log=True),
             },
         )
     )
@@ -190,7 +190,7 @@ def test_plot_parallel_coordinate_with_categorical_numeric_params() -> None:
             distributions={
                 "param_a": CategoricalDistribution(("preferred", "opt")),
                 "param_b": CategoricalDistribution((1, 2, 10)),
-                "param_c": LogUniformDistribution(1, 1000),
+                "param_c": FloatDistribution(1, 1000, log=True),
                 "param_d": CategoricalDistribution((1, -1, 2)),
             },
         )
@@ -203,7 +203,7 @@ def test_plot_parallel_coordinate_with_categorical_numeric_params() -> None:
             distributions={
                 "param_a": CategoricalDistribution(("preferred", "opt")),
                 "param_b": CategoricalDistribution((1, 2, 10)),
-                "param_c": LogUniformDistribution(1, 1000),
+                "param_c": FloatDistribution(1, 1000, log=True),
                 "param_d": CategoricalDistribution((1, -1, 2)),
             },
         )
@@ -216,7 +216,7 @@ def test_plot_parallel_coordinate_with_categorical_numeric_params() -> None:
             distributions={
                 "param_a": CategoricalDistribution(("preferred", "opt")),
                 "param_b": CategoricalDistribution((1, 2, 10)),
-                "param_c": LogUniformDistribution(1, 1000),
+                "param_c": FloatDistribution(1, 1000, log=True),
                 "param_d": CategoricalDistribution((1, -1, 2)),
             },
         )
@@ -229,7 +229,7 @@ def test_plot_parallel_coordinate_with_categorical_numeric_params() -> None:
             distributions={
                 "param_a": CategoricalDistribution(("preferred", "opt")),
                 "param_b": CategoricalDistribution((1, 2, 10)),
-                "param_c": LogUniformDistribution(1, 1000),
+                "param_c": FloatDistribution(1, 1000, log=True),
                 "param_d": CategoricalDistribution((-1, 1, 2)),
             },
         )
