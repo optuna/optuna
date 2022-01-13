b39bce767d1deb6f56618385ec690c48b43cea4b
himkt <himkt@klis.tsukuba.ac.jp>
2022-01-13 22:38:38 +0900

    Stabilize allennlp integrations

--- optuna/integration/allennlp/_pruner.py
+++ optuna/integration/allennlp/_pruner.py
@@ -10,7 +10,6 @@ from optuna import load_study
 from optuna import pruners
 from optuna import Trial
 from optuna import TrialPruned
-from optuna._experimental import experimental
 from optuna._imports import try_import
 from optuna.integration.allennlp._variables import _VariableManager
 from optuna.integration.allennlp._variables import OPTUNA_ALLENNLP_DISTRIBUTED_FLAG
@@ -72,7 +71,6 @@ def _create_pruner(
     return pruner(**pruner_kwargs)
 
 
-@experimental("2.0.0")
 @TrainerCallback.register("optuna_pruner")
 class AllenNLPPruningCallback(TrainerCallback):
     """AllenNLP callback to prune unpromising trials.
