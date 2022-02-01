1aed89beb0fc5ee5b9725d567642218818e6df40
Tomokazu Higuchi <thiguchi@preferred.jp>
2022-02-01 21:34:28 +0900

    Add tests

---
+++ tests/study_tests/test_convert_positional_args.py
@@ -0,0 +1,96 @@
+from typing import List
+
+import pytest
+
+from optuna.study.study import _convert_positional_args
+
+
+def _sample_func(*, a: int, b: int, c: int) -> int:
+    return a + b + c
+
+
+def test_convert_positional_args_decorator() -> None:
+    previous_positional_arg_names: List[str] = []
+    decorator_converter = _convert_positional_args(
+        previous_positional_arg_names=previous_positional_arg_names
+    )
+
+    decorated_func = decorator_converter(_sample_func)
+    assert decorated_func.__name__ == _sample_func.__name__
+
+
+def test_convert_positional_args_user_warning() -> None:
+    previous_positional_arg_names: List[str] = ["a", "b"]
+    decorator_converter = _convert_positional_args(
+        previous_positional_arg_names=previous_positional_arg_names
+    )
+    assert callable(decorator_converter)
+
+    decorated_func = decorator_converter(_sample_func)
+    with pytest.warns(UserWarning) as record:
+        decorated_func(1, 2, c=3)
+        decorated_func(1, b=2, c=3)
+        decorated_func(a=1, b=2, c=3)  # no warn
+
+    assert len(record) == 2
+    for warn in record.list:
+        assert isinstance(warn.message, UserWarning)
+        assert _sample_func.__name__ in str(warn.message)
+
+
+def test_convert_positional_args_mypy_type_inference() -> None:
+    previous_positional_arg_names: List[str] = []
+    decorator_converter = _convert_positional_args(
+        previous_positional_arg_names=previous_positional_arg_names
+    )
+    assert callable(decorator_converter)
+
+    class _Sample(object):
+        def __init__(self) -> None:
+            pass
+
+        def method(self) -> bool:
+            return True
+
+    def _func_sample() -> _Sample:
+        return _Sample()
+
+    def _func_none() -> None:
+        pass
+
+    ret_none = decorator_converter(_func_none)()
+    assert ret_none is None
+
+    ret_sample = decorator_converter(_func_sample)()
+    assert isinstance(ret_sample, _Sample)
+    assert ret_sample.method()
+
+
+def test_convert_positional_args_too_many_previous_positional_arg_names() -> None:
+    previous_positional_arg_names: List[str] = ["a", "b", "c", "d"]
+    decorator_converter = _convert_positional_args(
+        previous_positional_arg_names=previous_positional_arg_names
+    )
+    assert callable(decorator_converter)
+
+    decorated_func = decorator_converter(_sample_func)
+    with pytest.raises(AssertionError):
+        decorated_func(1, 2, c=2)
+
+
+def test_convert_positional_args_invalid_positional_args() -> None:
+    previous_positional_arg_names: List[str] = ["a", "b"]
+    decorator_converter = _convert_positional_args(
+        previous_positional_arg_names=previous_positional_arg_names
+    )
+    assert callable(decorator_converter)
+
+    decorated_func = decorator_converter(_sample_func)
+    with pytest.warns(UserWarning):
+        with pytest.raises(TypeError) as record:
+            decorated_func(1, 2, 3)
+        assert str(record.value) == "_sample_func() takes 2 positional arguments but 3 were given."
+
+        with pytest.raises(TypeError) as record:
+            decorated_func(1, 3, b=2)
+        assert str(record.value) == "_sample_func() got multiple values for argument 'b'."
