288acffe73a94744ee3f6359b0c5415e38bbfd25
xadrianzetx <xadrianzetx@gmail.com>
2022-02-10 14:10:56 +0100

    Modify `BaseCrossover` API to accept transformed distribution bounds

--- optuna/samplers/nsgaii/_crossovers/_base.py
+++ optuna/samplers/nsgaii/_crossovers/_base.py
@@ -1,9 +1,7 @@
 import abc
-from typing import Dict
 
 import numpy as np
 
-from optuna.distributions import BaseDistribution
 from optuna.study import Study
 
 
@@ -12,6 +10,11 @@ class BaseCrossover(object, metaclass=abc.ABCMeta):
 
     A crossover operation is used by :class:`~optuna.samplers.NSGAIISampler`
     to create new parameter combination from parameters of ``n`` parent individuals.
+
+    .. note:
+        Concrete implementations of this class are expected to only accept parameters
+        from numerical distributions. At the moment, only crossover operation for categorical
+        parameters (uniform crossover) is built-in into :class:`~optuna.samplers.NSGAIISampler`.
     """
 
     def __str__(self) -> str:
@@ -31,7 +34,7 @@ class BaseCrossover(object, metaclass=abc.ABCMeta):
         parents_params: np.ndarray,
         rng: np.random.RandomState,
         study: Study,
-        search_space: Dict[str, BaseDistribution],
+        search_space_bounds: np.ndarray,
     ) -> np.ndarray:
         """Perform crossover of selected parent individuals.
 
@@ -41,15 +44,14 @@ class BaseCrossover(object, metaclass=abc.ABCMeta):
             parents_params:
                 A ``numpy.ndarray`` with dimensions ``num_parents x num_parameters``.
                 Represents a parameter space for each parent individual. This space is
-                continuous for numerical parameters and discrete for categorical parameters.
+                continuous for numerical parameters.
             rng:
                 An instance of ``numpy.random.RandomState``.
             study:
                 Target study object.
-            search_space:
-                The subspace of original search space returned by
-                :func:`~optuna.samplers.BaseSampler.infer_relative_search_space`,
-                containing either only categorical or numerical distributions.
+            search_space_bounds:
+                A ``numpy.ndarray`` with dimensions ``len_search_space x 2`` representing
+                numerical distribution bounds constructed from transformed search space.
 
         Returns:
             A 1-dimensional ``numpy.ndarray`` containing new parameter combination.
