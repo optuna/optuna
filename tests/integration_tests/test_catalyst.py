2fd7474f077ff9ed33668af614931d70ee75bde3
HideakiImamura <gohome.x105.gn@gmail.com>
2022-02-14 12:06:48 +0900

    Use keyword arguments

--- tests/integration_tests/test_catalyst.py
+++ tests/integration_tests/test_catalyst.py
@@ -7,4 +7,9 @@ def test_warning() -> None:
     study = optuna.create_study()
     trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
     with pytest.warns(FutureWarning):
-        optuna.integration.CatalystPruningCallback(trial, "abc", "def", False)
+        optuna.integration.CatalystPruningCallback(
+            trial=trial,
+            loader_key="abc",
+            metric_key="def",
+            minimize=False,
+        )
