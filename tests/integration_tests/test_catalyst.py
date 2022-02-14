4886fadf3641d34091802262775a6a4a2823e757
HideakiImamura <gohome.x105.gn@gmail.com>
2022-02-14 09:45:00 +0900

    Fix the order of arguments in the catalyst integration test

--- tests/integration_tests/test_catalyst.py
+++ tests/integration_tests/test_catalyst.py
@@ -7,4 +7,4 @@ def test_warning() -> None:
     study = optuna.create_study()
     trial = optuna.trial.Trial(study, study._storage.create_new_trial(study._study_id))
     with pytest.warns(FutureWarning):
-        optuna.integration.CatalystPruningCallback("abc", "def", False, trial=trial)
+        optuna.integration.CatalystPruningCallback(trial, "abc", "def", False)
