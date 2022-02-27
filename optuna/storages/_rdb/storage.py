bab8d1895f0d5d6476a62a9463f3559dbcf7db5c
tianqi xu <tianqi@preferred.jp>
2022-02-27 02:07:13 +0000

    remove in storage

--- optuna/storages/_rdb/storage.py
+++ optuna/storages/_rdb/storage.py
@@ -734,17 +734,6 @@ class RDBStorage(BaseStorage):
                 distribution_json=distributions.distribution_to_json(distribution),
             ).check_and_add(session)
 
-    def get_trial_param(self, trial_id: int, param_name: str) -> float:
-
-        with _create_scoped_session(self.scoped_session) as session:
-            trial = models.TrialModel.find_or_raise_by_id(trial_id, session)
-            trial_param = models.TrialParamModel.find_or_raise_by_trial_and_param_name(
-                trial, param_name, session
-            )
-            param_value = trial_param.param_value
-
-        return param_value
-
     @staticmethod
     def _ensure_numerical_limit(value: float) -> float:
 
@@ -877,11 +866,6 @@ class RDBStorage(BaseStorage):
                     )
                 )
             return trial_id[0]
-
-    def get_trial_number_from_id(self, trial_id: int) -> int:
-
-        trial_number = self.get_trial(trial_id).number
-        return trial_number
 
     def get_trial(self, trial_id: int) -> FrozenTrial:
 
