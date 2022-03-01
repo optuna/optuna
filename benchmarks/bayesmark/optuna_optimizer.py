e13c70c7f23de76444d28fcdb6acaf62f3f4e6ff
xadrianzetx <xadrianzetx@gmail.com>
2022-03-01 20:26:10 +0100

    Implemented `OptunaOptimizer` for bayesmark

---
+++ benchmarks/bayesmark/optuna_optimizer.py
@@ -0,0 +1,116 @@
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Union
+
+import numpy as np
+
+from bayesmark.abstract_optimizer import AbstractOptimizer
+from bayesmark.experiment import experiment_main
+import optuna
+from optuna import pruners
+from optuna import samplers
+
+
+_SAMPLERS = {
+    "GridSampler": samplers.GridSampler,
+    "RandomSampler": samplers.RandomSampler,
+    "TPESampler": samplers.TPESampler,
+    "CmaEsSampler": samplers.CmaEsSampler,
+    "NSGAIISampler": samplers.NSGAIISampler,
+}
+
+_PRUNERS = {
+    "NopPruner": pruners.NopPruner,
+    "MedianPruner": pruners.MedianPruner,
+    "PatientPruner": pruners.PatientPruner,
+    "PercentilePruner": pruners.PercentilePruner,
+    "SuccessiveHalvingPruner": pruners.SuccessiveHalvingPruner,
+    "HyperbandPruner": pruners.HyperbandPruner,
+    "ThresholdPruner": pruners.ThresholdPruner,
+}
+
+
+Suggestion = Dict[str, Union[int, float]]
+ApiConfig = Dict[str, Dict[str, str]]
+
+
+class OptunaOptimizer(AbstractOptimizer):
+
+    primary_import = "optuna"
+
+    def __init__(self, api_config: ApiConfig, **kwargs: Any) -> None:
+
+        super().__init__(api_config, **kwargs)
+
+        sampler_name = kwargs.get("sampler")
+        if sampler_name is None:
+            raise ValueError("Unknown sampler passed to Optuna optimizer.")
+
+        sampler = _SAMPLERS[sampler_name]
+        sampler_kwargs: Dict[str, Any] = kwargs["sampler_kwargs"]
+
+        pruner_name = kwargs.get("pruner")
+        if pruner_name is None:
+            raise ValueError("Unknown pruner passed to Optuna optimizer.")
+
+        pruner = _PRUNERS[pruner_name]
+        pruner_kwargs: Dict[str, Any] = kwargs["pruner_kwargs"]
+
+        # We are using negative log-likelihood for classification
+        # and MSE for regression. Both are minimized.
+        self.study = optuna.create_study(
+            direction="minimize",
+            sampler=sampler(**sampler_kwargs),
+            pruner=pruner(**pruner_kwargs),
+        )
+        self.current_trials: Dict[int, int] = dict()
+
+    def _suggest(self, trial: optuna.trial.Trial) -> Suggestion:
+
+        suggestions: Suggestion = dict()
+        for name, config in self.api_config.items():
+            low, high = config["range"]
+            log = config["space"] == "log"
+
+            if config["space"] == "logit":
+                low = np.log(low / (1 - low))
+                high = np.log(high / (1 - high))
+
+            if config["type"] == "real":
+                param = trial.suggest_float(name, low, high, log=log)
+
+            elif config["type"] == "int":
+                param = trial.suggest_int(name, low, high, log=log)
+
+            else:
+                # TODO(xadrianzetx) Support `suggest_categorical` if benchmark is extended.
+                raise RuntimeError("CategoricalDistribution is not supported in bayesmark.")
+
+            suggestions[name] = param if config["space"] != "logit" else 1 / (1 + np.exp(-param))
+
+        return suggestions
+
+    def suggest(self, n_suggestions: int) -> List[Suggestion]:
+
+        suggestions: List[Suggestion] = list()
+        for _ in range(n_suggestions):
+            trial = self.study.ask()
+            params = self._suggest(trial)
+            sid = hash(frozenset(params.items()))
+            self.current_trials[sid] = trial.number
+            suggestions.append(params)
+
+        return suggestions
+
+    def observe(self, X: List[Suggestion], y: List[float]) -> None:
+
+        for params, objective_value in zip(X, y):
+            sid = hash(frozenset(params.items()))
+            trial = self.current_trials.pop(sid)
+            self.study.tell(trial, objective_value)
+
+
+if __name__ == "__main__":
+    optuna.logging.disable_default_handler()
+    experiment_main(OptunaOptimizer)
