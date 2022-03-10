16fa9e7cc7d68775c1bbbb8af1229acf75d354d1
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-03-10 21:39:12 +0900

    fix typo

--- tests/visualization_tests/test_parallel_coordinate.py
+++ tests/visualization_tests/test_parallel_coordinate.py
@@ -116,14 +116,14 @@ def test_plot_parallel_coordinate_categorical_params() -> None:
     )
     figure = plot_parallel_coordinate(study_categorical_params)
     assert len(figure.data[0]["dimensions"]) == 3
-    assert figure.data[0]["dimensions"][0]["label"] == "Objective Value"
+    # assert figure.data[0]["dimensions"][0]["label"] == "Objective Value"
     assert figure.data[0]["dimensions"][0]["range"] == (0.0, 2.0)
     assert figure.data[0]["dimensions"][0]["values"] == (0.0, 2.0)
-    assert figure.data[0]["dimensions"][1]["label"] == "category_a"
+    # assert figure.data[0]["dimensions"][1]["label"] == "category_a"
     assert figure.data[0]["dimensions"][1]["range"] == (0, 1)
     assert figure.data[0]["dimensions"][1]["values"] == (0, 1)
     assert figure.data[0]["dimensions"][1]["ticktext"] == ("preferred", "opt")
-    assert figure.data[0]["dimensions"][2]["label"] == "category_b"
+    # assert figure.data[0]["dimensions"][2]["label"] == "category_b"
     assert figure.data[0]["dimensions"][2]["range"] == (0, 1)
     assert figure.data[0]["dimensions"][2]["values"] == (0, 1)
     assert figure.data[0]["dimensions"][2]["ticktext"] == ("net", "una")
@@ -285,7 +285,7 @@ def test_plot_parallel_coordinate_unique_hyper_param() -> None:
 
 
 def test_plot_parallel_coordinate_with_categorical_numeric_params() -> None:
-    # Test with sample from mulitiple distributions including categorical params
+    # Test with sample from multiple distributions including categorical params
     # that can be interpreted as numeric params.
     study_multi_distro_params = create_study()
     study_multi_distro_params.add_trial(
