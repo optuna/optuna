52e445dc4fec0cb0d67759785ed77b22513f26eb
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-03-10 21:44:34 +0900

    revert local change

--- tests/visualization_tests/test_parallel_coordinate.py
+++ tests/visualization_tests/test_parallel_coordinate.py
@@ -116,14 +116,14 @@ def test_plot_parallel_coordinate_categorical_params() -> None:
     )
     figure = plot_parallel_coordinate(study_categorical_params)
     assert len(figure.data[0]["dimensions"]) == 3
-    # assert figure.data[0]["dimensions"][0]["label"] == "Objective Value"
+    assert figure.data[0]["dimensions"][0]["label"] == "Objective Value"
     assert figure.data[0]["dimensions"][0]["range"] == (0.0, 2.0)
     assert figure.data[0]["dimensions"][0]["values"] == (0.0, 2.0)
-    # assert figure.data[0]["dimensions"][1]["label"] == "category_a"
+    assert figure.data[0]["dimensions"][1]["label"] == "category_a"
     assert figure.data[0]["dimensions"][1]["range"] == (0, 1)
     assert figure.data[0]["dimensions"][1]["values"] == (0, 1)
     assert figure.data[0]["dimensions"][1]["ticktext"] == ("preferred", "opt")
-    # assert figure.data[0]["dimensions"][2]["label"] == "category_b"
+    assert figure.data[0]["dimensions"][2]["label"] == "category_b"
     assert figure.data[0]["dimensions"][2]["range"] == (0, 1)
     assert figure.data[0]["dimensions"][2]["values"] == (0, 1)
     assert figure.data[0]["dimensions"][2]["ticktext"] == ("net", "una")
