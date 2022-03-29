3612258990d977319e5f858259004672b32bffe3
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-03-29 20:15:46 +0900

    Revert change using Study.optimize

--- tests/study_tests/test_optimize.py
+++ tests/study_tests/test_optimize.py
@@ -3,20 +3,12 @@ from unittest import mock
 import pytest
 
 from optuna import create_study
-from optuna import Study
 from optuna import Trial
+from optuna.study import _optimize
 from optuna.study._tell import _tell_with_warning
 from optuna.testing.storage import STORAGE_MODES
 from optuna.testing.storage import StorageSupplier
 from optuna.trial import TrialState
-
-
-N_TRIALS = 1
-
-
-def check_trial_state(study: Study, state: TrialState) -> None:
-    assert len(study.trials) == N_TRIALS
-    assert study.trials[0].state == state
 
 
 @pytest.mark.parametrize("storage_mode", STORAGE_MODES)
@@ -26,8 +18,8 @@ def test_optimize_catch_exception(storage_mode: str) -> None:
 
     with StorageSupplier(storage_mode) as storage:
         study = create_study(storage=storage)
-        study.optimize(func_value_error, catch=(ValueError,), n_trials=N_TRIALS)
-        check_trial_state(study, TrialState.FAIL)
+        frozen_trial = _optimize._run_trial(study, func_value_error, catch=(ValueError,))
+        assert frozen_trial.state == TrialState.FAIL
 
 
 @pytest.mark.parametrize("storage_mode", STORAGE_MODES)
@@ -38,40 +30,7 @@ def test_optimize_exception(storage_mode: str) -> None:
     with StorageSupplier(storage_mode) as storage:
         study = create_study(storage=storage)
         with pytest.raises(ValueError):
-            study.optimize(func_value_error, n_trials=N_TRIALS)
-
-
-@pytest.mark.parametrize("storage_mode", STORAGE_MODES)
-def test_optimize_none(storage_mode: str) -> None:
-    def func_none(_: Trial) -> float:
-        return None  # type: ignore
-
-    with StorageSupplier(storage_mode) as storage:
-        study = create_study(storage=storage)
-        study.optimize(func_none, n_trials=N_TRIALS)
-        check_trial_state(study, TrialState.FAIL)
-
-
-@pytest.mark.parametrize("storage_mode", STORAGE_MODES)
-def test_optimize_nan(storage_mode: str) -> None:
-    def func_nan(_: Trial) -> float:
-        return float("nan")
-
-    with StorageSupplier(storage_mode) as storage:
-        study = create_study(storage=storage)
-        study.optimize(func_nan, n_trials=N_TRIALS)
-        check_trial_state(study, TrialState.FAIL)
-
-
-@pytest.mark.parametrize("storage_mode", STORAGE_MODES)
-def test_optimize_nonnumerical(storage_mode: str) -> None:
-    def func_nonnumerical(_: Trial) -> float:
-        return "value"  # type: ignore
-
-    with StorageSupplier(storage_mode) as storage:
-        study = create_study(storage=storage)
-        study.optimize(func_nonnumerical, n_trials=N_TRIALS)
-        check_trial_state(study, TrialState.FAIL)
+            _optimize._run_trial(study, func_value_error, ())
 
 
 @pytest.mark.parametrize("storage_mode", STORAGE_MODES)
@@ -85,8 +44,7 @@ def test_optimize_invoke_tell_with_suppressing_warning(storage_mode: str) -> Non
         with mock.patch(
             "optuna.study._optimize._tell_with_warning", side_effect=_tell_with_warning
         ) as mock_obj:
-            study.tell = mock.MagicMock(side_effect=study.tell)  # type: ignore
-            study.optimize(func_numerical, n_trials=N_TRIALS)
+            _optimize._run_trial(study, func_numerical, ())
             mock_obj.assert_called_once_with(
                 study=mock.ANY,
                 trial=mock.ANY,
