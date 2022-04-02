73b4ef171e1778ce1093e25b0b9aeeba7c70a082
xadrianzetx <xadrianzetx@gmail.com>
2022-04-02 18:03:59 +0200

    Handle outliers in Plotly version of EDF plot

--- optuna/visualization/_edf.py
+++ optuna/visualization/_edf.py
@@ -14,6 +14,7 @@ from optuna.trial import FrozenTrial
 from optuna.trial import TrialState
 from optuna.visualization._plotly_imports import _imports
 from optuna.visualization._utils import _check_plot_args
+from optuna.visualization._utils import _filter_nonfinite
 
 
 if _imports.is_successful():
@@ -142,6 +143,7 @@ def _get_edf_plot(
             for study in studies
         )
     )
+    all_trials = _filter_nonfinite(all_trials, target)
 
     if len(all_trials) == 0:
         _logger.warning("There are no complete trials.")
@@ -160,14 +162,11 @@ def _get_edf_plot(
 
     traces = []
     for study in studies:
-        values = np.asarray(
-            [
-                target(trial)
-                for trial in study.get_trials(deepcopy=False)
-                if trial.state == TrialState.COMPLETE
-            ]
+        trials = _filter_nonfinite(
+            study.get_trials(deepcopy=False, states=(TrialState.COMPLETE,)), target=target
         )
 
+        values = np.asarray([target(trial) for trial in trials])
         y_values = np.sum(values[:, np.newaxis] <= x_values, axis=0) / values.size
 
         traces.append(go.Scatter(x=x_values, y=y_values, name=study.study_name, mode="lines"))
