16b59c9926eb47194a0c7d2882d2e0df4180ec18
contramundum53 <contramundum53@outlook.com>
2022-04-06 16:13:43 +0900

    Update tests/samplers_tests/tpe_tests/test_sampler.py
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- tests/samplers_tests/tpe_tests/test_sampler.py
+++ tests/samplers_tests/tpe_tests/test_sampler.py
@@ -758,9 +758,8 @@ def test_get_observation_pairs() -> None:
 
         x = trial.suggest_int("x", 5, 5)
         z = trial.suggest_categorical("z", [None])
-        z  # Use z here
         if trial.number == 0:
-            return x
+            return x * int(z == None)
         elif trial.number == 1:
             trial.report(1, 4)
             trial.report(2, 7)
