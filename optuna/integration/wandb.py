2a91445af4cd29c67c8c2206a7b7d7196a706356
Adrish Dey <adrishd.cse2017@nsec.ac.in>
2022-04-12 15:22:00 +0530

    Update optuna/integration/wandb.py
    Co-authored-by: Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>

--- optuna/integration/wandb.py
+++ optuna/integration/wandb.py
@@ -162,7 +162,7 @@ class WeightsAndBiasesCallback(object):
     def track_in_wandb(self) -> Callable:
         """Decorator for using W&B for logging inside the objective function.
 
-        The run is initialized with the same `wandb_kwargs` that are passed to the callback.
+        The run is initialized with the same ``wandb_kwargs`` that are passed to the callback.
         All the metrics from inside the objective function will be logged into the same run
         which stores the parameters for a given trial.
 
