de1f83fa059b42d3f69ca7f7904e111f1e0221ce
Contramundum <contramundum53@outlook.com>
2022-04-12 14:01:35 +0900

    add NasBench201 with naslib

---
+++ benchmarks/naslib/problem.py
@@ -0,0 +1,137 @@
+
+import sys
+import os
+
+sys.stdout = open(os.devnull, 'w') # Suppress output
+
+from typing import Any, Dict, List, Optional
+
+from naslib.search_spaces.core.graph import Graph
+from naslib.search_spaces.core.query_metrics import Metric
+import naslib.search_spaces as ss
+from kurobako import problem
+import naslib.search_spaces.transbench101.graph as transbench101_graph
+import naslib.search_spaces.nasbench201.graph as nasbench201_graph
+
+class NASLibProblemFactory(problem.ProblemFactory):
+    def __init__(self, search_space: Graph, **config: Dict[str, Any]) -> None:
+        self._search_space = search_space
+        self._config = config
+
+    def specification(self):
+        if isinstance(self._search_space, ss.TransBench101SearchSpaceMicro):
+            params = [problem.Var(f"x{i}", problem.CategoricalRange(transbench101_graph.OP_NAMES)) for i in range(6)]
+            self._converter = lambda x: [int(z) for z in x]
+        # elif isinstance(self._search_space, ss.NATSBenchSizeSearchSpace):
+        #     params = [problem.Var(f"x{i}", problem.DiscreteRange(1, 8)) for i in range(5)]
+        #     self._converter = lambda x: [z * 8 for z in x]
+        elif isinstance(self._search_space, ss.NasBench201SearchSpace):
+            params = [problem.Var(f"x{i}", problem.CategoricalRange(nasbench201_graph.OP_NAMES)) for i in range(6)]
+            self._converter = lambda x: [int(z) for z in x]
+        else:
+            raise NotImplementedError(f"{self._search_space} is not supported")
+        return problem.ProblemSpec(
+            name=f"{self._search_space}",
+            params=params,
+            values=[problem.Var("value")],
+        )
+
+    def create_problem(self, seed):
+        return NASLibProblem(self._search_space, self._converter, **self._config)
+
+
+class NASLibProblem(problem.Problem):
+    def __init__(self, search_space: Graph, converter, direction: str, **config: Dict[str, Any]) -> None:
+        """
+        params:
+            metric      : Metric to query for
+            dataset     : Dataset to query for
+            epoch       : If specified, returns the metric of the arch at that epoch of training
+            full_lc     : If true, returns the curve of the given metric from the first to the last epoch
+            dataset_api : API to use for querying metrics
+            hp          : Number of epochs the model was trained for. Value is in {1, 12, 90}
+            is_random   : When True, the performance of a random architecture will be returned
+                          When False, the performanceo of all trials will be averaged.
+        """
+        super().__init__()
+        self._search_space = search_space
+        self._config = config
+        self._converter = converter
+        self._scale = 1 if direction == "minimize" else -1
+
+    def create_evaluator(self, params):
+        return NASLibEvaluator(self._converter(params), self._search_space.copy(), self._scale, self._config)
+
+
+class NASLibEvaluator(problem.Evaluator):
+    def __init__(self, params: List[Optional[float]], search_space: Graph, scale: float, config: Dict[str, Any]) -> None:
+        self._search_space = search_space
+        # print(params, file=sys.stderr)
+        self._search_space.set_spec(params)
+        self._config = config
+        self._current_step = 0
+        self._scale = scale
+
+    def current_step(self):
+        return self._current_step
+
+    def evaluate(self, next_step):
+        self._current_step = 1
+        return [self._search_space.query(**self._config) * self._scale]
+
+
+import sys
+import ast
+from naslib.utils import get_dataset_api
+
+
+if __name__ == "__main__":
+    sys.stdout.close()
+    sys.stdout =sys.__stdout__
+
+    optimize_direction = {
+        "TRAIN_ACCURACY": "maximize",
+        "VAL_ACCURACY": "maximize",
+        "TEST_ACCURACY": "maximize",
+
+        "TRAIN_LOSS": "minimize",
+        "VAL_LOSS": "minimize",
+        "TEST_LOSS": "minimize",
+
+        "TRAIN_TIME": "minimize",
+        "VAL_TIME": "minimize",
+        "TEST_TIME": "minimize",
+
+        "FLOPS": "minimize",
+        "LATENCY": "minimize",
+    }
+
+    default_config = {"metric": "TEST_ACCURACY", "epoch": -1, "full_lc": False}
+
+    if len(sys.argv) < 1 + 2:
+        print("Usage: python3 nas_bench_suite/problems.py <search_space> <dataset> [<config>]")
+        print("Example: python3 nas_bench_suite/problems.py nasbench201 cifar10 \"{'metric':'TEST_ACCURACY'}\"")
+        exit(1)
+
+    search_space_name = sys.argv[1]
+    dataset = sys.argv[2]
+    search_spaces = {
+        # "nasbench101": ss.NasBench101SearchSpace(),
+        "nasbench201": ss.NasBench201SearchSpace(),
+        # "darts": ss.DartsSearchSpace(),
+        # "nlp": ss.NasBenchNLPSearchSpace(),
+        # "natsbenchsize": ss.NATSBenchSizeSearchSpace(),
+        'transbench101_micro': ss.TransBench101SearchSpaceMicro(dataset),
+        # 'transbench101_macro': ss.TransBench101SearchSpaceMacro(),
+        # "asr": ss.NasBenchASRSearchSpace(),
+    }
+    config = default_config
+    if len(sys.argv) >= 1 + 3:
+        config.update(ast.literal_eval(sys.argv[3]))
+    config["search_space"] = search_spaces[search_space_name]
+    config["direction"] = optimize_direction[config["metric"]]
+    config["metric"] = Metric[config["metric"]]
+    config["dataset"] = dataset
+    config["dataset_api"] = get_dataset_api(search_space_name, dataset)
+    runner = problem.ProblemRunner(NASLibProblemFactory(**config))
+    runner.run()
--- benchmarks/naslib/run_naslib.py
+++ benchmarks/naslib/run_naslib.py
@@ -11,32 +11,39 @@ def run(args: argparse.Namespace) -> None:
         raise ValueError(f"Data directory {args.data_dir} cannot be found.")
 
     os.makedirs(args.out_dir, exist_ok=True)
-    study_json_fn = os.path.join(args.out_dir, "studies.json")
+    study_json_filename = os.path.join(args.out_dir, "studies.json")
+    subprocess.check_call(f"echo >| {study_json_filename}", shell=True)
     solvers_filename = os.path.join(args.out_dir, "solvers.json")
+    subprocess.check_call(f"echo >| {solvers_filename}", shell=True)
     problems_filename = os.path.join(args.out_dir, "problems.json")
+    subprocess.check_call(f"echo >| {problems_filename}", shell=True)
 
-    # Create HPO bench problem.
-    datasets = [
-        "fcnet_tabular_benchmarks/fcnet_naval_propulsion_data.hdf5",
-        "fcnet_tabular_benchmarks/fcnet_parkinsons_telemonitoring_data.hdf5",
-        "fcnet_tabular_benchmarks/fcnet_protein_structure_data.hdf5",
-        "fcnet_tabular_benchmarks/fcnet_slice_localization_data.hdf5",
-    ]
-    for dataset in datasets:
-        dataset = os.path.join(args.data_dir, dataset)
-        cmd = f'{kurobako_cmd} problem hpobench "{dataset}" | tee -a {problems_filename}'
+
+    searchspace_datasets = [
+                            "nasbench201 cifar10",
+                            "nasbench201 cifar100",
+                            "nasbench201 ImageNet16-120",
+                            # "transbench101_micro class_scene",
+                            # "transbench101_micro class_object",
+                            # "transbench101_micro jigsaw",
+                            # "transbench101_micro room_layout",
+                            # "transbench101_micro segmentsemantic",
+                            # "transbench101_micro normal",
+                            # "transbench101_micro autoencoder",
+                            ]
+
+    for searchspace_dataset in searchspace_datasets:
+
+        python_command = f"benchmarks/naslib/problem.py {searchspace_dataset}"
+        cmd = (
+            f"{kurobako_cmd} problem command python3 {python_command}"
+            f"| tee -a {problems_filename}"
+        )
         subprocess.run(cmd, shell=True)
-
-    # Create NAS bench problem.
-    dataset = os.path.join(args.data_dir, "nasbench_full.bin")
-    cmd = f'{kurobako_cmd} problem nasbench "{dataset}" | tee -a {problems_filename}'
-    subprocess.run(cmd, shell=True)
 
     # Create solvers.
     sampler_list = args.sampler_list.split()
     sampler_kwargs_list = args.sampler_kwargs_list.split()
-    pruner_list = args.pruner_list.split()
-    pruner_kwargs_list = args.pruner_kwargs_list.split()
 
     if len(sampler_list) != len(sampler_kwargs_list):
         raise ValueError(
@@ -44,39 +51,35 @@ def run(args: argparse.Namespace) -> None:
             f"sampler_list: {sampler_list}, sampler_kwargs_list: {sampler_kwargs_list}."
         )
 
-    if len(pruner_list) != len(pruner_kwargs_list):
-        raise ValueError(
-            "The number of pruners does not match the given keyword arguments. \n"
-            f"pruner_list: {pruner_list}, pruner_keyword_arguments: {pruner_kwargs_list}."
-        )
-
     for sampler, sampler_kwargs in zip(sampler_list, sampler_kwargs_list):
-        for pruner, pruner_kwargs in zip(pruner_list, pruner_kwargs_list):
-            name = f"{args.name_prefix}_{sampler}_{pruner}"
+        # for pruner, pruner_kwargs in zip(pruner_list, pruner_kwargs_list):
+            name = f"{args.name_prefix}_{sampler}" # f"_{pruner}"
             cmd = (
                 f"{kurobako_cmd} solver --name {name} optuna --loglevel debug "
                 f"--sampler {sampler} --sampler-kwargs {sampler_kwargs} "
-                f"--pruner {pruner} --pruner-kwargs {pruner_kwargs} "
+                # f"--pruner {pruner} --pruner-kwargs {pruner_kwargs} "
                 f"| tee -a {solvers_filename}"
             )
             subprocess.run(cmd, shell=True)
 
+
     # Create study.
     cmd = (
-        f"{kurobako_cmd} studies --budget 80 "
+        f"{kurobako_cmd} studies --budget 100 "
         f"--solvers $(cat {solvers_filename}) --problems $(cat {problems_filename}) "
         f"--repeats {args.n_runs} --seed {args.seed} "
-        f"> {study_json_fn}"
+        f"> {study_json_filename}"
     )
     subprocess.run(cmd, shell=True)
 
     result_filename = os.path.join(args.out_dir, "results.json")
     cmd = (
-        f"cat {study_json_fn} | {kurobako_cmd} run --parallelism {args.n_jobs} "
+        f"cat {study_json_filename} | {kurobako_cmd} run --parallelism {args.n_jobs} -q "
         f"> {result_filename}"
     )
     subprocess.run(cmd, shell=True)
 
+    # Report
     report_filename = os.path.join(args.out_dir, "report.md")
     cmd = f"cat {result_filename} | {kurobako_cmd} report > {report_filename}"
     subprocess.run(cmd, shell=True)
@@ -86,12 +89,13 @@ def run(args: argparse.Namespace) -> None:
     )
     subprocess.run(cmd, shell=True)
 
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--path-to-kurobako", type=str, default="")
     parser.add_argument("--name-prefix", type=str, default="")
-    parser.add_argument("--n-runs", type=int, default=100)
+    parser.add_argument("--n-runs", type=int, default=1)
     parser.add_argument("--n-jobs", type=int, default=10)
     parser.add_argument("--sampler-list", type=str, default="RandomSampler TPESampler")
     parser.add_argument(
