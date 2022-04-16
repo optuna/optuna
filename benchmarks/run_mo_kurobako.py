5457413988f7ef9ed3982bb3184d9d20c2dff79e
Hiroshi UMEKI <31867338+drumehiron@users.noreply.github.com>
2022-04-16 11:07:43 +0900

    Update benchmarks/run_mo_kurobako.py
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- benchmarks/run_mo_kurobako.py
+++ benchmarks/run_mo_kurobako.py
@@ -93,7 +93,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--name-prefix", type=str, default="")
     parser.add_argument("--n-runs", type=int, default=100)
-    parser.add_argument("--n-jobs", type=int, default=8)
+    parser.add_argument("--n-jobs", type=int, default=10)
     parser.add_argument(
         "--sampler-list",
         type=str,
