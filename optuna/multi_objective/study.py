a151439eed91376c724c8aa503c6779eb8426d1a
Spren <baslaarakker@gmail.com>
2022-04-19 21:18:50 +0200

    import

--- optuna/multi_objective/study.py
+++ optuna/multi_objective/study.py
@@ -7,6 +7,7 @@ from typing import Iterable
 from typing import List
 from typing import Optional
 from typing import Sequence
+from typing import Tuple
 from typing import Type
 from typing import Union
 
