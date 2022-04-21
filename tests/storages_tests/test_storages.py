4bc32893ea29128bac92683593d8a10660abcf54
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-04-22 08:41:13 +0900

    Remove duplicated test case

--- tests/storages_tests/test_storages.py
+++ tests/storages_tests/test_storages.py
@@ -273,8 +273,6 @@ def test_set_and_get_study_user_attrs(storage_mode: str) -> None:
             storage.set_study_user_attr(non_existent_study_id, "key", "value")
         with pytest.raises(KeyError):
             storage.get_study_user_attrs(non_existent_study_id)
-        with pytest.raises(KeyError):
-            storage.get_study_user_attrs(non_existent_study_id)
 
 
 @pytest.mark.parametrize("storage_mode", STORAGE_MODES)
