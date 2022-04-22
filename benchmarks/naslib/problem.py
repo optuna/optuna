1575fad5012d69bd40e63f1f7e7dbc8e39665553
Contramundum <contramundum53@outlook.com>
2022-04-22 16:15:58 +0900

    Apply code review

--- benchmarks/naslib/problem.py
+++ benchmarks/naslib/problem.py
@@ -16,9 +16,11 @@ import naslib.search_spaces as ss  # NOQA
 from naslib.search_spaces.core.graph import Graph  # NOQA
 from naslib.search_spaces.core.query_metrics import Metric  # NOQA
 import naslib.search_spaces.nasbench201.graph as nasbench201_graph  # NOQA
+import naslib.search_spaces.nasbench201.conversions as nasbench201_conversion  # NOQA
 from naslib.utils import get_dataset_api  # NOQA
-    sys.stdout.close()
-    sys.stdout = sys.__stdout__
+
+sys.stdout.close()
+sys.stdout = sys.__stdout__
 
 class NASLibProblemFactory(problem.ProblemFactory):
     def __init__(self, search_space: Graph, **config: Any) -> None:
@@ -29,7 +31,7 @@ class NASLibProblemFactory(problem.ProblemFactory):
         if isinstance(self._search_space, ss.NasBench201SearchSpace):
             params = [
                 problem.Var(f"x{i}", problem.CategoricalRange(nasbench201_graph.OP_NAMES))
-                for i in range(len(nasbench201_graph.EDGE_LIST))
+                for i in range(len(nasbench201_conversion.EDGE_LIST))
             ]
             self._converter = lambda x: [int(z) for z in x]
         else:
@@ -40,7 +42,7 @@ class NASLibProblemFactory(problem.ProblemFactory):
         config_copy = config.copy()
         del config_copy["direction"]
         out = dummy.query(**config_copy)
-        steps = len(out) - 1
+        steps = len(out) - 1 # -1 because the zeroth step (untrained) is ignored
         return problem.ProblemSpec(
             name=f"{self._search_space}", params=params, values=[problem.Var("value")], steps=steps
         )
@@ -89,52 +91,29 @@ class NASLibEvaluator(problem.Evaluator):
 
 if __name__ == "__main__":
 
-    optimize_direction = {
-        "TRAIN_ACCURACY": "maximize",
-        "VAL_ACCURACY": "maximize",
-        "TEST_ACCURACY": "maximize",
-        "TRAIN_LOSS": "minimize",
-        "VAL_LOSS": "minimize",
-        "TEST_LOSS": "minimize",
-        "TRAIN_TIME": "minimize",
-        "VAL_TIME": "minimize",
-        "TEST_TIME": "minimize",
-        "FLOPS": "minimize",
-        "LATENCY": "minimize",
-    }
-
-    default_config = {"metric": "TEST_ACCURACY", "epoch": -1, "full_lc": True}
-
-        # config: arguments provided to `query` method of `search_space` in NASLib
-        # metric      : Metric to query for
-        # dataset     : Dataset to query for
-        # epoch       : If specified, returns the metric of the arch at that epoch of training
-        # full_lc     : If true, returns the curve of the given metric in all epochs
-        # dataset_api : API to use for querying metrics
-        # hp          : Number of epochs the model was trained for. Value is in {1, 12, 90}
-        # is_random   : When True, the performance of a random architecture will be returned
-        #               When False, the performanceo of all trials will be averaged.
     if len(sys.argv) < 1 + 2:
-        print("Usage: python3 nas_bench_suite/problems.py <search_space> <dataset> [<config>]")
-        print(
-            "Example: python3 nas_bench_suite/problems.py nasbench201"
-            " cifar10 \"{'metric':'TEST_ACCURACY'}\""
-        )
+        print("Usage: python3 nas_bench_suite/problems.py <search_space> <dataset>")
+        print("Example: python3 nas_bench_suite/problems.py nasbench201 cifar10")
         exit(1)
 
     search_space_name = sys.argv[1]
     dataset = sys.argv[2]
+
     search_spaces = {
         "nasbench201": ss.NasBench201SearchSpace(),
     }
-    config = default_config
-    if len(sys.argv) >= 1 + 3:
-        config.update(ast.literal_eval(sys.argv[3]))
+    
+    config = {"metric": Metric.TEST_ACCURACY, "epoch": -1, "full_lc": True, "direction": "maximize"}
+    # config: arguments provided to `query` method of `search_space` in NASLib
+    # metric      : Metric to query for
+    # dataset     : Dataset to query for
+    # epoch       : If specified, returns the metric of the arch at that epoch of training
+    # full_lc     : If true, returns the curve of the given metric in all epochs
+    # dataset_api : API to use for querying metrics
+
+
     search_space = search_spaces[search_space_name]
 
-    assert isinstance(config["metric"], str)
-    config["direction"] = optimize_direction[config["metric"]]
-    config["metric"] = Metric[config["metric"]]
     config["dataset"] = dataset
     config["dataset_api"] = get_dataset_api(search_space_name, dataset)
     runner = problem.ProblemRunner(NASLibProblemFactory(search_space, **config))
