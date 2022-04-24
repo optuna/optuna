df115a6b24fcf7e6380ee7f63a832ed37c81482f
Tomokazu Higuchi <thiguchi@preferred.jp>
2022-04-24 16:09:34 +0900

    Use coverage and pytest-cov to complement each other

--- setup.py
+++ setup.py
@@ -132,6 +132,7 @@ def get_extras_require() -> Dict[str, List[str]]:
             "fakeredis<=1.7.1; python_version<'3.8'",
             "fakeredis ; python_version>='3.8'",
             "pytest",
+            "pytest-cov",
         ],
     }
 
