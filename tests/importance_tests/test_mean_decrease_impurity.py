55656c9335909667c86538a53efc9968e43c6c91
takoika <>
2022-04-27 17:14:28 +0900

    Add a unit test to ensure that inf trial is excluded

--- tests/importance_tests/test_mean_decrease_impurity.py
+++ tests/importance_tests/test_mean_decrease_impurity.py
@@ -1,7 +1,11 @@
+import pytest
+
 from optuna import create_study
 from optuna import Trial
+from optuna.distributions import FloatDistribution
 from optuna.importance import MeanDecreaseImpurityImportanceEvaluator
 from optuna.samplers import RandomSampler
+from optuna.trial import create_trial
 
 
 def objective(trial: Trial) -> float:
@@ -71,3 +75,40 @@ def test_mean_decrease_impurity_importance_evaluator_with_target() -> None:
     )
 
     assert param_importance != param_importance_with_target
+
+
+@pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
+def test_mean_decrease_impurity_importance_evaluator_with_infinite(inf_value: float) -> None:
+    # The test ensures that trials with infinite values are ignored to calculate importance scores.
+    n_trial = 10
+    trial_sampling_seed = 13
+    evaluator_seed = 67
+
+    # Importance scores are calculated without a trial with an inf value.
+    study_without_inf = create_study(sampler=RandomSampler(seed=trial_sampling_seed))
+    study_without_inf.optimize(objective, n_trials=n_trial)
+
+    evaluator_without_inf = MeanDecreaseImpurityImportanceEvaluator(seed=evaluator_seed)
+    param_importance_without_inf = evaluator_without_inf.evaluate(study_without_inf)
+
+    # Importance scores are calculated with a trial with an inf value.
+    study_with_inf = create_study(sampler=RandomSampler(seed=trial_sampling_seed))
+    study_with_inf.optimize(objective, n_trials=n_trial)
+    # A trial with an inf value is added into the study manually.
+    study_with_inf.add_trial(
+        create_trial(
+            value=inf_value,
+            params={"x1": 1.0, "x2": 1.0, "x3": 3.0},
+            distributions={
+                "x1": FloatDistribution(low=0.1, high=3),
+                "x2": FloatDistribution(low=0.1, high=3, log=True),
+                "x3": FloatDistribution(low=2, high=4, log=True),
+            },
+        )
+    )
+    evaluator_with_inf = MeanDecreaseImpurityImportanceEvaluator(seed=evaluator_seed)
+    param_importance_with_inf = evaluator_with_inf.evaluate(study_with_inf)
+
+    # Obtained importance scores should be same among with inf and without inf,
+    # if an trial with an inf value is ignored.
+    assert param_importance_with_inf == param_importance_without_inf
