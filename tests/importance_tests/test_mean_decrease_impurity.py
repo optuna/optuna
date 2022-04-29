bf82752d394bd1bf5f310dca2880fd4e9eb1a3ae
takoika <>
2022-04-29 22:47:40 +0900

    Add unit tests for calculating importance scores with multi objective

--- tests/importance_tests/test_mean_decrease_impurity.py
+++ tests/importance_tests/test_mean_decrease_impurity.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import pytest
 
 from optuna import create_study
@@ -13,6 +15,13 @@ def objective(trial: Trial) -> float:
     x2 = trial.suggest_float("x2", 0.1, 3, log=True)
     x3 = trial.suggest_float("x3", 2, 4, log=True)
     return x1 + x2 * x3
+
+
+def multi_objective_function(trial: Trial) -> Tuple[float, float]:
+    x1 = trial.suggest_float("x1", 0.1, 3)
+    x2 = trial.suggest_float("x2", 0.1, 3, log=True)
+    x3 = trial.suggest_float("x3", 2, 4, log=True)
+    return x1, x2 * x3
 
 
 def test_mean_decrease_impurity_importance_evaluator_n_trees() -> None:
@@ -104,6 +113,42 @@ def test_mean_decrease_impurity_importance_evaluator_with_infinite(inf_value: fl
     )
     # Importance scores are calculated with a trial with an inf value.
     param_importance_with_inf = evaluator.evaluate(study)
+
+    # Obtained importance scores should be the same between with inf and without inf,
+    # because the last trial whose objective value is an inf is ignored.
+    assert param_importance_with_inf == param_importance_without_inf
+
+
+@pytest.mark.parametrize("target_idx", [0, 1])
+@pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
+def test_multi_objective_mean_decrease_impurity_importance_evaluator_with_infinite(
+    target_idx: int, inf_value: float
+) -> None:
+    # The test ensures that trials with infinite values are ignored to calculate importance scores.
+    n_trial = 10
+    seed = 13
+
+    # Importance scores are calculated without a trial with an inf value.
+    study = create_study(directions=["minimize", "minimize"], sampler=RandomSampler(seed=seed))
+    study.optimize(multi_objective_function, n_trials=n_trial)
+
+    evaluator = MeanDecreaseImpurityImportanceEvaluator(seed=seed)
+    param_importance_without_inf = evaluator.evaluate(study, target=lambda t: t.values[target_idx])
+
+    # A trial with an inf value is added into the study manually.
+    study.add_trial(
+        create_trial(
+            values=[inf_value, inf_value],
+            params={"x1": 1.0, "x2": 1.0, "x3": 3.0},
+            distributions={
+                "x1": FloatDistribution(low=0.1, high=3),
+                "x2": FloatDistribution(low=0.1, high=3, log=True),
+                "x3": FloatDistribution(low=2, high=4, log=True),
+            },
+        )
+    )
+    # Importance scores are calculated with a trial with an inf value.
+    param_importance_with_inf = evaluator.evaluate(study, target=lambda t: t.values[target_idx])
 
     # Obtained importance scores should be the same between with inf and without inf,
     # because the last trial whose objective value is an inf is ignored.
