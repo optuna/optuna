f9238652b16f2005ef102a52aa298ff92ccb822d
takoika <>
2022-04-29 14:11:00 +0900

    Add a unit test to ignore an infinite trial

--- tests/visualization_tests/matplotlib_tests/test_param_importances.py
+++ tests/visualization_tests/matplotlib_tests/test_param_importances.py
@@ -5,9 +5,13 @@ from matplotlib.patches import Rectangle
 import matplotlib.pyplot as plt
 import pytest
 
+from optuna.distributions import FloatDistribution
+from optuna.importance import FanovaImportanceEvaluator
 from optuna.importance import MeanDecreaseImpurityImportanceEvaluator
+from optuna.samplers import RandomSampler
 from optuna.study import create_study
 from optuna.testing.visualization import prepare_study_with_trials
+from optuna.trial import create_trial
 from optuna.trial import Trial
 from optuna.visualization.matplotlib import plot_param_importances
 
@@ -139,3 +143,43 @@ def test_switch_label_when_param_insignificant() -> None:
     labels = ax.figure.findobj(lambda obj: "<0.01" in str(obj))
     assert len(labels) == 1
     plt.savefig(BytesIO())
+
+
+@pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
+@pytest.mark.parametrize(
+    "evaluator_class", [MeanDecreaseImpurityImportanceEvaluator, FanovaImportanceEvaluator]
+)
+@pytest.mark.parametrize("n_trial", [0, 10])
+def test_trial_with_infinite_value_ignored(inf_value, evaluator_class, n_trial) -> None:
+    def _objective(trial: Trial) -> float:
+        x1 = trial.suggest_float("x1", 0.1, 3)
+        x2 = trial.suggest_float("x2", 0.1, 3, log=True)
+        x3 = trial.suggest_float("x3", 2, 4, log=True)
+        return x1 + x2 * x3
+
+    seed = 13
+    evaluator = evaluator_class(seed=seed)
+
+    study = create_study(sampler=RandomSampler(seed=seed))
+    study.optimize(_objective, n_trials=n_trial)
+
+    # A figure is created without a trial with an inf value.
+    figure_without_inf = plot_param_importances(study, evaluator=evaluator)
+
+    study.add_trial(
+        create_trial(
+            value=inf_value,
+            params={"x1": 1.0, "x2": 1.0, "x3": 3.0},
+            distributions={
+                "x1": FloatDistribution(low=0.1, high=3),
+                "x2": FloatDistribution(low=0.1, high=3, log=True),
+                "x3": FloatDistribution(low=2, high=4, log=True),
+            },
+        )
+    )
+    # A figure is created with a trial with an inf value.
+    figure_with_inf = plot_param_importances(study, evaluator=evaluator)
+
+    # Obtained figures should be the same between with inf and without inf,
+    # because the last trial whose objective value is an inf is ignored.
+    assert figure_without_inf == figure_with_inf
