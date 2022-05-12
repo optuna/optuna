2bc2b8df78f927b6f727e4ebb32b3cee3dda31cf
Lisong <liaison@users.noreply.github.com>
2022-05-12 22:20:00 +0200

    Update optuna/integration/shap.py
    Co-authored-by: Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>

--- optuna/integration/shap.py
+++ optuna/integration/shap.py
@@ -27,7 +27,7 @@ class ShapleyImportanceEvaluator(BaseImportanceEvaluator):
     .. note::
 
         This evaluator requires the `sklearn <https://scikit-learn.org/stable/>`_ Python package and
-        `SHAP <https://shap.readthedocs.io/en/latest/index.html>`_.
+        `SHAP <https://shap.readthedocs.io/en/stable/index.html>`_.
         The model for the SHAP calculation is based on `sklearn.ensemble.RandomForestClassifier
         <https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html>`_.
 
