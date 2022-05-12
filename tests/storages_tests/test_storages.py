ab89ad8d79d497759f9268b13067a62e367113f4
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-12 18:57:39 +0900

    Add test for set_trial_state_values

--- tests/storages_tests/test_storages.py
+++ tests/storages_tests/test_storages.py
@@ -490,9 +490,9 @@ def test_set_trial_state(storage_mode: str) -> None:
 
         # Non-existent study.
         with pytest.raises(KeyError):
-            latest_trial_id = trial_ids[-1]
+            non_existent_trial_id = max(trial_ids) + 1
             storage.set_trial_state(
-                latest_trial_id + 1,
+                non_existent_trial_id,
                 state=TrialState.COMPLETE,
             )
 
@@ -532,6 +532,14 @@ def test_set_trial_state_values_for_state(storage_mode: str) -> None:
                 assert storage.get_trial(trial_id).datetime_complete is not None
             else:
                 assert storage.get_trial(trial_id).datetime_complete is None
+
+        # Non-existent study.
+        with pytest.raises(KeyError):
+            non_existent_trial_id = max(trial_ids) + 1
+            storage.set_trial_state_values(
+                non_existent_trial_id,
+                state=TrialState.COMPLETE,
+            )
 
         for state in ALL_STATES:
             if not state.is_finished():
