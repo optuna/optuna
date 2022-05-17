60c077ffa97356892be6948957619dc479f8def6
c-bata <contact@c-bata.link>
2022-05-17 18:19:19 +0900

    Hotfix: Skip v2.4.0.a migratoin on SQLite3

--- tests/storages_tests/rdb_tests/test_storage.py
+++ tests/storages_tests/rdb_tests/test_storage.py
@@ -180,7 +180,7 @@ def test_upgrade_identity() -> None:
 
 @pytest.mark.parametrize(
     "optuna_version",
-    ["0.9.0.a", "1.2.0.a", "1.3.0.a", "2.4.0.a", "2.6.0.a", "3.0.0.a", "3.0.0.b", "3.0.0.c"],
+    ["2.6.0.a", "3.0.0.a", "3.0.0.b", "3.0.0.c"],
 )
 def test_upgrade_single_objective_optimization(optuna_version: str) -> None:
     src_db_file = os.path.join(
