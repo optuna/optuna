0b6fc73c7d856416f7209b17429abe3ddda548cc
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-05-18 00:00:36 +0900

    Add assert for random sampler

--- tests/samplers_tests/test_nsgaii.py
+++ tests/samplers_tests/test_nsgaii.py
@@ -461,6 +461,7 @@ def test_study_system_attr_for_population_cache() -> None:
 def test_reseed_rng() -> None:
     sampler = NSGAIISampler(population_size=10)
     original_random_state = sampler._rng.get_state()
+    original_random_sampler_random_state = sampler._random_sampler._sampler._rng.get_state()
 
     with patch.object(
         sampler._random_sampler, "reseed_rng", wraps=sampler._random_sampler.reseed_rng
@@ -468,6 +469,9 @@ def test_reseed_rng() -> None:
         sampler.reseed_rng()
         assert mock_object.call_count == 1
     assert str(original_random_state) != str(sampler._rng.get_state())
+    assert str(original_random_sampler_random_state) != str(
+        sampler._random_sampler._sampler._rng.get_state()
+    )
 
 
 def test_constraints_func_experimental_warning() -> None:
--- tests/samplers_tests/tpe_tests/test_sampler.py
+++ tests/samplers_tests/tpe_tests/test_sampler.py
@@ -963,6 +963,7 @@ def build_state_fn(state: optuna.trial.TrialState) -> Callable[[int], optuna.tri
 def test_reseed_rng() -> None:
     sampler = TPESampler()
     original_random_state = sampler._rng.get_state()
+    original_random_sampler_random_state = sampler._random_sampler._rng.get_state()
 
     with patch.object(
         sampler._random_sampler, "reseed_rng", wraps=sampler._random_sampler.reseed_rng
@@ -970,6 +971,9 @@ def test_reseed_rng() -> None:
         sampler.reseed_rng()
         assert mock_object.call_count == 1
     assert str(original_random_state) != str(sampler._rng.get_state())
+    assert str(original_random_sampler_random_state) != str(
+        sampler._random_sampler._rng.get_state()
+    )
 
 
 def test_call_after_trial_of_random_sampler() -> None:
