bab2a578aae6a6a770588d699652e8d3edc9727e
xadrianzetx <xadrianzetx@gmail.com>
2022-05-24 19:11:37 +0200

    Add minimal implementation of Dewancker ranker
    The same ranker is used in kurobako to summarize optimizer performance,
    which now makes kurobako and bayesmark benchmarks directly comparable.

--- benchmarks/report_bayesmark.py
+++ benchmarks/report_bayesmark.py
@@ -1,11 +1,173 @@
-from collections import Counter
+from abc import ABC
+from collections import defaultdict
 import io
-import json
+import itertools
 import os
 from typing import Dict
+from typing import Generator
+from typing import List
+from typing import Optional
+from typing import Set
+from typing import Tuple
+
+import numpy as np
+import pandas as pd
+from scipy.special import binom
+from scipy.stats import mannwhitneyu
 
 
-_TABLE_HEADER = "\n|Ranking|Solver|Score|\n|---:|:---|---:|\n"
+_LINE_BREAK = "\n"
+_TABLE_HEADER = "|Ranking|Solver|"
+_HEADER_FORMAT = "|:---|---:|"
+_OVERALL_HEADER = "|Solver|Borda|Firsts|\n|:---|---:|---:|\n"
+
+Moments = Tuple[float, float]
+Samples = Dict[str, List[float]]
+
+
+class BaseMetric(ABC):
+    @property
+    def name(self) -> str:
+        """Metric name displayed in final report."""
+
+    def calculate(self, data: pd.DataFrame) -> List[float]:
+        """Calculates metric for each study in data frame."""
+
+
+class BestValueMetric(BaseMetric):
+    name = "Best value"
+
+    def calculate(self, data: pd.DataFrame) -> List[float]:
+
+        return data.groupby("uuid").generalization.min().values
+
+
+class AUCMetric(BaseMetric):
+    name = "AUC"
+
+    def calculate(self, data: pd.DataFrame) -> List[float]:
+
+        aucs: List[float] = list()
+        for _, grp in data.groupby("uuid"):
+            auc = np.sum(grp.generalization.cummin() - grp.generalization.min())
+            aucs.append(auc / grp.shape[0])
+        return aucs
+
+
+class ElapsedMetric(BaseMetric):
+    name = "Elapsed"
+
+    def calculate(self, data: pd.DataFrame) -> List[float]:
+
+        # Total time does not include evaluation of bayesmark
+        # objective function (no Optuna APIs are called there).
+        time_cols = ["suggest", "observe"]
+        return data.groupby("uuid")[time_cols].sum().sum(axis=1).values
+
+
+class PartialReport:
+    def __init__(self, data: pd.DataFrame) -> None:
+
+        self._data = data
+
+    @property
+    def optimizers(self) -> List[str]:
+
+        return list(self._data.opt.unique())
+
+    @classmethod
+    def from_json(cls, path: str) -> "PartialReport":
+
+        data = pd.read_json(path)
+        return cls(data)
+
+    def summarize_solver(self, solver: str, metric: BaseMetric) -> Moments:
+
+        solver_data = self._data[self._data.opt == solver]
+        if solver_data.shape[0] == 0:
+            raise ValueError(f"{solver} not found in report.")
+
+        run_metrics = metric.calculate(solver_data)
+        return np.mean(run_metrics), np.var(run_metrics)
+
+    def sample_performance(self, metric: BaseMetric) -> Samples:
+
+        performance: Dict[str, List[float]] = {}
+        for solver, data in self._data.groupby("opt"):
+            run_metrics = metric.calculate(data)
+            performance[solver] = run_metrics
+        return performance
+
+
+class DewanckerRanker:
+    def __init__(self, metrics: List[BaseMetric]) -> None:
+        self._metrics = metrics
+        self._ranking: Optional[List[str]] = None
+        self._borda: Optional[np.ndarray] = None
+
+    def __iter__(self) -> Generator[Tuple[str, int], None, None]:
+
+        yield from zip(self.solvers, self.borda)
+
+    @property
+    def solvers(self) -> List[str]:
+
+        if self._ranking is None:
+            raise ValueError("Call rank first.")
+        return self._ranking
+
+    @property
+    def borda(self) -> np.ndarray:
+
+        if self._borda is None:
+            raise ValueError("Call rank first.")
+        return self._borda
+
+    @staticmethod
+    def pick_alpha(report: PartialReport) -> float:
+
+        # https://github.com/optuna/kurobako/blob/788dd4cf618965a4a5158aa4e13607a5803dea9d/src/report.rs#L412-L424  # noqa E501
+        num_optimizers = len(report.optimizers)
+        candidates = [0.075, 0.05, 0.025, 0.01] * 4 / np.repeat([1, 10, 100, 1000], 4)
+
+        for cand in candidates:
+            if 1 - np.power((1 - cand), binom(num_optimizers, 2)) < 0.05:
+                return cand
+        return candidates[-1]
+
+    def _set_ranking(self, wins: Dict[str, int]) -> None:
+
+        sorted_wins = [k for k, _ in sorted(wins.items(), key=lambda x: x[1])]
+        self._ranking = sorted_wins[::-1]
+
+    def _set_borda(self, wins: Dict[str, int]) -> None:
+
+        sorted_wins = np.array(sorted(wins.values()))
+        num_wins, num_ties = np.unique(sorted_wins, return_counts=True)
+        points = np.searchsorted(sorted_wins, num_wins)
+        self._borda = np.repeat(points, num_ties)[::-1]
+
+    def rank(self, report: PartialReport) -> None:
+
+        # Implements Section 2.1.1
+        # https://proceedings.mlr.press/v64/dewancker_strategy_2016.pdf
+        wins: Dict[str, int] = defaultdict(int)
+        alpha = DewanckerRanker.pick_alpha(report)
+        for metric in self._metrics:
+            samples = report.sample_performance(metric)
+            for optim_a, optim_b in itertools.permutations(samples, 2):
+                _, p_val = mannwhitneyu(samples[optim_a], samples[optim_b], alternative="less")
+                if p_val < alpha:
+                    wins[optim_a] += 1
+
+            all_wins = [wins[optimizer] for optimizer in report.optimizers]
+            no_ties = len(all_wins) == len(np.unique(all_wins))
+            if no_ties:
+                break
+
+        wins = {optimzier: wins[optimzier] for optimzier in report.optimizers}
+        self._set_ranking(wins)
+        self._set_borda(wins)
 
 
 class BayesmarkReportBuilder:
