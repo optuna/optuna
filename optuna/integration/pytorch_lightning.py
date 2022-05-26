b9bcaf185b5ddbeabe384b677356dfc32d153da2
Naoto Mizuno <naotomizuno@preferred.jp>
2022-05-26 09:57:30 +0900

    mypy for PyTorch Lightning 1.6.0

--- optuna/integration/pytorch_lightning.py
+++ optuna/integration/pytorch_lightning.py
@@ -58,7 +58,9 @@ class PyTorchLightningPruningCallback(Callback):
         self.is_ddp_backend = False
 
     def on_init_start(self, trainer: Trainer) -> None:
-        self.is_ddp_backend = trainer._accelerator_connector.distributed_backend is not None
+        self.is_ddp_backend = (
+            trainer._accelerator_connector.distributed_backend is not None  # type: ignore
+        )
         if self.is_ddp_backend:
             if version.parse(pl.__version__) < version.parse("1.5.0"):
                 raise ValueError("PyTorch Lightning>=1.5.0 is required in DDP.")
