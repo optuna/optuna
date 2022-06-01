b15975e4804b479e9b257865580b084ce497f20c
Contramundum <contramundum53@outlook.com>
2022-06-01 09:35:08 +0900

    revert test

--- tests/storages_tests/test_with_server.py
+++ tests/storages_tests/test_with_server.py
@@ -107,10 +107,13 @@ def test_loaded_trials(storage_url: str) -> None:
 
 
 @pytest.mark.parametrize(
-    "value",
-    [1.0, float("inf"), -float("inf"), float("nan")],
+    "input_value,expected",
+    [
+        (float("inf"), float("inf")),
+        (-float("inf"), -float("inf")),
+    ],
 )
-def test_store_values(value: float, storage_url: str) -> None:
+def test_store_infinite_values(input_value: float, expected: float, storage_url: str) -> None:
 
     storage: Union[RDBStorage, RedisStorage]
     if storage_url.startswith("redis"):
@@ -119,15 +122,27 @@ def test_store_values(value: float, storage_url: str) -> None:
         storage = optuna.storages.RDBStorage(url=storage_url)
     study_id = storage.create_new_study()
     trial_id = storage.create_new_trial(study_id)
+    storage.set_trial_intermediate_value(trial_id, 1, input_value)
+    storage.set_trial_state_values(trial_id, state=TrialState.COMPLETE, values=(input_value,))
+    assert storage.get_trial(trial_id).value == expected
+    assert storage.get_trial(trial_id).intermediate_values[1] == expected
+
+
+def test_store_nan_intermediate_values(storage_url: str) -> None:
+
+    storage: Union[RDBStorage, RedisStorage]
+    if storage_url.startswith("redis"):
+        storage = optuna.storages.RedisStorage(url=storage_url)
+    else:
+        storage = optuna.storages.RDBStorage(url=storage_url)
+    study_id = storage.create_new_study()
+    trial_id = storage.create_new_trial(study_id)
+
+    value = float("nan")
     storage.set_trial_intermediate_value(trial_id, 1, value)
 
     got_value = storage.get_trial(trial_id).intermediate_values[1]
-    assert got_value == value or (np.isnan(got_value) and np.isnan(value))
-
-    # A `nan` is not acceptable as the final value.
-    if not np.isnan(value):
-        storage.set_trial_state_values(trial_id, state=TrialState.COMPLETE, values=(value,))
-        assert storage.get_trial(trial_id).value == value
+    assert np.isnan(got_value)
 
 
 def test_multiprocess(storage_url: str) -> None:
