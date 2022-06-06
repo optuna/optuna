829ed6d8d9dc45944753df30ce8004520c714da9
Toshihiko Yanase <toshihiko.yanase@gmail.com>
2022-06-06 17:43:23 +0900

    Fix black version.

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_extras_require() -> Dict[str, List[str]]:
     requirements = {
         # TODO(HideakiImamura) Unpin mypy version after fixing "Duplicate modules" error in
         # tutorials.
-        "checking": ["black", "hacking", "isort", "mypy==0.790", "blackdoc"],
+        "checking": ["black==21.9b0", "hacking", "isort", "mypy==0.790", "blackdoc"],
         "codecov": ["codecov", "pytest-cov"],
         "doctest": [
             "cma",
