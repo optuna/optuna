98251d2aa2942f7541a4d937ddaf0b9351cbda4a
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-06-21 22:59:21 +0900

    Remove unnecessary tuple

--- optuna/visualization/_optimization_history.py
+++ optuna/visualization/_optimization_history.py
@@ -103,8 +103,7 @@ def _get_optimization_history_plot(
 
     all_trials = list(
         itertools.chain.from_iterable(
-            (trial for trial in study.get_trials(deepcopy=False, states=(TrialState.COMPLETE,)))
-            for study in studies
+            study.get_trials(deepcopy=False, states=(TrialState.COMPLETE,)) for study in studies
         )
     )
 
