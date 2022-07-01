c7e5d2e50d64baf6b9e0c7ac2adee73c9247e312
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-07-01 22:06:25 +0900

    Implemenet test code suggested by mamu

--- tests/visualization_tests/test_edf.py
+++ tests/visualization_tests/test_edf.py
@@ -2,6 +2,7 @@ from io import BytesIO
 from typing import Any
 from typing import Callable
 from typing import List
+from typing import Optional
 from typing import Union
 
 import numpy as np
@@ -13,6 +14,7 @@ from optuna.study import create_study
 from optuna.testing.visualization import prepare_study_with_trials
 from optuna.trial import create_trial
 from optuna.visualization import plot_edf as plotly_plot_edf
+from optuna.visualization._edf import _EDFInfo
 from optuna.visualization._edf import _get_edf_info
 from optuna.visualization._edf import NUM_SAMPLES_X_AXIS
 from optuna.visualization._plotly_imports import _imports as plotly_imports
@@ -38,7 +40,7 @@ def save_static_image(figure: Union[go.Figure, Axes, np.ndarray]) -> None:
         plt.savefig(BytesIO())
 
 
-@parametrized_plot_contour
+@parametrized_plot_edf
 def _validate_edf_values(edf_values: np.ndarray) -> None:
     np_values = np.array(edf_values)
     # Confirms that the values are monotonically non-decreasing.
@@ -48,7 +50,7 @@ def _validate_edf_values(edf_values: np.ndarray) -> None:
     assert np.all((0 <= np_values) & (np_values <= 1))
 
 
-@parametrized_plot_contour
+@parametrized_plot_edf
 def test_target_is_none_and_study_is_multi_obj(plot_edf: Callable[..., Any]) -> None:
 
     study = create_study(directions=["minimize", "minimize"])
@@ -56,14 +58,14 @@ def test_target_is_none_and_study_is_multi_obj(plot_edf: Callable[..., Any]) -> 
         plot_edf(study)
 
 
-@parametrized_plot_contour
+@parametrized_plot_edf
 @pytest.mark.parametrize("direction", ["minimize", "maximize"])
 def test_edf_plot_no_trials(plot_edf: Callable[..., Any], direction: str) -> None:
     figure = plot_edf(create_study(direction=direction))
     save_static_image(figure)
 
 
-@parametrized_plot_contour
+@parametrized_plot_edf
 @pytest.mark.parametrize("direction", ["minimize", "maximize"])
 @pytest.mark.parametrize("num_studies", [0, 1, 2])
 def test_edf_plot_no_trials_studies(
@@ -74,10 +76,10 @@ def test_edf_plot_no_trials_studies(
     save_static_image(figure)
 
 
-@parametrized_plot_contour
+@parametrized_plot_edf
 @pytest.mark.parametrize("direction", ["minimize", "maximize"])
 @pytest.mark.parametrize("num_studies", [0, 1, 2])
-def test_edf_plot_target_label_studies(
+def test_plot_edf_with_multiple_studies(
     plot_edf: Callable[..., Any], direction: str, num_studies: int
 ) -> None:
     studies = []
@@ -86,45 +88,76 @@ def test_edf_plot_target_label_studies(
         study.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
         studies.append(study)
     figure = plot_edf(studies)
+    save_static_image(figure)
 
-    expected = "Objective Value"
-    if isinstance(figure, go.Figure):
-        assert figure.layout.xaxis.title.text == expected
-    elif isinstance(figure, Axes):
-        assert figure.xaxis.label.get_text() == expected
 
+@parametrized_plot_edf
+def test_plot_edf_with_target(plot_edf: Callable[..., Any]) -> None:
+    study = create_study()
+    study.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
     with pytest.warns(UserWarning):
-        figure = plot_edf(studies, target=lambda t: t.params["x"])
+        figure = plot_edf(study, target=lambda t: t.params["x"])
+    save_static_image(figure)
+
+
+@parametrized_plot_edf
+@pytest.mark.parametrize("target_name", [None, "Target Name"])
+def test_plot_edf_with_target_name(
+    plot_edf: Callable[..., Any], target_name: Optional[str]
+) -> None:
+    study = create_study()
+    study.optimize(lambda t: t.suggest_float("x", 0, 5), n_trials=10)
+    if target_name is None:
+        figure = plot_edf(study)
+    else:
+        figure = plot_edf(study, target_name=target_name)
+
+    expected = target_name if target_name is not None else "Objective Value"
     if isinstance(figure, go.Figure):
         assert figure.layout.xaxis.title.text == expected
     elif isinstance(figure, Axes):
         assert figure.xaxis.label.get_text() == expected
 
-    # Test with a customized target name.
-    figure = plot_edf(studies, target_name="Target Name")
-    expected = "Target Name"
-    if isinstance(figure, go.Figure):
-        assert figure.layout.xaxis.title.text == expected
-    elif isinstance(figure, Axes):
-        assert figure.xaxis.label.get_text() == expected
+    save_static_image(figure)
 
 
 def test_empty_edf_info() -> None:
+    def _assert_empty(info: _EDFInfo) -> None:
+        assert info.lines == []
+        np.testing.assert_array_equal(info.x_values, np.array([]))
 
     edf_info = _get_edf_info([])
-    assert edf_info.lines == []
-    np.testing.assert_array_equal(edf_info.x_values, np.array([]))
+    _assert_empty(edf_info)
 
     study = create_study()
     edf_info = _get_edf_info(study)
-    assert edf_info.lines == []
-    np.testing.assert_array_equal(edf_info.x_values, np.array([]))
+    _assert_empty(edf_info)
 
     trial = study.ask()
     study.tell(trial, state=optuna.trial.TrialState.PRUNED)
     edf_info = _get_edf_info(study)
-    assert edf_info.lines == []
-    np.testing.assert_array_equal(edf_info.x_values, np.array([]))
+    _assert_empty(edf_info)
+
+
+@pytest.mark.parametrize("n_studies", [1, 2, 3])
+@pytest.mark.parametrize("target", [None, lambda t: t.params["x"]])
+def test_get_edf_info(n_studies: int, target: Callable[[optuna.trial.FrozenTrial], float]) -> None:
+    studies = []
+    n_trials = 3
+    max_target = 5
+    for i in range(n_studies):
+        study = create_study(study_name=str(i))
+        study.optimize(lambda t: t.suggest_float("x", 0, max_target) ** 2, n_trials=n_trials)
+        studies.append(study)
+
+    info = _get_edf_info(studies, target=target)
+    assert info.x_values.shape == (NUM_SAMPLES_X_AXIS,)
+    assert all([0 <= x <= max_target for x in info.x_values])
+    assert len(info.lines) == n_studies
+    for i, line in enumerate(info.lines):
+        assert str(i) == line.study_name
+        assert line.y_values.shape == (NUM_SAMPLES_X_AXIS,)
+        _validate_edf_values(line.y_values)
 
 
 @pytest.mark.parametrize("value", [float("inf"), -float("inf"), float("nan")])
