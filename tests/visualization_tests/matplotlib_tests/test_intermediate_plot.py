efd7763e39a313b331a80df6f1b0eff3f78dad2c
Contramundum <contramundum53@outlook.com>
2022-07-01 15:36:26 +0900

    format

--- tests/visualization_tests/matplotlib_tests/test_intermediate_plot.py
+++ tests/visualization_tests/matplotlib_tests/test_intermediate_plot.py
@@ -2,8 +2,9 @@ from io import BytesIO
 
 import matplotlib.pyplot as plt
 
+from optuna.visualization._intermediate_values import _IntermediatePlotInfo
+from optuna.visualization._intermediate_values import _TrialInfo
 from optuna.visualization.matplotlib._intermediate_values import _get_intermediate_plot
-from optuna.visualization._intermediate_values import _IntermediatePlotInfo, _TrialInfo
 
 
 def test_plot_intermediate_values() -> None:
@@ -14,9 +15,7 @@ def test_plot_intermediate_values() -> None:
     plt.savefig(BytesIO())
 
     # Test with a trial with intermediate values.
-    figure = _get_intermediate_plot(_IntermediatePlotInfo([
-        _TrialInfo(0, [(0, 1.0), (1, 2.0)])
-    ]))
+    figure = _get_intermediate_plot(_IntermediatePlotInfo([_TrialInfo(0, [(0, 1.0), (1, 2.0)])]))
     assert len(figure.get_lines()) == 1
     assert list(figure.get_lines()[0].get_xdata()) == [0, 1]
     assert list(figure.get_lines()[0].get_ydata()) == [1.0, 2.0]
--- tests/visualization_tests/test_intermediate_plot.py
+++ tests/visualization_tests/test_intermediate_plot.py
@@ -2,7 +2,9 @@ from optuna.study import create_study
 from optuna.testing.visualization import prepare_study_with_trials
 from optuna.trial import Trial
 from optuna.visualization._intermediate_values import _get_intermediate_plot
-from optuna.visualization._intermediate_values import _get_intermediate_plot_info, _IntermediatePlotInfo, _TrialInfo
+from optuna.visualization._intermediate_values import _get_intermediate_plot_info
+from optuna.visualization._intermediate_values import _IntermediatePlotInfo
+from optuna.visualization._intermediate_values import _TrialInfo
 
 
 def test_intermediate_plot_info() -> None:
@@ -17,28 +19,27 @@ def test_intermediate_plot_info() -> None:
             trial.report(1.0, step=0)
             trial.report(2.0, step=1)
         return 0.0
+
     study = create_study()
     study.optimize(lambda t: objective(t, True), n_trials=1)
 
-    assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo([
-        _TrialInfo(0, [(0, 1.0), (1, 2.0)])
-    ])
-
+    assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo(
+        [_TrialInfo(0, [(0, 1.0), (1, 2.0)])]
+    )
 
     # Test a study with one trial with intermediate values and
     # one trial without intermediate values.
     # Expect the trial with no intermediate values to be ignored.
     study.optimize(lambda t: objective(t, False), n_trials=1)
-    
-    assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo([
-        _TrialInfo(0, [(0, 1.0), (1, 2.0)])
-    ])
+
+    assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo(
+        [_TrialInfo(0, [(0, 1.0), (1, 2.0)])]
+    )
 
     # Test a study of only one trial that has no intermediate values.
     study = create_study()
     study.optimize(lambda t: objective(t, False), n_trials=1)
     assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo([])
-
 
     # Ignore failed trials.
     def fail_objective(trial: Trial) -> float:
@@ -49,16 +50,14 @@ def test_intermediate_plot_info() -> None:
     study = create_study()
     study.optimize(fail_objective, n_trials=1, catch=(ValueError,))
     assert _get_intermediate_plot_info(study) == _IntermediatePlotInfo([])
-    
+
 
 def test_plot_intermediate_values() -> None:
     figure = _get_intermediate_plot(_IntermediatePlotInfo([]))
     assert not figure.data
 
     # Test with a trial with intermediate values.
-    figure = _get_intermediate_plot(_IntermediatePlotInfo([
-        _TrialInfo(0, [(0, 1.0), (1, 2.0)])
-    ]))
+    figure = _get_intermediate_plot(_IntermediatePlotInfo([_TrialInfo(0, [(0, 1.0), (1, 2.0)])]))
     assert len(figure.data) == 1
     assert figure.data[0].x == (0, 1)
     assert figure.data[0].y == (1.0, 2.0)
