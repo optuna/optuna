287b0c6ac756934762e1854ad37a1e717b052f58
Kento Nozawa <k_nzw@klis.tsukuba.ac.jp>
2022-07-06 22:54:36 +0900

    Unify test for plot_param_importances

--- tests/visualization_tests/test_param_importances.py
+++ tests/visualization_tests/test_param_importances.py
@@ -1,87 +1,141 @@
-import math
+from io import BytesIO
+from typing import Any
+from typing import Callable
+from typing import List
+from typing import Optional
 from typing import Tuple
 
 import pytest
 
+import optuna
 from optuna.distributions import FloatDistribution
 from optuna.importance import FanovaImportanceEvaluator
 from optuna.importance import MeanDecreaseImpurityImportanceEvaluator
 from optuna.importance._base import BaseImportanceEvaluator
 from optuna.samplers import RandomSampler
 from optuna.study import create_study
+from optuna.study import Study
 from optuna.testing.visualization import prepare_study_with_trials
 from optuna.trial import create_trial
 from optuna.trial import Trial
-from optuna.visualization import plot_param_importances
+from optuna.visualization import plot_param_importances as plotly_plot_param_importances
+from optuna.visualization._param_importances import _get_importances_info
+from optuna.visualization._plotly_imports import _imports as plotly_imports
+from optuna.visualization.matplotlib import plot_param_importances as plt_plot_param_importances
+from optuna.visualization.matplotlib._matplotlib_imports import _imports as plt_imports
+
+
+if plotly_imports.is_successful():
+    from optuna.visualization._plotly_imports import go
+
+
+if plt_imports.is_successful():
+    from optuna.visualization.matplotlib._matplotlib_imports import Axes
+    from optuna.visualization.matplotlib._matplotlib_imports import plt
+
+parametrize_plot_param_importances = pytest.mark.parametrize(
+    "plot_param_importances", [plotly_plot_param_importances, plt_plot_param_importances]
+)
+
+
+def _create_study_with_only_error_trial() -> optuna.Study:
+    study = create_study()
+    trial = study.ask()
+    study.tell(trial, state=optuna.trial.TrialState.FAIL)
+    return study
 
 
 def test_target_is_none_and_study_is_multi_obj() -> None:
-
     study = create_study(directions=["minimize", "minimize"])
     with pytest.raises(ValueError):
-        plot_param_importances(study)
-
-
-def test_plot_param_importances() -> None:
-
-    # Test with no trial.
-    study = create_study()
-    figure = plot_param_importances(study)
-    assert len(figure.data) == 0
-
-    study = prepare_study_with_trials(with_c_d=True)
-
-    # Test with a trial.
-    figure = plot_param_importances(study)
-    assert len(figure.data) == 1
-    assert set(figure.data[0].y) == set(
-        ("param_b", "param_d")
-    )  # "param_a", "param_c" are conditional.
-    assert math.isclose(1.0, sum(i for i in figure.data[0].x), abs_tol=1e-5)
-    assert figure.layout.xaxis.title.text == "Importance for Objective Value"
-
-    # Test with an evaluator.
-    plot_param_importances(study, evaluator=MeanDecreaseImpurityImportanceEvaluator())
-    assert len(figure.data) == 1
-    assert set(figure.data[0].y) == set(
-        ("param_b", "param_d")
-    )  # "param_a", "param_c" are conditional.
-    assert math.isclose(1.0, sum(i for i in figure.data[0].x), abs_tol=1e-5)
-
-    # Test with a trial to select parameter.
-    figure = plot_param_importances(study, params=["param_b"])
-    assert len(figure.data) == 1
-    assert figure.data[0].y == ("param_b",)
-    assert math.isclose(1.0, sum(i for i in figure.data[0].x), abs_tol=1e-5)
-
-    # Test with a customized target value.
-    with pytest.warns(UserWarning):
-        figure = plot_param_importances(
-            study, target=lambda t: t.params["param_b"] + t.params["param_d"]
+        _get_importances_info(
+            study=study, evaluator=None, params=None, target=None, target_name="Objective Value"
         )
-    assert len(figure.data) == 1
-    assert set(figure.data[0].y) == set(
-        ("param_b", "param_d")
-    )  # "param_a", "param_c" are conditional.
-    assert math.isclose(1.0, sum(i for i in figure.data[0].x), abs_tol=1e-5)
 
-    # Test with a customized target name.
-    figure = plot_param_importances(study, target_name="Target Name")
-    assert figure.layout.xaxis.title.text == "Importance for Target Name"
 
-    # Test with wrong parameters.
-    with pytest.raises(ValueError):
-        plot_param_importances(study, params=["optuna"])
-
-    # Ignore failed trials.
-    def fail_objective(_: Trial) -> float:
-
-        raise ValueError
+def test_get_param_importances_info_for_one_trial() -> None:
+    # Cannot obtain importance scores from one complete trial.
 
     study = create_study()
-    study.optimize(fail_objective, n_trials=1, catch=(ValueError,))
-    figure = plot_param_importances(study)
-    assert len(figure.data) == 0
+    study.optimize(lambda t: t.suggest_float("x", 0, 1), n_trials=1)
+
+    with pytest.raises(ValueError):
+        _get_importances_info(study, None, None, None, "Objective Value")
+
+
+@parametrize_plot_param_importances
+def test_plot_param_importances_customized_target_name(
+    plot_param_importances: Callable[..., Any]
+) -> None:
+
+    params = ["param_a", "param_b"]
+    study = prepare_study_with_trials()
+    figure = plot_param_importances(study, params=params, target_name="Target Name")
+    if isinstance(figure, go.Figure):
+        assert figure.layout.xaxis.title.text == "Importance for Target Name"
+    elif isinstance(figure, Axes):
+        assert figure.figure.axes[0].get_xlabel() == "Importance for Target Name"
+
+
+@parametrize_plot_param_importances
+@pytest.mark.parametrize(
+    "specific_create_study",
+    [
+        lambda: prepare_study_with_trials(no_trials=True),
+        _create_study_with_only_error_trial,
+        prepare_study_with_trials,
+    ],
+)
+@pytest.mark.parametrize(
+    "params",
+    [
+        [],
+        ["param_a"],
+        ["param_a", "param_b"],
+        ["param_a", "param_b", "param_c"],
+        ["param_a", "param_b", "param_c", "param_d"],
+        None,
+    ],
+)
+def test_plot_param_importances(
+    plot_param_importances: Callable[..., Any],
+    specific_create_study: Callable[[], Study],
+    params: Optional[List[str]],
+) -> None:
+    study = specific_create_study()
+    figure = plot_param_importances(study, params=params)
+    if isinstance(figure, go.Figure):
+        figure.write_image(BytesIO())
+    else:
+        plt.savefig(BytesIO())
+
+
+@pytest.mark.parametrize(
+    "specific_create_study",
+    [lambda: prepare_study_with_trials(no_trials=True), _create_study_with_only_error_trial],
+)
+@pytest.mark.parametrize(
+    "params",
+    [
+        [],
+        ["param_a"],
+        ["param_a", "param_b"],
+        ["param_a", "param_b", "param_c"],
+        ["param_a", "param_b", "param_c", "param_d"],
+        None,
+    ],
+)
+def test_get_param_importances_info_empty(
+    specific_create_study: Callable[[], Study], params: Optional[List[str]]
+) -> None:
+
+    study = specific_create_study()
+    info = _get_importances_info(
+        study, None, params=params, target=None, target_name="Objective Value"
+    )
+    assert len(info.importance_values) == 0
+    assert len(info.param_names) == 0
+    assert len(info.importance_labels) == 0
 
 
 def test_switch_label_when_param_insignificant() -> None:
@@ -95,11 +149,11 @@ def test_switch_label_when_param_insignificant() -> None:
         study.enqueue_trial({"x": x, "y": 0})
 
     study.optimize(_objective, n_trials=2)
-    figure = plot_param_importances(study)
+
+    info = _get_importances_info(study, None, None, None, "Objective Value")
 
     # Test if label for `y` param has been switched to `<0.01`.
-    labels = figure.data[0].text
-    assert labels == ("<0.01", "1.00")
+    assert info.importance_labels == ["<0.01", "1.00"]
 
 
 @pytest.mark.parametrize("inf_value", [float("inf"), -float("inf")])
@@ -107,9 +161,9 @@ def test_switch_label_when_param_insignificant() -> None:
     "evaluator",
     [MeanDecreaseImpurityImportanceEvaluator(seed=10), FanovaImportanceEvaluator(seed=10)],
 )
-@pytest.mark.parametrize("n_trial", [0, 10])
-def test_trial_with_infinite_value_ignored(
-    inf_value: int, evaluator: BaseImportanceEvaluator, n_trial: int
+@pytest.mark.parametrize("n_trials", [0, 10])
+def test_get_info_importances_nonfinite_removed(
+    inf_value: float, evaluator: BaseImportanceEvaluator, n_trials: int
 ) -> None:
     def _objective(trial: Trial) -> float:
         x1 = trial.suggest_float("x1", 0.1, 3)
@@ -118,12 +172,15 @@ def test_trial_with_infinite_value_ignored(
         return x1 + x2 * x3
 
     seed = 13
+    target_name = "Objective Value"
 
     study = create_study(sampler=RandomSampler(seed=seed))
-    study.optimize(_objective, n_trials=n_trial)
+    study.optimize(_objective, n_trials=n_trials)
 
-    # A figure is created without a trial with an inf value.
-    figure_without_inf = plot_param_importances(study, evaluator=evaluator)
+    # Create param importances info without inf value.
+    info_without_inf = _get_importances_info(
+        study, evaluator=evaluator, params=None, target=None, target_name=target_name
+    )
 
     # A trial with an inf value is added into the study manually.
     study.add_trial(
@@ -138,12 +195,14 @@ def test_trial_with_infinite_value_ignored(
         )
     )
 
-    # A figure is created with a trial with an inf value.
-    figure_with_inf = plot_param_importances(study, evaluator=evaluator)
+    # Create param importances info with inf value.
+    info_with_inf = _get_importances_info(
+        study, evaluator=evaluator, params=None, target=None, target_name=target_name
+    )
 
-    # Obtained figures should be the same between with inf and without inf,
+    # Obtained info instances should be the same between with inf and without inf,
     # because the last trial whose objective value is an inf is ignored.
-    assert figure_without_inf == figure_with_inf
+    assert info_with_inf == info_without_inf
 
 
 @pytest.mark.parametrize("target_idx", [0, 1])
@@ -163,13 +222,18 @@ def test_multi_objective_trial_with_infinite_value_ignored(
         return x1, x2 * x3
 
     seed = 13
+    target_name = "Objective Value"
 
     study = create_study(directions=["minimize", "minimize"], sampler=RandomSampler(seed=seed))
     study.optimize(_multi_objective_function, n_trials=n_trial)
 
-    # A figure is created without a trial with an inf value.
-    figure_without_inf = plot_param_importances(
-        study, evaluator=evaluator, target=lambda t: t.values[target_idx]
+    # Create param importances info without inf value.
+    info_without_inf = _get_importances_info(
+        study,
+        evaluator=evaluator,
+        params=None,
+        target=lambda t: t.values[target_idx],
+        target_name=target_name,
     )
 
     # A trial with an inf value is added into the study manually.
@@ -185,11 +249,15 @@ def test_multi_objective_trial_with_infinite_value_ignored(
         )
     )
 
-    # A figure is created with a trial with an inf value.
-    figure_with_inf = plot_param_importances(
-        study, evaluator=evaluator, target=lambda t: t.values[target_idx]
+    # Create param importances info with inf value.
+    info_with_inf = _get_importances_info(
+        study,
+        evaluator=evaluator,
+        params=None,
+        target=lambda t: t.values[target_idx],
+        target_name=target_name,
     )
 
-    # Obtained figures should be the same between with inf and without inf,
+    # Obtained info instances should be the same between with inf and without inf,
     # because the last trial whose objective value is an inf is ignored.
-    assert figure_without_inf == figure_with_inf
+    assert info_with_inf == info_without_inf
