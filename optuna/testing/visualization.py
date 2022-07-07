901d0765159836adc39545e79b41bbe00325bc47
c-bata <contact@c-bata.link>
2022-07-07 17:12:42 +0900

    Remove more_than_three option from prepare_study_with_trials

--- optuna/testing/visualization.py
+++ optuna/testing/visualization.py
@@ -5,7 +5,6 @@ from optuna.trial import create_trial
 
 
 def prepare_study_with_trials(
-    more_than_three: bool = False,
     with_c_d: bool = True,
     n_objectives: int = 1,
     direction: str = "minimize",
@@ -15,9 +14,6 @@ def prepare_study_with_trials(
     """Prepare a study for tests.
 
     Args:
-        more_than_three: If :obj:`True`, create a study with two/four hyperparameters where
-            'param_a' (and 'param_c') appear(s) only three times while 'param_b' (and 'param_d')
-            appear(s) four times in `study.trials`.
         with_c_d: If :obj:`True`, the study has four hyperparameters named 'param_a',
             'param_b', 'param_c', and 'param_d'. Otherwise, there are only two
             hyperparameters ('param_a' and 'param_b').
@@ -82,25 +78,4 @@ def prepare_study_with_trials(
             },
         )
     )
-
-    if more_than_three:
-        study.add_trial(
-            create_trial(
-                values=[1.5] * n_objectives,
-                params={"param_a": 0.5, "param_b": 1.5, "param_c": 2.0, "param_d": 3.0}
-                if with_c_d
-                else {"param_a": 0.5, "param_b": 1.5},
-                distributions={
-                    "param_a": FloatDistribution(0.0, 3.0),
-                    "param_b": FloatDistribution(0.0, 3.0),
-                    "param_c": FloatDistribution(2.0, 5.0),
-                    "param_d": FloatDistribution(2.0, 5.0),
-                }
-                if with_c_d
-                else {
-                    "param_a": FloatDistribution(0.0, 3.0),
-                    "param_b": FloatDistribution(0.0, 3.0),
-                },
-            )
-        )
     return study
