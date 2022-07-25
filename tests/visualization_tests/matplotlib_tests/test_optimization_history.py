9db71348d2b8a5cdecb3a48bfd74c50c8d353ccb
Kenshin Abe <abe.kenshin@gmail.com>
2022-07-25 18:37:26 +0900

    Add cases for test_get_optimization_history_plot

--- tests/visualization_tests/matplotlib_tests/test_optimization_history.py
+++ tests/visualization_tests/matplotlib_tests/test_optimization_history.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import pytest
 
 from optuna.visualization._optimization_history import _OptimizationHistoryInfo
@@ -6,18 +8,42 @@ from optuna.visualization.matplotlib._optimization_history import _get_optimizat
 
 
 @pytest.mark.parametrize("target_name", ["Objective Value", "Target Name"])
-def test_get_optimization_history_plot(target_name: str) -> None:
-    # Empty info.
-    figure = _get_optimization_history_plot([], target_name=target_name)
-    assert figure.get_ylabel() == target_name
-
-    # Info with error bar.
-    info_list = [
-        _OptimizationHistoryInfo(
-            [0, 1, 2],
-            _ValuesInfo([1.0, 2.0, 0.0], [0.0, 0.0, 0.0], "Dummy"),
-            _ValuesInfo([1.0, 1.0, 1.0], [0.0, 0.0, 0.0], "Best Value"),
-        )
-    ]
-    figure = _get_optimization_history_plot(info_list, target_name)
+@pytest.mark.parametrize(
+    "info_list",
+    [
+        [],  # Empty info.
+        [  # Vanilla.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], None, "Dummy"),
+                None,
+            )
+        ],
+        [  # With best values.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], None, "Dummy"),
+                _ValuesInfo([1.0, 1.0, 1.0], None, "Best Value"),
+            )
+        ],
+        [  # With error bar.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], [1.0, 2.0, 0.0], "Dummy"),
+                None,
+            )
+        ],
+        [  # With best values and error bar.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], [1.0, 2.0, 0.0], "Dummy"),
+                _ValuesInfo([1.0, 1.0, 1.0], [1.0, 2.0, 0.0], "Best Value"),
+            )
+        ],
+    ],
+)
+def test_get_optimization_history_plot(
+    target_name: str, info_list: List[_OptimizationHistoryInfo]
+) -> None:
+    figure = _get_optimization_history_plot(info_list, target_name=target_name)
     assert figure.get_ylabel() == target_name
--- tests/visualization_tests/test_optimization_history.py
+++ tests/visualization_tests/test_optimization_history.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import numpy as np
 import pytest
 
@@ -234,18 +236,42 @@ def test_error_bar_in_optimization_history(direction: str) -> None:
 
 
 @pytest.mark.parametrize("target_name", ["Objective Value", "Target Name"])
-def test_get_optimization_history_plot(target_name: str) -> None:
-    # Empty info.
-    figure = _get_optimization_history_plot([], target_name=target_name)
-    assert figure.layout.yaxis.title.text == target_name
-
-    # Info with error bar.
-    info_list = [
-        _OptimizationHistoryInfo(
-            [0, 1, 2],
-            _ValuesInfo([1.0, 2.0, 0.0], [0.0, 0.0, 0.0], "Dummy"),
-            _ValuesInfo([1.0, 1.0, 1.0], [0.0, 0.0, 0.0], "Best Value"),
-        )
-    ]
-    figure = _get_optimization_history_plot(info_list, target_name)
+@pytest.mark.parametrize(
+    "info_list",
+    [
+        [],  # Empty info.
+        [  # Vanilla.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], None, "Dummy"),
+                None,
+            )
+        ],
+        [  # With best values.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], None, "Dummy"),
+                _ValuesInfo([1.0, 1.0, 1.0], None, "Best Value"),
+            )
+        ],
+        [  # With error bar.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], [1.0, 2.0, 0.0], "Dummy"),
+                None,
+            )
+        ],
+        [  # With best values and error bar.
+            _OptimizationHistoryInfo(
+                [0, 1, 2],
+                _ValuesInfo([1.0, 2.0, 0.0], [1.0, 2.0, 0.0], "Dummy"),
+                _ValuesInfo([1.0, 1.0, 1.0], [1.0, 2.0, 0.0], "Best Value"),
+            )
+        ],
+    ],
+)
+def test_get_optimization_history_plot(
+    target_name: str, info_list: List[_OptimizationHistoryInfo]
+) -> None:
+    figure = _get_optimization_history_plot(info_list, target_name=target_name)
     assert figure.layout.yaxis.title.text == target_name
