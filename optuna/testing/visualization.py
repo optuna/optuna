62d0353e794a5d6b4b317a1a6abb276b3c6e9d02
Masashi Shibata <c-bata@users.noreply.github.com>
2022-07-27 10:29:20 +0900

    Apply suggestions from code review
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/testing/visualization.py
+++ optuna/testing/visualization.py
@@ -14,7 +14,7 @@ def prepare_study_with_trials(
 
     This function is added to reduce the code to set up dummy study object in each test case.
     However, you can only use this function for unit tests that are loosely coupled with the
-    dummy study object. Unittests that are tightly coupled with the study become difficult to
+    dummy study object. Unit tests that are tightly coupled with the study become difficult to
     read because of `Mystery Guest <http://xunitpatterns.com/Obscure%20Test.html#Mystery%20Guest>`_
     and/or `Eager Test <http://xunitpatterns.com/Obscure%20Test.html#Eager%20Test>`_ anti-patterns.
 
