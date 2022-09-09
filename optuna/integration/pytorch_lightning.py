b212419fa50ac87b1f01cc9f95d783d18f7120a4
regius <gilgemesh2012@gmail.com>
2022-09-09 11:58:16 +0800

    fix AttributeError problem

--- optuna/integration/pytorch_lightning.py
+++ optuna/integration/pytorch_lightning.py
@@ -57,9 +57,7 @@ class PyTorchLightningPruningCallback(Callback):
         self.is_ddp_backend = False
 
     def on_init_start(self, trainer: Trainer) -> None:
-        self.is_ddp_backend = (
-            trainer._accelerator_connector.distributed_backend is not None  # type: ignore
-        )
+        self.is_ddp_backend = trainer._accelerator_connector._strategy_flag == "ddp"
         if self.is_ddp_backend:
             if version.parse(pl.__version__) < version.parse("1.5.0"):
                 raise ValueError("PyTorch Lightning>=1.5.0 is required in DDP.")
