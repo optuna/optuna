dd7ae85749ed532ed24b1b2f4c0c7551fbcf2261
c-bata <contact@c-bata.link>
2022-09-26 12:00:48 +0900

    Fix lint errors

--- optuna/storages/_rdb/alembic/versions/v3.0.0.c.py
+++ optuna/storages/_rdb/alembic/versions/v3.0.0.c.py
@@ -75,7 +75,9 @@ def upgrade():
             batch_op.add_column(
                 sa.Column(
                     "intermediate_value_type",
-                    sa.Enum("FINITE", "INF_POS", "INF_NEG", "NAN", name="trialintermediatevaluetype"),
+                    sa.Enum(
+                        "FINITE", "INF_POS", "INF_NEG", "NAN", name="trialintermediatevaluetype"
+                    ),
                     nullable=False,
                     server_default="FINITE",
                 ),
