21038e795dd90e79cc2fbeaf28c646896807d6ba
Luigi <luigi3000@gmail.com>
2022-10-15 13:03:14 +0200

    Fixing the flake8

--- tests/study_tests/test_study.py
+++ tests/study_tests/test_study.py
@@ -5,11 +5,12 @@ import pickle
 import tempfile
 import threading
 import time
-from typing import Any, Union
+from typing import Any
 from typing import Callable
 from typing import Dict
 from typing import List
 from typing import Optional
+from typing import Union
 from unittest.mock import Mock
 from unittest.mock import patch
 import uuid
