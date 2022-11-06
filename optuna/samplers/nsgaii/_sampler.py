6cbd3f5ba92398ebba65e62100dacc87334e5a52
mist714 <formuladevice@gmail.com>
2022-11-06 12:01:48 +0900

    Update optuna/samplers/nsgaii/_sampler.py
    Co-authored-by: Naoto Mizuno <naotomizuno@preferred.jp>

--- optuna/samplers/nsgaii/_sampler.py
+++ optuna/samplers/nsgaii/_sampler.py
@@ -345,10 +345,12 @@ class NSGAIISampler(BaseSampler):
         population: List[FrozenTrial],
         directions: List[optuna.study.StudyDirection],
     ) -> List[List[FrozenTrial]]:
-        for _trial in population:
-            _constraints = _trial.system_attrs.get(_CONSTRAINTS_KEY)
-            if np.any(np.isnan(_constraints)):
-                raise ValueError("NaN is not acceptable as constraint value.")
+        if self._constraints_func is not None:
+            for _trial in population:
+                _constraints = _trial.system_attrs.get(_CONSTRAINTS_KEY)
+                if np.any(np.isnan(_constraints)):
+                    raise ValueError("NaN is not acceptable as constraint value.")
+
         dominated_count: DefaultDict[int, int] = defaultdict(int)
         dominates_list = defaultdict(list)
 
