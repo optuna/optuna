b33a4cb74f51cdfd29291abe214d04819ef94967
jpbianchi <jpbianchi@gmail.com>
2022-12-08 09:32:26 +0100

    WIP removed commented import

--- optuna/_hypervolume/utils.py
+++ optuna/_hypervolume/utils.py
@@ -1,7 +1,5 @@
 import numpy as np
-import optuna 
-
-# from optuna._hypervolume import WFG
+import optuna
 
 def _compute_2points_volume(point1: np.ndarray, point2: np.ndarray) -> float:
     """Compute the hypervolume of the hypercube, whose diagonal endpoints are given 2 points.
