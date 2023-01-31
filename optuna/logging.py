332b493fd93040fb09c0cbb2c1c81f2382a603ac
gen740 <genfujimoto@gmail.com>
2023-01-31 11:15:44 +0900

    Add `NO_COLOR` support

--- optuna/logging.py
+++ optuna/logging.py
@@ -6,6 +6,8 @@ from logging import FATAL
 from logging import INFO
 from logging import WARN
 from logging import WARNING
+import os
+import platform
 import sys
 import threading
 from typing import Optional
@@ -33,12 +35,24 @@ def create_default_formatter() -> colorlog.ColoredFormatter:
     This function is not supposed to be directly accessed by library users.
     """
 
-    if sys.stdout.isatty():
+    if _color_supported():
         return colorlog.ColoredFormatter(
             "%(log_color)s[%(levelname)1.1s %(asctime)s]%(reset)s %(message)s"
         )
     else:
         return colorlog.ColoredFormatter("[%(levelname)1.1s %(asctime)s] %(message)s")
+
+
+def _color_supported() -> bool:
+    """Detection of color support."""
+    # NO_COLOR environment variable:
+    if os.environ.get("NO_COLOR", None):
+        return False
+
+    if not hasattr(sys.stderr, "isatty") or not sys.stderr.isatty():
+        return False
+    else:
+        return True
 
 
 def _get_library_name() -> str:
