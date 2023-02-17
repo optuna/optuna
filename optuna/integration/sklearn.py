fa54271e3eead304adbd8978059eb8d5a575d312
Masashi Shibata <c-bata@users.noreply.github.com>
2023-02-17 14:11:46 +0900

    Revert "Merge pull request #4183 from contramundum53/refactor-parzen-estimator4" (#4429)
    This reverts commit cc7f873412256fd9864627cc245c881be46a73cc, reversing
    changes made to e81a267d845c53dbbc989c3b25d3162aa1e69c9c.

--- optuna/integration/sklearn.py
+++ optuna/integration/sklearn.py
@@ -40,13 +40,9 @@ with try_import() as _imports:
     from sklearn.model_selection import BaseCrossValidator
     from sklearn.model_selection import check_cv
     from sklearn.model_selection import cross_validate
+    from sklearn.utils import _safe_indexing as sklearn_safe_indexing
     from sklearn.utils import check_random_state
     from sklearn.utils.metaestimators import _safe_split
-
-    if sklearn.__version__ >= "0.22":
-        from sklearn.utils import _safe_indexing as sklearn_safe_indexing
-    else:
-        from sklearn.utils import safe_indexing as sklearn_safe_indexing
     from sklearn.utils.validation import check_is_fitted
 
 if not _imports.is_successful():
@@ -184,7 +180,7 @@ class _Objective:
 
     def __init__(
         self,
-        estimator: "BaseEstimator",
+        estimator: "sklearn.base.BaseEstimator",
         param_distributions: Mapping[str, distributions.BaseDistribution],
         X: TwoDimArrayLikeType,
         y: Optional[Union[OneDimArrayLikeType, TwoDimArrayLikeType]],
@@ -236,7 +232,7 @@ class _Objective:
         return trial.user_attrs["mean_test_score"]
 
     def _cross_validate_with_pruning(
-        self, trial: Trial, estimator: "BaseEstimator"
+        self, trial: Trial, estimator: "sklearn.base.BaseEstimator"
     ) -> Mapping[str, OneDimArrayLikeType]:
         if is_classifier(estimator):
             partial_fit_params = self.fit_params.copy()
@@ -289,7 +285,7 @@ class _Objective:
 
     def _partial_fit_and_score(
         self,
-        estimator: "BaseEstimator",
+        estimator: "sklearn.base.BaseEstimator",
         train: List[int],
         test: List[int],
         partial_fit_params: Dict[str, Any],
@@ -677,7 +673,7 @@ class OptunaSearchCV(BaseEstimator):
 
     def __init__(
         self,
-        estimator: "BaseEstimator",
+        estimator: "sklearn.base.BaseEstimator",
         param_distributions: Mapping[str, distributions.BaseDistribution],
         *,
         cv: Optional[Union[int, "BaseCrossValidator", Iterable]] = None,
