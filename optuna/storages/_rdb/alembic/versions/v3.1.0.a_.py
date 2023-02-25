b6b7c271bf92dfcbb65fa0a7eac9b2b290aa60ff
ilevk <kcs93023@gmail.com>
2023-02-25 13:42:19 +0900

    style - apply black

--- optuna/storages/_rdb/alembic/versions/v3.1.0.a_.py
+++ optuna/storages/_rdb/alembic/versions/v3.1.0.a_.py
@@ -9,14 +9,15 @@ from alembic import op
 
 
 # revision identifiers, used by Alembic.
-revision = 'v3.1.0.a'
-down_revision = 'v3.0.0.d'
+revision = "v3.1.0.a"
+down_revision = "v3.0.0.d"
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
-    op.create_index(op.f('trials_study_id_key'), 'trials', ['study_id'], unique=False)
+    op.create_index(op.f("trials_study_id_key"), "trials", ["study_id"], unique=False)
 
 
 def downgrade():
-    op.drop_index(op.f('trials_study_id_key'), table_name='trials')
+    op.drop_index(op.f("trials_study_id_key"), table_name="trials")
