b707aca6796112cb0cbf5b5bee1cbfc79872ff43
Hiroki Takizawa <contact@hiroki-takizawa.name>
2023-03-15 12:18:47 +0900

    Update test_timeline.py

--- tests/visualization_tests/test_timeline.py
+++ tests/visualization_tests/test_timeline.py
@@ -1,3 +1,4 @@
+import datetime
 from io import BytesIO
 from typing import List
 
@@ -11,52 +12,39 @@ from optuna.visualization._timeline import _get_timeline_info
 from optuna.visualization._timeline import plot_timeline
 
 
-@pytest.mark.parametrize(
-    "n",
-    [0, 3],
-)
-def test_get_timeline_info(n: int) -> None:
-    states = [TrialState.COMPLETE, TrialState.RUNNING, TrialState.WAITING]
-    study = optuna.create_study()
-    for i in range(n):
-        study.add_trial(
-            optuna.trial.create_trial(
-                params={"x": 0.5},
-                distributions={"x": optuna.distributions.FloatDistribution(0, 1)},
-                value=0,
-                state=states[i],
-            )
-        )
-    info = _get_timeline_info(study)
-    assert len(info.bars) == n
-    if n == 3:
-        trials = study.get_trials()
-        assert trials[0].datetime_start is not None
-        assert trials[1].datetime_start is not None
-        assert trials[2].datetime_start is None
-        assert trials[0].datetime_complete is not None
-        assert trials[1].datetime_complete is None
-        assert trials[2].datetime_complete is None
-        for x in info.bars:
-            assert type(x.hovertext) is str
-            assert x.start is not None
-            assert x.end is not None
-            assert x.start <= x.end
-
-
 def _create_study(trial_states_list: List[TrialState]) -> Study:
     study = optuna.create_study()
     fmax = float(len(trial_states_list))
-    for i, x in enumerate(trial_states_list):
+    for i, s in enumerate(trial_states_list):
         study.add_trial(
             optuna.trial.create_trial(
                 params={"x": float(i)},
                 distributions={"x": optuna.distributions.FloatDistribution(-1.0, fmax)},
                 value=0.0,
-                state=x,
+                state=s,
             )
         )
     return study
+
+
+def test_get_timeline_info_empty() -> None:
+    study = _create_study([])
+    info = _get_timeline_info(study)
+    assert len(info.bars) == 0
+
+
+def test_get_timeline_info() -> None:
+    states = [TrialState.COMPLETE, TrialState.RUNNING, TrialState.WAITING]
+    study = _create_study(states)
+    info = _get_timeline_info(study)
+    assert len(info.bars) == 3
+    for i, bar in enumerate(info.bars):
+        assert bar.number == i
+        assert bar.state == states[i]
+        assert type(bar.hovertext) is str
+        assert isinstance(bar.start, datetime.datetime)
+        assert isinstance(bar.end, datetime.datetime)
+        assert bar.start <= bar.end
 
 
 @pytest.mark.parametrize(
