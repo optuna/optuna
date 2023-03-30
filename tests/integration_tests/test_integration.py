5d8ca833a86e8e55a01ea015beee64d100fe25b7
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-03-30 22:44:02 +0900

    move integration/keras.py to optuna-integration

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -7,8 +7,6 @@ pytestmark = pytest.mark.integration
 def test_import() -> None:
     from optuna.integration import dask  # NOQA
     from optuna.integration import DaskStorage  # NOQA
-    from optuna.integration import keras  # NOQA
-    from optuna.integration import KerasPruningCallback  # NOQA
     from optuna.integration import lightgbm  # NOQA
     from optuna.integration import LightGBMPruningCallback  # NOQA
     from optuna.integration import mxnet  # NOQA
@@ -26,12 +24,10 @@ def test_module_attributes() -> None:
     import optuna
 
     assert hasattr(optuna.integration, "dask")
-    assert hasattr(optuna.integration, "keras")
     assert hasattr(optuna.integration, "lightgbm")
     assert hasattr(optuna.integration, "mxnet")
     assert hasattr(optuna.integration, "tensorflow")
     assert hasattr(optuna.integration, "xgboost")
-    assert hasattr(optuna.integration, "KerasPruningCallback")
     assert hasattr(optuna.integration, "LightGBMPruningCallback")
     assert hasattr(optuna.integration, "MXNetPruningCallback")
     assert hasattr(optuna.integration, "TensorFlowPruningHook")
