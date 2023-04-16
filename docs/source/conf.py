e4b71cd7bb1ab33bca879a4bc8a33c302db3b712
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-04-16 10:49:56 +0900

    Add sphinxcontrib.jquery extension to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,6 +55,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
+    "sphinxcontrib.jquery",
     "sphinx_copybutton",
     "sphinx_gallery.gen_gallery",
     "matplotlib.sphinxext.plot_directive",
