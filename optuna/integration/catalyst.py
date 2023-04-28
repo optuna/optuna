0845498aef0eda5a413efac2f2e394fd5fb46a32
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-04-28 16:32:11 +0900

    Remove catalyst from optuna/integration

--- optuna/integration/catalyst.py
+++ optuna/integration/catalyst.py
@@ -1,30 +1,4 @@
-from packaging import version
-
-from optuna._deprecated import deprecated_class
-from optuna._imports import try_import
+from optuna_integration.catalyst import CatalystPruningCallback
 
 
-with try_import() as _imports:
-    import catalyst
-
-    if version.parse(catalyst.__version__) < version.parse("21.3"):
-        raise ImportError(
-            f"You don't have Catalyst>=21.3 installed! Catalyst version: {catalyst.__version__}"
-        )
-    from catalyst.dl import OptunaPruningCallback
-
-if not _imports.is_successful():
-    OptunaPruningCallback = object  # NOQA
-
-
-@deprecated_class("2.7.0", "4.0.0")
-class CatalystPruningCallback(OptunaPruningCallback):
-    """Catalyst callback to prune unpromising trials.
-
-    This class is an alias to Catalyst's
-    `OptunaPruningCallback <https://catalyst-team.github.io/catalyst/api/callbacks.html?highlight=optuna#catalyst.callbacks.optuna.OptunaPruningCallback>`_.
-
-    See the Catalyst's documentation for the detailed description.
-    """  # NOQA
-
-    pass
+__all__ = ["CatalystPruningCallback"]
