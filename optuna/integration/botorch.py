d938969edc62153dca22151f3e191a8ddcf60ea5
KI <30857855+kstoneriv3@users.noreply.github.com>
2023-05-02 21:24:21 +0100

    Update optuna/integration/botorch.py
    Co-authored-by: contramundum53 <contramundum53@outlook.com>

--- optuna/integration/botorch.py
+++ optuna/integration/botorch.py
@@ -521,7 +521,7 @@ class BoTorchSampler(BaseSampler):
         ] = None,
         constraints_func: Optional[Callable[[FrozenTrial], Sequence[float]]] = None,
         n_startup_trials: int = 10,
-        consider_running_trials: bool = False,
+        consider_running_trials: bool = True,
         independent_sampler: Optional[BaseSampler] = None,
         seed: Optional[int] = None,
         device: Optional["torch.device"] = None,
