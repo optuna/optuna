718a77700b713c6e5beb27aa88ff746b3b0fb301
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-05-13 00:00:36 +0900

    Add docstring to `TerminatorCallback`

--- optuna/terminator/callback.py
+++ optuna/terminator/callback.py
@@ -13,6 +13,53 @@ _logger = get_logger(__name__)
 
 @experimental_class("3.2.0")
 class TerminatorCallback:
+    """A callback that terminates the optimization using Terminator.
+
+    This class implements a callback that terminates the optimization using the terminator
+    mechanism with the :func:`~optuna.study.Study.optimize` method. The callback terminates the
+    study when the statistical error, such as cross-validation error, exceeds the room left for
+    optimization.
+
+    Args:
+        terminator:
+            A terminator object which determines whether to terminate the optimization by
+            assessing the room for optimization and statistical error. Defaults to a
+            :class:`~optuna.terminator.terminator.Terminator` object with default
+            improvement_evaluator and error_evaluator.
+
+    Example:
+        from sklearn.datasets import load_wine
+        from sklearn.ensemble import RandomForestClassifier
+        from sklearn.model_selection import cross_val_score
+        from sklearn.model_selection import KFold
+
+        import optuna
+        from optuna.terminator.terminator import TerminatorCallback
+        from optuna.terminator.serror import report_cross_validation_scores
+
+
+        def objective(trial):
+            X, y = load_wine(return_X_y=True)
+
+            clf = RandomForestClassifier(
+                max_depth=trial.suggest_int("max_depth", 2, 32),
+                min_samples_split=trial.suggest_float("min_samples_split", 0, 1),
+                criterion=trial.suggest_categorical("criterion", ("gini", "entropy")),
+            )
+
+            scores = cross_val_score(clf, X, y, cv=KFold(n_splits=5, shuffle=True))
+            report_cross_validation_scores(trial, scores)
+            return scores.mean()
+
+        study = optuna.create_study(direction="maximize")
+        terminator = TerminatorCallback()
+        study.optimize(objective, n_trials=50, callbacks=[terminator])
+
+    .. seealso::
+        Please refer to :class:`~optuna.terminator.terminator.Terminator` for the details of
+        the terminator mechanism.
+    """
+
     def __init__(
         self,
         terminator: Optional[BaseTerminator] = None,
