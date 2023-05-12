a1ff842d0ba0c1016de2aaaf126b27a7ef4662ed
Contramundum <contramundum53@outlook.com>
2023-05-12 19:01:38 +0900

    Support log-int

--- optuna/samplers/_cmaes.py
+++ optuna/samplers/_cmaes.py
@@ -560,7 +560,7 @@ class CmaEsSampler(BaseSampler):
             for i, dist in enumerate(trans._search_space.values()):
                 assert isinstance(dist, (IntDistribution, FloatDistribution))
                 # Set step 0.0 for continuous search space.
-                if dist.step is None:
+                if dist.step is None or dist.log:
                     steps[i] = 0.0
                 elif dist.low == dist.high:
                     steps[i] = 1.0
