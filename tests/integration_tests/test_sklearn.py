74622759b72aaf475bc42cce02d3b35b175f9922
jak_tkvs <32125829+jckkvs@users.noreply.github.com>
2023-06-08 16:52:17 +0900

    Update test_sklearn.py

--- tests/integration_tests/test_sklearn.py
+++ tests/integration_tests/test_sklearn.py
@@ -325,7 +325,7 @@ def test_objective_error_score_invalid() -> None:
 def test_objective_error_score_nan_regression(param_dist: dict, expect: bool) -> None:
     X, y = make_regression(n_samples=100, n_features=10)
     est = DecisionTreeRegressor()
-    study = optuna.create_study()
+    study = create_study()
     optuna_search = integration.OptunaSearchCV(
         est,
         param_dist,
