6e34cb3149150635cecf2dad5a9a5300f7dee195
jak_tkvs <32125829+jckkvs@users.noreply.github.com>
2023-06-15 12:58:50 +0900

    Update optuna/integration/sklearn.py
    Co-authored-by: Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>

--- optuna/integration/sklearn.py
+++ optuna/integration/sklearn.py
@@ -231,7 +231,9 @@ class _Objective:
                 n_splits = self.cv.get_n_splits(self.X, self.y, self.groups)
                 fit_time = np.array([np.nan] * n_splits)
                 score_time = np.array([np.nan] * n_splits)
-                test_score = np.array([self.error_score] * n_splits)
+                test_score = np.array(
+                    [self.error_score if self.error_score is not None else np.nan] * n_splits
+                )
 
                 scores = {
                     "fit_time": fit_time,
