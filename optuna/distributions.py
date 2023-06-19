23ca34b153a3ef1d3d3640906254fa7c9ac57db3
Kenshin Abe <abe.kenshin@gmail.com>
2023-06-19 18:54:49 +0900

    Remove unused import

--- optuna/distributions.py
+++ optuna/distributions.py
@@ -7,9 +7,7 @@ from typing import Any
 from typing import Callable
 from typing import cast
 from typing import Dict
-from typing import Generic
 from typing import Sequence
-from typing import TypeVar
 from typing import Union
 import warnings
 
