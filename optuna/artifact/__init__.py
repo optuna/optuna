26ba7b86e2c3a52835987b2d3f4f9499e09ce967
gen740 <gen740.f@gmail.com>
2023-06-23 15:46:06 +0900

    Delete downloader

--- optuna/artifact/__init__.py
+++ optuna/artifact/__init__.py
@@ -1,5 +1,4 @@
-from optuna.artifact._downloader import download_artifacts
 from optuna.artifact._filesystem import FileSystemArtifactStore
 
 
-__all__ = ["FileSystemArtifactStore", "download_artifacts"]
+__all__ = ["FileSystemArtifactStore"]
--- optuna/artifact/_downloader.py
+++
@@ -1,6 +0,0 @@
-import shutil
-
-
-def download_artifacts(artifact: dict[str, str], file_path: str):
-    # study.artifact.open()
-    shutil.move(artifact["filepath"], file_path)
