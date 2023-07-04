35e641edbc375268d4a2870b8040ac619bdfa3ea
hrntsm <contact@hrntsm.com>
2023-07-04 09:03:45 +0900

    Update _optimization_history to support constraint

--- optuna/visualization/_optimization_history.py
+++ optuna/visualization/_optimization_history.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import math
 from typing import Callable
 from typing import cast
 from typing import NamedTuple
@@ -8,6 +9,7 @@ from typing import Sequence
 import numpy as np
 
 from optuna.logging import get_logger
+from optuna.samplers._base import _CONSTRAINTS_KEY
 from optuna.study import Study
 from optuna.study._study_direction import StudyDirection
 from optuna.trial import FrozenTrial
@@ -26,6 +28,7 @@ class _ValuesInfo(NamedTuple):
     values: list[float]
     stds: list[float] | None
     label_name: str
+    is_feasibles: list[bool]
 
 
 class _OptimizationHistoryInfo(NamedTuple):
@@ -48,27 +51,44 @@ def _get_optimization_history_info_list(
 
     info_list: list[_OptimizationHistoryInfo] = []
     for study in studies:
-        trials = study.get_trials(states=(TrialState.COMPLETE,))
+        completed_trials = study.get_trials(states=(TrialState.COMPLETE,))
         label_name = target_name if len(studies) == 1 else f"{target_name} of {study.study_name}"
+        values = []
+        is_feasibles = []
+        for trial in completed_trials:
+            has_constraints = _CONSTRAINTS_KEY in trial.system_attrs
+            constraints_value = trial.system_attrs[_CONSTRAINTS_KEY] if has_constraints else [0]
+            is_feasibles.append(all(map(lambda x: x <= 0.0, constraints_value)))
+            if target is not None:
+                values.append(target(trial))
+            else:
+                values.append(cast(float, trial.value))
         if target is not None:
-            values = [target(t) for t in trials]
             # We don't calculate best for user-defined target function since we cannot tell
             # which direction is better.
             best_values_info: _ValuesInfo | None = None
         else:
-            values = [cast(float, t.value) for t in trials]
+            feasible_best_values = []
             if study.direction == StudyDirection.MINIMIZE:
-                best_values = list(np.minimum.accumulate(values))
+                feasible_best_values = [
+                    value if is_feasible else float("inf")
+                    for value, is_feasible in zip(values, is_feasibles)
+                ]
+                best_values = list(np.minimum.accumulate(feasible_best_values))
             else:
-                best_values = list(np.maximum.accumulate(values))
+                feasible_best_values = [
+                    value if is_feasible else -float("inf")
+                    for value, is_feasible in zip(values, is_feasibles)
+                ]
+                best_values = list(np.maximum.accumulate(feasible_best_values))
             best_label_name = (
                 "Best Value" if len(studies) == 1 else f"Best Value of {study.study_name}"
             )
-            best_values_info = _ValuesInfo(best_values, None, best_label_name)
+            best_values_info = _ValuesInfo(best_values, None, best_label_name, is_feasibles)
         info_list.append(
             _OptimizationHistoryInfo(
-                trial_numbers=[t.number for t in trials],
-                values_info=_ValuesInfo(values, None, label_name),
+                trial_numbers=[t.number for t in completed_trials],
+                values_info=_ValuesInfo(values, None, label_name, is_feasibles),
                 best_values_info=best_values_info,
             )
         )
@@ -92,17 +112,38 @@ def _get_optimization_history_info_list(
     def _aggregate(label_name: str, use_best_value: bool) -> tuple[list[int], _ValuesInfo]:
         # Calculate mean and std of values for each trial number.
         values: list[list[float]] = [[] for _ in range(max_num_trial)]
+        is_feasibles: list[list[bool]] = [[] for _ in range(max_num_trial)]
         assert info_list is not None
         for trial_numbers, values_info, best_values_info in info_list:
             if use_best_value:
                 assert best_values_info is not None
                 values_info = best_values_info
-            for trial_number, value in zip(trial_numbers, values_info.values):
-                values[trial_number].append(value)
-        trial_numbers_union = [i for i in range(max_num_trial) if len(values[i]) > 0]
-        value_means = [np.mean(v).item() for v in values if len(v) > 0]
-        value_stds = [np.std(v).item() for v in values if len(v) > 0]
-        return trial_numbers_union, _ValuesInfo(value_means, value_stds, label_name)
+                for trial_number, value, is_feasible in zip(
+                    trial_numbers, values_info.values, values_info.is_feasibles
+                ):
+                    if not math.isinf(value):
+                        values[trial_number].append(value)
+                    is_feasibles[trial_number].append(is_feasible)
+            else:
+                for trial_number, value, is_feasible in zip(
+                    trial_numbers, values_info.values, values_info.is_feasibles
+                ):
+                    if is_feasible and not math.isinf(value):
+                        values[trial_number].append(value)
+                    is_feasibles[trial_number].append(is_feasible)
+        value_feasibles = [any(f) for f in is_feasibles if len(f) > 0]
+        trial_numbers_union = [
+            i for i in range(max_num_trial) if len(values[i]) > 0 and value_feasibles[i]
+        ]
+        value_means = [
+            np.mean(v).item() for v, f in zip(values, value_feasibles) if len(v) > 0 and f
+        ]
+        value_stds = [
+            np.std(v).item() for v, f in zip(values, value_feasibles) if len(v) > 0 and f
+        ]
+        return trial_numbers_union, _ValuesInfo(
+            value_means, value_stds, label_name, value_feasibles
+        )
 
     eb_trial_numbers, eb_values_info = _aggregate(target_name, False)
     eb_best_values_info: _ValuesInfo | None = None
@@ -179,26 +220,51 @@ def _get_optimization_history_plot(
 
     traces = []
     for trial_numbers, values_info, best_values_info in info_list:
+        infeasible_trial_numbers = [
+            number
+            for number, is_feasible in zip(trial_numbers, values_info.is_feasibles)
+            if not is_feasible
+        ]
         if values_info.stds is None:
             error_y = None
+            feasible_trial_numbers = [
+                number
+                for number, is_feasible in zip(trial_numbers, values_info.is_feasibles)
+                if is_feasible
+            ]
+            feasible_trial_values = []
+            for i in feasible_trial_numbers:
+                feasible_trial_values.append(values_info.values[i])
+            infeasible_trial_values = []
+            for i in infeasible_trial_numbers:
+                infeasible_trial_values.append(values_info.values[i])
         else:
+            if False in values_info.is_feasibles:
+                _logger.warning(
+                    "Your study contains infeasible trials. "
+                    "In optimization history plot,"
+                    "error bars are calculated for only feasible trial values."
+                )
             error_y = {"type": "data", "array": values_info.stds, "visible": True}
+            feasible_trial_numbers = trial_numbers
+            feasible_trial_values = values_info.values
+            infeasible_trial_values = []
         traces.append(
             go.Scatter(
-                x=trial_numbers,
-                y=values_info.values,
+                x=feasible_trial_numbers,
+                y=feasible_trial_values,
                 error_y=error_y,
                 mode="markers",
                 name=values_info.label_name,
             )
         )
-
         if best_values_info is not None:
             traces.append(
                 go.Scatter(
                     x=trial_numbers,
                     y=best_values_info.values,
                     name=best_values_info.label_name,
+                    mode="lines",
                 )
             )
             if best_values_info.stds is not None:
@@ -223,5 +289,15 @@ def _get_optimization_history_plot(
                         fillcolor="rgba(255,0,0,0.2)",
                     )
                 )
-
+        traces.append(
+            go.Scatter(
+                x=infeasible_trial_numbers,
+                y=infeasible_trial_values,
+                error_y=error_y,
+                mode="markers",
+                name="Infeasible Trial",
+                marker={"color": "#cccccc"},
+                showlegend=False,
+            )
+        )
     return go.Figure(data=traces, layout=layout)
