f05b7de6dc3697e4f5eb9bbf3c4a01173c1edd4b
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-07-27 13:57:48 +0900

    Remove example

--- optuna/artifacts/_filesystem.py
+++ optuna/artifacts/_filesystem.py
@@ -12,24 +12,7 @@ if TYPE_CHECKING:
 
 
 class FileSystemArtifactStore:
-    """An artifact backend for file systems.
-
-    Example:
-       .. code-block:: python
-
-           import optuna
-           from optuna_dashboard.artifact import upload_artifact
-           from optuna_dashboard.artifact.file_system import FileSystemBackend
-
-           artifact_backend = FileSystemBackend("./artifacts")
-
-
-           def objective(trial: optuna.Trial) -> float:
-               ... = trial.suggest_float("x", -10, 10)
-               file_path = generate_example_png(...)
-               upload_artifact(artifact_backend, trial, file_path)
-               return ...
-    """
+    """An artifact backend for file systems."""
 
     def __init__(self, base_path: str) -> None:
         self._base_path = base_path
