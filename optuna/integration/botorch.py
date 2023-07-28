02f6b927020862fbcd6331a400af8b46eb5b1368
Naoto Mizuno <naotomizuno@preferred.jp>
2023-07-28 16:38:48 +0900

    Remove ordered_dict argument from IntersectionSearchSpace

--- optuna/integration/botorch.py
+++ optuna/integration/botorch.py
@@ -721,7 +721,7 @@ class BoTorchSampler(BaseSampler):
             raise RuntimeError("BoTorchSampler cannot handle multiple studies.")
 
         search_space: Dict[str, BaseDistribution] = {}
-        for name, distribution in self._search_space.calculate(study, ordered_dict=True).items():
+        for name, distribution in self._search_space.calculate(study).items():
             if distribution.single():
                 # built-in `candidates_func` cannot handle distributions that contain just a
                 # single value, so we skip them. Note that the parameter values for such
