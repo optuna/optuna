cda084130e68141271f2e0ea8c9186a3d8eff8f5
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2023-08-03 17:05:07 +0900

    Fix import

--- optuna/artifacts/_boto3.py
+++ optuna/artifacts/_boto3.py
@@ -11,11 +11,11 @@ from optuna.artifacts.exceptions import ArtifactNotFound
 if TYPE_CHECKING:
     from typing import BinaryIO
 
-    from botocore.exceptions import ClientError
     from mypy_boto3_s3 import S3Client
 
 with try_import():
     import boto3
+    from botocore.exceptions import ClientError
 
 
 class Boto3ArtifactStore:
