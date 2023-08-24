dea661674d655477b79f3507f9cf81c6c5d055c6
Dan Jacobson <danrowejacobson@gmail.com>
2023-08-23 23:51:22 -0400

    formaters

--- optuna/multi_objective/samplers/__init__.py
+++ optuna/multi_objective/samplers/__init__.py
@@ -1,9 +1,10 @@
 from optuna.multi_objective.samplers._adapter import _MultiObjectiveSamplerAdapter
 from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
+from optuna.multi_objective.samplers._carbs import CARBSSampler
 from optuna.multi_objective.samplers._motpe import MOTPEMultiObjectiveSampler
 from optuna.multi_objective.samplers._nsga2 import NSGAIIMultiObjectiveSampler
 from optuna.multi_objective.samplers._random import RandomMultiObjectiveSampler
-from optuna.multi_objective.samplers._carbs import CARBSSampler
+
 
 __all__ = [
     "_MultiObjectiveSamplerAdapter",
--- optuna/multi_objective/samplers/_carbs.py
+++ optuna/multi_objective/samplers/_carbs.py
@@ -1,6 +1,10 @@
 from typing import Dict
 
 import numpy as np
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import DotProduct
+from sklearn.gaussian_process.kernels import Matern
+from sklearn.gaussian_process.kernels import RBF
 
 import optuna
 from optuna import multi_objective
@@ -9,13 +13,11 @@ from optuna.multi_objective.samplers import BaseMultiObjectiveSampler
 from optuna.study import Study
 from optuna.trial import FrozenTrial
 
-from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import DotProduct, Matern, RBF
 
 class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
     def __init__(self, temperature=100):
         self._rng = np.random.default_rng()
-        self._current_trial = None  # Current state. 
+        self._current_trial = None  # Current state.
 
     def reseed_rng(self) -> None:
         self._rng.seed()
@@ -33,7 +35,6 @@ class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
 
         # Check the direction of "performance" optimization
         if study.directions[1] == optuna.study.StudyDirection.MAXIMIZE:
-
             for t in sorted_trials:
                 if t.values[1] > best_performance:
                     pareto_front.append(t)
@@ -45,33 +46,33 @@ class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
                     best_performance = t.values[1]
 
         return pareto_front
- 
-        
-    #TODO: make this work right
+
+    # TODO: make this work right
     def _sample_candidates(self, trial, n_candidates, search_space):
-        
         xi = np.array(list(trial.hparams.values()))
         samples = self._rng.multivariate_normal(xi, covariate_matrix, n_candidates)
-        
+
         # convert covar matrix to sigma ?
         sigma = np.magnitude(covariate_matrix)
-    
+
         # calculate p_search (CARBS eq. 2)
-        p_search = np.exp(- abs(xi - samples)**2 / (2 * sigma**2))
+        p_search = np.exp(-abs(xi - samples) ** 2 / (2 * sigma**2))
 
         return [(samples[i], p_search[i]) for i in range(n_candidates)]
-    
+
     def sample_relative(self, study, trial, search_space):
         if search_space == {}:
             return {}
-        
+
         # CARBS algorithm.
         # 1. Calculate pareto front
         pareto_front = _calculate_pareto_front(study)
 
         # 2. Generate candidates
-        candidates = [_generate_candidates(point, n_candidates, search_space) for point in pareto_front]
-        
+        candidates = [
+            _generate_candidates(point, n_candidates, search_space) for point in pareto_front
+        ]
+
         # 3. fit GP models to the candidates
         X = [ob.hparams for ob in obs]
         performances = [ob.performance for ob in obs]
@@ -79,18 +80,19 @@ class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
 
         # fit performance model
         performance_kernel = DotProduct + Matern()
-        GPy = GaussianProcessRegressor(kernel=performance_kernel,
-                random_state=self._rng).fit(X, performances)
-        
+        GPy = GaussianProcessRegressor(kernel=performance_kernel, random_state=self._rng).fit(
+            X, performances
+        )
+
         # fit cost model
         cost_kernel = DotProduct + Matern()
-        GPc = GaussianProcessRegressor(kernel=cost_kernel,
-                random_state=self._rng).fit(X, costs)
-        
+        GPc = GaussianProcessRegressor(kernel=cost_kernel, random_state=self._rng).fit(X, costs)
+
         # fit pareto front model
         pareto_kernel = RBF()
-        GPpf = GaussianProcessRegressor(kernel=pareto_kernel,
-                random_state=self._rng).fit(costs[pareto_front], performances[pareto_front])
+        GPpf = GaussianProcessRegressor(kernel=pareto_kernel, random_state=self._rng).fit(
+            costs[pareto_front], performances[pareto_front]
+        )
 
         acquisitions = []
         for cand in x_candidates:
@@ -105,14 +107,14 @@ class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
             cand.acq = cand.p_search * expected_improvement
             acquisitions.append(cand)
 
-            #TODO model odds of failure
+            # TODO model odds of failure
 
         return max(acquisitions, key=lambda x: x.acq)
 
     # boilerplate
     def infer_relative_search_space(self, study, trial):
         return optuna.search_space.intersection_search_space(study.get_trials(deepcopy=False))
-    
+
     # maybe we randomly sample the search center ?
     def sample_independent(self, study, trial, param_name, param_distribution):
         independent_sampler = optuna.samplers.RandomSampler()
@@ -120,6 +122,7 @@ class CARBSSampler(optuna.samplers.BaseMultiObjectiveSampler):
 
 
 if __name__ == "__main__":
+
     def objective(trial):
         x = trial.suggest_float("x", -10, 10)
         y = trial.suggest_float("y", -5, 5)
@@ -129,9 +132,8 @@ if __name__ == "__main__":
 
         return cost, performance
 
-
-    #sampler = CARBSSampler()
-    study = optuna.create_study(['minimize', 'maximize'])
+    # sampler = CARBSSampler()
+    study = optuna.create_study(["minimize", "maximize"])
     study.optimize(objective, n_trials=100)
 
     best_trial = study.best_trial
