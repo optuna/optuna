6eebc63b54867da9275b9c50887d623825f92686
Dan Jacobson <danrowejacobson@gmail.com>
2023-08-29 11:06:58 -0400

    adds tests

--- optuna/multi_objective/samplers/__init__.py
+++ optuna/multi_objective/samplers/__init__.py
@@ -1,6 +1,6 @@
 from optuna.multi_objective.samplers._adapter import _MultiObjectiveSamplerAdapter
 from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
-#from optuna.multi_objective.samplers._carbs import CARBSSampler
+from optuna.multi_objective.samplers._carbs import CARBSSampler
 from optuna.multi_objective.samplers._motpe import MOTPEMultiObjectiveSampler
 from optuna.multi_objective.samplers._nsga2 import NSGAIIMultiObjectiveSampler
 from optuna.multi_objective.samplers._random import RandomMultiObjectiveSampler
@@ -12,5 +12,5 @@ __all__ = [
     "MOTPEMultiObjectiveSampler",
     "NSGAIIMultiObjectiveSampler",
     "RandomMultiObjectiveSampler",
-#    "CARBSSampler",
+    "CARBSSampler",
 ]
--- tests/multi_objective_tests/samplers_tests/test_carbs.py
+++ tests/multi_objective_tests/samplers_tests/test_carbs.py
@@ -8,27 +8,3 @@ import optuna
 import optuna.multi_objective
 from optuna.multi_objective.samplers._carbs import CARBSSampler
 
-
-def objective(trial):
-    x = trial.suggest_float("x", -10, 10)
-    y = trial.suggest_float("y", -5, 5)
-
-    cost = abs(x) / 2
-    performance = x**2 + y
-
-    return cost, performance
-
-
-sampler = CARBSSampler()
-study = optuna.create_study(directions=['minimize', 'maximize'], sampler=sampler)
-study.optimize(objective, n_trials=20)
-
-optuna.visualization.plot_pareto_front(study, target_names=["cost", "performance"])
-
-print(f"Number of trials on the Pareto front: {len(study.best_trials)}")
-
-trial_with_highest_performance = max(study.best_trials, key=lambda t: t.values[1])
-print(f"Trial with highest performance: ")
-print(f"\tnumber: {trial_with_highest_performance.number}")
-print(f"\tparams: {trial_with_highest_performance.params}")
-print(f"\tvalues: {trial_with_highest_performance.values}")
