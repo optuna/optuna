840ec553d97473bd45c778bd6401ea36b8bc6300
Dan Jacobson <danrowejacobson@gmail.com>
2023-08-29 11:07:34 -0400

    updates sample_ind and sample_relative

--- optuna/multi_objective/samplers/_carbs.py
+++ optuna/multi_objective/samplers/_carbs.py
@@ -1,23 +1,35 @@
-from typing import Dict
 from typing import Optional
 from typing import Sequence
+from typing import Dict
+from typing import Any
 from collections import namedtuple
 
 import numpy as np
 
 import optuna
-from optuna.multi_objective.samplers import BaseMultiObjectiveSampler
+from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
 from optuna.multi_objective.study import Study, StudyDirection
 from optuna.trial import FrozenTrial, TrialState
-
+from optuna.distributions import BaseDistribution
+from optuna.search_space import IntersectionSearchSpace
+from optuna.samplers._random import RandomSampler
 
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import DotProduct, Matern, RBF
 
 class CARBSSampler(BaseMultiObjectiveSampler):
-    def __init__(self, n_candidates=3):
-        self._rng = np.random.RandomState()
+    def __init__(
+            self, 
+            n_candidates: int = 3,
+            _n_startup_trials: int = 10,
+            seed: Optional[int] = None,
+        ) -> None:
         self.n_candidates = n_candidates
+        self._n_startup_trials = _n_startup_trials
+
+        self._rng = np.random.RandomState(seed=seed)
+        self._search_space = IntersectionSearchSpace(include_pruned=True)
+        self._random_sampler = RandomSampler(seed=seed)
         self._current_trial = None  # Current state. 
 
     def reseed_rng(self) -> None:
@@ -40,7 +52,7 @@ class CARBSSampler(BaseMultiObjectiveSampler):
                 if t.values[1] > best_performance:
                     pareto_front.append(t)
                     best_performance = t.values[1]
-        else:  # If performance is to be minimized
+        else:   # If we're minimizing performance
             for t in sorted_trials:
                 if t.values[1] < best_performance:
                     pareto_front.append(t)
@@ -49,7 +61,7 @@ class CARBSSampler(BaseMultiObjectiveSampler):
         return pareto_front
  
         
-    #TODO: I don't think i'm using sigma correctly
+    #TODO: not sure i'm using sigma correctly
     def _sample_candidates(self, trial, n_candidates, sigma):
         
         xi = np.array(list(trial.params.values()))
@@ -62,24 +74,34 @@ class CARBSSampler(BaseMultiObjectiveSampler):
         candidate = namedtuple('candidate', ['params', 'p_search'])
         return [candidate(params[i], p_search[i]) for i in range(n_candidates)]
     
-    def sample_relative(self, study, trial, search_space):
+    def sample_relative(
+        self,
+        study,
+        trial,
+        search_space
+    ) -> Dict[str, Any]:
         if search_space == {}:
             return {}
-        
-        complete_trials = [t for t in study.trials if t.state == optuna.trial.TrialState.COMPLETE]
+
+        states = (TrialState.COMPLETE, TrialState.PRUNED)
+        trials = study._get_trials(deepcopy=False, states=states, use_cache=True)
+        # If the number of samples is insufficient, we run random trial.
+        if len(trials) < self._n_startup_trials:
+            return {}
 
         # CARBS algorithm.
         # 1. Calculate pareto front, passing direction for performance
-        pareto_front = self._calculate_pareto_front(complete_trials, study.directions[1])
+        pareto_front = self._calculate_pareto_front(trials, study.directions[1])
 
         # 2. Generate candidates
-        candidates = [self._sample_candidates(point, self.n_candidates, sigma=.5) for point in pareto_front]
-        candidates = [item for row in candidates for item in row]
+        candidates = [self._sample_candidates(point, self.n_candidates, sigma=.5)
+                        for point in pareto_front]
+        candidates = [c for cands in candidates for c in cands]
         
         # 3. fit GP models to the candidates
-        X = [list(t.params.values()) for t in complete_trials]
-        costs = [t.values[0]for t in complete_trials]
-        performances = [t.values[1] for t in complete_trials]
+        X = [list(t.params.values()) for t in trials]
+        costs = [t.values[0]for t in trials]
+        performances = [t.values[1] for t in trials]
 
         pf_costs = np.array([t.values[0]for t in pareto_front]).reshape(-1, 1)
         pf_performances = [t.values[1] for t in pareto_front]
@@ -118,18 +140,39 @@ class CARBSSampler(BaseMultiObjectiveSampler):
                 best_acquisition = acq
                 best_cand = cand
 
-        return best_cand.params
-
-    # boilerplate
-    def infer_relative_search_space(self, study, trial):
-        return optuna.search_space.intersection_search_space(study.get_trials(deepcopy=False))
+        # obviously a hack
+        ret = dict(zip(trials[0].params.keys(), best_cand.params))
+        return ret
+ 
+    def infer_relative_search_space(
+        self, 
+        study: Study, 
+        trial: FrozenTrial
+    ) -> Dict[str, BaseDistribution]:
+        return self._search_space.calculate(study)
     
     # maybe we randomly sample the search center ?
-    def sample_independent(self, study, trial, param_name, param_distribution):
-        independent_sampler = optuna.samplers.RandomSampler()
-        return independent_sampler.sample_independent(study, trial, param_name, param_distribution)
+    def sample_independent(
+        self,
+        study,
+        trial,
+        param_name,
+        param_distribution
+    ):
+        states = (TrialState.COMPLETE, TrialState.PRUNED)
+        trials = study._get_trials(deepcopy=False, states=states, use_cache=True)
+
+        # If the number of samples is insufficient, we run random trial.
+        if len(trials) < self._n_startup_trials:
+            return self._random_sampler.sample_independent(
+                study, trial, param_name, param_distribution
+            )
     
-    def before_trial(self, study: Study, trial: FrozenTrial) -> None:
+    def before_trial(
+        self,
+        study: Study,
+        trial: FrozenTrial
+    ) -> None:
         pass
 
     def after_trial(
@@ -140,28 +183,3 @@ class CARBSSampler(BaseMultiObjectiveSampler):
         values: Optional[Sequence[float]],
     ) -> None:
         pass
-
-
-def objective(trial):
-    x = trial.suggest_float("x", -10, 10)
-    y = trial.suggest_float("y", -5, 5)
-
-    cost = abs(x) / 2
-    performance = x**2 + y
-
-    return cost, performance
-
-
-sampler = CARBSSampler()
-study = optuna.create_study(directions=['minimize', 'maximize'], sampler=sampler)
-study.optimize(objective, n_trials=20)
-
-optuna.visualization.plot_pareto_front(study, target_names=["cost", "performance"])
-
-print(f"Number of trials on the Pareto front: {len(study.best_trials)}")
-
-trial_with_highest_performance = max(study.best_trials, key=lambda t: t.values[1])
-print(f"Trial with highest performance: ")
-print(f"\tnumber: {trial_with_highest_performance.number}")
-print(f"\tparams: {trial_with_highest_performance.params}")
-print(f"\tvalues: {trial_with_highest_performance.values}")
