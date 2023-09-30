464b780bb439214f5241912af128c5ebb6ad4bd6
smygw <smygw72@gmail.com>
2023-09-30 17:34:39 +0900

    Fixed path
    Co-authored-by: Toshihiko Yanase <toshihiko.yanase@gmail.com>

--- optuna/artifacts/exceptions.py
+++ optuna/artifacts/exceptions.py
@@ -5,7 +5,7 @@ class ArtifactNotFound(OptunaError):
     """Exception raised when an artifact is not found.
 
     It is typically raised while calling
-    :meth:`~optuna.artifact.protocol.ArtifactBackend.open_reader` or
+    :meth:`~optuna.artifacts._protocol.ArtifactStore.open_reader` or
     :meth:`~optuna.artifact.protocol.ArtifactBackend.remove` methods.
     """
 
