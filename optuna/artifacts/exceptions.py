8ee192681c75be577d68594a06e6f6560d596dc1
smygw72 <smygw72@preferred.jp>
2023-09-30 17:17:58 +0900

    update documents

--- optuna/artifacts/exceptions.py
+++ optuna/artifacts/exceptions.py
@@ -5,8 +5,8 @@ class ArtifactNotFound(OptunaError):
     """Exception raised when an artifact is not found.
 
     It is typically raised while calling
-    :meth:`~optuna_dashboard.artifact.protocol.ArtifactBackend.open` or
-    :meth:`~optuna_dashboard.artifact.protocol.ArtifactBackend.remove` methods.
+    :meth:`~optuna.artifact.protocol.ArtifactBackend.open_reader` or
+    :meth:`~optuna.artifact.protocol.ArtifactBackend.remove` methods.
     """
 
     ...
