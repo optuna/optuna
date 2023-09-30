2ab224da9e1f321a9c767f815282821fda0272c6
Gen Tamaki <tamaki@qp.phys.sci.osaka-u.ac.jp>
2023-09-30 15:24:11 +0900

    fix import name

--- optuna/samplers/__init__.py
+++ optuna/samplers/__init__.py
@@ -3,12 +3,12 @@ from optuna.samplers._base import BaseSampler
 from optuna.samplers._brute_force import BruteForceSampler
 from optuna.samplers._cmaes import CmaEsSampler
 from optuna.samplers._grid import GridSampler
-from optuna.samplers._nsgaiii import NSGAIIISampler
 from optuna.samplers._partial_fixed import PartialFixedSampler
 from optuna.samplers._qmc import QMCSampler
 from optuna.samplers._random import RandomSampler
 from optuna.samplers._search_space import intersection_search_space
 from optuna.samplers._search_space import IntersectionSearchSpace
+from optuna.samplers._nsgaiii.sampler import NSGAIIISampler
 from optuna.samplers._tpe.multi_objective_sampler import MOTPESampler
 from optuna.samplers._tpe.sampler import TPESampler
 from optuna.samplers.nsgaii._sampler import NSGAIISampler
