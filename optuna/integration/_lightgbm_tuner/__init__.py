b8348aca7b1a869c105b40ffd359686e9e77dc39
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-10-12 09:39:20 +0200

    Change the import of train

--- optuna/integration/_lightgbm_tuner/__init__.py
+++ optuna/integration/_lightgbm_tuner/__init__.py
@@ -1,10 +1,10 @@
+from optuna.integration._lightgbm_tuner._train import train
 from optuna.integration._lightgbm_tuner.optimize import _imports
 from optuna.integration._lightgbm_tuner.optimize import LightGBMTuner
 from optuna.integration._lightgbm_tuner.optimize import LightGBMTunerCV
 
 
 if _imports.is_successful():
-    from optuna.integration._lightgbm_tuner._train import train
     from optuna.integration._lightgbm_tuner.sklearn import LGBMClassifier
     from optuna.integration._lightgbm_tuner.sklearn import LGBMModel
     from optuna.integration._lightgbm_tuner.sklearn import LGBMRegressor
