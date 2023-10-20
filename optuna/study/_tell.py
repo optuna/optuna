8eec977179f2db264d8626f843024c8b79185157
Vaibhav <vaibhavyangkai123@gmail.com>
2023-10-20 19:23:20 +0530

    changed from threading.local to contextvars

--- optuna/study/_tell.py
+++ optuna/study/_tell.py
@@ -100,7 +100,7 @@ def _tell_with_warning(
     """
 
     # We must invalidate all trials cache here as it is only valid within a trial.
-    study._thread_local.cached_all_trials = None
+    study._reset_cached_trials()
 
     # Validate the trial argument.
     frozen_trial = _get_frozen_trial(study, trial)
