5033525bffcbc0ee15551ac6d59ae0d7cafaa5f0
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2023-10-30 02:20:32 +0100

    Fix a mypy error

--- optuna/samplers/_tpe/sampler.py
+++ optuna/samplers/_tpe/sampler.py
@@ -646,7 +646,8 @@ def _split_complete_trials_multi_objective(
     n_below: int,
 ) -> tuple[list[FrozenTrial], list[FrozenTrial]]:
     if n_below == 0:
-        return [], trials
+        # The type of trials must be `list`, but not `Sequence`. 
+        return [], [t for t in trials]
 
     lvals = np.asarray([trial.values for trial in trials])
     for i, direction in enumerate(study.directions):
