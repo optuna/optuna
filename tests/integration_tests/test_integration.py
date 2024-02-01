29bc7ffe9ef3e75f9e07ad715a7c217859328dfa
buruzaemon <brooke.fujita@gmail.com>
2024-02-01 12:13:44 +0900

    removing unnecessary imports/references to xgboost

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -7,8 +7,6 @@ pytestmark = pytest.mark.integration
 def test_import() -> None:
     from optuna.integration import lightgbm  # NOQA
     from optuna.integration import LightGBMPruningCallback  # NOQA
-    from optuna.integration import xgboost  # NOQA
-    from optuna.integration import XGBoostPruningCallback  # NOQA
 
     with pytest.raises(ImportError):
         from optuna.integration import unknown_module  # type: ignore # NOQA
@@ -18,9 +16,7 @@ def test_module_attributes() -> None:
     import optuna
 
     assert hasattr(optuna.integration, "lightgbm")
-    assert hasattr(optuna.integration, "xgboost")
     assert hasattr(optuna.integration, "LightGBMPruningCallback")
-    assert hasattr(optuna.integration, "XGBoostPruningCallback")
 
     with pytest.raises(AttributeError):
         optuna.integration.unknown_attribute  # type: ignore
