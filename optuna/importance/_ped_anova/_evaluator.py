8f35675753a71a48cf0d14d4ef1330dc9edac34b
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-02-01 15:55:35 +0100

    Add custom filter

--- optuna/importance/_ped_anova/_evaluator.py
+++ optuna/importance/_ped_anova/_evaluator.py
@@ -17,24 +17,34 @@ from optuna.trial import FrozenTrial
 
 
 class QuantileFilter:
-    def __init__(self, quantile: float, min_n_top_trials: int):
+    def __init__(
+        self,
+        quantile: float,
+        is_lower_better: bool,
+        min_n_top_trials: int,
+        target: Callable[[FrozenTrial], float] | None,
+    ):
         if quantile < 0 or quantile > 1:
             raise ValueError(f"quantile must be in [0, 1], but got {quantile}.")
         if min_n_top_trials <= 0:
             raise ValueError(f"min_n_top_trials must be positive, but got {min_n_top_trials}.")
 
         self._quantile = quantile
+        self._is_lower_better = is_lower_better
         self._min_n_top_trials = min_n_top_trials
+        self._target = target
 
-    def filter(
-        self,
-        trials: list[FrozenTrial],
-        is_lower_better: bool,
-        target: Callable[[FrozenTrial], float] | None = None,
-    ) -> list[FrozenTrial]:
-        sign = 1.0 if is_lower_better else -1.0
+    def filter(self, trials: list[FrozenTrial]) -> list[FrozenTrial]:
+        target, min_n_top_trials = self._target, self._min_n_top_trials
+        if target is None and max([len(t.values) for t in trials], default=1) > 1:
+            raise ValueError(
+                "If the `study` is being used for multi-objective optimization, "
+                "please specify the `target`. For example, use "
+                "`target=lambda t: t.values[0]` for the first objective value."
+            )
+
+        sign = 1.0 if self._is_lower_better else -1.0
         loss_values = sign * np.asarray([t.value if target is None else target(t) for t in trials])
-        min_n_top_trials = self._min_n_top_trials
         if min_n_top_trials > loss_values.size:
             raise ValueError(
                 f"len(trials) must be larger than or equal to min_n_top_trials={min_n_top_trials}"
@@ -87,11 +97,44 @@ class PedAnovaImportanceEvaluator(BaseImportanceEvaluator):
             Meanwhile, `evaluate_on_local=False` gives the importances in the specified
             `search_space`. `evaluate_on_local=True` is especially useful when users modify search
             space during the specified `study`.
+        custom_filter:
+            If given, the importance of each parameter to achieve filtered trials will be computed.
+            If not specified, we compute the importance of achieving `baseline_quantile` in trials.
+
+            Example:
+                The following custom_filter allows calculating the importance of achieving
+                the value of lower than 1.0.
+
+                .. testcode::
+
+                    import optuna
+                    from optuna.importance import PedAnovaImportanceEvaluator
+
+
+                    def objective(trial):
+                        x1 = trial.suggest_float("x1", -10, 10)
+                        x2 = trial.suggest_float("x2", -10, 10)
+                        return x1 + x2 / 1000
+
+
+                    study = optuna.create_study()
+                    study.optimize(objective, n_trials=100)
+                    custom_filter = lambda trials: [t for t in trials if t.value < 1.0]
+                    evaluator = PedAnovaImportanceEvaluator(custom_filter=custom_filter)
+                    evaluator.evaluate(study)
+
     """
 
-    def __init__(self, *, baseline_quantile: float = 0.1, evaluate_on_local: bool = True):
+    def __init__(
+        self,
+        *,
+        baseline_quantile: float = 0.1,
+        evaluate_on_local: bool = True,
+        custom_filter: Callable[[list[FrozenTrial]], list[FrozenTrial]] | None = None,
+    ):
         self._baseline_quantile = baseline_quantile
         self._evaluate_on_local = evaluate_on_local
+        self._custom_filter = custom_filter
 
         # Advanced Setups.
         # Discretize a domain [low, high] as `np.linspace(low, high, n_steps)`.
@@ -110,13 +153,6 @@ class PedAnovaImportanceEvaluator(BaseImportanceEvaluator):
         params: list[str],
         target: Callable[[FrozenTrial], float] | None,
     ) -> list[FrozenTrial]:
-        if target is None and study._is_multi_objective():
-            raise ValueError(
-                "If the `study` is being used for multi-objective optimization, "
-                "please specify the `target`. For example, use "
-                "`target=lambda t: t.values[0]` for the first objective value."
-            )
-
         is_lower_better = study.directions[0] == StudyDirection.MINIMIZE
         if target is not None:
             warnings.warn(
@@ -125,12 +161,20 @@ class PedAnovaImportanceEvaluator(BaseImportanceEvaluator):
             )
             is_lower_better = True
 
-        top_trials = QuantileFilter(self._baseline_quantile, self._min_n_top_trials).filter(
-            trials, is_lower_better, target
-        )
+        if self._custom_filter is not None:
+            top_trials = self._custom_filter(trials)
+        else:
+            top_trials = QuantileFilter(
+                self._baseline_quantile, is_lower_better, self._min_n_top_trials, target
+            ).filter(trials)
 
         if len(trials) == len(top_trials):
             warnings.warn("All trials are in top trials, which gives equal importances.")
+        elif len(top_trials) < self._min_n_top_trials:
+            raise ValueError(
+                f"custom_filter must give at least {self._min_n_top_trials} trials "
+                f"after the filtering, but got {len(top_trials)} trials."
+            )
 
         return top_trials
 
