29be61638b1363f36e04e48fbdef8f998f1df309
Shinichi Hemmi <50256998+Alnusjaponica@users.noreply.github.com>
2024-02-08 17:36:27 +0900

    Update links to use DOI
    Co-authored-by: Yoshihiko Ozaki <30489874+y0z@users.noreply.github.com>

--- optuna/samplers/_cmaes.py
+++ optuna/samplers/_cmaes.py
@@ -101,7 +101,7 @@ class CmaEsSampler(BaseSampler):
       size. In Proceedings of the IEEE Congress on Evolutionary Computation (CEC 2005),
       pages 1769–1776. IEEE Press, 2005. <https://doi.org/10.1109/CEC.2005.1554902>`_
     - `N. Hansen. Benchmarking a BI-Population CMA-ES on the BBOB-2009 Function Testbed.
-      GECCO Workshop, 2009. <https://dl.acm.org/doi/10.1145/1570256.1570333>`_
+      GECCO Workshop, 2009. <https://doi.org/10.1145/1570256.1570333>`_
     - `Raymond Ros, Nikolaus Hansen. A Simple Modification in CMA-ES Achieving Linear Time and
       Space Complexity. 10th International Conference on Parallel Problem Solving From Nature,
       Sep 2008, Dortmund, Germany. inria-00287367. <https://doi.org/10.1007/978-3-540-87700-4_30>`_
