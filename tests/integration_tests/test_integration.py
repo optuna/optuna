afd31f1843d77929aed2e3c7e7862334a37dd87b
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-02-09 03:24:52 +0100

    Salvage buruzaemon

--- tests/integration_tests/test_integration.py
+++ tests/integration_tests/test_integration.py
@@ -5,10 +5,10 @@ pytestmark = pytest.mark.integration
 
 
 def test_import() -> None:
+    from optuna.integration import dask  # NOQA
+    from optuna.integration import DaskStorage  # NOQA
     from optuna.integration import lightgbm  # NOQA
     from optuna.integration import LightGBMPruningCallback  # NOQA
-    from optuna.integration import xgboost  # NOQA
-    from optuna.integration import XGBoostPruningCallback  # NOQA
 
     with pytest.raises(ImportError):
         from optuna.integration import unknown_module  # type: ignore # NOQA
@@ -17,10 +17,9 @@ def test_import() -> None:
 def test_module_attributes() -> None:
     import optuna
 
+    assert hasattr(optuna.integration, "dask")
     assert hasattr(optuna.integration, "lightgbm")
-    assert hasattr(optuna.integration, "xgboost")
     assert hasattr(optuna.integration, "LightGBMPruningCallback")
-    assert hasattr(optuna.integration, "XGBoostPruningCallback")
 
     with pytest.raises(AttributeError):
         optuna.integration.unknown_attribute  # type: ignore
