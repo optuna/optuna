596cb9b4d2aebb4c551ddf8bedc2fcd0979d5aa4
y0z <y0z@users.noreply.github.com>
2024-02-14 17:53:40 +0900

    Fix __init__.py.

--- optuna/integration/lightgbm.py
+++ optuna/integration/lightgbm.py
@@ -1,6 +1,9 @@
-from optuna_integration.lightgbm import LightGBMPruningCallback
-from optuna_integration.lightgbm import LightGBMTuner
-from optuna_integration.lightgbm import LightGBMTunerCV
+import os
+import sys
+from types import ModuleType
+from typing import Any
+
+import optuna_integration.lightgbm as lgb
 
 
 __all__ = [
@@ -8,3 +11,17 @@ __all__ = [
     "LightGBMTuner",
     "LightGBMTunerCV",
 ]
+
+
+class _LightGBMModule(ModuleType):
+    """Module class that implements `optuna.integration.lightgbm` package."""
+
+    __all__ = __all__
+    __file__ = globals()["__file__"]
+    __path__ = [os.path.dirname(__file__)]
+
+    def __getattr__(self, name: str) -> Any:
+        return lgb.__dict__[name]
+
+
+sys.modules[__name__] = _LightGBMModule(__name__)
