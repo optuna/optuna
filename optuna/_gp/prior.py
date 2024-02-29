6d914c9988d026dfe918c8489c03920305b09de1
Contramundum <contramundum53@outlook.com>
2024-02-29 20:44:54 +0900

    add comments

--- optuna/_gp/prior.py
+++ optuna/_gp/prior.py
@@ -23,7 +23,8 @@ def default_log_prior(kernel_params: "gp.KernelParamsTensor") -> "torch.Tensor":
         # We omit the constant factor `rate ** concentration / Gamma(concentration)`.
         return (concentration - 1) * torch.log(x) - rate * x
 
-    # NOTE(contramundum53): The priors below (the functions and its params) were picked by heuristics.
+    # NOTE(contramundum53): The priors below (params and function 
+    # shape for inverse_squared_lengthscales) were picked by heuristics.
     # TODO(contramundum53): Check whether these priors are appropriate.
     return (
         -(
