3f024752556caa9911c21a6697b7cabd675b5df7
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-03-12 18:03:46 +0100

    Remove compute 2d with two points

--- optuna/_hypervolume/__init__.py
+++ optuna/_hypervolume/__init__.py
@@ -1,14 +1,12 @@
 from optuna._hypervolume.base import BaseHypervolume
 from optuna._hypervolume.hssp import _solve_hssp
 from optuna._hypervolume.utils import _compute_2d
-from optuna._hypervolume.utils import _compute_2points_volume
 from optuna._hypervolume.wfg import WFG
 
 
 __all__ = [
     "BaseHypervolume",
     "_compute_2d",
-    "_compute_2points_volume",
     "_solve_hssp",
     "WFG",
 ]
