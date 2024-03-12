3dc3db733eb43536b12e9aa06206544c72d3cf13
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-03-12 17:15:13 +0100

    Adapt the tests for get_pareto_front

--- tests/test_multi_objective.py
+++ tests/test_multi_objective.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import pytest
+from typing import Sequence
+
 from optuna import create_study
 from optuna import trial
-from optuna.study._multi_objective import _get_pareto_front_trials_2d
-from optuna.study._multi_objective import _get_pareto_front_trials_nd
+from optuna.study import StudyDirection
+from optuna.study._multi_objective import _get_pareto_front_trials_by_trials
 from optuna.trial import FrozenTrial
 
 
@@ -12,126 +15,64 @@ def _trial_to_values(t: FrozenTrial) -> tuple[float, ...]:
     return tuple(t.values)
 
 
-def test_get_pareto_front_trials_2d() -> None:
-    study = create_study(directions=["minimize", "maximize"])
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == set()
-
-    study.optimize(lambda t: [2, 2], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(2, 2)}
-
-    study.optimize(lambda t: [1, 1], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(1, 1), (2, 2)}
-
-    study.optimize(lambda t: [3, 1], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(1, 1), (2, 2)}
-
-    study.optimize(lambda t: [3, 2], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(1, 1), (2, 2)}
-
-    study.optimize(lambda t: [1, 3], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(1, 3)}
-    assert len(_get_pareto_front_trials_2d(study.trials, study.directions)) == 1
-
-    study.optimize(lambda t: [1, 3], n_trials=1)  # The trial result is the same as the above one.
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_2d(study.trials, study.directions)
-    } == {(1, 3)}
-    assert len(_get_pareto_front_trials_2d(study.trials, study.directions)) == 2
+def assert_is_output_equal_to_ans(
+    trials: list[FrozenTrial], directions: Sequence[StudyDirection], ans: set[int]
+) -> None:
+    res = {_trial_to_values(t) for t in _get_pareto_front_trials_by_trials(trials, directions)}
+    assert res == ans
 
 
-def test_get_pareto_front_trials_2d_with_constraint() -> None:
-    study = create_study(directions=["minimize", "maximize"])
+@pytest.mark.parametrize("directions,values_set,ans_set", [
+    (
+        ["minimize", "maximize"],
+        [[2, 2], [1, 1], [3, 1], [3, 2], [1, 3]],
+        [{(2, 2)}] + [{(1, 1), (2, 2)}] * 3 + [{(1, 3)}],
+    ),
+    (
+        ["minimize", "maximize", "minimize"],
+        [[2, 2, 2], [1, 1, 1], [3, 1, 3], [3, 2, 3], [1, 3, 1]],
+        [{(2, 2, 2)}] + [{(1, 1, 1), (2, 2, 2)}] * 3 + [{(1, 3, 1)}],
+    ),
+])
+def test_get_pareto_front_trials(
+    directions: list[str], values_set: list[list[int]], ans_set: list[set[tuple[int]]]
+) -> None:
+    study = create_study(directions=directions)
+    assert_is_output_equal_to_ans(study.trials, study.directions, set())
+    for values, ans in zip(values_set, ans_set):
+        study.optimize(lambda t: values, n_trials=1)
+        assert_is_output_equal_to_ans(study.trials, study.directions, ans)
+
+    assert len(_get_pareto_front_trials_by_trials(study.trials, study.directions)) == 1
+    # The trial result is the same as the last one.
+    study.optimize(lambda t: values_set[-1], n_trials=1)
+    assert_is_output_equal_to_ans(study.trials, study.directions, ans_set[-1])
+    assert len(_get_pareto_front_trials_by_trials(study.trials, study.directions)) == 2
+
+
+@pytest.mark.parametrize("directions,values_set,ans_set", [
+    (
+        ["minimize", "maximize"], [[1, 1], [2, 2], [3, 2]], [{(1, 1), (2, 2)}, {(1, 1), (3, 2)}]
+    ),
+    (
+        ["minimize", "maximize", "minimize"],
+        [[1, 1, 1], [2, 2, 2], [3, 2, 3]],
+        [{(1, 1, 1), (2, 2, 2)}, {(1, 1, 1), (3, 2, 3)}],
+    ),
+])
+def test_get_pareto_front_trials_with_constraint(
+    directions: list[str], values_set: list[list[int]], ans_set: list[set[tuple[int]]]
+) -> None:
+    study = create_study(directions=directions)
     trials = [
-        trial.create_trial(values=[1, 1], system_attrs={"constraints": [0]}),
-        trial.create_trial(values=[2, 2], system_attrs={"constraints": [1]}),
-        trial.create_trial(values=[3, 2], system_attrs={"constraints": [0]}),
+        trial.create_trial(values=values, system_attrs={"constraints": [i % 2]})
+        for i, values in enumerate(values_set)
     ]
     study.add_trials(trials)
-    assert {
-        _trial_to_values(t)
-        for t in _get_pareto_front_trials_2d(study.trials, study.directions, False)
-    } == {(1, 1), (2, 2)}
-    assert {
-        _trial_to_values(t)
-        for t in _get_pareto_front_trials_2d(study.trials, study.directions, True)
-    } == {(1, 1), (3, 2)}
-
-
-def test_get_pareto_front_trials_nd() -> None:
-    study = create_study(directions=["minimize", "maximize", "minimize"])
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == set()
-
-    study.optimize(lambda t: [2, 2, 2], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {(2, 2, 2)}
-
-    study.optimize(lambda t: [1, 1, 1], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {
-        (1, 1, 1),
-        (2, 2, 2),
-    }
-
-    study.optimize(lambda t: [3, 1, 3], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {
-        (1, 1, 1),
-        (2, 2, 2),
-    }
-
-    study.optimize(lambda t: [3, 2, 3], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {
-        (1, 1, 1),
-        (2, 2, 2),
-    }
-
-    study.optimize(lambda t: [1, 3, 1], n_trials=1)
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {(1, 3, 1)}
-    assert len(_get_pareto_front_trials_nd(study.trials, study.directions)) == 1
-
-    study.optimize(
-        lambda t: [1, 3, 1], n_trials=1
-    )  # The trial result is the same as the above one.
-    assert {
-        _trial_to_values(t) for t in _get_pareto_front_trials_nd(study.trials, study.directions)
-    } == {(1, 3, 1)}
-    assert len(_get_pareto_front_trials_nd(study.trials, study.directions)) == 2
-
-
-def test_get_pareto_front_trials_nd_with_constraint() -> None:
-    study = create_study(directions=["minimize", "maximize", "minimize"])
-    trials = [
-        trial.create_trial(values=[1, 1, 1], system_attrs={"constraints": [0]}),
-        trial.create_trial(values=[2, 2, 2], system_attrs={"constraints": [1]}),
-        trial.create_trial(values=[3, 2, 3], system_attrs={"constraints": [0]}),
-    ]
-    study.add_trials(trials)
-    assert {
-        _trial_to_values(t)
-        for t in _get_pareto_front_trials_nd(study.trials, study.directions, False)
-    } == {(1, 1, 1), (2, 2, 2)}
-    assert {
-        _trial_to_values(t)
-        for t in _get_pareto_front_trials_nd(study.trials, study.directions, True)
-    } == {(1, 1, 1), (3, 2, 3)}
+    for consider_constraint, ans in zip([False, True], ans_set):
+        trials = study.trials
+        study_dirs = study.directions
+        assert ans == {
+            _trial_to_values(t)
+            for t in _get_pareto_front_trials_by_trials(trials, study_dirs, consider_constraint)
+        }
