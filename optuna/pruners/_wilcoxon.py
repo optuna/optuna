868099084e70c60045804c38c045830c1411d168
Contramundum <contramundum53@outlook.com>
2024-03-14 14:29:45 +0900

    Fix phrasing in doc

--- optuna/pruners/_wilcoxon.py
+++ optuna/pruners/_wilcoxon.py
@@ -34,7 +34,7 @@ class WilcoxonPruner(BasePruner):
     - the accuracy of outputs of a large language model (LLM) on a set of questions.
 
     There can be "easy" or "hard" instances (the pruner handles correspondence of the instances between different trials).
-    In each trial, it is recommended to shuffle the order in which data is processed, so that the optimization doesn't overfit to the instances in the beginning.
+    In each trial, it is recommended to shuffle the evaluation order, so that the optimization doesn't overfit to the instances in the beginning.
 
     When you use this pruner, you must call `Trial.report(value, step)` function for each step (instance id) with
     the evaluated value. The instance id may not be in ascending order.
