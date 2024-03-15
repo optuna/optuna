321953ae35c8d3841e6f954f4d9f5ea1c9a7745e
Shuhei Watanabe <47781922+nabenabe0928@users.noreply.github.com>
2024-03-15 11:09:21 +0900

    Update optuna/importance/_fanova/_evaluator.py
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- optuna/importance/_fanova/_evaluator.py
+++ optuna/importance/_fanova/_evaluator.py
@@ -126,7 +126,7 @@ class FanovaImportanceEvaluator(BaseImportanceEvaluator):
         param_importances = numpy.array(
             [evaluator.get_importance(i)[0] for i in range(len(non_single_distributions))]
         )
-        # NOTE(nabenabe0928): Normalize is here to keep the backward compatibility.
+        #Normalize is here to keep the backward compatibility.
         # TODO(nabenabe0928): Add the `normalize` argument optionally.
         param_importances /= numpy.sum(param_importances)
 
