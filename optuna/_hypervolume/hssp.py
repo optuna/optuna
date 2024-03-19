8ad8269a6110b8aef9c8ba828e7677144fab5a13
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-03-19 18:07:46 +0100

    Reduce the time complexity of hssp 2d from O(NK^2 log K) to O(NK)

--- optuna/_hypervolume/hssp.py
+++ optuna/_hypervolume/hssp.py
@@ -5,6 +5,35 @@ import numpy as np
 import optuna
 
 
+def _solve_hssp_2d(
+    rank_i_loss_vals: np.ndarray,
+    rank_i_indices: np.ndarray,
+    subset_size: int,
+    reference_point: np.ndarray,
+) -> np.ndarray:
+    # The time complexity is O(subset_size * rank_i_loss_vals.shape[0]).
+    assert rank_i_loss_vals.shape[-1] == 2 and subset_size <= rank_i_loss_vals.shape[0]
+    n_trials = rank_i_loss_vals.shape[0]
+    is_chosen = np.zeros(n_trials, dtype=bool)
+    indices = np.arange(n_trials, dtype=int)
+    order = np.argsort(rank_i_loss_vals[:, 0])
+    sorted_loss_vals = rank_i_loss_vals[order]
+    rectangle_diagonal_points = np.repeat(reference_point[np.newaxis, :], n_trials, axis=0)
+    for i in range(subset_size):
+        contribs = np.prod(rectangle_diagonal_points - sorted_loss_vals, axis=-1)
+        # NOTE(nabenabe0928): `is_chosen` is necessary for loss_vals with `nan` or `inf`.
+        max_index = indices[~is_chosen][np.argmax(contribs[~is_chosen])]
+        is_chosen[max_index] = True
+        rectangle_diagonal_points[:max_index + 1, 0] = np.minimum(
+            sorted_loss_vals[max_index, 0], rectangle_diagonal_points[:max_index + 1, 0]
+        )
+        rectangle_diagonal_points[max_index:, 1] = np.minimum(
+            sorted_loss_vals[max_index, 1], rectangle_diagonal_points[max_index:, 1]
+        )
+
+    return rank_i_indices[order[is_chosen]]
+
+
 def _solve_hssp(
     rank_i_loss_vals: np.ndarray,
     rank_i_indices: np.ndarray,
@@ -21,6 +50,9 @@ def _solve_hssp(
     - `Greedy Hypervolume Subset Selection in Low Dimensions
        <https://doi.org/10.1162/EVCO_a_00188>`_
     """
+    if rank_i_loss_vals.shape[-1] == 2:
+        return _solve_hssp_2d(rank_i_loss_vals, rank_i_indices, subset_size, reference_point)
+
     selected_vecs: List[np.ndarray] = []
     selected_indices: List[int] = []
     contributions = [
