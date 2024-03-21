ffdc34fc785b10d240c3c4bba4da29df8e075793
Shuhei Watanabe <47781922+nabenabe0928@users.noreply.github.com>
2024-03-21 06:29:25 +0100

    Update optuna/study/_multi_objective.py
    Co-authored-by: Gen <54583542+gen740@users.noreply.github.com>

--- optuna/study/_multi_objective.py
+++ optuna/study/_multi_objective.py
@@ -185,12 +185,12 @@ def _calculate_nondomination_rank(
     if n_objectives == 1:
         _, ranks = np.unique(loss_values[:, 0], return_inverse=True)
         return ranks
-    else:
-        # It ensures that trials[j] will not dominate trials[i] for i < j.
-        # np.unique does lexsort.
-        unique_lexsorted_loss_values, order_inv = np.unique(
-            loss_values, return_inverse=True, axis=0
-        )
+
+    # It ensures that trials[j] will not dominate trials[i] for i < j.
+    # np.unique does lexsort.
+    unique_lexsorted_loss_values, order_inv = np.unique(
+        loss_values, return_inverse=True, axis=0
+    )
 
     n_unique = unique_lexsorted_loss_values.shape[0]
     # Clip n_below.
