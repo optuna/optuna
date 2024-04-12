97af6fa8646035f1f76a3cd7b6fba5dd7912b998
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-04-12 08:57:26 +0200

    Remove multi_objective.base_sampler

--- optuna/multi_objective/samplers/__init__.py
+++ optuna/multi_objective/samplers/__init__.py
@@ -1,8 +1,6 @@
 from optuna.multi_objective.samplers._adapter import _MultiObjectiveSamplerAdapter
-from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
 
 
 __all__ = [
     "_MultiObjectiveSamplerAdapter",
-    "BaseMultiObjectiveSampler",
 ]
