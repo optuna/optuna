62185788325af1e57f35d6eb0f5497960c8d23ff
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-04-12 08:42:25 +0200

    Remove multi_objective

--- optuna/multi_objective/samplers/__init__.py
+++ optuna/multi_objective/samplers/__init__.py
@@ -1,6 +1,5 @@
 from optuna.multi_objective.samplers._adapter import _MultiObjectiveSamplerAdapter
 from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
-from optuna.multi_objective.samplers._motpe import MOTPEMultiObjectiveSampler
 from optuna.multi_objective.samplers._nsga2 import NSGAIIMultiObjectiveSampler
 from optuna.multi_objective.samplers._random import RandomMultiObjectiveSampler
 
@@ -8,7 +7,6 @@ from optuna.multi_objective.samplers._random import RandomMultiObjectiveSampler
 __all__ = [
     "_MultiObjectiveSamplerAdapter",
     "BaseMultiObjectiveSampler",
-    "MOTPEMultiObjectiveSampler",
     "NSGAIIMultiObjectiveSampler",
     "RandomMultiObjectiveSampler",
 ]
--- optuna/samplers/__init__.py
+++ optuna/samplers/__init__.py
@@ -10,7 +10,6 @@ from optuna.samplers._qmc import QMCSampler
 from optuna.samplers._random import RandomSampler
 from optuna.samplers._search_space import intersection_search_space
 from optuna.samplers._search_space import IntersectionSearchSpace
-from optuna.samplers._tpe.multi_objective_sampler import MOTPESampler
 from optuna.samplers._tpe.sampler import TPESampler
 from optuna.samplers.nsgaii._sampler import NSGAIISampler
 
@@ -21,7 +20,6 @@ __all__ = [
     "CmaEsSampler",
     "GridSampler",
     "IntersectionSearchSpace",
-    "MOTPESampler",
     "NSGAIISampler",
     "NSGAIIISampler",
     "PartialFixedSampler",
