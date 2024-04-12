4f12658578b28a405ad52711d7572625123e05cd
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-04-12 08:50:38 +0200

    Remove NSGA-II and Random from multi_objective.samplers

--- optuna/multi_objective/samplers/__init__.py
+++ optuna/multi_objective/samplers/__init__.py
@@ -1,12 +1,8 @@
 from optuna.multi_objective.samplers._adapter import _MultiObjectiveSamplerAdapter
 from optuna.multi_objective.samplers._base import BaseMultiObjectiveSampler
-from optuna.multi_objective.samplers._nsga2 import NSGAIIMultiObjectiveSampler
-from optuna.multi_objective.samplers._random import RandomMultiObjectiveSampler
 
 
 __all__ = [
     "_MultiObjectiveSamplerAdapter",
     "BaseMultiObjectiveSampler",
-    "NSGAIIMultiObjectiveSampler",
-    "RandomMultiObjectiveSampler",
 ]
