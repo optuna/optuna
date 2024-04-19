a06d2e97918d93d6263a0620c5410b40e7e2659e
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-04-19 07:39:54 +0200

    Add an __init__.py to raise an error for multi_objective import

---
+++ optuna/multi_objective/__init__.py
@@ -0,0 +1,5 @@
+# TODO(nabenabe0928): Come up with any ways to remove this file.
+raise ModuleNotFoundError(
+    "`optuna.multi_objective` was removed at v4.0. "
+    "Please update your code or downgrade your Optuna version."
+)
