149e46279c7bb270bda288f2872b7e16723ad1c1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-04-22 09:44:12 +0200

    Address the not's comments
    Co-authored-by: Naoto Mizuno <gobou522@gmail.com>

--- optuna/multi_objective/__init__.py
+++ optuna/multi_objective/__init__.py
@@ -1,5 +1,6 @@
 # TODO(nabenabe0928): Come up with any ways to remove this file.
+# NOTE(nabenabe0928): Discuss when to remove this class.
 raise ModuleNotFoundError(
-    "`optuna.multi_objective` was removed at v4.0. "
+    "`optuna.multi_objective` was removed at v4.0.0. "
     "Please update your code or downgrade your Optuna version."
 )
--- optuna/samplers/__init__.py
+++ optuna/samplers/__init__.py
@@ -35,7 +35,8 @@ __all__ = [
 class MOTPESampler:
     def __init__(self, *args, **kwargs):  # type: ignore[no-untyped-def]
         # TODO(nabenabe0928): Come up with any ways to remove this file.
-        raise ModuleNotFoundError(
-            "`MOTPESampler` was removed at v4.0. "
+        # NOTE(nabenabe0928): Discuss when to remove this class.
+        raise ImportError(
+            "`MOTPESampler` was removed at v4.0.0. "
             "Please use `TPESampler` instead or downgrade your Optuna version."
         )
