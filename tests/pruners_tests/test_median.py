6b778ca83e492f21ee7300128848f04435b4bc1c
Hiroki Takizawa <contact@hiroki-takizawa.name>
2024-05-09 17:55:03 +0900

    Update test_median.py

--- tests/pruners_tests/test_median.py
+++ tests/pruners_tests/test_median.py
@@ -1,9 +1,12 @@
-from typing import List
-from typing import Tuple
+from __future__ import annotations
+
+import warnings
 
 import pytest
 
 import optuna
+from optuna.exceptions import ExperimentalWarning
+from optuna.study import StudyDirection
 
 
 def test_median_pruner_with_one_trial() -> None:
@@ -17,7 +20,7 @@ def test_median_pruner_with_one_trial() -> None:
 
 
 @pytest.mark.parametrize("direction_value", [("minimize", 2), ("maximize", 0.5)])
-def test_median_pruner_intermediate_values(direction_value: Tuple[str, float]) -> None:
+def test_median_pruner_intermediate_values(direction_value: tuple[str, float]) -> None:
     direction, intermediate_value = direction_value
     pruner = optuna.pruners.MedianPruner(0, 0)
     study = optuna.study.create_study(direction=direction, pruner=pruner)
@@ -110,7 +113,7 @@ def test_median_pruner_n_warmup_steps() -> None:
     ],
 )
 def test_median_pruner_interval_steps(
-    n_warmup_steps: int, interval_steps: int, report_steps: int, expected_prune_steps: List[int]
+    n_warmup_steps: int, interval_steps: int, report_steps: int, expected_prune_steps: list[int]
 ) -> None:
     pruner = optuna.pruners.MedianPruner(0, n_warmup_steps, interval_steps)
     study = optuna.study.create_study(pruner=pruner)
@@ -158,3 +161,58 @@ def test_median_pruner_n_min_trials() -> None:
     trial.report(3, 2)
     # A pruner is activated after the values at step 2 observed n_min_trials times.
     assert trial.should_prune()
+
+
+def test_median_pruner_direction() -> None:
+    optuna.pruners.MedianPruner(direction="minimize")
+    optuna.pruners.MedianPruner(direction="maximize")
+    optuna.pruners.MedianPruner(direction=StudyDirection.MINIMIZE)
+    optuna.pruners.MedianPruner(direction=StudyDirection.MAXIMIZE)
+    optuna.pruners.MedianPruner(direction=None)
+
+    with pytest.raises(ValueError):
+        optuna.pruners.MedianPruner(direction="1")
+
+
+def test_median_pruner_requires_direction_if_multi_objective() -> None:
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=0)
+    study = optuna.study.create_study(directions=["minimize", "minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    study.tell(trial, (1, 1))
+    trial = study.ask()
+    trial.report(1, 1)
+
+    # The direction of pruner must be set for multi-objective optimization.
+    with pytest.raises(ValueError):
+        trial.should_prune()
+
+
+def test_median_pruner_ignores_direction_if_single_objective() -> None:
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=0, direction="minimize")
+    study = optuna.study.create_study(directions=["minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    study.tell(trial, 1)
+    trial = study.ask()
+    trial.report(1, 1)
+
+    # The direction of pruner should not be set for single-objective optimization.
+    with pytest.warns(UserWarning):
+        trial.should_prune()
+
+
+@pytest.mark.parametrize("intermediate_expected", [(0, False), (2, True)])
+def test_median_pruner_works_if_multi_objective(intermediate_expected: tuple[int, bool]) -> None:
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=0, direction="minimize")
+    study = optuna.study.create_study(directions=["minimize", "minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    study.tell(trial, (1, 1))
+    trial = study.ask()
+    trial.report(intermediate_expected[0], 1)
+
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("ignore", ExperimentalWarning)
+        assert trial.should_prune() == intermediate_expected[1]
+        assert len(w) == 0
