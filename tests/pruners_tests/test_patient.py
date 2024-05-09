bd8ff874ba8d1cbbd4bad142613e36f2e679b717
Hiroki Takizawa <contact@hiroki-takizawa.name>
2024-05-09 18:10:35 +0900

    Update test_patient.py

--- tests/pruners_tests/test_patient.py
+++ tests/pruners_tests/test_patient.py
@@ -1,8 +1,12 @@
-from typing import List
+from __future__ import annotations
+
+import warnings
 
 import pytest
 
 import optuna
+from optuna.exceptions import ExperimentalWarning
+from optuna.study import StudyDirection
 
 
 def test_patient_pruner_experimental_warning() -> None:
@@ -76,8 +80,8 @@ def test_patient_pruner_intermediate_values(
     patience: int,
     min_delta: float,
     direction: str,
-    intermediates: List[int],
-    expected_prune_steps: List[int],
+    intermediates: list[int],
+    expected_prune_steps: list[int],
 ) -> None:
     pruner = optuna.pruners.PatientPruner(None, patience, min_delta)
     study = optuna.study.create_study(pruner=pruner, direction=direction)
@@ -90,3 +94,64 @@ def test_patient_pruner_intermediate_values(
         if trial.should_prune():
             pruned.append(step)
     assert pruned == expected_prune_steps
+
+
+def test_patient_pruner_direction() -> None:
+    optuna.pruners.PatientPruner(None, patience=0, direction="minimize")
+    optuna.pruners.PatientPruner(None, patience=0, direction="maximize")
+    optuna.pruners.PatientPruner(None, patience=0, direction=StudyDirection.MINIMIZE)
+    optuna.pruners.PatientPruner(None, patience=0, direction=StudyDirection.MAXIMIZE)
+    optuna.pruners.PatientPruner(None, patience=0, direction=None)
+
+    with pytest.raises(ValueError):
+        optuna.pruners.PatientPruner(None, patience=0, direction="1")
+
+
+def test_patient_pruner_requires_direction_if_multi_objective() -> None:
+    pruner = optuna.pruners.PatientPruner(None, patience=0)
+    study = optuna.study.create_study(directions=["minimize", "minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(1, 2)
+    study.tell(trial, (1, 1))
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(1, 2)
+
+    # The direction of pruner must be set for multi-objective optimization.
+    with pytest.raises(ValueError):
+        trial.should_prune()
+
+
+def test_patient_pruner_ignores_direction_if_single_objective() -> None:
+    pruner = optuna.pruners.PatientPruner(None, patience=0, direction="minimize")
+    study = optuna.study.create_study(directions=["minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(1, 2)
+    study.tell(trial, 1)
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(1, 2)
+
+    # The direction of pruner should not be set for single-objective optimization.
+    with pytest.warns(UserWarning):
+        trial.should_prune()
+
+
+@pytest.mark.parametrize("intermediate_expected", [(0, False), (2, True)])
+def test_patient_pruner_works_if_multi_objective(intermediate_expected: tuple[int, bool]) -> None:
+    pruner = optuna.pruners.PatientPruner(None, patience=0, direction="minimize")
+    study = optuna.study.create_study(directions=["minimize", "minimize"], pruner=pruner)
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(1, 2)
+    study.tell(trial, (1, 1))
+    trial = study.ask()
+    trial.report(1, 1)
+    trial.report(intermediate_expected[0], 2)
+
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("ignore", ExperimentalWarning)
+        assert trial.should_prune() == intermediate_expected[1]
+        assert len(w) == 0
