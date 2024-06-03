2c1a46a9481b97e2dc5ffcbdb66296cc418a20b4
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-03 09:41:04 +0200

    Add a unittest for HSSP with cache

--- optuna/_hypervolume/__init__.py
+++ optuna/_hypervolume/__init__.py
@@ -1,5 +1,6 @@
 from optuna._hypervolume.base import BaseHypervolume
 from optuna._hypervolume.hssp import _solve_hssp
+from optuna._hypervolume.hssp import _solve_hssp_with_cache
 from optuna._hypervolume.utils import _compute_2d
 from optuna._hypervolume.wfg import WFG
 
@@ -8,5 +9,6 @@ __all__ = [
     "BaseHypervolume",
     "_compute_2d",
     "_solve_hssp",
+    "_solve_hssp_with_cache",
     "WFG",
 ]
--- tests/hypervolume_tests/test_hssp.py
+++ tests/hypervolume_tests/test_hssp.py
@@ -59,3 +59,53 @@ def test_solve_hssp_duplicated_infinite_loss() -> None:
         rank_i_loss_vals=test_case, rank_i_indices=np.arange(4), subset_size=2, reference_point=r
     )
     assert (0 not in res) or (1 not in res)
+
+
+def _solve_hssp_and_check_cache(
+    study: optuna.Study,
+    pareto_sols: np.ndarray,
+    pareto_indices: np.ndarray,
+    subset_size: int, 
+    ref_point: np.ndarray,
+) -> np.ndarray:
+    selected_indices = optuna._hypervolume._solve_hssp_with_cache(
+        study, pareto_sols, pareto_indices, subset_size=subset_size, reference_point=ref_point
+    )
+    hssp_cache = study._storage.get_study_system_attrs(study._study_id)["hssp_cache"]
+    assert np.allclose(hssp_cache["rank_i_loss_vals"], pareto_sols)
+    assert np.array_equal(hssp_cache["rank_i_indices"], pareto_indices)
+    assert hssp_cache["subset_size"] == subset_size
+    assert np.allclose(hssp_cache["reference_point"], ref_point)
+    return selected_indices
+
+
+@pytest.mark.parametrize("storage", (optuna.storages.RDBStorage("sqlite:///:memory:"), optuna.storages.InMemoryStorage()))
+def test_solve_hssp_with_cache(storage: optuna.storages.BaseStorage) -> None:
+    study = optuna.create_study(directions=["minimize"]*2, storage=storage)
+    n_trials = 100
+    n_objectives = 3
+    rng = np.random.RandomState(42)
+    loss_vals = rng.random((n_trials, n_objectives))
+    indices = np.arange(n_trials)
+    on_front = optuna.study._multi_objective._is_pareto_front(
+        loss_vals, assume_unique_lexsorted=False
+    )
+    pareto_sols = loss_vals[on_front]
+    pareto_indices = indices[on_front]
+    ref_point = np.ones(n_objectives, dtype=float)
+    is_rank2 = optuna.study._multi_objective._is_pareto_front(
+        loss_vals[~on_front], assume_unique_lexsorted=False
+    )
+    rank_2_loss_vals = loss_vals[~on_front][is_rank2]
+    rank_2_indices = indices[~on_front][is_rank2]
+    subset_size = min(rank_2_indices.size, pareto_indices.size) // 2
+    for i in range(2):
+        selected_indices_for_pareto = _solve_hssp_and_check_cache(
+            study, pareto_sols, pareto_indices, subset_size, ref_point
+        )
+
+    selected_indices_for_rank_2 = _solve_hssp_and_check_cache(
+        study, rank_2_loss_vals, rank_2_indices, subset_size, ref_point
+    )
+    # Cache should be deleted, meaning that the following condition must be satisfied.
+    assert np.all(np.sort(selected_indices_for_pareto) != np.sort(selected_indices_for_rank_2))
