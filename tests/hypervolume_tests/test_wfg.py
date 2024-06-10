ae3036fa2501d42daa7818f7b27b94cdc84184eb
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-10 07:52:12 +0200

    Add test for assume_pareto=True

--- tests/hypervolume_tests/test_wfg.py
+++ tests/hypervolume_tests/test_wfg.py
@@ -4,56 +4,47 @@ import pytest
 import optuna
 
 
-def test_wfg_2d() -> None:
+def _shuffle_and_filter_sols(sols: np.ndarray, assume_pareto: bool) -> np.ndarray:
+    np.random.shuffle(sols)
+    if assume_pareto:
+        on_front = optuna.study._multi_objective._is_pareto_front(sols, False)
+        sols = sols[on_front]
+    return sols
+
+
+@pytest.mark.parametrize("assume_pareto", (True, False))
+def test_wfg_2d(assume_pareto: bool) -> None:
     for n in range(2, 30):
         r = n * np.ones(2)
-        s = np.asarray([[n - 1 - i, i] for i in range(n)])
-        for i in range(n + 1):
-            s = np.vstack((s, np.asarray([i, n - i])))
-        np.random.shuffle(s)
-        v = optuna._hypervolume.WFG().compute(s, r)
-        assert v == n * n - n * (n - 1) // 2
+        s = np.empty((2 * n + 1, 2), dtype=int)
+        s[:n, 0] = np.arange(n)
+        s[:n, 1] = np.arange(n)[::-1]
+        s[n:, 0] = np.arange(n + 1)
+        s[n:, 1] = np.arange(n + 1)[::-1]
+        s = _shuffle_and_filter_sols(s, assume_pareto)
+        hv = optuna._hypervolume.WFG().compute(s, r, assume_pareto=assume_pareto)
+        assert hv == n * n - n * (n - 1) // 2
 
 
-def test_wfg_3d() -> None:
+@pytest.mark.parametrize("n_objs", list(range(2, 10)))
+@pytest.mark.parametrize("assume_pareto", (True, False))
+def test_wfg_nd(n_objs: int, assume_pareto: bool) -> None:
+    r = 10 * np.ones(n_objs)
+    s = np.vstack([np.identity(n_objs), np.random.randint(1, 10, size=(10, n_objs))])
+    s = _shuffle_and_filter_sols(s, assume_pareto)
+    hv = optuna._hypervolume.WFG().compute(s, r, assume_pareto=assume_pareto)
+    assert optuna._hypervolume.WFG().compute(s, r, assume_pareto=assume_pareto) == 10**n_objs - 1
+
+
+@pytest.mark.parametrize("assume_pareto", (True, False))
+def test_wfg_duplicate_points(assume_pareto: bool) -> None:
     n = 3
     r = 10 * np.ones(n)
-    s = [np.hstack((np.zeros(i), [1], np.zeros(n - i - 1))) for i in range(n)]
-    for _ in range(10):
-        s.append(np.random.randint(1, 10, size=(n,)))
-    o = np.asarray(s)
-    np.random.shuffle(o)
-    v = optuna._hypervolume.WFG().compute(o, r)
-    assert v == 10**n - 1
-
-
-def test_wfg_nd() -> None:
-    for n in range(2, 10):
-        r = 10 * np.ones(n)
-        s = [np.hstack((np.zeros(i), [1], np.zeros(n - i - 1))) for i in range(n)]
-        for _ in range(10):
-            s.append(np.random.randint(1, 10, size=(n,)))
-        o = np.asarray(s)
-        np.random.shuffle(o)
-        v = optuna._hypervolume.WFG().compute(o, r)
-        assert v == 10**n - 1
-
-
-def test_wfg_duplicate_points() -> None:
-    n = 3
-    r = 10 * np.ones(n)
-    s = [np.hstack((np.zeros(i), [1], np.zeros(n - i - 1))) for i in range(n)]
-    for _ in range(10):
-        s.append(np.random.randint(1, 10, size=(n,)))
-    o = np.asarray(s)
-    v = optuna._hypervolume.WFG().compute(o, r)
-
-    # Add an already existing point.
-    o = np.vstack([o, o[-1]])
-
-    np.random.shuffle(o)
-    v_with_duplicate_point = optuna._hypervolume.WFG().compute(o, r)
-    assert v == v_with_duplicate_point
+    s = np.vstack([np.identity(n), np.random.randint(1, 10, size=(10, n))])
+    ground_truth = optuna._hypervolume.WFG().compute(s, r, assume_pareto=False)
+    s = np.vstack([s, s[-1]])  # Add an already existing point.
+    s = _shuffle_and_filter_sols(s, assume_pareto)
+    assert optuna._hypervolume.WFG().compute(s, r, assume_pareto=assume_pareto) == ground_truth
 
 
 def test_invalid_input() -> None:
