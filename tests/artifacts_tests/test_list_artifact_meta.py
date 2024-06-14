ce1a1b52dfe6a25643e41e84e4e37b4066594b81
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-14 08:12:04 +0200

    Simplify the unittests

--- tests/artifacts_tests/test_list_artifact_meta.py
+++ tests/artifacts_tests/test_list_artifact_meta.py
@@ -15,6 +15,12 @@ from optuna.trial import FrozenTrial
 from optuna.trial import Trial
 
 
+parametrize_artifact_meta = pytest.mark.parametrize(
+    "filename,mimetype,expected_mimetype,encoding",
+    [("dummy.txt", None, "text/plain", None), ("dummy.obj", "model/obj", "model/obj", "utf-8")],
+)
+
+
 @pytest.fixture(params=["FileSystem"])
 def artifact_store(tmp_path: pathlib.PurePath, request: pytest.FixtureRequest) -> ArtifactStore:
     if request.param == "FileSystem":
@@ -22,15 +28,15 @@ def artifact_store(tmp_path: pathlib.PurePath, request: pytest.FixtureRequest) -
     assert False, f"Unknown artifact store: {request.param}"
 
 
-def _check_artifact_meta(
+def _check_uploaded_artifact_meta(
+    study_or_trial: Study | Trial | FrozenTrial,
+    storage: BaseStorage,
     artifact_store: ArtifactStore,
-    study_or_trial: Trial | FrozenTrial | Study,
-    file_path: str,
     filename: str,
+    file_path: str,
     mimetype: str | None,
+    expected_mimetype: str,
     encoding: str | None,
-    storage: BaseStorage | None,
-    n_linked_artifact_meta: int,
 ) -> None:
     artifact_id = upload_artifact(
         study_or_trial,
@@ -40,27 +46,50 @@ def _check_artifact_meta(
         mimetype=mimetype,
         encoding=encoding,
     )
-    mimetype = "text/plain" if mimetype is None else mimetype
-    if isinstance(study_or_trial, FrozenTrial):
-        artifact_meta_list = get_all_artifact_meta(study_or_trial, storage=storage)
-    else:
-        artifact_meta_list = get_all_artifact_meta(study_or_trial)
-
-    assert len(artifact_meta_list) == n_linked_artifact_meta
-    assert artifact_meta_list[-1].artifact_id == artifact_id
-    assert artifact_meta_list[-1].filename == filename
-    assert artifact_meta_list[-1].mimetype == mimetype
-    assert artifact_meta_list[-1].encoding == encoding
+    artifact_meta_list = get_all_artifact_meta(study_or_trial, storage=storage)
+    assert len(artifact_meta_list) == 1
+    assert artifact_meta_list[0].artifact_id == artifact_id
+    assert artifact_meta_list[0].filename == filename
+    assert artifact_meta_list[0].mimetype == expected_mimetype
+    assert artifact_meta_list[0].encoding == encoding
 
 
-@pytest.mark.parametrize(
-    "filename,mimetype,encoding", [("dummy.txt", None, None), ("dummy.obj", "model/obj", "utf-8")]
-)
-def test_get_all_artifact_meta(
+@parametrize_artifact_meta
+def test_get_all_artifact_meta_in_trial(
     tmp_path: pathlib.PurePath,
     artifact_store: ArtifactStore,
     filename: str,
     mimetype: str | None,
+    expected_mimetype: str,
+    encoding: str | None,
+) -> None:
+    file_path = str(tmp_path / filename)
+
+    with open(file_path, "w") as f:
+        f.write("foo")
+
+    storage = optuna.storages.InMemoryStorage()
+    study = optuna.create_study(storage=storage)
+    trial = study.ask()
+    _check_uploaded_artifact_meta(
+        study_or_trial=trial,
+        storage=storage,
+        artifact_store=artifact_store,
+        filename=filename,
+        file_path=file_path,
+        mimetype=mimetype,
+        expected_mimetype=expected_mimetype,
+        encoding=encoding,
+    )
+
+
+@parametrize_artifact_meta
+def test_get_all_artifact_meta_in_frozen_trial(
+    tmp_path: pathlib.PurePath,
+    artifact_store: ArtifactStore,
+    filename: str,
+    mimetype: str | None,
+    expected_mimetype: str,
     encoding: str | None,
 ) -> None:
     file_path = str(tmp_path / filename)
@@ -72,15 +101,41 @@ def test_get_all_artifact_meta(
     study = optuna.create_study(storage=storage)
     trial = study.ask()
     frozen_trial = study._storage.get_trial(trial._trial_id)
-    for study_or_trial, n_linked_artifact_meta in zip([study, trial, frozen_trial], [1, 1, 2]):
-        assert isinstance(study_or_trial, (Study, Trial, FrozenTrial))  # MyPy redefinition.
-        _check_artifact_meta(
-            artifact_store,
-            study_or_trial,
-            file_path,
-            filename,
-            mimetype=mimetype,
-            encoding=encoding,
-            storage=storage,
-            n_linked_artifact_meta=n_linked_artifact_meta,
-        )
+    _check_uploaded_artifact_meta(
+        study_or_trial=frozen_trial,
+        storage=storage,
+        artifact_store=artifact_store,
+        filename=filename,
+        file_path=file_path,
+        mimetype=mimetype,
+        expected_mimetype=expected_mimetype,
+        encoding=encoding,
+    )
+
+
+@parametrize_artifact_meta
+def test_get_all_artifact_meta_in_study(
+    tmp_path: pathlib.PurePath,
+    artifact_store: ArtifactStore,
+    filename: str,
+    mimetype: str | None,
+    expected_mimetype: str,
+    encoding: str | None,
+) -> None:
+    file_path = str(tmp_path / filename)
+
+    with open(file_path, "w") as f:
+        f.write("foo")
+
+    storage = optuna.storages.InMemoryStorage()
+    study = optuna.create_study(storage=storage)
+    _check_uploaded_artifact_meta(
+        study_or_trial=study,
+        storage=storage,
+        artifact_store=artifact_store,
+        filename=filename,
+        file_path=file_path,
+        mimetype=mimetype,
+        expected_mimetype=expected_mimetype,
+        encoding=encoding,
+    )
