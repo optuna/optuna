bf295d925d232aabf1e105fc46e5a5c4c7a40243
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-17 11:45:56 +0200

    Apply formatter

--- optuna/integration/lightgbm.py
+++ optuna/integration/lightgbm.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 
 from optuna._imports import _INTEGRATION_IMPORT_ERROR_TEMPLATE
 
+
 try:
     import optuna_integration.lightgbm as lgb
 except ModuleNotFoundError:
@@ -13,15 +14,18 @@ except ModuleNotFoundError:
 
 
 if TYPE_CHECKING:
+    # These modules are from optuna-integration.
     from optuna.integration.lightgbm_tuner import LightGBMPruningCallback
     from optuna.integration.lightgbm_tuner import LightGBMTuner
     from optuna.integration.lightgbm_tuner import LightGBMTunerCV
+    from optuna.integration.lightgbm_tuner import train
 
 
 __all__ = [
     "LightGBMPruningCallback",
     "LightGBMTuner",
     "LightGBMTunerCV",
+    "train",
 ]
 
 
@@ -33,6 +37,7 @@ class _LightGBMModule(ModuleType):
     __path__ = [os.path.dirname(__file__)]
 
     def __getattr__(self, name: str) -> Any:
+        print(lgb.__dict__)
         return lgb.__dict__[name]
 
 
