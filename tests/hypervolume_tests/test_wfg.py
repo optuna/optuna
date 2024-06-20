dd52e79b5a9413976d4be1b761599ec28bf92e59
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-20 05:25:16 +0200

    Make the unittests deterministic
    Co-authored-by: Hideaki Imamura <38826298+HideakiImamura@users.noreply.github.com>

--- tests/hypervolume_tests/test_wfg.py
+++ tests/hypervolume_tests/test_wfg.py
@@ -4,8 +4,10 @@ import pytest
 import optuna
 
 
-def _shuffle_and_filter_sols(sols: np.ndarray, assume_pareto: bool) -> np.ndarray:
-    np.random.shuffle(sols)
+def _shuffle_and_filter_sols(
+    sols: np.ndarray, assume_pareto: bool, rng: np.random.RandomState
+) -> np.ndarray:
+    rng.shuffle(sols)
     if assume_pareto:
         on_front = optuna.study._multi_objective._is_pareto_front(sols, False)
         sols = sols[on_front]
@@ -14,32 +16,35 @@ def _shuffle_and_filter_sols(sols: np.ndarray, assume_pareto: bool) -> np.ndarra
 
 @pytest.mark.parametrize("assume_pareto", (True, False))
 def test_wfg_2d(assume_pareto: bool) -> None:
+    rng = np.random.RandomState(42)
     for n in range(2, 30):
         r = n * np.ones(2)
         s = np.empty((2 * n + 1, 2), dtype=int)
         s[:n] = np.stack([np.arange(n), np.arange(n)[::-1]], axis=-1)
         s[n:] = np.stack([np.arange(n + 1), np.arange(n + 1)[::-1]], axis=-1)
-        s = _shuffle_and_filter_sols(s, assume_pareto)
+        s = _shuffle_and_filter_sols(s, assume_pareto, rng)
         assert optuna._hypervolume.WFG().compute(s, r, assume_pareto) == n * n - n * (n - 1) // 2
 
 
 @pytest.mark.parametrize("n_objs", list(range(2, 10)))
 @pytest.mark.parametrize("assume_pareto", (True, False))
 def test_wfg_nd(n_objs: int, assume_pareto: bool) -> None:
+    rng = np.random.RandomState(42)
     r = 10 * np.ones(n_objs)
-    s = np.vstack([np.identity(n_objs), np.random.randint(1, 10, size=(10, n_objs))])
-    s = _shuffle_and_filter_sols(s, assume_pareto)
+    s = np.vstack([np.identity(n_objs), rng.randint(1, 10, size=(10, n_objs))])
+    s = _shuffle_and_filter_sols(s, assume_pareto, rng)
     assert optuna._hypervolume.WFG().compute(s, r, assume_pareto) == 10**n_objs - 1
 
 
 @pytest.mark.parametrize("assume_pareto", (True, False))
 def test_wfg_duplicate_points(assume_pareto: bool) -> None:
+    rng = np.random.RandomState(42)
     n = 3
     r = 10 * np.ones(n)
-    s = np.vstack([np.identity(n), np.random.randint(1, 10, size=(10, n))])
+    s = np.vstack([np.identity(n), rng.randint(1, 10, size=(10, n))])
     ground_truth = optuna._hypervolume.WFG().compute(s, r, assume_pareto=False)
     s = np.vstack([s, s[-1]])  # Add an already existing point.
-    s = _shuffle_and_filter_sols(s, assume_pareto)
+    s = _shuffle_and_filter_sols(s, assume_pareto, rng)
     assert optuna._hypervolume.WFG().compute(s, r, assume_pareto) == ground_truth
 
 
