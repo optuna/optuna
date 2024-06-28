7bd42db88422582543bf3c3a69bd212eebb53e9d
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-06-28 12:15:20 +0200

    Make WFG a function

--- optuna/_hypervolume/__init__.py
+++ optuna/_hypervolume/__init__.py
@@ -1,5 +1,5 @@
 from optuna._hypervolume.hssp import _solve_hssp
-from optuna._hypervolume.wfg import WFG
+from optuna._hypervolume.wfg import compute_hypervolume
 
 
-__all__ = ["_solve_hssp", "WFG"]
+__all__ = ["_solve_hssp", "compute_hypervolume"]
--- optuna/_hypervolume/wfg.py
+++ optuna/_hypervolume/wfg.py
@@ -5,7 +5,17 @@ import numpy as np
 from optuna.study._multi_objective import _is_pareto_front
 
 
-class WFG:
+def _compute_2d(sorted_pareto_sols: np.ndarray, reference_point: np.ndarray) -> float:
+    assert sorted_pareto_sols.shape[1] == 2 and reference_point.shape[0] == 2
+    rect_diag_y = np.append(reference_point[1], sorted_pareto_sols[:-1, 1])
+    edge_length_x = reference_point[0] - sorted_pareto_sols[:, 0]
+    edge_length_y = rect_diag_y - sorted_pareto_sols[:, 1]
+    return edge_length_x @ edge_length_y
+
+
+def compute_hypervolume(
+    solution_set: np.ndarray, reference_point: np.ndarray, assume_pareto: bool = False
+) -> float:
     """Hypervolume calculator for any dimension.
 
     This class exactly calculates the hypervolume for any dimension.
@@ -22,61 +32,46 @@ class WFG:
 
     """
 
-    def __init__(self) -> None:
-        self._reference_point: np.ndarray | None = None
+    if not np.all(solution_set <= reference_point):
+        raise ValueError(
+            "All points must dominate or equal the reference point. "
+            "That is, for all points in the solution_set and the coordinate `i`, "
+            "`solution_set[i] <= reference_point[i]`."
+        )
+    if not np.all(np.isfinite(reference_point)):
+        # reference_point does not have nan, because BaseHypervolume._validate will filter out.
+        return float("inf")
 
-    @staticmethod
-    def _compute_2d(sorted_pareto_sols: np.ndarray, reference_point: np.ndarray) -> float:
-        assert sorted_pareto_sols.shape[1] == 2 and reference_point.shape[0] == 2
-        rect_diag_y = np.append(reference_point[1], sorted_pareto_sols[:-1, 1])
-        edge_length_x = reference_point[0] - sorted_pareto_sols[:, 0]
-        edge_length_y = rect_diag_y - sorted_pareto_sols[:, 1]
-        return edge_length_x @ edge_length_y
+    if not assume_pareto:
+        unique_lexsorted_sols = np.unique(solution_set, axis=0)
+        sorted_pareto_sols = unique_lexsorted_sols[_is_pareto_front(unique_lexsorted_sols)]
+    else:
+        sorted_pareto_sols = solution_set[solution_set[:, 0].argsort()]
 
-    def compute(
-        self, solution_set: np.ndarray, reference_point: np.ndarray, assume_pareto: bool = False
-    ) -> float:
-        if not np.all(solution_set <= reference_point):
-            raise ValueError(
-                "All points must dominate or equal the reference point. "
-                "That is, for all points in the solution_set and the coordinate `i`, "
-                "`solution_set[i] <= reference_point[i]`."
-            )
-        if not np.all(np.isfinite(reference_point)):
-            # reference_point does not have nan, because BaseHypervolume._validate will filter out.
-            return float("inf")
-
-        if not assume_pareto:
-            unique_lexsorted_sols = np.unique(solution_set, axis=0)
-            sorted_pareto_sols = unique_lexsorted_sols[_is_pareto_front(unique_lexsorted_sols)]
-        else:
-            sorted_pareto_sols = solution_set[solution_set[:, 0].argsort()]
-
-        self._reference_point = reference_point.astype(np.float64)
-        if self._reference_point.shape[0] == 2:
-            return self._compute_2d(sorted_pareto_sols, self._reference_point)
-
-        return self._compute_hv(sorted_pareto_sols)
-
-    def _compute_hv(self, sorted_sols: np.ndarray) -> float:
-        assert self._reference_point is not None
-        inclusive_hvs = np.prod(self._reference_point - sorted_sols, axis=-1)
+    def _compute_hv(sorted_sols: np.ndarray) -> float:
+        assert reference_point is not None
+        inclusive_hvs = np.prod(reference_point - sorted_sols, axis=-1)
         if inclusive_hvs.shape[0] == 1:
             return float(inclusive_hvs[0])
         elif inclusive_hvs.shape[0] == 2:
             # S(A v B) = S(A) + S(B) - S(A ^ B).
-            intersec = np.prod(self._reference_point - np.maximum(sorted_sols[0], sorted_sols[1]))
+            intersec = np.prod(reference_point - np.maximum(sorted_sols[0], sorted_sols[1]))
             return np.sum(inclusive_hvs) - intersec
 
         limited_sols_array = np.maximum(sorted_sols[:, np.newaxis], sorted_sols)
         return sum(
-            self._compute_exclusive_hv(limited_sols_array[i, i + 1 :], inclusive_hv)
+            _compute_exclusive_hv(limited_sols_array[i, i + 1 :], inclusive_hv)
             for i, inclusive_hv in enumerate(inclusive_hvs)
         )
 
-    def _compute_exclusive_hv(self, limited_sols: np.ndarray, inclusive_hv: float) -> float:
+    def _compute_exclusive_hv(limited_sols: np.ndarray, inclusive_hv: float) -> float:
         if limited_sols.shape[0] == 0:
             return inclusive_hv
 
         on_front = _is_pareto_front(limited_sols, assume_unique_lexsorted=False)
-        return inclusive_hv - self._compute_hv(limited_sols[on_front])
+        return inclusive_hv - _compute_hv(limited_sols[on_front])
+
+    if reference_point.shape[0] == 2:
+        return _compute_2d(sorted_pareto_sols, reference_point)
+
+    return _compute_hv(sorted_pareto_sols)
