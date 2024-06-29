3924ed951723c954eebc206f2016190d8d36b032
Hiroki Takizawa <contact@hiroki-takizawa.name>
2024-06-29 16:08:58 +0900

    Update storage.py

--- optuna/storages/_journal/storage.py
+++ optuna/storages/_journal/storage.py
@@ -78,7 +78,7 @@ class JournalStorage(BaseStorage):
 
 
             storage = optuna.storages.JournalStorage(
-                optuna.storages.JournalFileStorage("./optuna-file-storage.journal"),
+                optuna.storages.JournalFileStorage("./JournalFileStorageLog.jsonl"),
             )
 
             study = optuna.create_study(storage=storage)
@@ -90,7 +90,7 @@ class JournalStorage(BaseStorage):
 
     .. code::
 
-        file_path = "./optuna-file-storage.journal"
+        file_path = "./JournalFileStorageLog.jsonl"
         lock_obj = optuna.storages.JournalFileOpenLock(file_path)
 
         storage = optuna.storages.JournalStorage(
