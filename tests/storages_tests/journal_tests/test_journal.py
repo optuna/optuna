881707e3f706cf4e7f19cf8244ffc934019d4b83
eukaryo <c3375c15@gmail.com>
2024-07-02 22:46:31 +0900

    mypy

--- tests/storages_tests/journal_tests/test_journal.py
+++ tests/storages_tests/journal_tests/test_journal.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from concurrent.futures import as_completed
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
