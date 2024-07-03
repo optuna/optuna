718b56e979aa7638d17a90ec67804350d8f220fd
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-07-03 19:08:03 +0200

    Address mamu's comment

--- optuna/_hypervolume/wfg.py
+++ optuna/_hypervolume/wfg.py
@@ -13,6 +13,32 @@ def _compute_2d(sorted_pareto_sols: np.ndarray, reference_point: np.ndarray) -> 
     return edge_length_x @ edge_length_y
 
 
+def _compute_hv(sorted_sols: np.ndarray, reference_point: np.ndarray) -> float:
+    inclusive_hvs = np.prod(reference_point - sorted_sols, axis=-1)
+    if inclusive_hvs.shape[0] == 1:
+        return float(inclusive_hvs[0])
+    elif inclusive_hvs.shape[0] == 2:
+        # S(A v B) = S(A) + S(B) - S(A ^ B).
+        intersec = np.prod(reference_point - np.maximum(sorted_sols[0], sorted_sols[1]))
+        return np.sum(inclusive_hvs) - intersec
+
+    limited_sols_array = np.maximum(sorted_sols[:, np.newaxis], sorted_sols)
+    return sum(
+        _compute_exclusive_hv(limited_sols_array[i, i + 1 :], inclusive_hv, reference_point)
+        for i, inclusive_hv in enumerate(inclusive_hvs)
+    )
+
+
+def _compute_exclusive_hv(
+    limited_sols: np.ndarray, inclusive_hv: float, reference_point: np.ndarray
+) -> float:
+    if limited_sols.shape[0] == 0:
+        return inclusive_hv
+
+    on_front = _is_pareto_front(limited_sols, assume_unique_lexsorted=False)
+    return inclusive_hv - _compute_hv(limited_sols[on_front], reference_point)
+
+
 def compute_hypervolume(
     solution_set: np.ndarray, reference_point: np.ndarray, assume_pareto: bool = False
 ) -> float:
@@ -42,28 +68,6 @@ def compute_hypervolume(
         # reference_point does not have nan, because BaseHypervolume._validate will filter out.
         return float("inf")
 
-    def _compute_hv(sorted_sols: np.ndarray) -> float:
-        inclusive_hvs = np.prod(reference_point - sorted_sols, axis=-1)
-        if inclusive_hvs.shape[0] == 1:
-            return float(inclusive_hvs[0])
-        elif inclusive_hvs.shape[0] == 2:
-            # S(A v B) = S(A) + S(B) - S(A ^ B).
-            intersec = np.prod(reference_point - np.maximum(sorted_sols[0], sorted_sols[1]))
-            return np.sum(inclusive_hvs) - intersec
-
-        limited_sols_array = np.maximum(sorted_sols[:, np.newaxis], sorted_sols)
-        return sum(
-            _compute_exclusive_hv(limited_sols_array[i, i + 1 :], inclusive_hv)
-            for i, inclusive_hv in enumerate(inclusive_hvs)
-        )
-
-    def _compute_exclusive_hv(limited_sols: np.ndarray, inclusive_hv: float) -> float:
-        if limited_sols.shape[0] == 0:
-            return inclusive_hv
-
-        on_front = _is_pareto_front(limited_sols, assume_unique_lexsorted=False)
-        return inclusive_hv - _compute_hv(limited_sols[on_front])
-
     if not assume_pareto:
         unique_lexsorted_sols = np.unique(solution_set, axis=0)
         on_front = _is_pareto_front(unique_lexsorted_sols, assume_unique_lexsorted=True)
@@ -74,4 +78,4 @@ def compute_hypervolume(
     if reference_point.shape[0] == 2:
         return _compute_2d(sorted_pareto_sols, reference_point)
 
-    return _compute_hv(sorted_pareto_sols)
+    return _compute_hv(sorted_pareto_sols, reference_point)
