bf3bdbbfc7f24fe1e4a26ac42085bb638cec92f8
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-07-04 16:10:57 +0200

    Remove new journal related objects from the past file

--- optuna/storages/_journal/file.py
+++ optuna/storages/_journal/file.py
@@ -1,6 +1,4 @@
-from optuna.storages._journal.backend import JournalFileBackend
 from optuna.storages._journal.backend import JournalFileStorage
-from optuna.storages._journal.base import BaseJournalFileLock
 from optuna.storages._journal.base import JournalFileBaseLock
 from optuna.storages._journal.file_lock import get_lock_file
 from optuna.storages._journal.file_lock import JournalFileOpenLock
@@ -10,12 +8,10 @@ from optuna.storages._journal.file_lock import RENAME_FILE_SUFFIX
 
 
 __all__ = [
-    "BaseJournalFileLock",
     "JournalFileBaseLock",
     "JournalFileSymlinkLock",
     "JournalFileOpenLock",
     "get_lock_file",
-    "JournalFileBackend",
     "JournalFileStorage",
     "LOCK_FILE_SUFFIX",
     "RENAME_FILE_SUFFIX",
