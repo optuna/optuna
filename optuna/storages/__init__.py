09eeeed0c3268c5d0e7a6835199d6642537ca504
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-07-05 08:51:40 +0200

    Remove new journal modules from storage init

--- optuna/storages/__init__.py
+++ optuna/storages/__init__.py
@@ -6,29 +6,23 @@ from optuna.storages._cached_storage import _CachedStorage
 from optuna.storages._heartbeat import fail_stale_trials
 from optuna.storages._in_memory import InMemoryStorage
 from optuna.storages._rdb.storage import RDBStorage
-from optuna.storages.journal._backend import JournalFileBackend
 from optuna.storages.journal._backend import JournalFileStorage
-from optuna.storages.journal._base import BaseJournalBackend
 from optuna.storages.journal._base import BaseJournalLogStorage
 from optuna.storages.journal._file_lock import JournalFileOpenLock
 from optuna.storages.journal._file_lock import JournalFileSymlinkLock
-from optuna.storages.journal._redis import JournalRedisBackend
 from optuna.storages.journal._redis import JournalRedisStorage
 from optuna.storages.journal._storage import JournalStorage
 
 
 __all__ = [
     "BaseStorage",
-    "BaseJournalBackend",
     "BaseJournalLogStorage",
     "InMemoryStorage",
     "RDBStorage",
     "JournalStorage",
     "JournalFileSymlinkLock",
     "JournalFileOpenLock",
-    "JournalFileBackend",
     "JournalFileStorage",
-    "JournalRedisBackend",
     "JournalRedisStorage",
     "RetryFailedTrialCallback",
     "_CachedStorage",
