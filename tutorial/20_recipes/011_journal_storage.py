91c4feb6bd3ed61f5c24a6935cd1dc286e438124
Hiroki Takizawa <contact@hiroki-takizawa.name>
2024-07-10 12:10:59 +0900

    Update 011_journal_storage.py

--- tutorial/20_recipes/011_journal_storage.py
+++ tutorial/20_recipes/011_journal_storage.py
@@ -19,7 +19,7 @@ import optuna
 # Add stream handler of stdout to show the messages
 optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
 study_name = "example-study"  # Unique identifier of the study.
-file_path = "./journal_file_storage_jsonl.log"
+file_path = "./optuna_journal_storage.log"
 storage = optuna.storages.JournalStorage(
     optuna.storages.journal.JournalFileBackend(file_path),  # NFS path for distributed optimization
 )
