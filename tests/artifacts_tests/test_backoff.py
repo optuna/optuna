ce67adcea327c2c26404ccf25db45ab1cdaaca01
RektPunk <rektpunk@gmail.com>
2024-07-11 17:26:31 +0900

    black test_backoff

--- tests/artifacts_tests/test_backoff.py
+++ tests/artifacts_tests/test_backoff.py
@@ -7,14 +7,8 @@ from tests.artifacts_tests.stubs import FailArtifactStore
 from tests.artifacts_tests.stubs import InMemoryArtifactStore
 
 
-
 def test_backoff_time() -> None:
-    backend = Backoff(
-        backend=FailArtifactStore(),
-        min_delay=0.1,
-        multiplier=10,
-        max_delay=10,
-    )
+    backend = Backoff(backend=FailArtifactStore(), min_delay=0.1, multiplier=10, max_delay=10,)
     assert backend._get_sleep_secs(0) == 0.1
     assert backend._get_sleep_secs(1) == 1
     assert backend._get_sleep_secs(2) == 10
@@ -24,12 +18,7 @@ def test_read_and_write() -> None:
     artifact_id = f"test-{uuid.uuid4()}"
     dummy_content = b"Hello World"
 
-    backend = Backoff(
-        backend=InMemoryArtifactStore(),
-        min_delay=0.1,
-        multiplier=10,
-        max_delay=10,
-    )
+    backend = Backoff(backend=InMemoryArtifactStore(), min_delay=0.1, multiplier=10, max_delay=10,)
     backend.write(artifact_id, io.BytesIO(dummy_content))
     with backend.open_reader(artifact_id) as f:
         actual = f.read()
