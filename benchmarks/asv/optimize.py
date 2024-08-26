bd47df9f10c5721d22035d285db81f09de70f4cc
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-08-26 03:36:31 +0200

    Add a tri-objective problem to speed benchmarking

--- benchmarks/asv/optimize.py
+++ benchmarks/asv/optimize.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import itertools
 from typing import cast
 
 import optuna
@@ -42,60 +43,40 @@ def create_sampler(sampler_mode: str) -> BaseSampler:
 
 
 class OptimizeSuite:
-    def objective(self, trial: optuna.Trial) -> float:
+    def objective(self, trial: optuna.Trial, n_objectives: int) -> list[float]:
         x = trial.suggest_float("x", -100, 100)
         y = trial.suggest_int("y", -100, 100)
-        return x**2 + y**2
-
-    def multi_objective(self, trial: optuna.Trial) -> tuple[float, float]:
-        x = trial.suggest_float("x", -100, 100)
-        y = trial.suggest_int("y", -100, 100)
-        return (x**2 + y**2, (x - 2) ** 2 + (y - 2) ** 2)
+        objective_values = [x**2 + y**2, (x - 2) ** 2 + (y - 2) ** 2, (x + 2) ** 2 + (y + 2) ** 2]
+        return objective_values[:n_objectives]
 
     def optimize(
-        self, storage_mode: str, sampler_mode: str, n_trials: int, objective_type: str
+        self, storage_mode: str, sampler_mode: str, n_trials: int, n_objectives: int
     ) -> None:
         with StorageSupplier(storage_mode) as storage:
             sampler = create_sampler(sampler_mode)
-            if objective_type == "single":
-                directions = ["minimize"]
-            elif objective_type == "multi":
-                directions = ["minimize", "minimize"]
-            else:
-                assert False
+            assert n_objectives in [1, 2, 3]
+            directions = ["minimize"] * n_objectives
             study = optuna.create_study(storage=storage, sampler=sampler, directions=directions)
-            if objective_type == "single":
-                study.optimize(self.objective, n_trials=n_trials)
-            elif objective_type == "multi":
-                study.optimize(self.multi_objective, n_trials=n_trials)
-            else:
-                assert False
+            study.optimize(lambda trial: self.objective(trial, n_objectives), n_trials=n_trials)
 
     def time_optimize(self, args: str) -> None:
-        storage_mode, sampler_mode, n_trials, objective_type = parse_args(args)
+        storage_mode, sampler_mode, n_trials, n_objectives = parse_args(args)
         storage_mode = cast(str, storage_mode)
         sampler_mode = cast(str, sampler_mode)
         n_trials = cast(int, n_trials)
-        objective_type = cast(str, objective_type)
-        self.optimize(storage_mode, sampler_mode, n_trials, objective_type)
+        n_objectives = cast(int, n_objectives)
+        self.optimize(storage_mode, sampler_mode, n_trials, n_objectives)
 
-    params = (
-        "inmemory, random, 1000, single",
-        "inmemory, random, 10000, single",
-        "inmemory, tpe, 1000, single",
-        "inmemory, cmaes, 1000, single",
-        "sqlite, random, 1000, single",
-        "sqlite, tpe, 1000, single",
-        "sqlite, cmaes, 1000, single",
-        "journal, random, 1000, single",
-        "journal, tpe, 1000, single",
-        "journal, cmaes, 1000, single",
-        "inmemory, tpe, 1000, multi",
-        "inmemory, nsgaii, 1000, multi",
-        "sqlite, tpe, 1000, multi",
-        "sqlite, nsgaii, 1000, multi",
-        "journal, tpe, 1000, multi",
-        "journal, nsgaii, 1000, multi",
-    )
-    param_names = ["storage, sampler, n_trials, objective_type"]
+    params = ["inmemory, random, 10000, 1"]
+    storage_modes = ["inmemory", "sqlite", "journal"]
+    sampler_modes = ["random", "tpe", "cmaes"]
+    for storage_mode, sampler_mode in itertools.product(*(storage_modes, sampler_modes)):
+        params.append(f"{storage_mode}, {sampler_mode}, 1000, 1")
+
+    sampler_modes = ["tpe", "nsgaii"]
+    for storage_mode, sampler_mode in itertools.product(*(storage_modes, sampler_modes)):
+        params.append(f"{storage_mode}, {sampler_mode}, 1000, 2")
+        params.append(f"{storage_mode}, {sampler_mode}, 300, 3")
+
+    param_names = ["storage, sampler, n_trials, n_objectives"]
     timeout = 600
