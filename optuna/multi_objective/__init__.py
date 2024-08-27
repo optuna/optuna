722d33f267dfa128788265cf0d66c3720fbd70f1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2024-08-27 07:56:23 +0200

    Enhance the error message of multi_objective deletion

--- optuna/multi_objective/__init__.py
+++ optuna/multi_objective/__init__.py
@@ -1,6 +1,10 @@
 # TODO(nabenabe0928): Come up with any ways to remove this file.
 # NOTE(nabenabe0928): Discuss when to remove this class.
+migration_url = "https://github.com/optuna/optuna/discussions/5573"
 raise ModuleNotFoundError(
-    "`optuna.multi_objective` was removed at v4.0.0. "
-    "Please update your code or downgrade your Optuna version."
+    "\nThe features in ``optuna.multi_objective`` were integrated with the"
+    "\nsingle objective optimization API and ``optuna.multi_objective`` were"
+    "\ndeleted at v4.0.0. Please update your code based on the migration guide"
+    f"\nat {migration_url}"
+    "\nor downgrade your Optuna version."
 )
