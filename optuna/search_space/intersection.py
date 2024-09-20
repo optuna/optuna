1f3fc20fa2fc79a934199ff74c16cdd7c9856f09
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2024-09-20 15:59:56 +0900

    Add references

--- optuna/search_space/intersection.py
+++ optuna/search_space/intersection.py
@@ -96,8 +96,8 @@ class IntersectionSearchSpace:
         if self._study_id is None:
             self._study_id = study._study_id
         else:
-            # Note that the check below is meaningless when `InMemoryStorage` is used
-            # because `InMemoryStorage.create_new_study` always returns the same study ID.
+            # Note that the check below is meaningless when :class:`~optuna.storages.InMemoryStorage` is used
+            # because :func:`~optuna.storages.InMemoryStorage.create_new_study` always returns the same study ID.
             if self._study_id != study._study_id:
                 raise ValueError("`IntersectionSearchSpace` cannot handle multiple studies.")
 
