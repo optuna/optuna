aed7de8d35bb7658e2385dffb060e1a4a9b646be
boringbyte <kottapallisivaram@gmail.com>
2024-11-30 10:37:04 -0500

    fix type annotations for 'tests/pruners_tests/test_successive_halving.py'

--- tests/pruners_tests/test_successive_halving.py
+++ tests/pruners_tests/test_successive_halving.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from __future__ import annotations
 
 import pytest
 
@@ -6,7 +6,7 @@ import optuna
 
 
 @pytest.mark.parametrize("direction_value", [("minimize", 2), ("maximize", 0.5)])
-def test_successive_halving_pruner_intermediate_values(direction_value: Tuple[str, float]) -> None:
+def test_successive_halving_pruner_intermediate_values(direction_value: tuple[str, float]) -> None:
     direction, intermediate_value = direction_value
     pruner = optuna.pruners.SuccessiveHalvingPruner(
         min_resource=1, reduction_factor=2, min_early_stopping_rate=0
