5bde38fd125292770b13d34de5c6bc42c30075f3
Yoshihiko Ozaki <30489874+y0z@users.noreply.github.com>
2025-01-14 12:50:14 +0900

    Merge pull request #5924 from c-bata/fix-load-study-sampler-multi-objective
    Fix the default sampler of `load_study` function

--- optuna/study/study.py
+++ optuna/study/study.py
@@ -1347,7 +1347,10 @@ def load_study(
             "study found in the storage."
         )
 
-    return Study(study_name=study_name, storage=storage, sampler=sampler, pruner=pruner)
+    study = Study(study_name=study_name, storage=storage, sampler=sampler, pruner=pruner)
+    if sampler is None and len(study.directions) > 1:
+        study.sampler = samplers.NSGAIISampler()
+    return study
 
 
 @convert_positional_args(
