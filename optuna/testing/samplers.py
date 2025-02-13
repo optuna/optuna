80cdc1e6e6c995fd37cce2601ce04c48959c2ae8
mehakmander <m.mehakmander@gmail.com>
2025-02-13 18:15:58 +0000

    removed FirstTrialOnlyRandomSampler

--- optuna/testing/samplers.py
+++ optuna/testing/samplers.py
@@ -35,28 +35,3 @@ class DeterministicSampler(optuna.samplers.BaseSampler):
         return param_value
 
 
-class FirstTrialOnlyRandomSampler(optuna.samplers.RandomSampler):
-    def sample_relative(
-        self,
-        study: "optuna.study.Study",
-        trial: "optuna.trial.FrozenTrial",
-        search_space: dict[str, BaseDistribution],
-    ) -> dict[str, float]:
-        if len(study.trials) > 1:
-            raise RuntimeError("`FirstTrialOnlyRandomSampler` only works on the first trial.")
-
-        return super(FirstTrialOnlyRandomSampler, self).sample_relative(study, trial, search_space)
-
-    def sample_independent(
-        self,
-        study: "optuna.study.Study",
-        trial: "optuna.trial.FrozenTrial",
-        param_name: str,
-        param_distribution: BaseDistribution,
-    ) -> float:
-        if len(study.trials) > 1:
-            raise RuntimeError("`FirstTrialOnlyRandomSampler` only works on the first trial.")
-
-        return super(FirstTrialOnlyRandomSampler, self).sample_independent(
-            study, trial, param_name, param_distribution
-        )
