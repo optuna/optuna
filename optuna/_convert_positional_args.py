82d8addf02e0affff000615e5781643a4d26c156
fusawayugo <fusawa0712@gmail.com>
2025-03-13 15:54:34 +0900

    Add arguments of versions to convert_positional_args

--- optuna/_convert_positional_args.py
+++ optuna/_convert_positional_args.py
@@ -5,10 +5,15 @@ from collections.abc import Sequence
 from functools import wraps
 from inspect import Parameter
 from inspect import signature
+import textwrap
 from typing import Any
 from typing import TYPE_CHECKING
 from typing import TypeVar
 import warnings
+
+from optuna._deprecated import _get_docstring_indent
+from optuna._deprecated import _validate_two_version
+from optuna._experimental import _validate_version
 
 
 if TYPE_CHECKING:
@@ -16,6 +21,22 @@ if TYPE_CHECKING:
 
     _P = ParamSpec("_P")
     _T = TypeVar("_T")
+
+_DEPRECATION_NOTE_TEMPLATE = """
+
+.. warning::
+    Using positional arguments is deprecated since v{d_ver}. Support for positional arguments
+    will be removed in the future. The removal is currently scheduled for v{r_ver}, but this
+    schedule is subject to change. Please use keyword arguments instead.
+    See https://github.com/optuna/optuna/releases/tag/v{d_ver}.
+"""
+
+_DEPRECATION_WARNING_TEMPLATE = (
+    "Positional arguments in {name} have been deprecated since v{d_ver}. "
+    "They will be removed in v{r_ver}. "
+    "Please use keyword arguments instead. "
+    "See https://github.com/optuna/optuna/releases/tag/v{d_ver} for details."
+)
 
 
 def _get_positional_arg_names(func: "Callable[_P, _T]") -> list[str]:
@@ -37,15 +58,38 @@ def convert_positional_args(
     *,
     previous_positional_arg_names: Sequence[str],
     warning_stacklevel: int = 2,
+    deprecated_version: str | None = None,
+    removed_version: str | None = None,
 ) -> "Callable[[Callable[_P, _T]], Callable[_P, _T]]":
     """Convert positional arguments to keyword arguments.
 
     Args:
-        previous_positional_arg_names: List of names previously given as positional arguments.
-        warning_stacklevel: Level of the stack trace where decorated function locates.
+        previous_positional_arg_names:
+            List of names previously given as positional arguments.
+        warning_stacklevel:
+            Level of the stack trace where decorated function locates.
+        deprecated_version:
+            Version number in which the use of positional arguments is deprecated.
+        removed_version:
+            Version number in which the use of positional arguments was completely removed.
     """
 
+    if deprecated_version is not None or removed_version is not None:
+        _validate_version(deprecated_version)
+        _validate_version(removed_version)
+        _validate_two_version(deprecated_version, removed_version)
+
     def converter_decorator(func: "Callable[_P, _T]") -> "Callable[_P, _T]":
+        if deprecated_version or removed_version:
+            if func.__doc__ is None:
+                func.__doc__ = ""
+
+            note = _DEPRECATION_NOTE_TEMPLATE.format(
+                d_ver=deprecated_version, r_ver=removed_version
+            )
+            indent = _get_docstring_indent(func.__doc__)
+            func.__doc__ = func.__doc__.strip() + textwrap.indent(note, indent) + indent
+
         assert set(previous_positional_arg_names).issubset(set(signature(func).parameters)), (
             f"{set(previous_positional_arg_names)} is not a subset of"
             f" {set(signature(func).parameters)}"
@@ -53,6 +97,16 @@ def convert_positional_args(
 
         @wraps(func)
         def converter_wrapper(*args: Any, **kwargs: Any) -> "_T":
+            if deprecated_version or removed_version:
+                warnings.warn(
+                    _DEPRECATION_WARNING_TEMPLATE.format(
+                        name=func.__name__,
+                        d_ver=deprecated_version,
+                        r_ver=removed_version,
+                    ),
+                    FutureWarning,
+                    stacklevel=warning_stacklevel,
+                )
             positional_arg_names = _get_positional_arg_names(func)
             inferred_kwargs = _infer_kwargs(previous_positional_arg_names, *args)
             if len(inferred_kwargs) > len(positional_arg_names):
