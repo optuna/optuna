85500292f2ba982addb9644176f214a5a5d402d6
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-04-25 07:10:51 +0200

    Address mamu's comment

--- optuna/_hypervolume/box_decomposition.py
+++ optuna/_hypervolume/box_decomposition.py
@@ -143,8 +143,8 @@ def get_non_dominated_box_bounds(
     assert n_objectives > 1, "This function is used only for multi-objective problems."
     if n_objectives > 4:
         warnings.warn(
-            "GPSampler might be significantly slow for n_objectives > 4. "
-            "Please consider using another sampler instead."
+            "Box decomposition (typically for `GPSampler`) might be significantly slow for "
+            "n_objectives > 4. Please consider using another sampler instead."
         )
 
     return _get_non_dominated_box_bounds(sorted_pareto_sols, ref_point)
