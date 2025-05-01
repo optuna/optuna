617c7ca8a87838358aeb04f5410699900d984038
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-05-01 07:48:50 +0200

    Apply formatter

--- optuna/_gp/acqf.py
+++ optuna/_gp/acqf.py
@@ -28,9 +28,9 @@ def _sample_from_normal_sobol(dim: int, n_samples: int, seed: int | None) -> tor
     # NOTE(nabenabe): Normal Sobol sampling based on BoTorch.
     # https://github.com/pytorch/botorch/blob/v0.13.0/botorch/sampling/qmc.py#L26-L97
     # https://github.com/pytorch/botorch/blob/v0.13.0/botorch/utils/sampling.py#L109-L138
-    sobol_samples = torch.quasirandom.SobolEngine(dimension=dim, scramble=True, seed=seed).draw(  # type: ignore[no-untyped-call]
-        n_samples, dtype=torch.float64
-    )
+    sobol_samples = torch.quasirandom.SobolEngine(  # type: ignore[no-untyped-call]
+        dimension=dim, scramble=True, seed=seed
+    ).draw(n_samples, dtype=torch.float64)
     samples = 2.0 * (sobol_samples - 0.5)  # The Sobol sequence in [-1, 1].
     # Inverse transform to standard normal (values to close to -1 or 1 result in infinity).
     return torch.erfinv(samples) * float(np.sqrt(2))
