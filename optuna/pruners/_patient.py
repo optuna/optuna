a4c1d1dc634cbff95314371cbf98b23ecccf21ee
Parag Ekbote <thecoolekbote189@gmail.com>
2025-05-02 18:11:25 +0530

    Update optuna/pruners/_patient.py
    Co-authored-by: Eri Sawada <125344906+sawa3030@users.noreply.github.com>

--- optuna/pruners/_patient.py
+++ optuna/pruners/_patient.py
@@ -13,9 +13,13 @@ class PatientPruner(BasePruner):
     """Pruner which wraps another pruner with tolerance.
 
     This pruner monitors intermediate values in a trial and prunes the trial if the improvement in
-    the intermediate values after a patience period is less than a threshold. NaNs will be ignored
-    during pruning calculations, meaning that they are not counted into the patience period.
-
+    the intermediate values after a patience period is less than a threshold.
+        
+    The pruner handles NaN values in the following manner:
+        1. If all intermediate values before or during the patient period are NaN, the trial will 
+        not be pruned
+        2. During the pruning calculations, NaN values are ignored. Only valid numeric values are 
+        considered.
     Example:
 
         .. testcode::
