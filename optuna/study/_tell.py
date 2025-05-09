21ac24495c15d0aa7a8ea1bca8acef5ff383c436
Eri Sawada <esawada2003@gmail.com>
2025-05-09 12:03:45 +0900

    avoid deepcopy and change interface of _tell_with_warning

--- optuna/study/_tell.py
+++ optuna/study/_tell.py
@@ -18,10 +18,6 @@ if TYPE_CHECKING:
     from optuna import Trial
 
 
-# This is used for propagating warning message to Study.optimize.
-STUDY_TELL_WARNING_KEY = "STUDY_TELL_WARNING"
-
-
 _logger = logging.get_logger(__name__)
 
 
@@ -91,7 +87,7 @@ def _tell_with_warning(
     state: TrialState | None = None,
     skip_if_finished: bool = False,
     suppress_warning: bool = False,
-) -> FrozenTrial:
+) -> tuple[FrozenTrial, str | None]:
     """Internal method of :func:`~optuna.study.Study.tell`.
 
     Refer to the document for :func:`~optuna.study.Study.tell` for the reference.
@@ -115,7 +111,7 @@ def _tell_with_warning(
             f"{value_or_values} and state {state} since trial was already finished. "
             f"Finished trial has values {frozen_trial.values} and state {frozen_trial.state}."
         )
-        return copy.deepcopy(frozen_trial)
+        return copy.copy(frozen_trial), None
     elif frozen_trial.state != TrialState.RUNNING:
         raise ValueError(f"Cannot tell a {frozen_trial.state.name} trial.")
 
@@ -180,8 +176,6 @@ def _tell_with_warning(
     finally:
         study._storage.set_trial_state_values(frozen_trial._trial_id, state, values)
 
-    frozen_trial = copy.deepcopy(study._storage.get_trial(frozen_trial._trial_id))
+    frozen_trial = copy.copy(study._storage.get_trial(frozen_trial._trial_id))
 
-    if warning_message is not None:
-        frozen_trial._system_attrs[STUDY_TELL_WARNING_KEY] = warning_message
-    return frozen_trial
+    return frozen_trial, warning_message
