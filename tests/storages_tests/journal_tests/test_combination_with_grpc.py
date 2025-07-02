fbb20bc8a46251945ba1f16a114cb700c2bddf96
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-07-02 09:18:30 +0200

    Refactor

--- tests/storages_tests/journal_tests/test_combination_with_grpc.py
+++ tests/storages_tests/journal_tests/test_combination_with_grpc.py
@@ -14,6 +14,7 @@ from optuna.testing.storages import StorageSupplier
 
 if TYPE_CHECKING:
     from concurrent.futures import Executor
+    from typing import Callable
     from typing import Generator
 
 
@@ -31,21 +32,23 @@ def grpc_journal_file_context() -> Generator[optuna.Study]:
         yield study
 
 
-def pop_waiting_trial(master_study: optuna.Study, port: int | None = None) -> int | None:
-    if port is not None:
-        storage = GrpcStorageProxy(port=port)
-        storage.wait_server_ready(timeout=60)
-        study_in_spawned_proc = optuna.load_study(
-            storage=storage, study_name=master_study.study_name
-        )
-        return study_in_spawned_proc._pop_waiting_trial_id()
-    else:
-        return master_study._pop_waiting_trial_id()
+def _pop_waiting_trial_id(study: optuna.Study) -> int | None:
+    storage = study._storage
+    assert isinstance(storage, GrpcStorageProxy)
+    port = storage._port
+    storage = GrpcStorageProxy(port=port)
+    storage.wait_server_ready(timeout=60)
+    study_in_spawned_proc = optuna.load_study(storage=storage, study_name=study.study_name)
+    return study_in_spawned_proc._pop_waiting_trial_id()
 
 
-def _verify_racing_condition(pool: Executor, study: optuna.Study, port: int | None = None) -> None:
+def _verify_racing_condition(
+    pool: Executor,
+    study: optuna.Study,
+    pop_waiting_trial_id_wrapper: Callable[[optuna.Study], int | None],
+) -> None:
     n_enqueued = len(study.trials)
-    futures = [pool.submit(pop_waiting_trial, study, port) for _ in range(n_enqueued)]
+    futures = [pool.submit(pop_waiting_trial_id_wrapper, study) for _ in range(n_enqueued)]
     trial_id_set = set()
     for future in as_completed(futures):
         trial_id = future.result()
@@ -56,15 +59,11 @@ def _verify_racing_condition(pool: Executor, study: optuna.Study, port: int | No
 
 def test_pop_waiting_trial_multiprocess_safe() -> None:
     with grpc_journal_file_context() as study:
-        storage = study._storage
-        assert isinstance(storage, GrpcStorageProxy)
-        port = storage._port
-        mp_context = multiprocessing.get_context("spawn")
-        with ProcessPoolExecutor(10, mp_context=mp_context) as pool:
-            _verify_racing_condition(pool, study, port)
+        with ProcessPoolExecutor(10, mp_context=multiprocessing.get_context("spawn")) as pool:
+            _verify_racing_condition(pool, study, _pop_waiting_trial_id)
 
 
 def test_pop_waiting_trial_thread_safe() -> None:
     with grpc_journal_file_context() as study:
         with ThreadPoolExecutor(10) as pool:
-            _verify_racing_condition(pool, study)
+            _verify_racing_condition(pool, study, lambda study: study._pop_waiting_trial_id())
