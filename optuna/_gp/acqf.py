89b8f8a7a93befdde624a11ea3f26825f752bfbd
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-07-25 16:14:29 +0900

    Update eval_acqf

--- optuna/_gp/acqf.py
+++ optuna/_gp/acqf.py
@@ -299,7 +299,11 @@ class ConstrainedLogEHVI(BaseAcquisitionFunc):
         super().__init__(np.mean([gpr.length_scales for gpr in gpr_list], axis=0), search_space)
 
     def eval_acqf(self, x: torch.Tensor) -> torch.Tensor:
-        log_feas_prob = sum(acqf.eval_acqf(x) for acqf in self._constraints_acqf_list)
+        constraints_acqf_values = sum(
+            [acqf.eval_acqf(x) for acqf in self._constraints_acqf_list], torch.tensor(0.0)
+        )
         if self._acqf is None:
-            return log_feas_prob
-        return log_feas_prob + self._acqf.eval_acqf(x)
+            return constraints_acqf_values
+        return constraints_acqf_values + self._acqf.eval_acqf(x)
+
+    a = sum(torch.randn(3) for _ in range(3))
