6e29fd7b1c50fe928ca527aa2d249fce19a143f1
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-08-15 11:53:16 +0200

    Refactor

--- optuna/_gp/acqf.py
+++ optuna/_gp/acqf.py
@@ -82,10 +82,8 @@ def standard_logei(z: torch.Tensor) -> torch.Tensor:
     ).log()
     if (z_small := z[(small := z < -25)]).numel():
         # Second condition
-        out[small] = (
-            -0.5 * z_small**2
+        out[small] = -0.5 * z_small**2 - _LOG_SQRT_2PI + (
             + (1 + _SQRT_HALF_PI * z_small * torch.special.erfcx(-_SQRT_HALF * z_small)).log()
-            - _LOG_SQRT_2PI
         )
     return out
 
