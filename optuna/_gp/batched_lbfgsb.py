0f06a890ace83d98aac946f954d25683563967ec
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2025-09-12 06:28:46 +0200

    Refactor batched L-BFGS-B

--- optuna/_gp/batched_lbfgsb.py
+++ optuna/_gp/batched_lbfgsb.py
@@ -13,6 +13,7 @@ with try_import() as _greenlet_imports:
 
 if TYPE_CHECKING:
     from collections.abc import Callable
+    from typing import Any
 
     import scipy.optimize as so
 
@@ -31,8 +32,9 @@ if not _greenlet_imports.is_successful():
 
 
 def _batched_lbfgsb(
-    func_and_grad: Callable[[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]],
+    func_and_grad: Callable[[np.ndarray, Any], tuple[np.ndarray, np.ndarray]],
     x0_batched: np.ndarray,
+    args_list: list[Any] | None,
     bounds: list[tuple[float, float]] | None,
     m: int,
     factr: float,
@@ -48,17 +50,18 @@ def _batched_lbfgsb(
     xs_opt = np.empty_like(x0_batched)
     fvals_opt = np.empty(batch_size, dtype=float)
     n_iterations = np.empty(batch_size, dtype=int)
-    is_remaining_batch = np.ones(batch_size, dtype=bool)
 
     def run(i: int) -> None:
-        def _func_and_grad(x: np.ndarray) -> tuple[float, np.ndarray]:
-            fval, grad = greenlet.getcurrent().parent.switch(x)
+        def _func_and_grad(x: np.ndarray, *args: Any) -> tuple[float, np.ndarray]:
+            args = None if len(args) == 0 else args
+            fval, grad = greenlet.getcurrent().parent.switch(x, args)
             # NOTE(nabenabe): copy is necessary to convert grad to writable.
             return float(fval), grad.copy()
 
         x_opt, fval_opt, info = so.fmin_l_bfgs_b(
             func=_func_and_grad,
             x0=x0_batched[i],
+            args=args_list[i] if args_list is not None else (),
             bounds=bounds,
             m=m,
             factr=factr,
@@ -70,30 +73,26 @@ def _batched_lbfgsb(
         xs_opt[i] = x_opt
         fvals_opt[i] = fval_opt
         n_iterations[i] = info["nit"]
-        is_remaining_batch[i] = False
+        greenlet.getcurrent().parent.switch(None, None)
 
     greenlets = [greenlet(run) for _ in range(batch_size)]
-    x_batched = [gl.switch(i) for i, gl in enumerate(greenlets)]
+    x_and_args = [gl.switch(i) for i, gl in enumerate(greenlets)]
+    args_batched = [args for _, args in x_and_args] if args_list is not None else None
+    while (x_batched := np.array([x for x, _ in x_and_args if x is not None])).size:
+        fvals, grads = func_and_grad(x_batched, *() if args_batched is None else (args_batched, ))
+        x_and_args = [gl.switch((fvals[i], grads[i])) for i, gl in enumerate(greenlets)]
+        args_batched = (
+            None if args_batched is None else [args for x, args in x_and_args if x is not None]
+        )
+        greenlets = [gl for (x, _), gl in zip(x_and_args, greenlets) if x is not None]
 
-    while np.any(is_remaining_batch):
-        remaining_batch_indices = np.where(is_remaining_batch)[0]
-        fvals, grads = func_and_grad(np.asarray(x_batched), np.asarray(remaining_batch_indices))
-
-        x_batched = []
-        next_greenlets = []
-        for i, gl in enumerate(greenlets):
-            x = gl.switch((fvals[i], grads[i]))
-            if x is None:
-                continue
-            x_batched.append(x)
-            next_greenlets.append(gl)
-        greenlets = next_greenlets
     return xs_opt, fvals_opt, n_iterations
 
 
 def batched_lbfgsb(
-    func_and_grad: Callable[[np.ndarray, np.ndarray], tuple[np.ndarray, np.ndarray]],
+    func_and_grad: Callable[[np.ndarray, Any], tuple[np.ndarray, np.ndarray]],
     x0_batched: np.ndarray,
+    args_list: list[Any] | None = None,
     bounds: list[tuple[float, float]] | None = None,
     m: int = 10,
     factr: float = 1e7,
@@ -102,12 +101,20 @@ def batched_lbfgsb(
     max_iters: int = 15000,
     max_line_search: int = 20,
 ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
-
+    if args_list is not None and x0_batched.ndim > 2:
+        raise ValueError("x0_batched must be 2D when args_list is provided.")
+    if args_list is not None and len(args_list) != len(x0_batched):
+        raise ValueError(
+            f"The length of args_list must be equal to the batch size, "
+            f"but got len(args_list)={len(args_list)} and batch size={len(x0_batched)}."
+        )
+    x0_batched = x0_batched.reshape(-1, x0_batched.shape[-1]) # Make 3+D array 2D.
     if _greenlet_imports.is_successful() and len(x0_batched) > 1:
         # NOTE(Kaichi-Irie): when batch size is 1, using greenlet causes context-switch overhead.
         xs_opt, fvals_opt, n_iterations = _batched_lbfgsb(
             func_and_grad=func_and_grad,
             x0_batched=x0_batched,
+            args_list=args_list,
             bounds=bounds,
             m=m,
             factr=factr,
@@ -123,11 +130,10 @@ def batched_lbfgsb(
         fvals_opt = np.empty(x0_batched.shape[0], dtype=float)
         n_iterations = np.empty(x0_batched.shape[0], dtype=int)
         for i, x0 in enumerate(x0_batched):
-            batch_indices = np.array([i])
             xs_opt[i], fvals_opt[i], info = so.fmin_l_bfgs_b(
                 func=func_and_grad,  # type: ignore[arg-type]
-                args=(batch_indices,),  # type: ignore[arg-type]
                 x0=x0,
+                args=args_list[i] if args_list is not None else (),
                 bounds=bounds,
                 m=m,
                 factr=factr,
