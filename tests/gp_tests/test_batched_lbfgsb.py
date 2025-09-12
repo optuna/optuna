e66e949743fc299314215f899094d8b76e26bbad
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-09-12 14:57:12 +0900

    Add tests for batched L-BFGS-B optimization with and without greenlet

---
+++ tests/gp_tests/test_batched_lbfgsb.py
@@ -0,0 +1,162 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+import importlib
+import sys
+from typing import Any
+from unittest.mock import MagicMock
+
+import numpy as np
+import pytest
+from scipy.optimize import fmin_l_bfgs_b
+
+from optuna._gp.batched_lbfgsb import batched_lbfgsb
+
+
+def rastrigin_and_grad(x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    if x.ndim == 1:
+        x = x[None]
+    A = 10.0
+    dim = x.shape[-1]
+    _2pi_x = 2 * np.pi * x
+
+    fval = A * dim + np.sum(x**2 - A * np.cos(_2pi_x), axis=-1)
+    grad = 2 * x + 2 * np.pi * A * np.sin(_2pi_x)
+    return fval, grad
+
+
+def styblinski_tang_and_grad(x: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    # Styblinski-Tang function, which has multiple local minima.
+    if x.ndim == 1:
+        x = x[None]
+    fval = np.sum(x**4 - 16 * x**2 + 5 * x, axis=-1) / 2
+    grad = 2 * x**3 - 16 * x + 2.5
+    return fval, grad
+
+
+def X0_and_bounds(dim: int, n_localopts: int) -> tuple[np.ndarray, np.ndarray]:
+    R = 5.12
+    rng = np.random.RandomState(0)
+    X0 = rng.random((n_localopts, dim)) * 2 * R - R
+    bounds = np.array([[-R, R]] * dim)
+    return X0, bounds
+
+
+def _verify_results(
+    X0: np.ndarray, func_and_grad: Callable, kwargs_ours: Any, kwargs_scipy: Any
+) -> None:
+    xs_opt1, fvals_opt1, n_iters1 = batched_lbfgsb(
+        func_and_grad=func_and_grad, x0_batched=X0, **kwargs_ours
+    )
+    xs_opt2 = []
+    fvals_opt2 = []
+    n_iters2 = []
+    for x0 in X0:
+
+        x_opt, fval, info = fmin_l_bfgs_b(func=func_and_grad, x0=x0, **kwargs_scipy)
+        xs_opt2.append(x_opt)
+        fvals_opt2.append(float(fval))
+        n_iters2.append(info["nit"])
+
+    assert np.all(n_iters1 == np.array(n_iters2))
+    assert np.all(fvals_opt1 == np.array(fvals_opt2))
+    assert np.all(xs_opt1 == np.array(xs_opt2))
+
+
+test_params = [
+    (rastrigin_and_grad, {}, {}),
+    (styblinski_tang_and_grad, {}, {}),
+    (rastrigin_and_grad, {"max_evals": 3}, {"maxfun": 3}),
+    (styblinski_tang_and_grad, {"max_evals": 3}, {"maxfun": 3}),
+    (rastrigin_and_grad, {"max_iters": 3}, {"maxiter": 3}),
+    (styblinski_tang_and_grad, {"max_iters": 3}, {"maxiter": 3}),
+]
+
+
+@pytest.mark.parametrize("func_and_grad,kwargs_ours,kwargs_scipy", test_params)
+def test_batched_lbfgsb(func_and_grad: Callable, kwargs_ours: Any, kwargs_scipy: Any) -> None:
+    dim = 10
+    n_localopts = 10
+    X0, bounds = X0_and_bounds(dim=dim, n_localopts=n_localopts)
+    kwargs_ours.update(bounds=bounds)
+    kwargs_scipy.update(bounds=bounds)
+    _verify_results(X0, func_and_grad, kwargs_ours, kwargs_scipy)
+
+
+@pytest.mark.parametrize("func_and_grad,kwargs_ours,kwargs_scipy", test_params)
+@pytest.mark.parametrize(
+    "lower_bound,upper_bound", [(-np.inf, None), (None, np.inf), (-np.inf, np.inf), (None, None)]
+)
+def test_batched_lbfgsb_without_bounds(
+    func_and_grad: Callable,
+    kwargs_ours: Any,
+    kwargs_scipy: Any,
+    lower_bound: float | None,
+    upper_bound: float | None,
+) -> None:
+    dim = 10
+    n_localopts = 10
+    X0, bounds = X0_and_bounds(dim=dim, n_localopts=n_localopts)
+    if lower_bound is not None:
+        bounds[:, 0] = lower_bound
+    if upper_bound is not None:
+        bounds[:, 1] = upper_bound
+    kwargs_ours.update(bounds=bounds)
+    kwargs_scipy.update(bounds=bounds)
+    _verify_results(X0, func_and_grad, kwargs_ours, kwargs_scipy)
+
+
+@pytest.mark.parametrize("func_and_grad,kwargs_ours,kwargs_scipy", test_params)
+def test_batched_lbfgsb_without_greenlet(
+    monkeypatch: pytest.MonkeyPatch, func_and_grad: Callable, kwargs_ours: Any, kwargs_scipy: Any
+) -> None:
+    monkeypatch.setitem(sys.modules, "greenlet", None)
+    import optuna._gp.batched_lbfgsb as my_module
+
+    importlib.reload(my_module)
+    assert my_module._greenlet_imports.is_successful() is False
+
+    dim = 10
+    n_localopts = 10
+    X0, bounds = X0_and_bounds(dim=dim, n_localopts=n_localopts)
+    kwargs_ours.update(bounds=bounds)
+    kwargs_scipy.update(bounds=bounds)
+
+    xs_opt1, fvals_opt1, n_iters1 = my_module.batched_lbfgsb(
+        func_and_grad=func_and_grad, x0_batched=X0, **kwargs_ours
+    )
+    xs_opt2 = []
+    fvals_opt2 = []
+    n_iters2 = []
+    for x0 in X0:
+        x_opt, fval, info = fmin_l_bfgs_b(func_and_grad, x0=x0, **kwargs_scipy)
+        xs_opt2.append(x_opt)
+        fvals_opt2.append(float(fval))
+        n_iters2.append(info["nit"])
+
+    assert np.all(n_iters1 == np.array(n_iters2))
+    assert np.all(fvals_opt1 == np.array(fvals_opt2))
+    assert np.all(xs_opt1 == np.array(xs_opt2))
+
+
+def test_behavior_with_greenlet(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setitem(sys.modules, "greenlet", MagicMock())
+    import optuna._gp.batched_lbfgsb as my_module
+
+    importlib.reload(my_module)
+    assert my_module._greenlet_imports.is_successful() is True
+
+
+def test_behavior_without_greenlet(monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setitem(sys.modules, "greenlet", None)
+    import optuna._gp.batched_lbfgsb as my_module
+
+    importlib.reload(my_module)
+    assert my_module._greenlet_imports.is_successful() is False
+
+    # See if optimization still works without greenlet
+    import optuna
+
+    sampler = optuna.samplers.GPSampler(seed=42)
+    study = optuna.create_study(sampler=sampler)
+    study.optimize(lambda trial: trial.suggest_float("x", -10, 10) ** 2, n_trials=15)
