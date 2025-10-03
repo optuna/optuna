3d9b8c368ab6953a21a109269fb99aed2a825517
gen740 <54583542+gen740@users.noreply.github.com>
2025-10-03 16:19:31 +0900

    feat(warnings): Add optional stacklevel parameter

--- optuna/_warnings.py
+++ optuna/_warnings.py
@@ -27,11 +27,16 @@ def find_stacklevel() -> int:
         return level
 
 
-def warn(message: str, category: type[Warning] = UserWarning) -> None:
+def warn(
+    message: str,
+    category: type[Warning] = UserWarning,
+    stacklevel=None,
+) -> None:
     """
     Warning utility that automatically sets the stacklevel to point to the user code.
     """
-    warnings.warn(message, category, stacklevel=find_stacklevel())
+    stacklevel = stacklevel or find_stacklevel()
+    warnings.warn(message, category, stacklevel=stacklevel)
 
 
 __all__ = ["warn"]
