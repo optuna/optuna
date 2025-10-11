a24dc0577747d0b7c2769aac58289fe1e12f98d9
Aditya Mehra <adityamehra@mac.mynetworksettings.com>
2025-10-11 15:50:29 -0400

    Use TYPE_CHECKING in optuna/samplers/_random.py
    Move distribution and trial imports into TYPE_CHECKING block
    to avoid potential circular import issues.
    
    Fixes #6029

--- optuna/samplers/_random.py
+++ optuna/samplers/_random.py
@@ -3,16 +3,16 @@ from __future__ import annotations
 from typing import Any
 from typing import TYPE_CHECKING
 
-from optuna import distributions
 from optuna._transform import _SearchSpaceTransform
-from optuna.distributions import BaseDistribution
 from optuna.samplers import BaseSampler
 from optuna.samplers._lazy_random_state import LazyRandomState
-from optuna.trial import FrozenTrial
 
 
 if TYPE_CHECKING:
+    from optuna import distributions
+    from optuna.distributions import BaseDistribution
     from optuna.study import Study
+    from optuna.trial import FrozenTrial
 
 
 class RandomSampler(BaseSampler):
