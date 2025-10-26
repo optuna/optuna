6663feaf1ec393144be7c5e60be61c3be2acb3dc
Shuhei Watanabe <47781922+nabenabe0928@users.noreply.github.com>
2025-10-26 02:07:55 +0100

    Update optuna/distributions.py
    Co-authored-by: c-bata <contact@c-bata.link>

--- optuna/distributions.py
+++ optuna/distributions.py
@@ -496,7 +496,7 @@ class CategoricalDistribution(BaseDistribution):
             if choice is not None and not isinstance(choice, (bool, int, float, str)):
                 message = (
                     "Choices for a categorical distribution should be a tuple of None, bool, int, "
-                    "float and str for persistent storage but contains"
+                    "float and str for persistent storage but contains "
                     f"{choice} which is of type {type(choice).__name__}."
                 )
                 warnings.warn(message)
