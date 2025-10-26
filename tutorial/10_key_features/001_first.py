2660da27e04cc2b6e5ac0945bf142d69d1d90770
Satyarth Srivastava <143537599+satyarth7srivastava@users.noreply.github.com>
2025-10-26 13:50:01 +0530

    Update tutorial/10_key_features/001_first.py
    Co-authored-by: Shuhei Watanabe <47781922+nabenabe0928@users.noreply.github.com>

--- tutorial/10_key_features/001_first.py
+++ tutorial/10_key_features/001_first.py
@@ -56,7 +56,7 @@ study.optimize(objective, n_trials=100)
 
 best_params = study.best_params
 found_x = best_params["x"]
-print(f"Found x: '{found_x}', (x - 2)^2: '{((found_x - 2) ** 2)}'")
+print(f"Found x: {found_x}, (x - 2)^2: {(found_x - 2) ** 2}")
 ###################################################################################################
 # We can see that the ``x`` value found by Optuna is close to the optimal value of ``2``.
 
