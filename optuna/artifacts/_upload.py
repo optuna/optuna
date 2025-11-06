45ea3978ed6576f289fb692ed9e033bbe9f90f0b
Satyarth Srivastava <satyarthsrivastava7@gmail.com>
2025-11-06 11:04:07 +0530

    updated (reformatted) _upload.py for TYPE_CHECKING

--- optuna/artifacts/_upload.py
+++ optuna/artifacts/_upload.py
@@ -5,16 +5,16 @@ from dataclasses import dataclass
 import json
 import mimetypes
 import os
+from typing import TYPE_CHECKING
 import uuid
 
 from optuna._convert_positional_args import convert_positional_args
+
 # from optuna.artifacts._protocol import ArtifactStore #remove
 # from optuna.storages import BaseStorage #remove
 from optuna.study import Study
 from optuna.trial import FrozenTrial
 from optuna.trial import Trial
-
-from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
