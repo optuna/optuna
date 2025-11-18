fce1fe09e4a5343e2e0a55f5a94dd5f6fb7dfb96
Harshavardhan <harsha2003hvd@gmail.com>
2025-11-18 17:59:48 +0530

    Replace .format with f-string in _param_importances.py

--- optuna/visualization/_param_importances.py
+++ optuna/visualization/_param_importances.py
@@ -205,12 +205,7 @@ def _get_distribution(param_name: str, study: Study) -> BaseDistribution:
 
 
 def _make_hovertext(param_name: str, importance: float, study: Study) -> str:
-    return (
-             f"{param_name} "
-             f"({_get_distribution(param_name, study).__class__.__name__}): "
-             f"{importance}<extra></extra>"
-            )
-
+    return f"{param_name} ({_get_distribution(param_name, study).__class__.__name__}): {importance}<extra></extra>"
 
 
 def _get_hover_template(importances_info: _ImportancesInfo, study: Study) -> list[str]:
