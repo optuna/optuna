0af759153ee0a65add99742843762729bba2cb24
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-12-05 19:03:44 +0900

    Remove test_baseline_quantile_is_1

--- tests/importance_tests/pedanova_tests/test_evaluator.py
+++ tests/importance_tests/pedanova_tests/test_evaluator.py
@@ -69,16 +69,6 @@ def test_n_trials_equal_to_min_n_top_trials() -> None:
     assert np.allclose(param_importance, np.zeros(n_params))
 
 
-def test_target_quantile_is_1() -> None:
-    study = get_study(seed=0, n_trials=100, is_multi_obj=False)
-    # baseline_quantile=1.0 enforces top_trials == all_trials identical.
-    evaluator = PedAnovaImportanceEvaluator(target_quantile=1.0)
-    param_importance = list(evaluator.evaluate(study).values())
-    n_params = len(param_importance)
-    # When top_trials == all_trials, all the importances become identical.
-    assert np.allclose(param_importance, np.zeros(n_params))
-
-
 def test_direction() -> None:
     study_minimize = get_study(seed=0, n_trials=20, is_multi_obj=False)
     study_maximize = optuna.create_study(direction="maximize")
