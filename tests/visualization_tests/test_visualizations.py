f4ea11aad9e3e6895535d3abb4117d81190aea69
Sip4818 <sip4818@gmail.com>
2025-12-09 23:47:45 +0530

    Move type-only imports to TYPE_CHECKING in test_visualizations.py

--- tests/visualization_tests/test_visualizations.py
+++ tests/visualization_tests/test_visualizations.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+from typing import TYPE_CHECKING
+
 
 from collections.abc import Callable
 
@@ -8,7 +10,11 @@ import plotly.graph_objects as go
 import pytest
 
 import optuna
-from optuna.study.study import ObjectiveFuncType
+
+if TYPE_CHECKING:
+    from optuna.study.study import ObjectiveFuncType
+    from optuna import Trial
+
 from optuna.visualization import plot_contour
 from optuna.visualization import plot_edf
 from optuna.visualization import plot_optimization_history
@@ -17,8 +23,12 @@ from optuna.visualization import plot_param_importances
 from optuna.visualization import plot_rank
 from optuna.visualization import plot_slice
 from optuna.visualization import plot_timeline
-from optuna.visualization.matplotlib import plot_contour as matplotlib_plot_contour
-from optuna.visualization.matplotlib import plot_edf as matplotlib_plot_edf
+from optuna.visualization.matplotlib import (
+    plot_contour as matplotlib_plot_contour
+)
+from optuna.visualization.matplotlib import (
+    plot_edf as matplotlib_plot_edf
+)
 from optuna.visualization.matplotlib import (
     plot_optimization_history as matplotlib_plot_optimization_history,
 )
@@ -28,35 +38,43 @@ from optuna.visualization.matplotlib import (
 from optuna.visualization.matplotlib import (
     plot_param_importances as matplotlib_plot_param_importances,
 )
-from optuna.visualization.matplotlib import plot_rank as matplotlib_plot_rank
-from optuna.visualization.matplotlib import plot_slice as matplotlib_plot_slice
-from optuna.visualization.matplotlib import plot_timeline as matplotlib_plot_timeline
-
-
-parametrize_visualization_functions_for_single_objective = pytest.mark.parametrize(
-    "plot_func",
-    [
-        plot_optimization_history,
-        plot_edf,
-        plot_contour,
-        plot_parallel_coordinate,
-        plot_rank,
-        plot_slice,
-        plot_timeline,
-        plot_param_importances,
-        matplotlib_plot_optimization_history,
-        matplotlib_plot_edf,
-        matplotlib_plot_contour,
-        matplotlib_plot_parallel_coordinate,
-        matplotlib_plot_rank,
-        matplotlib_plot_slice,
-        matplotlib_plot_timeline,
-        matplotlib_plot_param_importances,
-    ],
+from optuna.visualization.matplotlib import (
+    plot_rank as matplotlib_plot_rank
+)
+from optuna.visualization.matplotlib import (
+    plot_slice as matplotlib_plot_slice
+)
+from optuna.visualization.matplotlib import (
+    plot_timeline as matplotlib_plot_timeline
 )
 
 
-def objective_single_dynamic_with_categorical(trial: optuna.Trial) -> float:
+parametrize_visualization_functions_for_single_objective = (
+    pytest.mark.parametrize(
+        "plot_func",
+        [
+            plot_optimization_history,
+            plot_edf,
+            plot_contour,
+            plot_parallel_coordinate,
+            plot_rank,
+            plot_slice,
+            plot_timeline,
+            plot_param_importances,
+            matplotlib_plot_optimization_history,
+            matplotlib_plot_edf,
+            matplotlib_plot_contour,
+            matplotlib_plot_parallel_coordinate,
+            matplotlib_plot_rank,
+            matplotlib_plot_slice,
+            matplotlib_plot_timeline,
+            matplotlib_plot_param_importances,
+        ],
+    )
+)
+
+
+def objective_single_dynamic_with_categorical(trial: Trial) -> float:
     category = trial.suggest_categorical("category", ["foo", "bar"])
     if category == "foo":
         return (trial.suggest_float("x1", 0, 10) - 2) ** 2
@@ -64,7 +82,7 @@ def objective_single_dynamic_with_categorical(trial: optuna.Trial) -> float:
         return -((trial.suggest_float("x2", -10, 0) + 5) ** 2)
 
 
-def objective_single_none_categorical(trial: optuna.Trial) -> float:
+def objective_single_none_categorical(trial: Trial) -> float:
     x = trial.suggest_float("x", -100, 100)
     trial.suggest_categorical("y", ["foo", None])
     return x**2
@@ -82,7 +100,8 @@ parametrize_single_objective_functions = pytest.mark.parametrize(
 @parametrize_visualization_functions_for_single_objective
 @parametrize_single_objective_functions
 def test_visualizations_with_single_objectives(
-    plot_func: Callable[[optuna.study.Study], go.Figure | Axes], objective_func: ObjectiveFuncType
+    plot_func: Callable[[optuna.study.Study], go.Figure | Axes],
+    objective_func: ObjectiveFuncType
 ) -> None:
     study = optuna.create_study(sampler=optuna.samplers.RandomSampler())
     study.optimize(objective_func, n_trials=20)
