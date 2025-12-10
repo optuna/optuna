358a9857e3d6809a96d429cd9881b386810cc096
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2025-12-10 19:10:19 +0900

    Fix trials

--- optuna/importance/_ped_anova/evaluator.py
+++ optuna/importance/_ped_anova/evaluator.py
@@ -262,7 +262,7 @@ class PedAnovaImportanceEvaluator(BaseImportanceEvaluator):
         param_importances = {}
         for param_name, dist in non_single_dists.items():
             param_importances[param_name] = quantile**2 * self._compute_pearson_divergence(
-                param_name, dist, top_trials=top_trials, all_trials=trials
+                param_name, dist, top_trials=top_trials, all_trials=region_trials
             )
             importance_sum += param_importances[param_name]
 
