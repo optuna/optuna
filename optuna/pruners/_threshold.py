2da1acc7beee5534902e3cf3b7679eddc2e5b5db
varundevr <varundevr@gmail.com>
2025-12-11 20:51:14 +0530

    Fix CI failures: update f-string and line length compliance

--- optuna/pruners/_threshold.py
+++ optuna/pruners/_threshold.py
@@ -19,6 +19,8 @@ def _check_value(value: Any) -> float:
         )
         raise TypeError(message) from None
 
+    return value
+
 
 class ThresholdPruner(BasePruner):
     """Pruner to detect outlying metrics of the trials.
