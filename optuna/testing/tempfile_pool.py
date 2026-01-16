0450c8e2766732d26c5665e72f44069ab0fc8228
gen740 <54583542+gen740@users.noreply.github.com>
2026-01-16 15:36:02 +0900

    fix(tests): make NamedTemporaryFilePool thread-safe

--- optuna/testing/tempfile_pool.py
+++ optuna/testing/tempfile_pool.py
@@ -4,38 +4,40 @@
 from __future__ import annotations
 
 import atexit
-import gc
+import copy
 import os
 import tempfile
+import threading
 from types import TracebackType
 from typing import Any
+from typing import cast
+from typing import ClassVar
 from typing import IO
 
 
 class NamedTemporaryFilePool:
-    tempfile_pool: list[IO[Any]] = []
-
-    def __new__(cls, **kwargs: Any) -> "NamedTemporaryFilePool":
-        if not hasattr(cls, "_instance"):
-            cls._instance = super(NamedTemporaryFilePool, cls).__new__(cls)
-            atexit.register(cls._instance.cleanup)
-        return cls._instance
+    _lock: ClassVar[threading.Lock] = threading.Lock()
+    _path: ClassVar[list[str]] = []
+    _registered: ClassVar[bool] = False
 
     def __init__(self, **kwargs: Any) -> None:
         self.kwargs = kwargs
+        self._file: IO[bytes] | IO[str] | None = None
 
-    def tempfile(self) -> IO[Any]:
-        self._tempfile = tempfile.NamedTemporaryFile(delete=False, **self.kwargs)
-        self.tempfile_pool.append(self._tempfile)
-        return self._tempfile
+        with self.__class__._lock:
+            if not self.__class__._registered:
+                _ = atexit.register(self.__class__.cleanup)
+                self.__class__._registered = True
 
-    def cleanup(self) -> None:
-        gc.collect()
-        for i in self.tempfile_pool:
-            os.unlink(i.name)
-
-    def __enter__(self) -> IO[Any]:
+    def __enter__(self) -> IO[bytes] | IO[str]:
         return self.tempfile()
+
+    def tempfile(self) -> IO[bytes] | IO[str]:
+        f = cast("IO[bytes] | IO[str]", tempfile.NamedTemporaryFile(delete=False, **self.kwargs))
+        self._file = f
+        with self.__class__._lock:
+            self.__class__._path.append(f.name)
+        return self._file
 
     def __exit__(
         self,
@@ -43,4 +45,19 @@ class NamedTemporaryFilePool:
         exc_val: BaseException,
         exc_tb: TracebackType,
     ) -> None:
-        self._tempfile.close()
+        if self._file is not None:
+            self._file.close()
+
+    @classmethod
+    def cleanup(cls) -> None:
+        with cls._lock:
+            path = copy.deepcopy(cls._path)
+            cls._path = []
+
+        for p in path:
+            try:
+                os.unlink(p)
+            except FileNotFoundError:
+                pass
+            except PermissionError:
+                pass
