6dd9ef99b8daabcae557843b0d1191da5c28dd49
Naoto Mizuno <naotomizuno@preferred.jp>
2026-02-13 12:39:22 +0900

    Update _imports.py

--- optuna/_imports.py
+++ optuna/_imports.py
@@ -59,7 +59,7 @@ class _DeferredImportExceptionContextManager:
         if isinstance(exc_value, (ImportError, SyntaxError)):
             if isinstance(exc_value, ImportError):
                 message = (
-                    f"Tried to import {exc_value.name} but failed. Please make sure that the "
+                    f"Tried to import '{exc_value.name}' but failed. Please make sure that the "
                     "package is installed correctly to use this feature. Actual error: "
                     f"{exc_value}."
                 )
