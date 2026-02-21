9d081f10088d07221da9f551d4b3670815dcf2c7
sarthaksharma1177-rgb <sarthaksharma1177@gmail.com>
2026-02-22 03:33:05 +0530

    Fix: Correct f-string formatting syntax in cma.py

--- optuna/integration/cma.py
+++ optuna/integration/cma.py
@@ -1,12 +1,12 @@
 
 
 try:
-    from optuna_integration.cma import PyCmaSampler
+    from optuna.integration.cma import PyCamaSampler
 except ModuleNotFoundError:
     raise ModuleNotFoundError((
-            f"\nCould not find `optuna-integration` for `cma`.\n"
-            f"Please run `pip install optuna-integration[cma]`.\n"
-        )
+        f"\nCould not find `optuna-integration` for `cma`.\n"
+        f"Please run `pip install optuna-integration[cma]`.\n"
+    ))
 
 
-__all__ = ["PyCmaSampler"]
+__all__ = ["PyCamaSampler"]
