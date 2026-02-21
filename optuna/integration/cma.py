a6030b7f2b6ab828dc07484353c71c088c829eaa
sarthaksharma1177-rgb <sarthaksharma1177@gmail.com>
2026-02-22 03:40:40 +0530

    Fix: Use regular strings instead of f-strings (no placeholders)

--- optuna/integration/cma.py
+++ optuna/integration/cma.py
@@ -3,10 +3,11 @@
 try:
     from optuna.integration.cma import PyCamaSampler
 except ModuleNotFoundError:
-    raise ModuleNotFoundError((
-        f"nCould not find `optuna-integration` for `cma`.\n"
-        f"Please run `pip install optuna-integration[cma]`.\n"
-    ))
+    raise ModuleNotFoundError(
+            "\nCould not find `optuna-integration` for `cma`.\n"
+            "Please run `pip install optuna-integration[cma]`.\n"
+        )
+        
 
 
 __all__ = ["PyCamaSampler"]
