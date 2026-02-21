a6373872bd89396e4ff03325162883b9e2ae870c
sarthaksharma1177-rgb <sarthaksharma1177@gmail.com>
2026-02-22 03:38:09 +0530

    Fix: Remove unnecessary f-string prefix from plain strings

--- optuna/integration/cma.py
+++ optuna/integration/cma.py
@@ -4,7 +4,7 @@ try:
     from optuna.integration.cma import PyCamaSampler
 except ModuleNotFoundError:
     raise ModuleNotFoundError((
-        f"\nCould not find `optuna-integration` for `cma`.\n"
+        f"nCould not find `optuna-integration` for `cma`.\n"
         f"Please run `pip install optuna-integration[cma]`.\n"
     ))
 
