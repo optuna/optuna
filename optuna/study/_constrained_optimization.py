41541f8108250202eaecc80d8797a4647efc24ae
nightcityblade <nightcityblade@gmail.com>
2026-03-29 11:08:22 +0800

    Use TYPE_CHECKING in study/_constrained_optimization.py

--- optuna/study/_constrained_optimization.py
+++ optuna/study/_constrained_optimization.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from collections.abc import Sequence
 from typing import TYPE_CHECKING
 
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
+
     from optuna.trial import FrozenTrial
 
 
