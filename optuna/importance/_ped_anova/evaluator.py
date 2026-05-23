d28ca1cac546cf8b8b006439bb3566f483152de5
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-05-23 18:12:53 +0900

    Apply formatter

--- optuna/importance/_ped_anova/evaluator.py
+++ optuna/importance/_ped_anova/evaluator.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
 from collections import defaultdict
-from typing import cast
 from typing import TYPE_CHECKING
 
 import numpy as np
 
 from optuna._deprecated import _DEPRECATION_WARNING_TEMPLATE
 from optuna._experimental import experimental_class
-from optuna.importance._base import _get_filtered_trials
-from optuna.importance._base import _check_evaluate_args
 from optuna._warnings import optuna_warn
+from optuna.importance._base import _check_evaluate_args
+from optuna.importance._base import _get_filtered_trials
 from optuna.importance._base import _sort_dict_by_importance
 from optuna.importance._base import BaseImportanceEvaluator
 from optuna.importance._ped_anova.scott_parzen_estimator import _build_parzen_estimator
