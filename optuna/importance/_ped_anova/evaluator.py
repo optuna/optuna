914c6430e14c97498fd93874d4debddae40550a7
kAIto47802 <115693559+kAIto47802@users.noreply.github.com>
2026-07-08 15:40:45 +0900

    Add docstring

--- optuna/importance/_ped_anova/evaluator.py
+++ optuna/importance/_ped_anova/evaluator.py
@@ -268,6 +268,40 @@ class PedAnovaImportanceEvaluator(BaseImportanceEvaluator):
         *,
         target: Callable[[FrozenTrial], float] | None = None,
     ) -> dict[str, float]:
+        """Evaluate parameter importances based on completed trials in the given study.
+
+        .. note::
+
+            This method is not meant to be called by library users.
+
+        .. seealso::
+
+            Please refer to :func:`~optuna.importance.get_param_importances` for how a concrete
+            evaluator should implement this method.
+
+        Args:
+            study:
+                An optimized study.
+            params:
+                A list of names of parameters to assess.
+                If :obj:`None`, all parameters that are present in all of the completed trials are
+                assessed.
+            target:
+                A function to specify the value to evaluate importances.
+                If it is :obj:`None` and ``study`` is being used for single-objective optimization,
+                the objective values are used. Can also be used for other trial attributes, such as
+                the duration, like ``target=lambda t: t.duration.total_seconds()``.
+
+                .. note::
+                    Specify this argument if ``study`` is being used for multi-objective
+                    optimization. For example, to get the hyperparameter importance of the first
+                    objective, use ``target=lambda t: t.values[0]`` for the target parameter.
+
+        Returns:
+            A :obj:`dict` where the keys are parameter names and the values are assessed
+            importances.
+
+        """
         dists = _get_distributions_list(study, params=params)
         if params is None:
             params = list(dict.fromkeys(k for d in dists for k in d))
