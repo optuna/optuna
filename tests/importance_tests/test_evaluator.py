b99bb9f00ed5dcfe4c6cc85a29fd2f2895332ace
yen-0 <yen.142857.yen@gmail.com>
2026-07-29 11:12:34 +0900

    ruff

--- tests/importance_tests/test_evaluator.py
+++ tests/importance_tests/test_evaluator.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from collections.abc import Callable
 from typing import Any
-from typing import Literal
 
 from _pytest.fixtures import SubRequest
 import pytest
