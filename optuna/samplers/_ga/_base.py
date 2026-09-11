2c7d2a0d2a44bb04d58587bb61312ec444709ac6
yen-0 <yen.142857.yen@gmail.com>
2026-09-11 11:54:05 +0900

    Fix generation cache state initialization

--- optuna/samplers/_ga/_base.py
+++ optuna/samplers/_ga/_base.py
@@ -53,7 +53,10 @@ class BaseGASampler(BaseSampler, abc.ABC):
 
     def __init__(self, population_size: int | None):
         self._population_size = population_size
+        self._cached_study_id: int | None = None
+        self._cached_generation_to_numbers: dict[int, list[int]] = {}
         self._cached_unfinished_numbers: list[int] = []
+        self._cached_unseen_trial_start = 0
         self._thread_lock = threading.Lock()
 
     @property
